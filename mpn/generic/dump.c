81fc479453b916212bbede21fd7f114a65b4c6a7
tege <tege@gmplib.org>
1996-07-22 09:21:49 +0200

    Add missing `else'.

--- mpn/generic/dump.c
+++ mpn/generic/dump.c
@@ -9,12 +9,13 @@ mpn_dump (ptr, size)
 {
   if (size == 0)
     printf ("0\n");
-  {
-    while (size)
-      {
-	size--;
-	printf ("%0*lX", (int) (2 * BYTES_PER_MP_LIMB), ptr[size]);
-      }
-    printf ("\n");
-  }
+  else
+    {
+      while (size)
+	{
+	  size--;
+	  printf ("%0*lX", (int) (2 * BYTES_PER_MP_LIMB), ptr[size]);
+	}
+      printf ("\n");
+    }
 }
