cdc8a8451c4c8a1eb9ae509c325b1ee8c47bb23b
tege <tege@gmplib.org>
1997-07-25 18:58:26 +0200

    mpn/generic/mul_n.c: Misc optimizations from Robert Harley.
    (__gmpn_mul_n): New name for impn_mul_n.
    Call __gmpn_mul_basecase, not impn_mul_n_basecase; update parameter
    list to work with __gmpn_mul_basecase.
    (__gmpn_sqr): New name for impn_sqr_n.
    Call __gmpn_sqr_basecase, not impn_sqr_n_basecase; update parameter
    list to work with __gmpn_sqr_basecase.
    (mpn_mul_n): Update calls to match new names and parameter conventions.
    
    Don't #define KARATSUBA_THRESHOLD.
    (impn_mul_n, impn_sqr_n): Rewrite, based on code contributed by
    Robert Harley.
    (impn_sqr_n_basecase): Rewrite.

--- mpn/generic/mul_n.c
+++ mpn/generic/mul_n.c
@@ -21,6 +21,7 @@ MA 02111-1307, USA. */
 
 #include "gmp.h"
 #include "gmp-impl.h"
+#include "longlong.h"
 
 /* Multiply the natural numbers u (pointed to by UP) and v (pointed to by VP),
    both with SIZE limbs, and store the result at PRODP.  2 * SIZE limbs are
@@ -30,371 +31,330 @@ MA 02111-1307, USA. */
    1. PRODP != UP and PRODP != VP, i.e. the destination
       must be distinct from the multiplier and the multiplicand.  */
 
-/* If KARATSUBA_THRESHOLD is not already defined, define it to a
-   value which is good on most machines.  */
-#ifndef KARATSUBA_THRESHOLD
-#define KARATSUBA_THRESHOLD 32
-#endif
+static inline void
+mpn_incr (mp_ptr p, mp_limb_t incr)
+{
+  mp_limb_t x;
 
-/* The code can't handle KARATSUBA_THRESHOLD smaller than 2.  */
-#if KARATSUBA_THRESHOLD < 2
-#undef KARATSUBA_THRESHOLD
-#define KARATSUBA_THRESHOLD 2
-#endif
-
-/* Handle simple cases with traditional multiplication.
-
-   This is the most critical code of multiplication.  All multiplies rely
-   on this, both small and huge.  Small ones arrive here immediately.  Huge
-   ones arrive here as this is the base case for Karatsuba's recursive
-   algorithm below.  */
+  x = *p + incr;
+  *p = x;
+  if (x >= incr)
+    return;
+  while (++(*(++p)) == 0)
+    ;
+}
 
 void
 #if __STDC__
-impn_mul_n_basecase (mp_ptr prodp, mp_srcptr up, mp_srcptr vp, mp_size_t size)
+__gmpn_mul_n (mp_ptr prodp, mp_srcptr up, mp_srcptr vp, mp_size_t n, mp_ptr tspace)
 #else
-impn_mul_n_basecase (prodp, up, vp, size)
+__gmpn_mul_n (prodp, up, vp, n, tspace)
      mp_ptr prodp;
      mp_srcptr up;
      mp_srcptr vp;
-     mp_size_t size;
-#endif
-{
-  mp_size_t i;
-  mp_limb_t cy_limb;
-  mp_limb_t v_limb;
-
-  /* Multiply by the first limb in V separately, as the result can be
-     stored (not added) to PROD.  We also avoid a loop for zeroing.  */
-  v_limb = vp[0];
-  if (v_limb <= 1)
-    {
-      if (v_limb == 1)
-	MPN_COPY (prodp, up, size);
-      else
-	MPN_ZERO (prodp, size);
-      cy_limb = 0;
-    }
-  else
-    cy_limb = mpn_mul_1 (prodp, up, size, v_limb);
-
-  prodp[size] = cy_limb;
-  prodp++;
-
-  /* For each iteration in the outer loop, multiply one limb from
-     U with one limb from V, and add it to PROD.  */
-  for (i = 1; i < size; i++)
-    {
-      v_limb = vp[i];
-      if (v_limb <= 1)
-	{
-	  cy_limb = 0;
-	  if (v_limb == 1)
-	    cy_limb = mpn_add_n (prodp, prodp, up, size);
-	}
-      else
-	cy_limb = mpn_addmul_1 (prodp, up, size, v_limb);
-
-      prodp[size] = cy_limb;
-      prodp++;
-    }
-}
-
-void
-#if __STDC__
-impn_mul_n (mp_ptr prodp,
-	     mp_srcptr up, mp_srcptr vp, mp_size_t size, mp_ptr tspace)
-#else
-impn_mul_n (prodp, up, vp, size, tspace)
-     mp_ptr prodp;
-     mp_srcptr up;
-     mp_srcptr vp;
-     mp_size_t size;
+     mp_size_t n;
      mp_ptr tspace;
 #endif
 {
-  if ((size & 1) != 0)
+  if (n & 1)
     {
-      /* The size is odd, the code code below doesn't handle that.
-	 Multiply the least significant (size - 1) limbs with a recursive
-	 call, and handle the most significant limb of S1 and S2
-	 separately.  */
-      /* A slightly faster way to do this would be to make the Karatsuba
-	 code below behave as if the size were even, and let it check for
-	 odd size in the end.  I.e., in essence move this code to the end.
-	 Doing so would save us a recursive call, and potentially make the
-	 stack grow a lot less.  */
+      /* Odd length. */
+      mp_size_t n2, n3, np1, nm1;
+      int sign = 0;
 
-      mp_size_t esize = size - 1;	/* even size */
-      mp_limb_t cy_limb;
+      n2 = n >> 1;
+      n3 = n - n2; /* = n2 + 1 */
 
-      MPN_MUL_N_RECURSE (prodp, up, vp, esize, tspace);
-      cy_limb = mpn_addmul_1 (prodp + esize, up, esize, vp[esize]);
-      prodp[esize + esize] = cy_limb;
-      cy_limb = mpn_addmul_1 (prodp + esize, vp, size, up[esize]);
+      {
+	mp_limb_t w, w0, w1; mp_srcptr x, y;
+	/* Subtract smaller half of up[] from larger half. */
+	w = up[n2];
+	if (w != 0)
+	  w -= mpn_sub_n (prodp, up, up + n3, n2);
+	else
+	  {
+	    mp_size_t i;
+	    i = n2 - 1;
+	    while ((w0 = up[i]) == (w1 = up[n3 + i]) && i != 0)
+	      i--;
+	    x = y = up;
+	    if (w0 < w1)
+	      {
+		x += n3;
+		sign = 1;
+	      }
+	    else
+	      y += n3;
+	    mpn_sub_n (prodp, x, y, n2);
+	  }
+	prodp[n2] = w;
+      }
 
-      prodp[esize + size] = cy_limb;
+      {
+	mp_limb_t w, w0, w1; mp_srcptr x, y;
+	/* Subtract smaller half of vp[] from larger half. */
+	w = vp[n2];
+	if (w != 0)
+	  w -= mpn_sub_n (prodp + n3, vp, vp + n3, n2);
+	else
+	  {
+	    mp_size_t i;
+	    i = n2 - 1;
+	    while ((w0 = vp[i]) == (w1 = vp[n3 + i]) && i != 0)
+	      i--;
+	    x = y = vp;
+	    if (w0 < w1)
+	      {
+		x += n3;
+		sign ^= 1;
+	      }
+	    else
+	      y += n3;
+	    mpn_sub_n (prodp + n3, x, y, n2);
+	  }
+	prodp[n] = w;
+      }
+
+      np1 = n + 1;
+
+      if (n3 < KARATSUBA_MUL_THRESHOLD)
+	{
+	  __gmpn_mul_basecase (tspace, prodp, n3, prodp + n3, n3);
+	  __gmpn_mul_basecase (prodp, up, n3, vp, n3);
+	  __gmpn_mul_basecase (prodp + np1, up + n3, n2, vp + n3, n2);
+	}
+      else
+	{
+	  __gmpn_mul_n (tspace, prodp, prodp + n3, n3, tspace + np1);
+	  __gmpn_mul_n (prodp, up, vp, n3, tspace + np1);
+	  __gmpn_mul_n (prodp + np1, up + n3, vp + n3, n2, tspace + np1);
+	}
+
+      if (sign)
+	mpn_add_n (tspace, prodp, tspace, np1);
+      else
+	mpn_sub_n (tspace, prodp, tspace, np1);
+      nm1 = n - 1;
+      if (mpn_add_n (tspace, tspace, prodp + np1, nm1))
+	{
+	  mp_limb_t x = tspace[nm1] + 1;
+	  tspace[nm1] = x;
+	  if (x == 0)
+	    ++tspace[n];
+	}
+      if (mpn_add_n (prodp + n3, prodp + n3, tspace, np1))
+	mpn_incr (prodp + np1 + n3, (mp_limb_t) 1);
     }
   else
     {
-      /* Anatolij Alekseevich Karatsuba's divide-and-conquer algorithm.
-
-	 Split U in two pieces, U1 and U0, such that
-	 U = U0 + U1*(B**n),
-	 and V in V1 and V0, such that
-	 V = V0 + V1*(B**n).
-
-	 UV is then computed recursively using the identity
-
-		2n   n          n                     n
-	 UV = (B  + B )U V  +  B (U -U )(V -V )  +  (B + 1)U V
-			1 1        1  0   0  1              0 0
-
-	 Where B = 2**BITS_PER_MP_LIMB.  */
-
-      mp_size_t hsize = size >> 1;
+      /* Even length. */
       mp_limb_t cy;
-      int negflg;
+      mp_size_t n2 = n >> 1;
+      int sign = 0;
 
-      /*** Product H.	 ________________  ________________
-			|_____U1 x V1____||____U0 x V0_____|  */
-      /* Put result in upper part of PROD and pass low part of TSPACE
-	 as new TSPACE.  */
-      MPN_MUL_N_RECURSE (prodp + size, up + hsize, vp + hsize, hsize, tspace);
+      {
+	mp_limb_t w0, w1; mp_size_t i; mp_srcptr x, y;
+	/* Subtract smaller half of up[] from larger half. */
+	i = n2 - 1;
+	while ((w0 = up[i]) == (w1 = up[n2 + i]) && i != 0)
+	  i--;
+	sign = 0;
+	x = y = up;
+	if (w0 < w1)
+	  {
+	    x += n2;
+	    sign = 1;
+	  }
+	else
+	  y += n2;
+	mpn_sub_n (prodp, x, y, n2);
+      }
 
-      /*** Product M.	 ________________
-			|_(U1-U0)(V0-V1)_|  */
-      if (mpn_cmp (up + hsize, up, hsize) >= 0)
+      {
+	mp_limb_t w0, w1; mp_size_t i; mp_srcptr x, y;
+	/* Subtract smaller half of vp[] from larger half. */
+	i = n2-1UL;
+	while ((w0 = vp[i]) == (w1 = vp[n2+i]) && i)
+	  i--;
+	x = y = vp;
+	if (w0 < w1)
+	  {
+	    x += n2;
+	    sign ^= 1;
+	  }
+	else
+	  y += n2;
+	mpn_sub_n (prodp + n2, x, y, n2);
+      }
+
+      if (n2 < KARATSUBA_MUL_THRESHOLD)
 	{
-	  mpn_sub_n (prodp, up + hsize, up, hsize);
-	  negflg = 0;
+	  __gmpn_mul_basecase (tspace, prodp, n2, prodp + n2, n2);
+	  __gmpn_mul_basecase (prodp, up, n2, vp, n2);
+	  __gmpn_mul_basecase (prodp + n, up + n2, n2, vp + n2, n2);
 	}
       else
 	{
-	  mpn_sub_n (prodp, up, up + hsize, hsize);
-	  negflg = 1;
+	  __gmpn_mul_n (tspace, prodp, prodp + n2, n2, tspace + n);
+	  __gmpn_mul_n (prodp, up, vp, n2, tspace + n);
+	  __gmpn_mul_n (prodp + n, up + n2, vp + n2, n2, tspace + n);
 	}
-      if (mpn_cmp (vp + hsize, vp, hsize) >= 0)
-	{
-	  mpn_sub_n (prodp + hsize, vp + hsize, vp, hsize);
-	  negflg ^= 1;
-	}
+
+      if (sign)
+	cy = mpn_add_n (tspace, prodp, tspace, n);
       else
-	{
-	  mpn_sub_n (prodp + hsize, vp, vp + hsize, hsize);
-	  /* No change of NEGFLG.  */
-	}
-      /* Read temporary operands from low part of PROD.
-	 Put result in low part of TSPACE using upper part of TSPACE
-	 as new TSPACE.  */
-      MPN_MUL_N_RECURSE (tspace, prodp, prodp + hsize, hsize, tspace + size);
-
-      /*** Add/copy product H.  */
-      MPN_COPY (prodp + hsize, prodp + size, hsize);
-      cy = mpn_add_n (prodp + size, prodp + size, prodp + size + hsize, hsize);
-
-      /*** Add product M (if NEGFLG M is a negative number).  */
-      if (negflg)
-	cy -= mpn_sub_n (prodp + hsize, prodp + hsize, tspace, size);
-      else
-	cy += mpn_add_n (prodp + hsize, prodp + hsize, tspace, size);
-
-      /*** Product L.	 ________________  ________________
-			|________________||____U0 x V0_____|  */
-      /* Read temporary operands from low part of PROD.
-	 Put result in low part of TSPACE using upper part of TSPACE
-	 as new TSPACE.  */
-      MPN_MUL_N_RECURSE (tspace, up, vp, hsize, tspace + size);
-
-      /*** Add/copy Product L (twice).  */
-
-      cy += mpn_add_n (prodp + hsize, prodp + hsize, tspace, size);
-      if (cy)
-	mpn_add_1 (prodp + hsize + size, prodp + hsize + size, hsize, cy);
-
-      MPN_COPY (prodp, tspace, hsize);
-      cy = mpn_add_n (prodp + hsize, prodp + hsize, tspace + hsize, hsize);
-      if (cy)
-	mpn_add_1 (prodp + size, prodp + size, size, 1);
+	cy = -mpn_sub_n (tspace, prodp, tspace, n);
+      cy += mpn_add_n (tspace, tspace, prodp + n, n);
+      cy += mpn_add_n (prodp + n2, prodp + n2, tspace, n);
+      mpn_incr (prodp + n + n2, cy);
     }
 }
 
 void
 #if __STDC__
-impn_sqr_n_basecase (mp_ptr prodp, mp_srcptr up, mp_size_t size)
+__gmpn_sqr (mp_ptr prodp, mp_srcptr up, mp_size_t n, mp_ptr tspace)
 #else
-impn_sqr_n_basecase (prodp, up, size)
+__gmpn_sqr (prodp, up, n, tspace)
      mp_ptr prodp;
      mp_srcptr up;
-     mp_size_t size;
-#endif
-{
-  mp_size_t i;
-  mp_limb_t cy_limb;
-  mp_limb_t v_limb;
-
-  /* Multiply by the first limb in V separately, as the result can be
-     stored (not added) to PROD.  We also avoid a loop for zeroing.  */
-  v_limb = up[0];
-  if (v_limb <= 1)
-    {
-      if (v_limb == 1)
-	MPN_COPY (prodp, up, size);
-      else
-	MPN_ZERO (prodp, size);
-      cy_limb = 0;
-    }
-  else
-    cy_limb = mpn_mul_1 (prodp, up, size, v_limb);
-
-  prodp[size] = cy_limb;
-  prodp++;
-
-  /* For each iteration in the outer loop, multiply one limb from
-     U with one limb from V, and add it to PROD.  */
-  for (i = 1; i < size; i++)
-    {
-      v_limb = up[i];
-      if (v_limb <= 1)
-	{
-	  cy_limb = 0;
-	  if (v_limb == 1)
-	    cy_limb = mpn_add_n (prodp, prodp, up, size);
-	}
-      else
-	cy_limb = mpn_addmul_1 (prodp, up, size, v_limb);
-
-      prodp[size] = cy_limb;
-      prodp++;
-    }
-}
-
-void
-#if __STDC__
-impn_sqr_n (mp_ptr prodp,
-	     mp_srcptr up, mp_size_t size, mp_ptr tspace)
-#else
-impn_sqr_n (prodp, up, size, tspace)
-     mp_ptr prodp;
-     mp_srcptr up;
-     mp_size_t size;
+     mp_size_t n;
      mp_ptr tspace;
 #endif
 {
-  if ((size & 1) != 0)
+  if (n & 1)
     {
-      /* The size is odd, the code code below doesn't handle that.
-	 Multiply the least significant (size - 1) limbs with a recursive
-	 call, and handle the most significant limb of S1 and S2
-	 separately.  */
-      /* A slightly faster way to do this would be to make the Karatsuba
-	 code below behave as if the size were even, and let it check for
-	 odd size in the end.  I.e., in essence move this code to the end.
-	 Doing so would save us a recursive call, and potentially make the
-	 stack grow a lot less.  */
+      /* Odd length. */
+      mp_size_t n2, n3, np1, nm1;
 
-      mp_size_t esize = size - 1;	/* even size */
-      mp_limb_t cy_limb;
+      n2 = n >> 1;
+      n3 = n - n2; /* = n2 + 1 */
 
-      MPN_SQR_N_RECURSE (prodp, up, esize, tspace);
-      cy_limb = mpn_addmul_1 (prodp + esize, up, esize, up[esize]);
-      prodp[esize + esize] = cy_limb;
-      cy_limb = mpn_addmul_1 (prodp + esize, up, size, up[esize]);
+      {
+	mp_limb_t w, w0, w1; mp_size_t i; mp_srcptr x, y;
+	/* Subtract smaller half of up[] from larger half. */
+	w = up[n2];
+	if (w)
+	  w -= mpn_sub_n (prodp, up, up + n3, n2);
+	else
+	  {
+	    i = n2 - 1;
+	    while ((w0 = up[i]) == (w1 = up[n3 + i]) && i != 0)
+	      i--;
+	    x = y = up;
+	    if (w0 < w1)
+	      x += n3;
+	    else
+	      y += n3;
+	    mpn_sub_n (prodp, x, y, n2);
+	  }
+	prodp[n2] = w;
+      }
 
-      prodp[esize + size] = cy_limb;
-    }
-  else
-    {
-      mp_size_t hsize = size >> 1;
-      mp_limb_t cy;
+      np1 = n + 1;
 
-      /*** Product H.	 ________________  ________________
-			|_____U1 x U1____||____U0 x U0_____|  */
-      /* Put result in upper part of PROD and pass low part of TSPACE
-	 as new TSPACE.  */
-      MPN_SQR_N_RECURSE (prodp + size, up + hsize, hsize, tspace);
-
-      /*** Product M.	 ________________
-			|_(U1-U0)(U0-U1)_|  */
-      if (mpn_cmp (up + hsize, up, hsize) >= 0)
+      if (n3 < KARATSUBA_SQR_THRESHOLD)
 	{
-	  mpn_sub_n (prodp, up + hsize, up, hsize);
+	  __gmpn_sqr_basecase (tspace, prodp, n3);
+	  __gmpn_sqr_basecase (prodp, up, n3);
+	  __gmpn_sqr_basecase (prodp + np1, up + n3, n2);
 	}
       else
 	{
-	  mpn_sub_n (prodp, up, up + hsize, hsize);
+	  __gmpn_sqr (tspace, prodp, n3, tspace + np1);
+	  __gmpn_sqr (prodp, up, n3, tspace + np1);
+	  __gmpn_sqr (prodp + np1, up + n3, n2, tspace + np1);
 	}
 
-      /* Read temporary operands from low part of PROD.
-	 Put result in low part of TSPACE using upper part of TSPACE
-	 as new TSPACE.  */
-      MPN_SQR_N_RECURSE (tspace, prodp, hsize, tspace + size);
+      mpn_sub_n (tspace, prodp, tspace, np1);
+      nm1 = n - 1;
+      if (mpn_add_n (tspace, tspace, prodp + np1, nm1))
+	{
+	  mp_limb_t x = tspace[nm1] + 1;
+	  tspace[nm1] = x;
+	  if (x == 0)
+	    ++tspace[n];
+	}
+      if (mpn_add_n (prodp + n3, prodp + n3, tspace, np1))
+	mpn_incr (prodp + np1 + n3, (mp_limb_t) 1);
+    }
+  else
+    {
+      /* Even length. */
+      mp_limb_t cy;
+      mp_size_t n2 = n >> 1;
 
-      /*** Add/copy product H.  */
-      MPN_COPY (prodp + hsize, prodp + size, hsize);
-      cy = mpn_add_n (prodp + size, prodp + size, prodp + size + hsize, hsize);
+      {
+	mp_limb_t w0, w1; mp_size_t i; mp_srcptr x, y;
+	/* Subtract smaller half of up[] from larger half. */
+	i = n2 - 1;
+	while ((w0 = up[i]) == (w1 = up[n2 + i]) && i != 0)
+	  i--;
+	x = y = up;
+	if (w0 < w1)
+	  x += n2;
+	else
+	  y += n2;
+	mpn_sub_n (prodp, x, y, n2);
+      }
 
-      /*** Add product M (if NEGFLG M is a negative number).  */
-      cy -= mpn_sub_n (prodp + hsize, prodp + hsize, tspace, size);
+    if (n2 < KARATSUBA_SQR_THRESHOLD)
+      {
+	__gmpn_sqr_basecase (tspace, prodp, n2);
+	__gmpn_sqr_basecase (prodp, up, n2);
+	__gmpn_sqr_basecase (prodp + n, up + n2, n2);
+      }
+    else
+      {
+	__gmpn_sqr (tspace, prodp, n2, tspace + n);
+	__gmpn_sqr (prodp, up, n2, tspace + n);
+	__gmpn_sqr (prodp + n, up + n2, n2, tspace + n);
+      }
 
-      /*** Product L.	 ________________  ________________
-			|________________||____U0 x U0_____|  */
-      /* Read temporary operands from low part of PROD.
-	 Put result in low part of TSPACE using upper part of TSPACE
-	 as new TSPACE.  */
-      MPN_SQR_N_RECURSE (tspace, up, hsize, tspace + size);
-
-      /*** Add/copy Product L (twice).  */
-
-      cy += mpn_add_n (prodp + hsize, prodp + hsize, tspace, size);
-      if (cy)
-	mpn_add_1 (prodp + hsize + size, prodp + hsize + size, hsize, cy);
-
-      MPN_COPY (prodp, tspace, hsize);
-      cy = mpn_add_n (prodp + hsize, prodp + hsize, tspace + hsize, hsize);
-      if (cy)
-	mpn_add_1 (prodp + size, prodp + size, size, 1);
+      cy = -mpn_sub_n (tspace, prodp, tspace, n);
+      cy += mpn_add_n (tspace, tspace, prodp + n, n);
+      cy += mpn_add_n (prodp + n2, prodp + n2, tspace, n);
+      mpn_incr (prodp + n + n2, cy);
     }
 }
 
 /* This should be made into an inline function in gmp.h.  */
 inline void
 #if __STDC__
-mpn_mul_n (mp_ptr prodp, mp_srcptr up, mp_srcptr vp, mp_size_t size)
+mpn_mul_n (mp_ptr prodp, mp_srcptr up, mp_srcptr vp, mp_size_t n)
 #else
-mpn_mul_n (prodp, up, vp, size)
+mpn_mul_n (prodp, up, vp, n)
      mp_ptr prodp;
      mp_srcptr up;
      mp_srcptr vp;
-     mp_size_t size;
+     mp_size_t n;
 #endif
 {
   TMP_DECL (marker);
   TMP_MARK (marker);
   if (up == vp)
     {
-      if (size < KARATSUBA_THRESHOLD)
+      if (n < KARATSUBA_SQR_THRESHOLD)
 	{
-	  impn_sqr_n_basecase (prodp, up, size);
+	  __gmpn_sqr_basecase (prodp, up, n);
 	}
       else
 	{
 	  mp_ptr tspace;
-	  tspace = (mp_ptr) TMP_ALLOC (2 * size * BYTES_PER_MP_LIMB);
-	  impn_sqr_n (prodp, up, size, tspace);
+	  tspace = (mp_ptr) TMP_ALLOC (2 * (n + BITS_PER_MP_LIMB) * BYTES_PER_MP_LIMB);
+	  __gmpn_sqr (prodp, up, n, tspace);
 	}
     }
   else
     {
-      if (size < KARATSUBA_THRESHOLD)
+      if (n < KARATSUBA_MUL_THRESHOLD)
 	{
-	  impn_mul_n_basecase (prodp, up, vp, size);
+	  __gmpn_mul_basecase (prodp, up, n, vp, n);
 	}
       else
 	{
 	  mp_ptr tspace;
-	  tspace = (mp_ptr) TMP_ALLOC (2 * size * BYTES_PER_MP_LIMB);
-	  impn_mul_n (prodp, up, vp, size, tspace);
+	  tspace = (mp_ptr) TMP_ALLOC (2 * (n + BITS_PER_MP_LIMB) * BYTES_PER_MP_LIMB);
+	  __gmpn_mul_n (prodp, up, vp, n, tspace);
 	}
     }
   TMP_FREE (marker);
