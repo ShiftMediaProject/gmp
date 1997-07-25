80f3d688c2b3d8a05f7b2048c6c6cdcb3557193c
tege <tege@gmplib.org>
1997-07-25 20:08:51 +0200

    Add missing code after final `else'.

--- mpz/setbit.c
+++ mpz/setbit.c
@@ -108,6 +108,11 @@ mpz_setbit (d, bit_index)
 	    }
 	}
       else
-	;
+	{
+	  mpn_sub_1 (dp + limb_index, dp + limb_index, dsize - limb_index,
+		     (mp_limb_t) 1 << (bit_index % BITS_PER_MP_LIMB));
+	  dsize -= dp[dsize - 1] == 0;
+	  d->_mp_size = -dsize;
+	}
     }
 }
