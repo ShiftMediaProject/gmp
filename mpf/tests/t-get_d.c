9aa59b1654e2e53c0800889e3f6e486dcb337b92
tege <tege@gmplib.org>
1999-03-17 20:06:59 +0100

    *** empty log message ***

---
+++ mpf/tests/t-get_d.c
@@ -0,0 +1,46 @@
+/* Test mpf_set_d.
+
+   Copyright (C) 1996 Free Software Foundation, Inc.
+
+   This file is part of the GNU MP Library.
+
+   The GNU MP Library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or (at your
+   option) any later version.
+
+   The GNU MP Library is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+   License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+   MA 02111-1307, USA. */
+
+#include "gmp.h"
+
+main (int argc, char **argv)
+{
+  double d, e, r;
+  mpf_t u, v;
+
+  mpf_init (u);
+  mpf_init (v);
+
+  mpf_set_d (u, 1e-201);
+  for (d = 1e-200; d < 1e200; d *= 1.01)
+    {
+      mpf_set_d (v, d);
+      if (mpf_cmp (u, v) >= 0)
+	abort ();
+      e = mpf_get_d (v);
+      r = e/d;
+      if (r < 0.9999999999 | r > 1.00000000001)
+	abort ();
+      mpf_set (u, v);
+    }
+
+  exit (0);
+}
