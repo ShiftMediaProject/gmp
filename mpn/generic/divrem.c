0b764662162e7366944aa08aec525f4c3816ec7e
tege <tege@gmplib.org>
1999-07-27 22:59:04 +0200

    * Delete special cases, handle just divisors of more than 2 limbs. * Rework variable usage for better register allocation.

--- mpn/generic/divrem.c
+++ mpn/generic/divrem.c
@@ -72,223 +72,97 @@ mpn_divrem_classic (qp, qxn, np, nsize, dp, dsize)
 #endif
 {
   mp_limb_t most_significant_q_limb = 0;
+  mp_size_t i;
+  mp_limb_t dx, d1, n0;
+  mp_limb_t dxinv;
+  int have_preinv;
 
-  switch (dsize)
+  np += nsize - dsize;
+  dx = dp[dsize - 1];
+  d1 = dp[dsize - 2];
+  n0 = np[dsize - 1];
+
+  if (n0 >= dx)
     {
-    case 0:
-      DIVIDE_BY_ZERO;
-      return 0;
+      if (n0 > dx || mpn_cmp (np, dp, dsize - 1) >= 0)
+	{
+	  mpn_sub_n (np, np, dp, dsize);
+	  n0 = np[dsize - 1];
+	  most_significant_q_limb = 1;
+	}
+    }
 
-    case 1:
-      {
-	mp_size_t i;
-	mp_limb_t n1;
-	mp_limb_t d;
-
-	d = dp[0];
-	n1 = np[nsize - 1];
-
-	if (n1 >= d)
-	  {
-	    n1 -= d;
-	    most_significant_q_limb = 1;
-	  }
-
-	/* Develop integer part of quotient.  */
-	qp += qxn;
-	for (i = nsize - 2; i >= 0; i--)
-	  udiv_qrnnd (qp[i], n1, n1, np[i], d);
-	qp -= qxn;
-
-	/* Develop fraction part naively.  */
-	for (i = qxn - 1; i >= 0; i--)
-	  udiv_qrnnd (qp[i], n1, n1, 0, d);
-
-	np[0] = n1;
-      }
-      break;
-
-    case 2:
-      {
-	mp_size_t i;
-	mp_limb_t n1, n0, n2;
-	mp_limb_t d1, d0;
-	mp_limb_t d1inv;
-	int have_preinv;
-
-	np += nsize - 2;
-	d1 = dp[1];
-	d0 = dp[0];
-	n1 = np[1];
-	n0 = np[0];
-
-	if (n1 >= d1 && (n1 > d1 || n0 >= d0))
-	  {
-	    sub_ddmmss (n1, n0, n1, n0, d1, d0);
-	    most_significant_q_limb = 1;
-	  }
-
-	/* If multiplication is much faster than division, preinvert the
+  /* If multiplication is much faster than division, preinvert the
 	   most significant divisor limb before entering the loop.  */
-	if (UDIV_TIME > 2 * UMUL_TIME + 6)
-	  {
-	    have_preinv = 0;
-	    if ((UDIV_TIME - (2 * UMUL_TIME + 6)) * (nsize - 2) > UDIV_TIME)
-	      {
-		invert_limb (d1inv, d1);
-		have_preinv = 1;
-	      }
-	  }
+  if (UDIV_TIME > 2 * UMUL_TIME + 6)
+    {
+      have_preinv = 0;
+      if ((UDIV_TIME - (2 * UMUL_TIME + 6)) * (nsize - dsize) > UDIV_TIME)
+	{
+	  invert_limb (dxinv, dx);
+	  have_preinv = 1;
+	}
+    }
 
-	for (i = qxn + nsize - 2 - 1; i >= 0; i--)
-	  {
-	    mp_limb_t q;
-	    mp_limb_t r;
+  for (i = qxn + nsize - dsize - 1; i >= 0; i--)
+    {
+      mp_limb_t q;
+      mp_limb_t nx;
+      mp_limb_t cy_limb;
 
-	    if (i >= qxn)
-	      np--;
-	    else
-	      np[0] = 0;
+      nx = np[dsize - 1];
+      if (i >= qxn)
+	{
+	  np--;
+	}
+      else
+	{
+	  MPN_COPY_DECR (np + 1, np, dsize - 1);
+	  np[0] = 0;
+	}
 
-	    if (n1 == d1)
-	      {
-		/* Q should be either 111..111 or 111..110.  Need special
-		   treatment of this rare case as normal division would
-		   give overflow.  */
-		q = ~(mp_limb_t) 0;
+      if (nx == dx)
+	/* This might over-estimate q, but it's probably not worth
+	   the extra code here to find out.  */
+	q = ~(mp_limb_t) 0;
+      else
+	{
+	  mp_limb_t r, p1, p0;
 
-		r = n0 + d1;
-		if (r < d1)	/* Carry in the addition? */
-		  {
-		    add_ssaaaa (n1, n0, r - d0, np[0], 0, d0);
-		    qp[i] = q;
-		    continue;
-		  }
-		n1 = d0 - (d0 != 0);
-		n0 = -d0;
-	      }
-	    else
-	      {
-		if (UDIV_TIME > 2 * UMUL_TIME + 6 && have_preinv)
-		  udiv_qrnnd_preinv (q, r, n1, n0, d1, d1inv);
-		else
-		  udiv_qrnnd (q, r, n1, n0, d1);
-		umul_ppmm (n1, n0, d0, q);
-	      }
+	  if (UDIV_TIME > 2 * UMUL_TIME + 6 && have_preinv)
+	    udiv_qrnnd_preinv (q, r, nx, np[dsize - 1], dx, dxinv);
+	  else
+	    udiv_qrnnd (q, r, nx, np[dsize - 1], dx);
+	  umul_ppmm (p1, p0, d1, q);
 
-	    n2 = np[0];
-	  q_test:
-	    if (n1 > r || (n1 == r && n0 > n2))
-	      {
-		/* The estimated Q was too large.  */
-		q--;
+#if 0
+	  while (p1 > r || (p1 == r && p0 > np[dsize - 2]))
+	    {
+	      q--;
+	      r += dx;
+	      if (r < dx)	/* I.e. "carry in previous addition?"  */
+		break;
+	      p1 -= p0 < d1;
+	      p0 -= d1;
+	    }
+#else
+	  q -= p1 > r || (p1 == r && p0 > np[dsize - 2]);
+#endif
+	}
 
-		sub_ddmmss (n1, n0, n1, n0, 0, d0);
-		r += d1;
-		if (r >= d1)	/* If not carry, test Q again.  */
-		  goto q_test;
-	      }
+      /* Possible optimization: We already have (q * nx) and (1 * n1)
+	 after the calculation of q.  Taking advantage of that, we
+	 could make this loop make two iterations less.  */
 
-	    qp[i] = q;
-	    sub_ddmmss (n1, n0, r, n2, n1, n0);
-	  }
-	np[1] = n1;
-	np[0] = n0;
-      }
-      break;
+      cy_limb = mpn_submul_1 (np, dp, dsize, q);
 
-    default:
-      {
-	mp_size_t i;
-	mp_limb_t dX, d1, n0;
-	mp_limb_t dXinv;
-	int have_preinv;
+      if (nx != cy_limb)
+	{
+	  mpn_add_n (np, np, dp, dsize);
+	  q--;
+	}
 
-	np += nsize - dsize;
-	dX = dp[dsize - 1];
-	d1 = dp[dsize - 2];
-	n0 = np[dsize - 1];
-
-	if (n0 >= dX)
-	  {
-	    if (n0 > dX || mpn_cmp (np, dp, dsize - 1) >= 0)
-	      {
-		mpn_sub_n (np, np, dp, dsize);
-		n0 = np[dsize - 1];
-		most_significant_q_limb = 1;
-	      }
-	  }
-
-	/* If multiplication is much faster than division, preinvert the
-	   most significant divisor limb before entering the loop.  */
-	if (UDIV_TIME > 2 * UMUL_TIME + 6)
-	  {
-	    have_preinv = 0;
-	    if ((UDIV_TIME - (2 * UMUL_TIME + 6)) * (nsize - dsize) > UDIV_TIME)
-	      {
-		invert_limb (dXinv, dX);
-		have_preinv = 1;
-	      }
-	  }
-
-	for (i = qxn + nsize - dsize - 1; i >= 0; i--)
-	  {
-	    mp_limb_t q;
-	    mp_limb_t n1, n2;
-	    mp_limb_t cy_limb;
-
-	    n2 = np[dsize - 1];
-	    if (i >= qxn)
-	      {
-		np--;
-	      }
-	    else
-	      {
-		MPN_COPY_DECR (np + 1, np, dsize - 1);
-		np[0] = 0;
-	      }
-
-	    if (n0 == dX)
-	      /* This might over-estimate q, but it's probably not worth
-		 the extra code here to find out.  */
-	      q = ~(mp_limb_t) 0;
-	    else
-	      {
-		mp_limb_t r;
-
-		if (UDIV_TIME > 2 * UMUL_TIME + 6 && have_preinv)
-		  udiv_qrnnd_preinv (q, r, n0, np[dsize - 1], dX, dXinv);
-		else
-		  udiv_qrnnd (q, r, n0, np[dsize - 1], dX);
-		umul_ppmm (n1, n0, d1, q);
-
-		while (n1 > r || (n1 == r && n0 > np[dsize - 2]))
-		  {
-		    q--;
-		    r += dX;
-		    if (r < dX)	/* I.e. "carry in previous addition?"  */
-		      break;
-		    n1 -= n0 < d1;
-		    n0 -= d1;
-		  }
-	      }
-
-	    /* Possible optimization: We already have (q * n0) and (1 * n1)
-	       after the calculation of q.  Taking advantage of that, we
-	       could make this loop make two iterations less.  */
-
-	    cy_limb = mpn_submul_1 (np, dp, dsize, q);
-
-	    if (n2 != cy_limb)
-	      {
-		mpn_add_n (np, np, dp, dsize);
-		q--;
-	      }
-
-	    qp[i] = q;
-	    n0 = np[dsize - 1];
-	  }
-      }
+      qp[i] = q;
     }
 
   return most_significant_q_limb;
