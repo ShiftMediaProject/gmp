7b52f83284fd7bfbae670f79e6f0e439e0f8c4a1
Kevin Ryde <user42@zip.com.au>
2000-02-26 23:01:20 +0100

    New version

--- mpn/x86/k6/aors_n.asm
+++ mpn/x86/k6/aors_n.asm
@@ -1,10 +1,9 @@
 # AMD K6 mpn_add/sub_n -- mpn addition or subtraction.
 #
-# K6: 3.2 cycles/limb, or in-place 2.9 cycles/limb (at 16 limbs per loop),
-# PIC adds 3 cycles at the start.
+# K6: normal 3.25 cycles/limb, in-place 2.75 cycles/limb.
 
 
-# Copyright (C) 1999-2000 Free Software Foundation, Inc.
+# Copyright (C) 2000 Free Software Foundation, Inc.
 #
 # This file is part of the GNU MP Library.
 #
@@ -26,71 +25,50 @@
 include(`../config.m4')
 
 
-dnl  K6                cycles/limb
-dnl  UNROLL_COUNT  separate  in-place
-dnl       8           3.5      3.0
-dnl      16           3.2      2.9
-dnl      32           3.2      2.95
-dnl      64           3.18     2.9
-dnl  Maximum possible with the current code is 64.
-
-deflit(UNROLL_COUNT, 16)
-
-
 ifdef(`OPERATION_add_n', `
 	define(M4_inst,        adcl)
 	define(M4_function_n,  mpn_add_n)
 	define(M4_function_nc, mpn_add_nc)
 	define(M4_description, add)
-	define(M4_bitdesc,     carry)
 ',`ifdef(`OPERATION_sub_n', `
 	define(M4_inst,        sbbl)
 	define(M4_function_n,  mpn_sub_n)
 	define(M4_function_nc, mpn_sub_nc)
 	define(M4_description, subtract)
-	define(M4_bitdesc,     borrow)
 ',`m4_error(`Need OPERATION_add_n or OPERATION_sub_n
 ')')')
 
-MULFUNC_PROLOGUE(mpn_add_n mpn_add_nc mpn_sub_n mpn_sub_nc)
 
-
-`#' mp_limb_t M4_function_n (mp_ptr wp, mp_srcptr src1, mp_srcptr src2,
-`#'                            mp_size_t size);
+`#' mp_limb_t M4_function_n (mp_ptr dst, mp_srcptr src1, mp_srcptr src2,
+`#'                          mp_size_t size);
 `#' mp_limb_t M4_function_nc (mp_ptr dst, mp_srcptr src1, mp_srcptr src2,
-`#'                             mp_size_t size, mp_limb_t carry);
+`#'	                      mp_size_t size, mp_limb_t carry);
 `#'
 `#' Calculate src1,size M4_description src2,size, and store the result in
-`#' dst,size.  The return value is the M4_bitdesc bit from the top of the
-`#' result (1 or 0).
-`#'
-`#' The _nc version accepts 1 or 0 for an initial M4_bitdesc into the low limb
-# of the calculation.  Note values other than 1 or 0 will lead to garbage
+`#' dst,size.  The return value is the carry bit from the top of the result
+`#' (1 or 0).
+#
+# The _nc version accepts 1 or 0 for an initial carry into the low limb of
+# the calculation.  Note values other than 1 or 0 here will lead to garbage
 # results.
 #
-# In the unrolled loop, the adcl seems to take two cycles of decode, and
-# since it needs at least one more instruction, the current 3 cycles/limb is
-# as good as possible with this technique.
-#
-# The in-place unrolled loop looks like it should be 2.5 cycles/limb but
-# comes out about 2.9.  Maybe code fetching causes this.
+# Instruction decoding limits a normal dst=src1+src2 operation to 3 c/l, and
+# an in-place dst+=src to 2.5 c/l.  The unrolled loops have 1 cycle/loop of
+# loop control, which with 4 limbs/loop means an extra 0.25 c/l.
 
-ifdef(`PIC',`
-deflit(UNROLL_THRESHOLD, 10)
-',`
-deflit(UNROLL_THRESHOLD, 7)
-')
-
-defframe(PARAM_CARRY,20)
-defframe(PARAM_SIZE, 16)
-defframe(PARAM_SRC2, 12)
-defframe(PARAM_SRC1, 8)
-defframe(PARAM_DST,  4)
+define(PARAM_CARRY, `FRAME+20(%esp)')
+define(PARAM_SIZE,  `FRAME+16(%esp)')
+define(PARAM_SRC2,  `FRAME+12(%esp)')
+define(PARAM_SRC1,  `FRAME+8(%esp)')
+define(PARAM_DST,   `FRAME+4(%esp)')
 deflit(`FRAME',0)
 
-	.text
+dnl  minimum 5 because the unrolled code can't handle less
+deflit(UNROLL_THRESHOLD, 5)
 
+	.text
 	ALIGN(32)
+
 PROLOGUE(M4_function_nc)
 	movl	PARAM_CARRY, %eax
 	jmp	LF(M4_function_n,start)
@@ -98,25 +76,26 @@ EPILOGUE()
 
 
 PROLOGUE(M4_function_n)
-	xorl	%eax, %eax	# carry
+	xorl	%eax, %eax
 L(start):
 	movl	PARAM_SIZE, %ecx
-
-	movl	PARAM_SRC2, %edx
 	pushl	%ebx
-deflit(`FRAME',4)
+FRAME_pushl()
 
 	movl	PARAM_SRC1, %ebx
+	pushl	%edi
+FRAME_pushl()
+
+	movl	PARAM_SRC2, %edx
 	cmpl	$UNROLL_THRESHOLD, %ecx
 
-	pushl	%edi
-deflit(`FRAME',8)
+	movl	PARAM_DST, %edi
 	jae	L(unroll)
 
-	shrl	%eax
-	movl	PARAM_DST, %edi
 
-	# this is offset 0x22, so it's close enough to aligned
+	shrl	%eax		# initial carry flag
+
+	# offset 0x21 here, close enough to aligned
 L(simple):
 	# eax	scratch
 	# ebx	src1
@@ -150,212 +129,195 @@ L(simple):
 
 
 #------------------------------------------------------------------------------
-L(unroll):
-deflit(`FRAME',8)
-	movl	PARAM_DST, %edi
-	pushl	%esi
-deflit(`FRAME',12)
 
-	cmpl	%ebx, %edi
+L(unroll):
+	# eax	carry
+	# ebx	src1
+	# ecx	counter
+	# edx	src2
+	# esi
+	# edi	dst
+	# ebp
+
+	cmpl	%edi, %ebx
+	pushl	%esi
+
 	je	L(inplace)
 
 ifdef(`OPERATION_add_n',`
-	# reverse overlap can be handled in add
-	cmpl	%edx, %edi
+	cmpl	%edi, %edx
 	je	L(inplace_reverse)
 ')
 
-	movl	%ecx, %edi
-	decl	%ecx
+	movl	%ecx, %esi
 
-	shrl	$UNROLL_LOG2, %ecx
-	negl	%edi
+	andl	$~3, %ecx
+	andl	$3, %esi
 
-	andl	$UNROLL_MASK, %edi
+	leal	(%ebx,%ecx,4), %ebx
+	leal	(%edx,%ecx,4), %edx
+	leal	(%edi,%ecx,4), %edi
 
-ifdef(`PIC',`
-	call	L(pic_calc)
-L(pic_ret):
-',`
-	leal	L(entry) (%edi,%edi,8), %esi
-')
-	negl	%edi
-
-	# the following will decode as shown, but only catch up later
-
-	leal	ifelse(UNROLL_BYTES,256,128) (%ebx,%edi,4), %ebx
-	leal	ifelse(UNROLL_BYTES,256,128) (%edx,%edi,4), %edx
-
-	shll	$2, %edi
-	addl	PARAM_DST, %edi
-	ifelse(UNROLL_BYTES,256,`addl $128, %edi')
-
+	negl	%ecx
 	shrl	%eax
-	jmp	*%esi
+
+	ALIGN(32)
+L(normal_top):
+	# eax	counter, qwords, negative
+	# ebx	src1
+	# ecx	scratch
+	# edx	src2
+	# esi
+	# edi	dst
+	# ebp
+
+ 	movl	(%ebx,%ecx,4), %eax
+	leal	5(%ecx), %ecx
+ 	M4_inst	-20(%edx,%ecx,4), %eax
+ 	movl	%eax, -20(%edi,%ecx,4)
+
+ 	movl	4-20(%ebx,%ecx,4), %eax
+ 	M4_inst	4-20(%edx,%ecx,4), %eax
+ 	movl	%eax, 4-20(%edi,%ecx,4)
+
+ 	movl	8-20(%ebx,%ecx,4), %eax
+ 	M4_inst	8-20(%edx,%ecx,4), %eax
+ 	movl	%eax, 8-20(%edi,%ecx,4)
+
+ 	movl	12-20(%ebx,%ecx,4), %eax
+ 	M4_inst	12-20(%edx,%ecx,4), %eax
+ 	movl	%eax, 12-20(%edi,%ecx,4)
+
+	loop	L(normal_top)
+
+
+	decl	%esi
+	jz	L(normal_finish_one)
+	js	L(normal_done)
+
+	# two or three more limbs
+
+ 	movl	(%ebx), %eax
+ 	M4_inst	(%edx), %eax
+ 	movl	%eax, (%edi)
+
+ 	movl	4(%ebx), %eax
+ 	M4_inst	4(%edx), %eax
+	decl	%esi
+ 	movl	%eax, 4(%edi)
+
+	jz	L(normal_done)
+	movl	$2, %ecx
+
+L(normal_finish_one):
+ 	movl	(%ebx,%ecx,4), %eax
+ 	M4_inst	(%edx,%ecx,4), %eax
+ 	movl	%eax, (%edi,%ecx,4)
+
+L(normal_done):	
+	popl	%esi
+	popl	%edi
+
+	movl	$0, %eax
+	popl	%ebx
+
+	setc	%al
+
+	ret
 
 
 #------------------------------------------------------------------------------
-# This is within range of a short jump for the above tests.
-
-	ALIGN(16)
-deflit(`FRAME',12)
 
 ifdef(`OPERATION_add_n',`
 L(inplace_reverse):
+	# dst==src2
+
 	movl	%ebx, %edx
 ')
 
 L(inplace):
-	# eax
-	# ebx	(has been saved)
+	# eax	initial carry
+	# ebx
 	# ecx	size
 	# edx	src
-	# esi	(has been saved)
+	# esi
 	# edi	dst
 	# ebp
 
-	movl	%ecx, %ebx
-	andl	$~1, %ecx	# handle even num in loop
-
-	pushl	%ebp
-deflit(`FRAME',16)
-	movl	%ecx, %ebp
-
-	negl	%ebp
-	andl	$1, %ebx	# the remaining 0 or 1
-
-	andl	$UNROLL_MASK, %ebp
+	leal	-1(%ecx), %esi
 	decl	%ecx
 
-	addl	%ebp, %ebp
+	andl	$~3, %ecx
+	andl	$3, %esi
 
-ifdef(`PIC',`
-	call	L(inplace_pic_calc)
-L(inplace_pic_ret):
-',`
-	leal	L(inplace_entry) (%ebp,%ebp,2), %esi
-')
-	negl	%ebp
+ 	movl	(%edx), %ebx		# src low limb
+	leal	(%edx,%ecx,4), %edx
 
-	shrl	$UNROLL_LOG2, %ecx
-	leal	ifelse(UNROLL_BYTES,256,128) (%edx,%ebp,2), %edx
+	leal	(%edi,%ecx,4), %edi
+	negl	%ecx
 
 	shrl	%eax
-	leal	ifelse(UNROLL_BYTES,256,128) (%edi,%ebp,2), %edi
-
-	jmp	*%esi
 
 
-ifdef(`PIC',`
-L(pic_calc):
-	# See README.family about old gas bugs
-	leal	(%edi,%edi,8), %esi
-	addl	$L(entry)-L(pic_ret), %esi
-	addl	(%esp), %esi
-	ret
-')
-
-
-ifdef(`PIC',`
-L(inplace_pic_calc):
-	# See README.family about old gas bugs
-	leal	(%ebp,%ebp,2), %esi
-	addl	$L(inplace_entry)-L(inplace_pic_ret), %esi
-	addl	(%esp), %esi
-	ret
-')
-
-
-#------------------------------------------------------------------------------
-# needs to be 32-byte aligned to get the speed claimed
-	ALIGN(32)
-L(top):
-	# eax	zero
-	# ebx	src1
-	# ecx	counter
-	# edx	src2
-	# esi	scratch
-	# edi	dst
-	# ebp
-	#
-	# 9 code bytes per limb
-
-	leal	UNROLL_BYTES(%edx), %edx
-	leal	UNROLL_BYTES(%edi), %edi
-
-L(entry):
-forloop(`i', 0, UNROLL_COUNT-1, `
-	deflit(`disp', eval(i*4 ifelse(UNROLL_BYTES,256,-128)))
-
-	movl	disp(%ebx), %esi
-	M4_inst	disp(%edx), %esi
-	movl	%esi, disp(%edi)
-')
-
-	leal	UNROLL_BYTES(%ebx), %ebx
-
-	decl	%ecx
-	jns	L(top)
-
-
-	setc	%al
-
-	popl	%esi
-	popl	%edi
-
-	popl	%ebx
-	ret
-
-
-#------------------------------------------------------------------------------
-	# needs to be 32-byte aligned to get the speed claimed
 	ALIGN(32)
 L(inplace_top):
-	# eax	zero
-	# ebx	0 or 1 leftover limbs
-	# ecx	counter
+	# eax
+	# ebx	next src limb
+	# ecx	size
 	# edx	src
-	# esi	scratch
+	# esi
 	# edi	dst
-	# ebp	scratch
-	#
-	# 12 code bytes per 2 limbs
+	# ebp
 
-	leal	UNROLL_BYTES(%edx), %edx
-	leal	UNROLL_BYTES(%edi), %edi
+ 	M4_inst	%ebx, (%edi,%ecx,4)
 
-L(inplace_entry):
-deflit(`CHUNK_COUNT',2)
-forloop(`i', 0, UNROLL_COUNT/CHUNK_COUNT-1, `
-	deflit(`disp0', eval(i*CHUNK_COUNT*4 ifelse(UNROLL_BYTES,256,-128)))
-	deflit(`disp1', eval(disp0 + 4))
+ 	movl	4(%edx,%ecx,4), %eax
+	leal	5(%ecx), %ecx
 
-	movl	disp0(%edx), %esi
-	movl	disp1(%edx), %ebp
-	M4_inst	%esi, disp0(%edi)
-	M4_inst	%ebp, disp1(%edi)
-')
+ 	M4_inst	%eax, 4-20(%edi,%ecx,4)
 
-	decl	%ecx
-	jns	L(inplace_top)
+ 	movl	8-20(%edx,%ecx,4), %eax
+ 	movl	12-20(%edx,%ecx,4), %ebx
+
+ 	M4_inst	%eax, 8-20(%edi,%ecx,4)
+ 	M4_inst	%ebx, 12-20(%edi,%ecx,4)
+
+ 	movl	16-20(%edx,%ecx,4), %ebx
+	loop	L(inplace_top)
 
 
-deflit(`disp0', eval(UNROLL_BYTES ifelse(UNROLL_BYTES,256,-128)))
-	decl	%ebx	# 0 or 1 left over
-	popl	%ebp
+	# now %esi is 0 to 3 representing respectively 1 to 4 limbs more
 
-	jnz	L(inplace_even)
-	movl	disp0(%edx), %ecx
+ 	M4_inst	%ebx, (%edi)
 
-	M4_inst	%ecx, disp0(%edi)
-L(inplace_even):
+	decl	%esi
+	jz	L(inplace_finish_one)
+	js	L(inplace_done)
 
+	# two or three more limbs
+
+ 	movl	4(%edx), %eax
+ 	movl	8(%edx), %ebx
+ 	M4_inst	%eax, 4(%edi)
+ 	M4_inst	%ebx, 8(%edi)
+
+	decl	%esi
+	movl	$2, %ecx
+
+	jz	L(normal_done)
+
+L(inplace_finish_one):
+ 	movl	4(%edx,%ecx,4), %eax
+ 	M4_inst	%eax, 4(%edi,%ecx,4)
+
+L(inplace_done):	
 	popl	%esi
 	popl	%edi
 
-	setc	%al
-
+	movl	$0, %eax
 	popl	%ebx
+
+	setc	%al
 
 	ret
 
