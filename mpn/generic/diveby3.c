7d61f5202d1a7689df5ddeb188ae10d789f1ff03
Kevin Ryde <user42@zip.com.au>
2000-03-14 23:50:27 +0100

    Initial version.

---
+++ mpn/generic/diveby3.c
@@ -0,0 +1,75 @@
+/* mpn_divexact_by3 -- mpn division by 3, expecting no remainder. */
+
+/*
+Copyright (C) 2000 Free Software Foundation, Inc.
+
+This file is part of the GNU MP Library.
+
+The GNU MP Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The GNU MP Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+#include "gmp.h"
+#include "gmp-impl.h"
+
+
+#define MP_LIMB_T_MAX  (((mp_limb_t) 0) - 1)
+
+/* Multiplicative inverse of 3, modulo 2^BITS_PER_MP_LIMB.
+   0xAAAAAAAB for 32 bits, 0xAAAAAAAAAAAAAAAB for 64 bits. */
+#define INVERSE_3      ((MP_LIMB_T_MAX / 3) * 2 + 1)
+
+
+/* Divide src,size by 3 and store the quotient in dst,size.  If src,size
+   isn't exactly divisible by 3 the result in dst,size won't be very useful.
+   The return value is 0 if src,size was divisible by 3, or non-zero if not.
+
+   This code will be better than the equivalent mpn_divmod_1 for any CPUs
+   with a fast mul but slowish div.
+
+   The c += is adding the high word of 3*l to c.  That high word will be 0,
+   1 or 2. */
+
+mp_limb_t
+#if __STDC__
+mpn_divexact_by3 (mp_ptr dst, mp_srcptr src, mp_size_t size)
+#else
+mpn_divexact_by3 (dst, src, size)
+     mp_ptr    dst;
+     mp_srcptr src;
+     mp_size_t size;
+#endif
+{
+  mp_size_t  i;
+  mp_limb_t  c, l;
+
+  ASSERT (size >= 1);
+
+  c = 0;
+  i = 0;
+  do
+    {
+      l = src[i] - c;
+      c = (l > src[i]);
+
+      l *= INVERSE_3;
+      dst[i] = l;
+
+      c += (l > MP_LIMB_T_MAX/3) + (l > (MP_LIMB_T_MAX/3)*2);
+    }
+  while (++i < size);
+
+  return c;
+}
