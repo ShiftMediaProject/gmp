5fbff747e934cdbf82b7839424361bbd1c40763d
Kevin Ryde <user42@zip.com.au>
2000-03-23 01:31:03 +0100

    Add copyright comment, restore "const" somehow lost.

--- version.c
+++ version.c
@@ -1,3 +1,26 @@
+/* gmp_version -- version number compiled into the library */
+
+/*
+Copyright (C) 1996,1999,2000 Free Software Foundation, Inc.
+
+This file is part of the GNU MP Library.
+
+The GNU MP Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The GNU MP Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #include "gmp.h"
 #include "gmp-impl.h"
-static char *gmp_version = VERSION;
+
+static const char *gmp_version = VERSION;
