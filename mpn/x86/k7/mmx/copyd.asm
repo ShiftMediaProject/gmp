e9cc041fbd6e2bbcafea042d5f4b7fda2b29a37e
Kevin Ryde <user42@zip.com.au>
2000-03-25 00:34:16 +0100

    Fix FSF address.

--- mpn/x86/k7/mmx/copyd.asm
+++ mpn/x86/k7/mmx/copyd.asm
@@ -5,7 +5,7 @@
 # K7    0.75  1.0   1.0   0.75
 
 
-# Copyright (C) 1999-2000 Free Software Foundation, Inc.
+# Copyright (C) 1999, 2000 Free Software Foundation, Inc.
 #
 # This file is part of the GNU MP Library.
 #
@@ -21,7 +21,8 @@
 #
 # You should have received a copy of the GNU Library General Public License
 # along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
-# the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+# MA 02111-1307, USA.
 
 
 include(`../config.m4')
