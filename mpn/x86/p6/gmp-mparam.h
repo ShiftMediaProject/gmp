f8b1ebed87ea5b06b062583b72dd0e09c7b6a8c5
Kevin Ryde <user42@zip.com.au>
2000-04-06 02:34:42 +0200

    Tune thresholds, add UMUL_TIME, UDIV_TIME.

--- mpn/x86/p6/gmp-mparam.h
+++ mpn/x86/p6/gmp-mparam.h
@@ -1,6 +1,6 @@
 /* gmp-mparam.h -- Compiler/machine parameter header file.
 
-Copyright (C) 1991, 1993, 1994 Free Software Foundation, Inc.
+Copyright (C) 1991, 1993, 1994, 2000 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -19,6 +19,7 @@ along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
+
 #define BITS_PER_MP_LIMB 32
 #define BYTES_PER_MP_LIMB 4
 #define BITS_PER_LONGINT 32
@@ -26,9 +27,21 @@ MA 02111-1307, USA. */
 #define BITS_PER_SHORTINT 16
 #define BITS_PER_CHAR 8
 
+
+#define UMUL_TIME   5   /* cycles */
+#define UDIV_TIME   39  /* cycles */
+
+
 #ifndef KARATSUBA_MUL_THRESHOLD
-#define KARATSUBA_MUL_THRESHOLD 24
+#define KARATSUBA_MUL_THRESHOLD   23
 #endif
+#ifndef TOOM3_MUL_THRESHOLD
+#define TOOM3_MUL_THRESHOLD      163
+#endif
+
 #ifndef KARATSUBA_SQR_THRESHOLD
-#define KARATSUBA_SQR_THRESHOLD 64
+#define KARATSUBA_SQR_THRESHOLD   70
+#endif
+#ifndef TOOM3_SQR_THRESHOLD
+#define TOOM3_SQR_THRESHOLD      158
 #endif
