6ffd0ec763f488a803689c329ccaf40216c6bc41
Kevin Ryde <user42@zip.com.au>
2000-04-07 00:11:38 +0200

    Update copyright years.

--- gmp-impl.h
+++ gmp-impl.h
@@ -1,6 +1,7 @@
 /* Include file for internal GNU MP types and definitions.
 
-Copyright (C) 1991, 1993, 1994, 1995, 1996 Free Software Foundation, Inc.
+Copyright (C) 1991, 1993, 1994, 1995, 1996, 1997, 1999, 2000 Free Software
+Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
--- gmp.h
+++ gmp.h
@@ -1,6 +1,7 @@
 /* gmp.h -- Definitions for GNU multiple precision functions.
 
-Copyright (C) 1991, 1993, 1994, 1995, 1996, 1999 Free Software Foundation, Inc.
+Copyright (C) 1991, 1993, 1994, 1995, 1996, 1997, 1999, 2000 Free Software
+Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
