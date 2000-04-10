e4235335e01f09f8041a837cb9a14dcc72445b58
tege <tege@gmplib.org>
2000-04-10 23:43:06 +0200

    * Delete several unused variables. * Add copyright notice.

--- mpz/bin_uiui.c
+++ mpz/bin_uiui.c
@@ -1,3 +1,24 @@
+/* mpz_bin_uiui - compute n over k.
+
+Copyright (C) 1998, 2000 Free Software Foundation, Inc.
+
+This file is part of the GNU MP Library.
+
+The GNU MP Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The GNU MP Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA 02111-1307, USA. */
+
 #include "gmp.h"
 #include "gmp-impl.h"
 #include "longlong.h"
@@ -21,8 +42,6 @@ mpz_bin_uiui (r, n, k)
     {
       unsigned long int i, j;
       mp_limb_t nacc, kacc;
-      unsigned long int i2, j2;
-      int i2c, j2c;
       unsigned long int cnt;
 
       /* Rewrite bin(n,k) as bin(n,n-k) if that is simpler to compute.  */
