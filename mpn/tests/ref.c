324a3a649decd6aca357cd20e8cfaf980d21a88f
Kevin Ryde <user42@zip.com.au>
2000-04-11 22:35:47 +0200

    Add copyright. Use WANT_ASSERT.

--- mpn/tests/ref.c
+++ mpn/tests/ref.c
@@ -1,14 +1,36 @@
 /* Reference mpn functions, designed to be simple, portable and independent
-   of the normal gmp code.  Speed isn't a consideration.
+   of the normal gmp code.  Speed isn't a consideration.  */
 
-   Most routines have assertions representing what the mpn routines are
+/*
+Copyright (C) 1996, 1997, 1998, 1999, 2000 Free Software Foundation, Inc.
+
+This file is part of the GNU MP Library.
+
+The GNU MP Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The GNU MP Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA 02111-1307, USA.
+*/
+
+
+/* Most routines have assertions representing what the mpn routines are
    supposed to accept.  Many of these reference routines do sensible things
    outside these ranges (eg. for size==0), but the assertions are present to
    pick up bad parameters passed here that are about to be passed the same
    to a real mpn routine being compared.  */
 
 /* always do assertion checking */
-#define DEBUG  1
+#define WANT_ASSERT  1
 
 #include "gmp.h"
 #include "gmp-impl.h"
