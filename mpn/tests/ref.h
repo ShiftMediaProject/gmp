66e05c4baf498988648e6e4d9e2da99c6790d564
Kevin Ryde <user42@zip.com.au>
2000-04-11 22:37:22 +0200

    Add copyright. Use _PROTO.

--- mpn/tests/ref.h
+++ mpn/tests/ref.h
@@ -1,76 +1,112 @@
 /* Prototypes for refmpn functions. */
 
-mp_limb_t refmpn_add_n (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size);
-mp_limb_t refmpn_add_nc (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size,
-                         mp_limb_t carry);
-mp_limb_t refmpn_addmul_1 (mp_ptr wp, mp_srcptr xp, mp_size_t size,
-                           mp_limb_t multiplier);
-mp_limb_t refmpn_addmul_1c (mp_ptr wp, mp_srcptr xp, mp_size_t size,
-                            mp_limb_t multiplier, mp_limb_t carry);
-mp_limb_t refmpn_addsub_n (mp_ptr r1p, mp_ptr r2p,
-                           mp_srcptr s1p, mp_srcptr s2p, mp_size_t n);
+/*
+Copyright (C) 2000 Free Software Foundation, Inc.
 
-void refmpn_and_n  (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size);
-void refmpn_andn_n (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size);
+This file is part of the GNU MP Library.
 
-mp_limb_t refmpn_cmp (mp_srcptr s1p, mp_srcptr s2p, mp_size_t size);
+The GNU MP Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
 
-void refmpn_com_n (mp_ptr rp, mp_srcptr sp, mp_size_t size);
-void refmpn_copyi (mp_ptr rp, mp_srcptr sp, mp_size_t size);
-void refmpn_copyd (mp_ptr rp, mp_srcptr sp, mp_size_t size);
+The GNU MP Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+License for more details.
 
-mp_limb_t refmpn_divexact_by3 (mp_ptr rp, mp_srcptr sp, mp_size_t size);
+You should have received a copy of the GNU Library General Public License
+along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA 02111-1307, USA.
+*/
 
-mp_limb_t refmpn_divmod_1 (mp_ptr rp, mp_srcptr sp, mp_size_t size,
-                           mp_limb_t divisor);
-mp_limb_t refmpn_divmod_1c (mp_ptr rp, mp_srcptr sp, mp_size_t size,
-                            mp_limb_t divisor, mp_limb_t carry);
-mp_limb_t refmpn_divrem_1 (mp_ptr rp, mp_size_t xsize,
-                           mp_srcptr sp, mp_size_t size, mp_limb_t divisor);
-mp_limb_t refmpn_divrem_1c (mp_ptr rp, mp_size_t xsize,
-                            mp_srcptr sp, mp_size_t size, mp_limb_t divisor,
-                            mp_limb_t carry);
 
-void refmpn_fill (mp_ptr p, mp_size_t s, mp_limb_t v);
+mp_limb_t refmpn_add_n _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                                mp_size_t size));
+mp_limb_t refmpn_add_nc _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                                 mp_size_t size, mp_limb_t carry));
+mp_limb_t refmpn_addmul_1 _PROTO ((mp_ptr wp, mp_srcptr xp, mp_size_t size,
+                                   mp_limb_t multiplier));
+mp_limb_t refmpn_addmul_1c _PROTO ((mp_ptr wp, mp_srcptr xp, mp_size_t size,
+                                    mp_limb_t multiplier, mp_limb_t carry));
+mp_limb_t refmpn_addsub_n _PROTO ((mp_ptr r1p, mp_ptr r2p,
+                                   mp_srcptr s1p, mp_srcptr s2p, mp_size_t n));
 
-void refmpn_ior_n  (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size);
-void refmpn_iorn_n (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size);
+void refmpn_and_n  _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                            mp_size_t size));
+void refmpn_andn_n _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                            mp_size_t size));
 
-mp_limb_t refmpn_lshift (mp_ptr wp, mp_srcptr xp, mp_size_t size,
-                         unsigned shift);
+mp_limb_t refmpn_cmp _PROTO ((mp_srcptr s1p, mp_srcptr s2p, mp_size_t size));
 
-mp_limb_t refmpn_mod_1 (mp_srcptr sp, mp_size_t size, mp_limb_t divisor);
-mp_limb_t refmpn_mod_1c (mp_srcptr sp, mp_size_t size, mp_limb_t divisor,
-                         mp_limb_t carry);
+void refmpn_com_n _PROTO ((mp_ptr rp, mp_srcptr sp, mp_size_t size));
+void refmpn_copyi _PROTO ((mp_ptr rp, mp_srcptr sp, mp_size_t size));
+void refmpn_copyd _PROTO ((mp_ptr rp, mp_srcptr sp, mp_size_t size));
 
-mp_limb_t refmpn_mul_1 (mp_ptr wp, mp_srcptr xp, mp_size_t size,
-                        mp_limb_t multiplier);
-mp_limb_t refmpn_mul_1c (mp_ptr wp, mp_srcptr xp, mp_size_t size,
-                         mp_limb_t multiplier, mp_limb_t carry);
+mp_limb_t refmpn_divexact_by3 _PROTO ((mp_ptr rp, mp_srcptr sp,
+                                       mp_size_t size));
 
-void refmpn_mul_basecase (mp_ptr prodp,
-                          mp_srcptr up, mp_size_t usize,
-                          mp_srcptr vp, mp_size_t vsize);
-void refmpn_mul_n (mp_ptr prodp, mp_srcptr up, mp_srcptr vp, mp_size_t size);
+mp_limb_t refmpn_divmod_1 _PROTO ((mp_ptr rp, mp_srcptr sp, mp_size_t size,
+                                   mp_limb_t divisor));
+mp_limb_t refmpn_divmod_1c _PROTO ((mp_ptr rp, mp_srcptr sp, mp_size_t size,
+                                    mp_limb_t divisor, mp_limb_t carry));
+mp_limb_t refmpn_divrem_1 _PROTO ((mp_ptr rp, mp_size_t xsize,
+                                   mp_srcptr sp, mp_size_t size,
+                                   mp_limb_t divisor));
+mp_limb_t refmpn_divrem_1c _PROTO ((mp_ptr rp, mp_size_t xsize,
+                                    mp_srcptr sp, mp_size_t size,
+                                    mp_limb_t divisor, mp_limb_t carry));
 
-void refmpn_nand_n (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size);
-void refmpn_nior_n (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size);
+void refmpn_fill _PROTO ((mp_ptr p, mp_size_t s, mp_limb_t v));
 
-mp_limb_t refmpn_rshift (mp_ptr wp, mp_srcptr xp, mp_size_t size,
-                         unsigned shift);
+void refmpn_ior_n  _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                            mp_size_t size));
+void refmpn_iorn_n _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                            mp_size_t size));
 
-void refmpn_sqr (mp_ptr dst, mp_srcptr src, mp_size_t size);
+mp_limb_t refmpn_lshift _PROTO ((mp_ptr wp, mp_srcptr xp, mp_size_t size,
+                                 unsigned shift));
 
-mp_limb_t refmpn_sub_n (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size);
-mp_limb_t refmpn_sub_nc (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size,
-                         mp_limb_t carry);
-mp_limb_t refmpn_submul_1 (mp_ptr wp, mp_srcptr xp, mp_size_t size,
-                           mp_limb_t multiplier);
-mp_limb_t refmpn_submul_1c (mp_ptr wp, mp_srcptr xp, mp_size_t size,
-                            mp_limb_t multiplier, mp_limb_t carry);
+mp_limb_t refmpn_mod_1 _PROTO ((mp_srcptr sp, mp_size_t size,
+                                mp_limb_t divisor));
+mp_limb_t refmpn_mod_1c _PROTO ((mp_srcptr sp, mp_size_t size,
+                                 mp_limb_t divisor, mp_limb_t carry));
 
-void refmpn_xnor_n (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size);
-void refmpn_xor_n  (mp_ptr wp, mp_srcptr xp, mp_srcptr yp, mp_size_t size);
+mp_limb_t refmpn_mul_1 _PROTO ((mp_ptr wp, mp_srcptr xp, mp_size_t size,
+                                mp_limb_t multiplier));
+mp_limb_t refmpn_mul_1c _PROTO ((mp_ptr wp, mp_srcptr xp, mp_size_t size,
+                                 mp_limb_t multiplier, mp_limb_t carry));
 
-mp_ptr refmpn_memdup_limbs (mp_srcptr ptr, mp_size_t size);
-mp_ptr refmpn_malloc_limbs (mp_size_t size);
+void refmpn_mul_basecase _PROTO ((mp_ptr prodp,
+                                  mp_srcptr up, mp_size_t usize,
+                                  mp_srcptr vp, mp_size_t vsize));
+void refmpn_mul_n _PROTO ((mp_ptr prodp, mp_srcptr up, mp_srcptr vp,
+                           mp_size_t size));
+
+void refmpn_nand_n _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                            mp_size_t size));
+void refmpn_nior_n _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                            mp_size_t size));
+
+mp_limb_t refmpn_rshift _PROTO ((mp_ptr wp, mp_srcptr xp, mp_size_t size,
+                                 unsigned shift));
+
+void refmpn_sqr _PROTO ((mp_ptr dst, mp_srcptr src, mp_size_t size));
+
+mp_limb_t refmpn_sub_n _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                                mp_size_t size));
+mp_limb_t refmpn_sub_nc _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                                 mp_size_t size, mp_limb_t carry));
+mp_limb_t refmpn_submul_1 _PROTO ((mp_ptr wp, mp_srcptr xp, mp_size_t size,
+                                   mp_limb_t multiplier));
+mp_limb_t refmpn_submul_1c _PROTO ((mp_ptr wp, mp_srcptr xp, mp_size_t size,
+                                    mp_limb_t multiplier, mp_limb_t carry));
+
+void refmpn_xnor_n _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                            mp_size_t size));
+void refmpn_xor_n  _PROTO ((mp_ptr wp, mp_srcptr xp, mp_srcptr yp,
+                            mp_size_t size));
+
+mp_ptr refmpn_memdup_limbs _PROTO ((mp_srcptr ptr, mp_size_t size));
+mp_ptr refmpn_malloc_limbs _PROTO ((mp_size_t size));
