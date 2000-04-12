adb3593f409a95263c0cd7489f1eedaf94bf04d9
Kevin Ryde <user42@zip.com.au>
2000-04-12 20:51:32 +0200

    Add a copyright year.

--- mpz/ui_pow_ui.c
+++ mpz/ui_pow_ui.c
@@ -1,6 +1,6 @@
 /* mpz_ui_pow_ui(res, base, exp) -- Set RES to BASE**EXP.
 
-Copyright (C) 1991, 1993, 1994, 1996 Free Software Foundation, Inc.
+Copyright (C) 1991, 1993, 1994, 1996, 1997 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
