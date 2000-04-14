82ffba9ccbc058b14bb506f94e5c6197d55ce31e
tege <tege@gmplib.org>
2000-04-14 19:30:02 +0200

    Rewrite using mpn_tdiv_qr.

--- mpz/tdiv_q.c
+++ mpz/tdiv_q.c
@@ -33,104 +33,66 @@ mpz_tdiv_q (quot, num, den)
      mpz_srcptr den;
 #endif
 {
-  mp_srcptr np, dp;
-  mp_ptr qp, rp;
-  mp_size_t nsize = num->_mp_size;
-  mp_size_t dsize = den->_mp_size;
-  mp_size_t qsize, rsize;
-  mp_size_t sign_quotient = nsize ^ dsize;
-  unsigned normalization_steps;
-  mp_limb_t q_limb;
-  TMP_DECL (marker);
+  mp_size_t ql;
+  mp_size_t ns, ds, nl, dl;
+  mp_ptr np, dp, qp, rp;
+  TMP_DECL (marker)
 
-  nsize = ABS (nsize);
-  dsize = ABS (dsize);
+  ns = SIZ (num);
+  ds = SIZ (den);
+  nl = ABS (ns);
+  dl = ABS (ds);
+  ql = nl - dl + 1;
 
-  if (dsize == 0)
+  if (dl == 0)
     DIVIDE_BY_ZERO;
 
-  /* Ensure space is enough for quotient. */
-
-  qsize = nsize - dsize + 1;	/* qsize cannot be bigger than this.  */
-  if (qsize <= 0)
+  if (ql <= 0)
     {
-      quot->_mp_size = 0;
+      /* This needs to follow the assignment to rem, in case the
+	 numerator and quotient are the same.  */
+      SIZ (quot) = 0;
       return;
     }
 
-  if (quot->_mp_alloc < qsize)
-    _mpz_realloc (quot, qsize);
-
-  qp = quot->_mp_d;
-  np = num->_mp_d;
-  dp = den->_mp_d;
-
-  /* Optimize division by a single-limb divisor.  */
-  if (dsize == 1)
-    {
-      mpn_divmod_1 (qp, np, nsize, dp[0]);
-      qsize -= qp[qsize - 1] == 0;
-      quot->_mp_size = sign_quotient >= 0 ? qsize : -qsize;
-      return;
-    }
+  MPZ_REALLOC (quot, ql);
 
   TMP_MARK (marker);
+  qp = PTR (quot);
+  rp = (mp_ptr) TMP_ALLOC (dl * BYTES_PER_MP_LIMB);
+  np = PTR (num);
+  dp = PTR (den);
 
-  rp = (mp_ptr) TMP_ALLOC ((nsize + 1) * BYTES_PER_MP_LIMB);
+  /* FIXME: We should think about how to handle the temporary allocation.
+     Perhaps mpn_tdiv_qr should handle it, since it anyway often need to
+     allocate temp space.  */
 
-  count_leading_zeros (normalization_steps, dp[dsize - 1]);
-
-  /* Normalize the denominator, i.e. make its most significant bit set by
-     shifting it NORMALIZATION_STEPS bits to the left.  Also shift the
-     numerator the same number of steps (to keep the quotient the same!).  */
-  if (normalization_steps != 0)
+  /* Copy denominator to temporary space if it overlaps with the quotient
+     or remainder.  */
+  if (dp == qp)
     {
       mp_ptr tp;
-      mp_limb_t nlimb;
-
-      /* Shift up the denominator setting the most significant bit of
-	 the most significant word.  Use temporary storage not to clobber
-	 the original contents of the denominator.  */
-      tp = (mp_ptr) TMP_ALLOC (dsize * BYTES_PER_MP_LIMB);
-      mpn_lshift (tp, dp, dsize, normalization_steps);
+      tp = (mp_ptr) TMP_ALLOC (dl * BYTES_PER_MP_LIMB);
+      MPN_COPY (tp, dp, dl);
       dp = tp;
-
-      /* Shift up the numerator, possibly introducing a new most
-	 significant word.  Move the shifted numerator in the remainder
-	 meanwhile.  */
-      nlimb = mpn_lshift (rp, np, nsize, normalization_steps);
-      if (nlimb != 0)
-	{
-	  rp[nsize] = nlimb;
-	  rsize = nsize + 1;
-	}
-      else
-	rsize = nsize;
     }
-  else
+  /* Copy numerator to temporary space if it overlaps with the quotient or
+     remainder.  */
+  if (np == qp)
     {
-      /* The denominator is already normalized, as required.  Copy it to
-	 temporary space if it overlaps with the quotient.  */
-      if (dp == qp)
-	{
-	  dp = (mp_ptr) TMP_ALLOC (dsize * BYTES_PER_MP_LIMB);
-	  MPN_COPY ((mp_ptr) dp, qp, dsize);
-	}
-
-      /* Move the numerator to the remainder.  */
-      MPN_COPY (rp, np, nsize);
-      rsize = nsize;
+      mp_ptr tp;
+      tp = (mp_ptr) TMP_ALLOC (nl * BYTES_PER_MP_LIMB);
+      MPN_COPY (tp, np, nl);
+      np = tp;
     }
 
-  q_limb = mpn_divmod (qp, rp, rsize, dp, dsize);
+  {mp_limb_t x=rp[-1], y=rp[dl];
+  mpn_tdiv_qr (qp, rp, 0L, np, nl, dp, dl);
+  if (x != rp[-1] || y != rp[dl])
+    abort ();}
 
-  qsize = rsize - dsize;
-  if (q_limb)
-    {
-      qp[qsize] = q_limb;
-      qsize += 1;
-    }
+  ql -=  qp[ql - 1] == 0;
 
-  quot->_mp_size = sign_quotient >= 0 ? qsize : -qsize;
+  SIZ (quot) = (ns ^ ds) >= 0 ? ql : -ql;
   TMP_FREE (marker);
 }
