d90636614324757d9d927cc78441a2403a119782
Kevin Ryde <user42@zip.com.au>
2000-04-28 23:15:19 +0200

    Changes to the way pointer alignment is implemented. Remove "restriction" parameters from the speed macros.

--- tune/common.c
+++ tune/common.c
@@ -25,6 +25,7 @@ MA 02111-1307, USA.
 #include <fcntl.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h> /* for qsort */
 #include <string.h>
 #include <unistd.h>
 #if 0
@@ -40,7 +41,7 @@ MA 02111-1307, USA.
 #define TRACE(x)
 
 #define numberof(x)   (sizeof (x) / sizeof ((x)[0]))
-
+typedef int (*qsort_function_t) _PROTO ((const void *, const void *));
 
 void
 pentium_wbinvd(void)
@@ -110,7 +111,8 @@ double_cmp_ptr (const double *p, const double *q)
    s->r, -1.0 should be returned.  See the various base routines below.  */
 
 double
-speed_measure (double (*fun)(struct speed_params *s), struct speed_params *s)
+speed_measure (double (*fun) _PROTO ((struct speed_params *s)),
+               struct speed_params *s)
 {
 #define TOLERANCE    1.005  /* 0.5% */
 
@@ -165,7 +167,7 @@ speed_measure (double (*fun)(struct speed_params *s), struct speed_params *s)
          valid measurement.  Return smallest among them.  */
       if (i >= e)
         {
-          qsort (t, i, sizeof(t[0]), double_cmp_ptr);
+          qsort (t, i, sizeof(t[0]), (qsort_function_t) double_cmp_ptr);
           for (j = e-1; j < i; j++)
             if (t[j] <= t[j-e+1] * TOLERANCE)
               return t[j-e+1];
@@ -244,6 +246,28 @@ speed_cache_fill (struct speed_params *s)
   }
 }
 
+
+/* Return p advanced to the next multiple of "align" bytes.  "align" must be
+   a power of 2.  Care is taken not to assume sizeof(int)==sizeof(pointer).  */
+void *
+align_pointer (void *p, size_t align)
+{
+  unsigned  d;
+  d = ((unsigned) p) & (align-1);
+  d = (d != 0 ? align-d : 0);
+  p = (void *) (((char *) p) + d);
+  return p;
+}
+
+/* Note that memory allocated with this function can never be freed, because
+   the start address of the block allocated is discarded. */
+void *
+_mp_allocate_func_aligned (size_t bytes, size_t align)
+{
+  return align_pointer ((*_mp_allocate_func) (bytes + align-1), align);
+}
+
+
 void *
 _mp_allocate_or_reallocate (void *ptr, size_t oldsize, size_t newsize)
 {
@@ -254,24 +278,8 @@ _mp_allocate_or_reallocate (void *ptr, size_t oldsize, size_t newsize)
 }
 
 
-/* Note that memory allocated with this function can never be freed, because
-   the start address of the block allocated is discarded. */
-
-void *
-_mp_allocate_func_aligned (size_t bytes, size_t align)
-{
-  char  *p;
-  unsigned  d;
-  p = (char *) (*_mp_allocate_func) (bytes + align-1);
-  d = ((unsigned) p) & (align-1);
-  if (d != 0)
-    p += (align-d);
-  return p;
-}
-
-
-/* Adjust ptr to aligned to CACHE_LINE_SIZE plus "align".
-   ptr needs to have room for up to CACHE_LINE_SIZE-4 extra bytes.  */
+/* Adjust ptr to align to CACHE_LINE_SIZE plus "align".  ptr needs to have
+   room for up to CACHE_LINE_SIZE-4 extra bytes.  */
 
 mp_ptr
 speed_tmp_alloc_adjust (void *ptr, mp_size_t align)
@@ -291,6 +299,7 @@ speed_tmp_alloc_adjust (void *ptr, mp_size_t align)
 
 
 /* The following are basic speed running routines for various gmp functions.
+   Many are very similar and use speed.h macros.
 
    Each routine allocates it's own destination space for the result of the
    function, because only it can know what the function needs.
@@ -309,12 +318,9 @@ speed_tmp_alloc_adjust (void *ptr, mp_size_t align)
    Data alignment will be important, for source, destination and temporary
    workspace.  A routine can align its destination and workspace.  Programs
    using the routines will ensure s->xp and s->yp are aligned.  Aligning
-   onto a CACHE_LINE_SIZE boundary is suggested.
-
-   The effects of cache priming and data alignment are particularly
-   noticable on fast routines.  Ensure these are consistent between routines
-   that are going to be compared.  Experiment with poorly aligned data if
-   that could occur in a program.
+   onto a CACHE_LINE_SIZE boundary is suggested.  s->align_wp and
+   s->align_wp2 should be respected where it makes sense to do so.
+   SPEED_TMP_ALLOC is a good way to do this.
 
    A loop of the following form can be expected to turn into good assembler
    code on most CPUs, thereby minimizing overhead in the measurement.  It
@@ -333,115 +339,108 @@ speed_tmp_alloc_adjust (void *ptr, mp_size_t align)
    just a user-supplied parameter.  speed_mpn_lshift() uses it as a shift,
    speed_mpn_mul_1() uses it as a multiplier.
 
-  */
+*/
 
-/* MPN_COPY etc can be inlines, so the _CALL forms are necessary */
+
+/* MPN_COPY etc can be macros, so the _CALL forms are necessary */
 double
 speed_MPN_COPY (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_COPY_CALL (1, MPN_COPY (wp, s->xp, s->size));
+  SPEED_ROUTINE_MPN_COPY_CALL (MPN_COPY (wp, s->xp, s->size));
 }
-
 double
 speed_MPN_COPY_INCR (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_COPY_CALL (1, MPN_COPY_INCR (wp, s->xp, s->size));
+  SPEED_ROUTINE_MPN_COPY_CALL (MPN_COPY_INCR (wp, s->xp, s->size));
 }
-
 double
 speed_MPN_COPY_DECR (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_COPY_CALL (1, MPN_COPY_DECR (wp, s->xp, s->size));
+  SPEED_ROUTINE_MPN_COPY_CALL (MPN_COPY_DECR (wp, s->xp, s->size));
 }
-
 double
 speed_memcpy (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_COPY_CALL (1, memcpy (wp, s->xp,
-                                          s->size * BYTES_PER_MP_LIMB));
+  SPEED_ROUTINE_MPN_COPY_CALL
+    (memcpy (wp, s->xp, s->size * BYTES_PER_MP_LIMB));
 }
 
 
 double
 speed_mpn_addmul_1 (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_UNARY_1 (mpn_addmul_1, 1);
+  SPEED_ROUTINE_MPN_UNARY_1 (mpn_addmul_1);
 }
-
 double
-speed_mpn_divmod_1 (struct speed_params *s)
+speed_mpn_submul_1 (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_UNARY_1 (mpn_divmod_1, 1);
-}
-
-double
-speed_mpn_lshift (struct speed_params *s)
-{
-  SPEED_ROUTINE_MPN_UNARY_1 (mpn_lshift, 1);
+  SPEED_ROUTINE_MPN_UNARY_1 (mpn_submul_1);
 }
 
 
 double
 speed_mpn_mul_1 (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_UNARY_1 (mpn_mul_1, 1);
+  SPEED_ROUTINE_MPN_UNARY_1 (mpn_mul_1);
 }
 
 
+double
+speed_mpn_lshift (struct speed_params *s)
+{
+  SPEED_ROUTINE_MPN_UNARY_1 (mpn_lshift);
+}
 double
 speed_mpn_rshift (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_UNARY_1 (mpn_rshift, 1);
+  SPEED_ROUTINE_MPN_UNARY_1 (mpn_rshift);
 }
 
 
 double
-speed_mpn_submul_1 (struct speed_params *s)
+speed_mpn_divmod_1 (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_UNARY_1 (mpn_submul_1, 1);
+  SPEED_ROUTINE_MPN_UNARY_1_CALL (mpn_divmod_1 (wp, s->xp, s->size, s->r));
 }
-
-
-
 double
 speed_mpn_divrem_1 (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_DIVREM_1 (mpn_divrem_1, 1);
+  SPEED_ROUTINE_MPN_DIVREM_1 (mpn_divrem_1);
 }
-
-
 double
 speed_mpn_mod_1 (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_MOD_1 (mpn_mod_1, 1);
+  SPEED_ROUTINE_MPN_MOD_1 (mpn_mod_1);
 }
 
 
 double
+speed_mpn_divexact_by3c (struct speed_params *s)
+{
+  SPEED_ROUTINE_MPN_COPYC (mpn_divexact_by3c);
+}
+double
 speed_mpn_divexact_by3 (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_COPY (mpn_divexact_by3, 1);
+  /* mpn_divexact_by3 is a macro, so the _CALL form is necessary */
+  SPEED_ROUTINE_MPN_COPY_CALL(mpn_divexact_by3 (wp, s->xp, s->size));
 }
 
 
 double
 speed_mpn_bz_divrem_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BZ_DIVREM_N (mpn_bz_divrem_n, 1);
+  SPEED_ROUTINE_MPN_BZ_DIVREM_N (mpn_bz_divrem_n);
 }
-
-
 double
 speed_mpn_bz_divrem_sb (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BZ_DIVREM_SB (mpn_sb_divrem_mn, 1);
+  SPEED_ROUTINE_MPN_BZ_DIVREM_SB (mpn_sb_divrem_mn);
 }
-
-
 double
 speed_mpn_bz_tdiv_qr (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BZ_TDIV_QR (mpn_tdiv_qr, 1);
+  SPEED_ROUTINE_MPN_BZ_TDIV_QR (mpn_tdiv_qr);
 }
 
 
@@ -449,7 +448,7 @@ speed_mpn_bz_tdiv_qr (struct speed_params *s)
 double
 speed_mpn_popcount (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_POPCOUNT (mpn_popcount, 1);
+  SPEED_ROUTINE_MPN_POPCOUNT (mpn_popcount);
 }
 
 
@@ -457,22 +456,22 @@ speed_mpn_popcount (struct speed_params *s)
 double
 speed_mpn_add_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BINARY_N (mpn_add_n, 1);
+  SPEED_ROUTINE_MPN_BINARY_N (mpn_add_n);
 }
 double
 speed_mpn_sub_n (struct speed_params *s)
 {
-SPEED_ROUTINE_MPN_BINARY_N (mpn_sub_n, 1);
+SPEED_ROUTINE_MPN_BINARY_N (mpn_sub_n);
 }
 double
 speed_mpn_add_n_self (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BINARY_N_SELF (mpn_add_n, 1);
+  SPEED_ROUTINE_MPN_BINARY_N_SELF (mpn_add_n);
 }
 double
 speed_mpn_add_n_inplace (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BINARY_N_INPLACE (mpn_add_n, 1);
+  SPEED_ROUTINE_MPN_BINARY_N_INPLACE (mpn_add_n);
 }
 
 
@@ -481,42 +480,42 @@ speed_mpn_add_n_inplace (struct speed_params *s)
 double
 speed_mpn_and_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BINARY_N_CALL (0, mpn_and_n  (wp,s->xp,s->yp,s->size));
+  SPEED_ROUTINE_MPN_BINARY_N_CALL (mpn_and_n (wp, s->xp, s->yp, s->size));
 }
 double
 speed_mpn_andn_n (struct speed_params *s)
 {
-SPEED_ROUTINE_MPN_BINARY_N_CALL (0, mpn_andn_n (wp,s->xp,s->yp,s->size));
+SPEED_ROUTINE_MPN_BINARY_N_CALL (mpn_andn_n (wp, s->xp, s->yp, s->size));
 }
 double
 speed_mpn_nand_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BINARY_N_CALL (0, mpn_nand_n (wp,s->xp,s->yp,s->size));
+  SPEED_ROUTINE_MPN_BINARY_N_CALL (mpn_nand_n (wp, s->xp, s->yp, s->size));
 }
 double
 speed_mpn_ior_n (struct speed_params *s)
 {
-SPEED_ROUTINE_MPN_BINARY_N_CALL (0, mpn_ior_n (wp,s->xp,s->yp,s->size));
+SPEED_ROUTINE_MPN_BINARY_N_CALL (mpn_ior_n (wp, s->xp, s->yp, s->size));
 }
 double
 speed_mpn_iorn_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BINARY_N_CALL (0, mpn_iorn_n (wp,s->xp,s->yp,s->size));
+  SPEED_ROUTINE_MPN_BINARY_N_CALL (mpn_iorn_n (wp, s->xp, s->yp, s->size));
 }
 double
 speed_mpn_nior_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BINARY_N_CALL (0, mpn_nior_n (wp,s->xp,s->yp,s->size));
+  SPEED_ROUTINE_MPN_BINARY_N_CALL (mpn_nior_n (wp, s->xp, s->yp, s->size));
 }
 double
 speed_mpn_xor_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BINARY_N_CALL (0, mpn_xor_n (wp,s->xp,s->yp,s->size));
+  SPEED_ROUTINE_MPN_BINARY_N_CALL (mpn_xor_n (wp, s->xp, s->yp, s->size));
 }
 double
 speed_mpn_xnor_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_BINARY_N_CALL (0, mpn_xnor_n (wp,s->xp,s->yp,s->size));
+  SPEED_ROUTINE_MPN_BINARY_N_CALL (mpn_xnor_n (wp, s->xp, s->yp, s->size));
 }
 
 
@@ -535,29 +534,29 @@ speed_mpz_fib_ui (struct speed_params *s)
 double
 speed_mpn_mul_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_MUL_N (mpn_mul_n, 1);
+  SPEED_ROUTINE_MPN_MUL_N (mpn_mul_n);
 }
 double
 speed_mpn_sqr_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_SQR (mpn_sqr_n, 1);
+  SPEED_ROUTINE_MPN_SQR (mpn_sqr_n);
 }
 double
 speed_mpn_mul_n_sqr (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_SQR_CALL (1, mpn_mul_n(wp,s->xp,s->xp,s->size), 1);
+  SPEED_ROUTINE_MPN_SQR_CALL (mpn_mul_n (wp, s->xp, s->xp, s->size), 1);
 }
 
-/* FIXME: size restrictions on some sqr_basecase versions */
 double
 speed_mpn_mul_basecase (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_MUL_BASECASE(mpn_mul_basecase, 1);
+  SPEED_ROUTINE_MPN_MUL_BASECASE(mpn_mul_basecase);
 }
 double
 speed_mpn_sqr_basecase (struct speed_params *s)
 {
-  SPEED_ROUTINE_MPN_SQR (mpn_sqr_basecase, 1);
+  /* FIXME: size restrictions on some versions of sqr_basecase */
+  SPEED_ROUTINE_MPN_SQR (mpn_sqr_basecase);
 }
 
 /* FIXME: size restrictions on kara */
@@ -565,32 +564,27 @@ double
 speed_mpn_kara_mul_n (struct speed_params *s)
 {
   SPEED_ROUTINE_MPN_MUL_N_CALL
-    (1,
-     mpn_kara_mul_n (wp, s->xp, s->xp, s->size, tspace),
+    (mpn_kara_mul_n (wp, s->xp, s->xp, s->size, tspace),
      MPN_KARA_MUL_N_TSIZE (s->size));
 }
-
 double
 speed_mpn_kara_sqr_n (struct speed_params *s)
 {
-SPEED_ROUTINE_MPN_SQR_CALL
-  (1,
-   mpn_kara_sqr_n (wp, s->xp, s->size, tspace),
-   MPN_KARA_SQR_N_TSIZE (s->size));
+  SPEED_ROUTINE_MPN_SQR_CALL
+    (mpn_kara_sqr_n (wp, s->xp, s->size, tspace),
+     MPN_KARA_SQR_N_TSIZE (s->size));
 }
 
 /* FIXME: size restrictions on toom3 */
 double
 speed_mpn_toom3_mul_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_GMPN_TOOM3_MUL_N (mpn_toom3_mul_n, 1);
+  SPEED_ROUTINE_GMPN_TOOM3_MUL_N (mpn_toom3_mul_n);
 }
-
-
 double
 speed_mpn_toom3_sqr_n (struct speed_params *s)
 {
-  SPEED_ROUTINE_GMPN_TOOM3_SQR_N (mpn_toom3_sqr_n, 1);
+  SPEED_ROUTINE_GMPN_TOOM3_SQR_N (mpn_toom3_sqr_n);
 }
 
 
