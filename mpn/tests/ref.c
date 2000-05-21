0c38b6a04b3119e5cfae792a48589a91ec42c5fe
Kevin Ryde <user42@zip.com.au>
2000-05-22 00:31:21 +0200

    * mpn/tests/ref.c (refmpn_strip_twos): Use MPN_COPY_INCR.

--- mpn/tests/ref.c
+++ mpn/tests/ref.c
@@ -847,7 +847,7 @@ refmpn_strip_twos (mp_ptr p, mp_size_t size)
 
   for (limbs = 0; p[0] == 0; limbs++)
     {
-      mpn_copyi (p, p+1, size-1);
+      MPN_COPY_INCR (p, p+1, size-1);
       p[size-1] = 0;
     }
 
