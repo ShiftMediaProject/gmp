4c768b60014dc8a4d2e16926552291b69892752b
tege <tege@gmplib.org>
2000-05-27 04:24:55 +0200

    (_INCLUDE_POSIX_SOURCE): Define when __hpux before including stdio.h.

--- mpz/tests/io.c
+++ mpz/tests/io.c
@@ -19,6 +19,12 @@ along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
+#ifdef __hpux
+/* Need this braindamage to get the declaration for fdopen.
+   Would be nice if HP documented such things in their man pages...  */
+#define _INCLUDE_POSIX_SOURCE
+#endif
+
 #include <stdio.h>
 #include "gmp.h"
 #include "gmp-impl.h"
