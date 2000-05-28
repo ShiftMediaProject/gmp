b5f9416ff43408675a580ebafeebcea28bb89187
Kevin Ryde <user42@zip.com.au>
2000-05-28 02:21:57 +0200

    * tune/sparcv9.asm: New file, by Torbjorn.

---
+++ tune/sparcv9.asm
@@ -0,0 +1,40 @@
+dnl  Sparc v9 32-bit time stamp counter access routine.
+
+dnl  Copyright (C) 2000 Free Software Foundation, Inc.
+dnl 
+dnl  This file is part of the GNU MP Library.
+dnl 
+dnl  The GNU MP Library is free software; you can redistribute it and/or
+dnl  modify it under the terms of the GNU Library General Public License as
+dnl  published by the Free Software Foundation; either version 2 of the
+dnl  License, or (at your option) any later version.
+dnl 
+dnl  The GNU MP Library is distributed in the hope that it will be useful,
+dnl  but WITHOUT ANY WARRANTY; without even the implied warranty of
+dnl  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+dnl  Library General Public License for more details.
+dnl 
+dnl  You should have received a copy of the GNU Library General Public
+dnl  License along with the GNU MP Library; see the file COPYING.LIB.  If
+dnl  not, write to the Free Software Foundation, Inc., 59 Temple Place -
+dnl  Suite 330, Boston, MA 02111-1307, USA.
+
+include(`../config.m4')
+
+
+C void speed_cyclecounter_2 (unsigned p[2]);
+C
+C Get the sparc v9 tick counter, storing the least significant word in
+C p[0] and the most significant in p[1].  We cannot return 64 bits
+C since not all V9 systems run a 64-bit aware OS.
+
+ASM_START()
+PROLOGUE(speed_cyclecounter_2)
+	rd	%tick,%g1
+	srlx	%g1,32,%g4
+	st	%g4,[%o0+4]		C high 32 bits
+	srl	%g1,0,%g4
+	st	%g4,[%o0]		C low 32 bits
+	retl
+	nop
+EPILOGUE(speed_cyclecounter_2)
