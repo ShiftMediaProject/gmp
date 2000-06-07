aaa328c62e707cea4c03704d667e3d4faea74fdc
tege <tege@gmplib.org>
2000-06-07 10:32:29 +0200

    Add a copyright year. (Hi Kevin :-)

--- mpn/generic/bdivmod.c
+++ mpn/generic/bdivmod.c
@@ -1,7 +1,7 @@
 /* mpn/bdivmod.c: mpn_bdivmod for computing U/V mod 2^d.
 
-Copyright (C) 1991, 1993, 1994, 1995, 1996, 1999 Free Software Foundation,
-Inc.
+Copyright (C) 1991, 1993, 1994, 1995, 1996, 1999, 2000 Free Software
+Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
