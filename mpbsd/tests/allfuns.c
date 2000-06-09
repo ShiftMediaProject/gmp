0068b8f3a31a2c6f9898a32b240ad345335ee2b7
Kevin Ryde <user42@zip.com.au>
2000-06-09 02:17:24 +0200

    * mpbsd/tests/allfuns.c (main): Call exit() instead of doing return.

--- mpbsd/tests/allfuns.c
+++ mpbsd/tests/allfuns.c
@@ -22,7 +22,6 @@ the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA.
 */
 
-
 #include "mp.h"
 
 int
@@ -55,5 +54,5 @@ main (int argc, char *argv[])
 
   mfree(a);
 
-  return 0;
+  exit (0);
 }
