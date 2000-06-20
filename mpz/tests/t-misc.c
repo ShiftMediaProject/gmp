56a869c60fb9f7b345bcd2360655944d78a02472
Kevin Ryde <user42@zip.com.au>
2000-06-21 00:24:53 +0200

    * mpz/tests/t-misc.c: Move "bin" test from here ... 	* mpz/tests/t-bin.c: ... to here, and add a new test too.
    	* mpz/tests/t-misc.c (check_mpz_set_si, check_mpz_cmp_si):
    	New tests, for mpz_set_si, mpz_init_set_si, and mpz_cmp_si.
    (Actually in this update check_mpz_set_si already exists but is only
    testing mpz_set_si.)

--- mpz/tests/t-misc.c
+++ mpz/tests/t-misc.c
@@ -39,132 +39,7 @@ MA 02111-1307, USA.
 #define LONG_MAX       ((long) (ULONG_HIGHBIT-1))
 #endif
 
-
-void
-bin (void)
-{
-  static const struct {
-    const char     *n;
-    unsigned long  k;
-    const char     *want;
-  } data[] = {
-    {   "0",  0, "1"   },
-    {   "0",  1, "0"   },
-    {   "0",  2, "0"   },
-    {   "0",  3, "0"   },
-    {   "0",  4, "0"   },
-    {   "0", 123456, "0" },
-
-    {   "1",  0, "1"   },
-    {   "1",  1, "1"   },
-    {   "1",  2, "0"   },
-    {   "1",  3, "0"   },
-    {   "1",  4, "0"   },
-    {   "1", 123456, "0" },
-
-    {   "2",  0, "1"   },
-    {   "2",  1, "2"   },
-    {   "2",  2, "1"   },
-    {   "2",  3, "0"   },
-    {   "2",  4, "0"   },
-    {   "2", 123456, "0" },
-
-    {   "3",  0, "1"   },
-    {   "3",  1, "3"   },
-    {   "3",  2, "3"   },
-    {   "3",  3, "1"   },
-    {   "3",  4, "0"   },
-    {   "3",  5, "0"   },
-    {   "3", 123456, "0" },
-
-    {   "4",  0, "1"   },
-    {   "4",  1, "4"   },
-    {   "4",  2, "6"   },
-    {   "4",  3, "4"   },
-    {   "4",  4, "1"   },
-    {   "4",  5, "0"   },
-    {   "4",  6, "0"   },
-    {   "4", 123456, "0" },
-
-    {   "10",  0, "1"   },
-    {   "10",  1, "10"  },
-    {   "10",  2, "45"  },
-    {   "10",  3, "120" },
-    {   "10",  4, "210" },
-    {   "10",  5, "252" },
-    {   "10",  6, "210" },
-    {   "10",  7, "120" },
-    {   "10",  8, "45"  },
-    {   "10",  9, "10"  },
-    {   "10", 10, "1"   },
-    {   "10", 11,     "0" },
-    {   "10", 12,     "0" },
-    {   "10", 123456, "0" },
-
-    /* negatives, using bin(-n,k)=bin(n+k-1,k) */
-    {   "-1",  0,  "1"  },
-    {   "-1",  1, "-1"  },
-    {   "-1",  2,  "1"  },
-    {   "-1",  3, "-1"  },
-    {   "-1",  4,  "1"  },
-
-    {   "-2",  0,  "1"  },
-    {   "-2",  1, "-2"  },
-    {   "-2",  2,  "3"  },
-    {   "-2",  3, "-4"  },
-    {   "-2",  4,  "5"  },
-    {   "-2",  5, "-6"  },
-    {   "-2",  6,  "7"  },
-
-    {   "-3",  0,   "1"  },
-    {   "-3",  1,  "-3"  },
-    {   "-3",  2,   "6"  },
-    {   "-3",  3, "-10"  },
-    {   "-3",  4,  "15"  },
-    {   "-3",  5, "-21"  },
-    {   "-3",  6,  "28"  },
-  };
-
-  mpz_t  n, want, got;
-  int    i;
-
-  mpz_init (n);
-  mpz_init (want);
-  mpz_init (got);
-
-  for (i = 0; i < numberof (data); i++)
-    {
-      mpz_set_str (n, data[i].n, 0);
-      mpz_set_str (want, data[i].want, 0);
-
-      mpz_bin_ui (got, n, data[i].k);
-      if (mpz_cmp (got, want) != 0)
-        {
-          printf ("mpz_bin_ui wrong\n");
-          printf ("  n="); mpz_out_str (stdout, 10, n); printf ("\n");
-          printf ("  k=%lu\n", data[i].k);
-          printf ("  got="); mpz_out_str (stdout, 10, got); printf ("\n");
-          printf ("  want="); mpz_out_str (stdout, 10, want); printf ("\n");
-          abort();                                    
-        }
-
-      if (mpz_fits_ulong_p (n))
-        {
-          mpz_bin_uiui (got, mpz_get_ui (n), data[i].k);
-          if (mpz_cmp (got, want) != 0)
-            {
-              printf ("mpz_bin_uiui wrong\n");
-              printf ("  n=%lu\n", mpz_get_ui (n));
-              printf ("  k=%lu\n", data[i].k);
-              printf ("  got="); mpz_out_str (stdout, 10, got); printf ("\n");
-              printf ("  want="); mpz_out_str (stdout, 10, want); printf ("\n");
-              abort();               
-            }                     
-        }
-    }
-
-  mpz_clear (n);
-}
+#define SGN(x)  ((x) < 0 ? -1 : (x) == 0 ? 0 : 1)
 
 
 void
@@ -229,11 +104,14 @@ check_mpz_set_si (void)
     mp_size_t  want_size;
     mp_limb_t  want_limb;
   } data[] = {
-    { 0L,  0 },
-    { 1L,  1, 1 },
+
+    {  0L,  0 },
+    {  1L,  1, 1 },
     { -1L, -1, 1 },
-    { LONG_MAX, 1, LONG_MAX },
+
+    {  LONG_MAX,  1, LONG_MAX },
     { -LONG_MAX, -1, LONG_MAX },
+
     { LONG_HIGHBIT, -1, ULONG_HIGHBIT },
   };
 
@@ -243,26 +121,99 @@ check_mpz_set_si (void)
   mpz_init (n);
   for (i = 0; i < numberof (data); i++)
     {
+      mpz_init (n);
       mpz_set_si (n, data[i].n);
-
       if (n->_mp_size != data[i].want_size
           || (n->_mp_size != 0 && n->_mp_d[0] != data[i].want_limb))
         {
           printf ("mpz_set_si wrong on data[%d]\n", i); 
           abort();                                    
         }
+      mpz_clear (n);
+
+      mpz_init_set_si (n, data[i].n);
+      if (n->_mp_size != data[i].want_size
+          || (n->_mp_size != 0 && n->_mp_d[0] != data[i].want_limb))
+        {
+          printf ("mpz_init_set_si wrong on data[%d]\n", i); 
+          abort();                                    
+        }
+      mpz_clear (n);
+    }
+}
+
+
+void
+check_mpz_cmp_si (void)
+{
+  static const struct {
+    const char  *a, *b;
+    int         want;
+  } data[] = {
+    { "0",  "1", -1 },
+    { "0",  "0",  0 },
+    { "0", "-1",  1 },
+
+    { "1",  "1", 0 },
+    { "1",  "0", 1 },
+    { "1", "-1", 1 },
+
+    { "-1",  "1", -1 },
+    { "-1",  "0", -1 },
+    { "-1", "-1", 0 },
+
+    {           "0", "-0x80000000",  1 },
+    {  "0x80000000", "-0x80000000",  1 },
+    {  "0x80000001", "-0x80000000",  1 },
+    { "-0x80000000", "-0x80000000",  0 },
+    { "-0x80000001", "-0x80000000", -1 },
+
+    {                   "0", "-0x8000000000000000",  1 },
+    {  "0x8000000000000000", "-0x8000000000000000",  1 },
+    {  "0x8000000000000001", "-0x8000000000000000",  1 },
+    { "-0x8000000000000000", "-0x8000000000000000",  0 },
+    { "-0x8000000000000001", "-0x8000000000000000", -1 },
+  };
+
+  mpz_t  a, bz;
+  long   b;
+  int    got;
+  int    i;
+
+  mpz_init (a);
+  mpz_init (bz);
+  for (i = 0; i < numberof (data); i++)
+    {
+      mpz_set_str (a, data[i].a, 0);
+      mpz_set_str (bz, data[i].b, 0);
+
+      if (mpz_fits_slong_p (bz))
+        {
+          b = mpz_get_si (bz);
+          got = mpz_cmp_si (a, b);
+          if (SGN (got) != data[i].want)
+            {
+              printf ("mpz_cmp_si wrong on data[%d]\n", i);
+              printf ("  a="); mpz_out_str (stdout, 10, a); printf ("\n");
+              printf ("  b=%ld\n", b);
+              printf ("  got=%d\n", got);
+              printf ("  want=%d\n", data[i].want);
+              abort();                                    
+            }
+        }
     }
 
-  mpz_clear (n);
+  mpz_clear (a);
+  mpz_clear (bz);
 }
 
 
 int
 main (void)
 {
-  bin ();
   oddeven ();
   check_mpz_set_si ();
+  check_mpz_cmp_si ();
 
   exit (0);
 }
