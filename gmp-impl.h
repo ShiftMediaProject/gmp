61468d2d484c828bcda222000ae3c95a43fe4548
Kevin Ryde <user42@zip.com.au>
2000-06-30 02:58:00 +0200

    * gmp-impl.h, mpn/tests/try.c, mpn/tests/spinner.c, tune/speed.c: 	Use config.h unconditionally, not under HAVE_CONFIG_H.
    It's likely in the future gmp-impl.h will depend more rather than less
    on config.h.  Encouraging anyone to think they can get away without it
    is a bad idea.

--- gmp-impl.h
+++ gmp-impl.h
@@ -23,9 +23,7 @@ along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
-#ifdef HAVE_CONFIG_H
 #include "config.h"
-#endif
 #include "gmp-mparam.h"
 /* #include "longlong.h" */
 
