00645a15dfcc9fe430e9eb90f4d0da84e0b6620e
tege <tege@gmplib.org>
2000-07-26 01:47:15 +0200

    Update copyright year.

--- mpz/tests/t-mul.c
+++ mpz/tests/t-mul.c
@@ -1,6 +1,6 @@
 /* Test mpz_add, mpz_cmp, mpz_cmp_ui, mpz_divmod, mpz_mul.
 
-Copyright (C) 1991, 1993, 1994, 1996, 1997 Free Software Foundation, Inc.
+Copyright (C) 1991, 1993, 1994, 1996, 1997, 2000 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
