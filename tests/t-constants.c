c4fa614f72b615fcf156378540e76a7e1ad78fa6
Kevin Ryde <user42@zip.com.au>
2000-08-16 02:11:25 +0200

    * tests/t-constants.c: New file. (This guards against wrong values in gmp-mparam.h, and in particular detects the current problem with mpn/generic/gmp-mparam.h on a 64-bit system.)

---
+++ tests/t-constants.c
@@ -0,0 +1,73 @@
+/* Check the values of some constants. */
+
+/*
+Copyright (C) 2000 Free Software Foundation, Inc.
+
+This file is part of the GNU MP Library.
+
+The GNU MP Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at your
+option) any later version.
+
+The GNU MP Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA 02111-1307, USA.
+*/
+
+#include <stdio.h>
+#include "gmp.h"
+#include "gmp-impl.h"
+
+
+#if HAVE_STRINGIZE
+#define CHECK_CONSTANT(x,y)                                     \
+  if ((x) != (y))                                               \
+    {                                                           \
+      printf ("%s == %d, but %s == %d\n", #x, x, #y, y);        \
+      abort ();                                                 \
+    }
+#define CHECK_CONDITION(x)              \
+  if (!(x))                             \
+    {                                   \
+      printf ("%s is false\n", #x);     \
+      abort ();                         \
+    }
+
+#else
+#define CHECK_CONSTANT(x,y)                     \
+  if ((x) != (y))                               \
+    {                                           \
+      printf ("x == %d, but y == %d\n", x, y);  \
+      abort ();                                 \
+    }
+#define CHECK_CONDITION(x)      \
+  if (!(x))                     \
+    {                           \
+      printf ("x is false\n");  \
+      abort ();                 \
+    }
+#endif
+
+
+int
+main (void)
+{
+  CHECK_CONSTANT (BYTES_PER_MP_LIMB, sizeof(mp_limb_t));
+  CHECK_CONSTANT (BITS_PER_MP_LIMB,  8*sizeof(mp_limb_t));
+  CHECK_CONSTANT (mp_bits_per_limb,  8*sizeof(mp_limb_t));
+  CHECK_CONSTANT (BITS_PER_LONGINT,  8*sizeof(long));
+  CHECK_CONSTANT (BITS_PER_INT,      8*sizeof(int));
+  CHECK_CONSTANT (BITS_PER_SHORTINT, 8*sizeof(short));
+  CHECK_CONSTANT (BITS_PER_CHAR,     8*sizeof(char));
+
+  CHECK_CONDITION (2*sizeof(UHWtype) >= sizeof(UWtype));
+
+  exit (0);
+}
