137fb4b70b38fbcf01db0bb1e3e4a91772f0bd42
tege <tege@gmplib.org>
2000-08-29 20:13:00 +0200

    Include string.h.

--- demos/primes.c
+++ demos/primes.c
@@ -16,6 +16,7 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "gmp.h"
 
