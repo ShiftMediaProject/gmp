08b6c3626f59a4c871f2b188787037ed1972624a
Kevin Ryde <user42@zip.com.au>
2000-09-26 01:32:01 +0200

    * mpn/generic/zerop.c: New file, extracted from mpf/integer.c. 	* gmp-impl.h (mpn_zero_p): Add prototype.

--- gmp-impl.h
+++ gmp-impl.h
@@ -295,6 +295,9 @@ mp_limb_t mpn_sb_divrem_mn _PROTO ((mp_ptr, mp_ptr, mp_size_t, mp_srcptr, mp_siz
 mp_limb_t mpn_bz_divrem_n _PROTO ((mp_ptr, mp_ptr, mp_srcptr, mp_size_t));
 /* void mpn_tdiv_q _PROTO ((mp_ptr, mp_size_t, mp_srcptr, mp_size_t, mp_srcptr, mp_size_t)); */
 
+#define mpn_zero_p  __MPN(zero_p)
+int mpn_zero_p _PROTO ((mp_srcptr p, mp_size_t n));
+
 /* Copy NLIMBS *limbs* from SRC to DST, NLIMBS==0 allowed.  */
 #ifndef MPN_COPY_INCR
 #if HAVE_NATIVE_mpn_copyi
