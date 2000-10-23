b50d13f8547fa8ae5de44ddfd66d7fcd747dbee4
tege <tege@gmplib.org>
2000-10-23 05:29:24 +0200

    *** empty log message ***

---
+++ mpn/ia64/submul_1.c
@@ -0,0 +1,42 @@
+/* IA-64 mpn_submul_1 -- Multiply a limb vector with a limb and subtract
+   the result from a second limb vector.
+
+Copyright (C) 2000 Free Software Foundation, Inc.
+
+This file is part of the GNU MP Library.
+
+The GNU MP Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation; either version 2.1 of the License, or (at
+your option) any later version.
+
+The GNU MP Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA 02111-1307, USA.
+
+This code runs at 10 cycles/limb on the Itanium.  That's pretty poor, and
+very far from the peak execution speed of the Itanium pipeline.  With
+n-way unrolling we should be able to get close to 2 cycles/limb.  See
+README.  */
+
+
+#include "gmp.h"
+#include "gmp-impl.h"
+
+mp_limb_1
+mpn_submul_1 (mp_ptr rp, mp_srcptr s1p, mp_size_t n, mp_limb_1 s2d)
+{
+  mp_ptr tp;
+  mp_limb_t cy;
+
+  tp = alloca (n * BYTES_PER_MP_LIMB);
+  cy = mpn_mul_1 (tp, s1p, n, s2d);
+  cy -= mpn_sub_n (rp, rp, tp, n);
+  return cy;
+}
---
+++ tune/ia64.asm
@@ -0,0 +1,38 @@
+dnl  IA-64 time stamp counter access routine.
+
+dnl  Copyright 2000 Free Software Foundation, Inc.
+dnl 
+dnl  This file is part of the GNU MP Library.
+dnl 
+dnl  The GNU MP Library is free software; you can redistribute it and/or
+dnl  modify it under the terms of the GNU Lesser General Public License as
+dnl  published by the Free Software Foundation; either version 2.1 of the
+dnl  License, or (at your option) any later version.
+dnl 
+dnl  The GNU MP Library is distributed in the hope that it will be useful,
+dnl  but WITHOUT ANY WARRANTY; without even the implied warranty of
+dnl  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+dnl  Lesser General Public License for more details.
+dnl 
+dnl  You should have received a copy of the GNU Lesser General Public
+dnl  License along with the GNU MP Library; see the file COPYING.LIB.  If
+dnl  not, write to the Free Software Foundation, Inc., 59 Temple Place -
+dnl  Suite 330, Boston, MA 02111-1307, USA.
+
+include(`../config.m4')
+
+
+C void speed_cyclecounter (unsigned int p[2]);
+C
+
+ASM_START()
+PROLOGUE(speed_cyclecounter)
+	mov	r14 = ar.itc
+	;;
+	st4	[r32] = r14, 4
+	srl	r14 = r14, 32
+	;;
+	st4	[r32] = r14
+	br.ret.sptk.many b0
+EPILOGUE(speed_cyclecounter)
+ASM_END()
