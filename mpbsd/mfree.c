87af82fabb09db070b6e2273fdac3f97c370d50b
Kevin Ryde <user42@zip.com.au>
2000-10-24 02:16:03 +0200

    Add a copyright year. (Properly this time.)

--- mpbsd/mfree.c
+++ mpbsd/mfree.c
@@ -1,6 +1,6 @@
-/*, 2000 mfree -- BSD compatible mfree.
+/* mfree -- BSD compatible mfree.
 
-Copyright 1991, 1994, 1995 Free Software Foundation, Inc.
+Copyright 1991, 1994, 1995, 2000 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
