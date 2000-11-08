4df21bb763bf72a4ec05a7d5b179b82af52b729e
Kevin Ryde <user42@zip.com.au>
2000-11-09 00:08:59 +0100

    * mpn/x86/pentium/aorsmul_1.asm: Add 1c entrypoints, shave a couple 	of cycles at entry and exit.

--- mpn/x86/pentium/aorsmul_1.asm
+++ mpn/x86/pentium/aorsmul_1.asm
@@ -30,70 +30,107 @@ include(`../config.m4')
 ifdef(`OPERATION_addmul_1', `
       define(M4_inst,        addl)
       define(M4_function_1,  mpn_addmul_1)
+      define(M4_function_1c, mpn_addmul_1c)
 
 ',`ifdef(`OPERATION_submul_1', `
       define(M4_inst,        subl)
       define(M4_function_1,  mpn_submul_1)
+      define(M4_function_1c, mpn_submul_1c)
 
 ',`m4_error(`Need OPERATION_addmul_1 or OPERATION_submul_1
 ')')')
 
-MULFUNC_PROLOGUE(mpn_addmul_1 mpn_submul_1)
+MULFUNC_PROLOGUE(mpn_addmul_1 mpn_addmul_1c mpn_submul_1 mpn_submul_1c)
 
 
-C mp_limb_t M4_function_1 (mp_ptr dst, mp_srcptr src, mp_size_t size,
-C                          mp_limb_t mult);
+C mp_limb_t mpn_addmul_1 (mp_ptr dst, mp_srcptr src, mp_size_t size,
+C                         mp_limb_t mult);
+C mp_limb_t mpn_addmul_1c (mp_ptr dst, mp_srcptr src, mp_size_t size,
+C                          mp_limb_t mult, mp_limb_t carry);
+C
+C mp_limb_t mpn_submul_1 (mp_ptr dst, mp_srcptr src, mp_size_t size,
+C                         mp_limb_t mult);
+C mp_limb_t mpn_submul_1c (mp_ptr dst, mp_srcptr src, mp_size_t size,
+C                          mp_limb_t mult, mp_limb_t carry);
+C
 
+defframe(PARAM_CARRY,     20)
 defframe(PARAM_MULTIPLIER,16)
 defframe(PARAM_SIZE,      12)
 defframe(PARAM_SRC,       8)
 defframe(PARAM_DST,       4)
 
 	TEXT
+
 	ALIGN(8)
+PROLOGUE(M4_function_1c)
+deflit(`FRAME',0)
 
+	movl	PARAM_CARRY, %ecx
+	pushl	%esi		FRAME_pushl()
+
+	jmp	LF(M4_function_1,start_1c)
+
+EPILOGUE()
+
+
+	ALIGN(8)
 PROLOGUE(M4_function_1)
+deflit(`FRAME',0)
 
-	pushl	%edi
-	pushl	%esi
-	pushl	%ebx
-	pushl	%ebp
-deflit(`FRAME',16)
+	xorl	%ecx, %ecx
+	pushl	%esi		FRAME_pushl()
+
+L(start_1c):
+	movl	PARAM_SRC, %esi
+	movl	PARAM_SIZE, %eax
+
+	pushl	%edi		FRAME_pushl()
+	pushl	%ebx		FRAME_pushl()
 
 	movl	PARAM_DST, %edi
-	movl	PARAM_SRC, %esi
-	movl	PARAM_SIZE, %ecx
-	movl	PARAM_MULTIPLIER, %ebp
+	leal	-1(%eax), %ebx		C size-1
 
-	leal	(%edi,%ecx,4), %edi
-	leal	(%esi,%ecx,4), %esi
-	negl	%ecx
-	xorl	%ebx, %ebx
-	ALIGN(8)
+	leal	(%esi,%eax,4), %esi
+	xorl	$-1, %ebx		C -size, and clear carry
 
-L(oop):	adcl	$0, %ebx
-	movl	(%esi,%ecx,4), %eax
+	leal	(%edi,%eax,4), %edi
 
-	mull	%ebp
+L(top):
+	C eax
+	C ebx	counter, negative
+	C ecx	carry
+	C edx
+	C esi	src end
+	C edi	dst end
+	C ebp
 
-	addl	%ebx, %eax
-	movl	(%edi,%ecx,4), %ebx
+	adcl	$0, %ecx
+	movl	(%esi,%ebx,4), %eax
+
+	mull	PARAM_MULTIPLIER
+
+	addl	%ecx, %eax
+	movl	(%edi,%ebx,4), %ecx
 
 	adcl	$0, %edx
-	M4_inst	%eax, %ebx
+	M4_inst	%eax, %ecx
 
-	movl	%ebx, (%edi,%ecx,4)
-	incl	%ecx
+	movl	%ecx, (%edi,%ebx,4)
+	incl	%ebx
 
-	movl	%edx, %ebx
-	jnz	L(oop)
+	movl	%edx, %ecx
+	jnz	L(top)
 
-	adcl	$0, %ebx
-	movl	%ebx, %eax
-	popl	%ebp
+
+	adcl	$0, %ecx
 	popl	%ebx
-	popl	%esi
+
+	movl	%ecx, %eax
 	popl	%edi
+
+	popl	%esi
+
 	ret
 
 EPILOGUE()
