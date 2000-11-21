bd2083178cbbca7e21cd35e307f31e4796473442
tege <tege@gmplib.org>
2000-11-21 09:59:11 +0100

    *** empty log message ***

---
+++ tune/hppa2w.asm
@@ -0,0 +1,43 @@
+dnl  HPPA 64-bit time stamp counter access routine.
+
+dnl  Copyright 2000 Free Software Foundation, Inc.
+dnl 
+dnl  This file is part of the GNU MP Library.
+dnl 
+dnl  The GNU MP Library is free software; you can redistribute it and/or
+dnl  modify it under the terms of the GNU Lesser General Public License as
+dnl  published by the Free Software Foundation; either version 2.1 of the
+dnl  License, or (at your option) any later version.
+dnl 
+dnl  The GNU MP Library is distributed in the hope that it will be useful,
+dnl  but WITHOUT ANY WARRANTY; without even the implied warranty of
+dnl  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+dnl  Lesser General Public License for more details.
+dnl 
+dnl  You should have received a copy of the GNU Lesser General Public
+dnl  License along with the GNU MP Library; see the file COPYING.LIB.  If
+dnl  not, write to the Free Software Foundation, Inc., 59 Temple Place -
+dnl  Suite 330, Boston, MA 02111-1307, USA.
+
+dnl Don't include this, it's not ready for hppa
+dnl include(`../config.m4')
+
+dnl void speed_cyclecounter (unsigned p[2]);
+dnl
+dnl Get the HPPA interval timer.
+
+	.level 2.0w
+	.code
+	.export speed_cyclecounter,entry
+speed_cyclecounter
+	.proc
+	.callinfo	frame=0,args_saved
+	.entry
+
+	mfctl	%cr16,%r28
+	stw	%r28,0(0,%r26)		; low word
+	extrd,u	%r28,31,32,%r28
+	bv	0(%r2)
+	.exit
+	stw	%r28,4(0,%r26)		; high word
+	.procend
