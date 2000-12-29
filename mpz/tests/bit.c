684cc236e2482b588f1d561d8bee6ad82491d4cc
Kevin Ryde <user42@zip.com.au>
2000-12-29 22:31:13 +0100

    * mpz/tests/bit.c (check_tstbit): New test, and add a couple more 	diagnostics elsewhere.

--- mpz/tests/bit.c
+++ mpz/tests/bit.c
@@ -1,6 +1,6 @@
 /* Test mpz_setbit, mpz_clrbit, mpz_tstbit.
 
-Copyright 1997 Free Software Foundation, Inc.
+Copyright 1997, 2000 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -24,15 +24,95 @@ MA 02111-1307, USA. */
 #include "gmp-impl.h"
 #include "urandom.h"
 
-void debug_mp ();
-
 #ifndef SIZE
 #define SIZE 4
 #endif
 
-main (argc, argv)
-     int argc;
-     char **argv;
+
+void
+debug_mp (mpz_srcptr x, int base)
+{
+  mpz_out_str (stdout, base, x); fputc ('\n', stdout);
+}
+
+/* twos complement, return borrow */
+mp_limb_t
+mpn_neg (mp_ptr dst, mp_srcptr src, mp_size_t size)
+{
+  mpn_com_n (dst, src, size);
+  return mpn_add_1 (dst, dst, size, 1) ^ 1;
+}
+
+mp_limb_t
+mpn_tstbit (mp_srcptr ptr, mp_size_t size)
+{
+  return (ptr[size/BITS_PER_MP_LIMB]
+          & (CNST_LIMB(1) << size%BITS_PER_MP_LIMB)) != 0;
+}
+
+void
+mpz_set_n (mpz_ptr z, mp_srcptr p, mp_size_t size)
+{
+  ASSERT (size >= 0);
+  MPN_NORMALIZE (p, size);
+  MPZ_REALLOC (z, size);
+  MPN_COPY (PTR(z), p, size);
+  SIZ(z) = size;
+}
+
+
+/* See that mpz_tstbit matches a twos complement calculated explicitly, for
+   various low zeros.  */
+void
+check_tstbit (void)
+{
+#define MAX_ZEROS  3
+#define NUM_LIMBS  3
+
+  mp_limb_t      pos[1+NUM_LIMBS+MAX_ZEROS];
+  mp_limb_t      neg[1+NUM_LIMBS+MAX_ZEROS];
+  mpz_t          z;
+  unsigned long  i;
+  int            zeros, low1;
+  int            got, want;
+
+  mpz_init (z);
+  for (zeros = 0; zeros <= MAX_ZEROS; zeros++)
+    {
+      MPN_ZERO (pos, numberof(pos));
+      mpn_random2 (pos+zeros, NUM_LIMBS);
+
+      for (low1 = 0; low1 <= 1; low1++)
+        {
+          if (low1)
+            pos[0] |= 1;
+
+          mpn_neg (neg, pos, numberof(neg));
+          mpz_set_n (z, neg, numberof(neg));
+          mpz_neg (z, z);
+
+          for (i = 0; i < numberof(pos)*BITS_PER_MP_LIMB; i++)
+            {
+              got = mpz_tstbit (z, i);
+              want = mpn_tstbit (pos, i);
+              if (got != want)
+                {
+                  printf ("wrong at bit %lu, with %d zeros\n", i, zeros);
+                  printf ("z neg "); debug_mp (z, -16);
+                  mpz_set_n (z, pos, numberof(pos));
+                  printf ("pos   "); debug_mp (z, -16);
+                  mpz_set_n (z, neg, numberof(neg));
+                  printf ("neg   "); debug_mp (z, -16);
+                  exit (1);
+                }
+            }
+        }
+    }
+  mpz_clear (z);
+}
+
+int
+main (int argc, char *argv[])
 {
   mpz_t x, s0, s1, s2, s3, m;
   mp_size_t xsize;
@@ -40,9 +120,12 @@ main (argc, argv)
   int reps = 100000;
   int bit0, bit1, bit2, bit3;
   unsigned long int bitindex;
+  const char  *s = "";
 
   if (argc == 2)
-     reps = atoi (argv[1]);
+    reps = atoi (argv[1]);
+
+  check_tstbit ();
 
   mpz_init (x);
   mpz_init (s0);
@@ -75,8 +158,11 @@ main (argc, argv)
       mpz_set (s3, x);
       bit3 = mpz_tstbit (x, bitindex);
 
-      if (bit1 != 1 || bit2 != 0 || bit3 != 1)
-	abort ();
+#define FAIL(str) do { s = str; goto fail; } while (0)
+
+      if (bit1 != 1)  FAIL ("bit1 != 1");
+      if (bit2 != 0)  FAIL ("bit2 != 0");
+      if (bit3 != 1)  FAIL ("bit3 != 1");
 
       if (bit0 == 0)
 	{
@@ -110,11 +196,11 @@ main (argc, argv)
     }
 
   exit (0);
-}
 
-void
-debug_mp (x, base)
-     MP_INT *x;
-{
-  mpz_out_str (stderr, base, x); fputc ('\n', stderr);
+
+ fail:
+  printf ("%s\n", s);
+  printf ("bitindex = %lu\n", bitindex);
+  printf ("x = "); mpz_out_str (stdout, -16, x); printf (" hex\n");
+  exit (1);
 }
