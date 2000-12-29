5755c90a91a93338d323e7f7c465517e5844a3ee
Kevin Ryde <user42@zip.com.au>
2000-12-30 00:56:02 +0100

    Add a copyright year.

--- mpn/cray/gmp-mparam.h
+++ mpn/cray/gmp-mparam.h
@@ -1,6 +1,6 @@
 /* gmp-mparam.h -- Compiler/machine parameter header file.
 
-Copyright 1991, 1993, 1994, 1996 Free Software Foundation, Inc.
+Copyright 1991, 1993, 1994, 1996, 2000 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
