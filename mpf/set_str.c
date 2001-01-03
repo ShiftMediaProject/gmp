e82444a917996b2d65c084c856694a9a476ea24f
tege <tege@gmplib.org>
2001-01-03 01:37:58 +0100

    Fix typo in last change.

--- mpf/set_str.c
+++ mpf/set_str.c
@@ -22,7 +22,7 @@ along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
-#include <stdlib.h
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
