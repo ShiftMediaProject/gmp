101fabd8a014b9252072fd5108731f82cf05038a
tege <tege@gmplib.org>
2001-01-05 00:38:13 +0100

    Retune.

--- mpn/cray/ieee/gmp-mparam.h
+++ mpn/cray/ieee/gmp-mparam.h
@@ -31,12 +31,30 @@ MA 02111-1307, USA. */
 #define UDIV_TIME	2360
 #endif
 
-#define KARATSUBA_MUL_THRESHOLD     90
-#define TOOM3_MUL_THRESHOLD        115
-#define KARATSUBA_SQR_THRESHOLD    109
-#define TOOM3_SQR_THRESHOLD        218
-#define DC_THRESHOLD              1000 /* failed to determine */
-#define POWM_THRESHOLD              55
-#define FIB_THRESHOLD              204
-#define GCD_ACCEL_THRESHOLD          3
-#define GCDEXT_THRESHOLD            25
+#define KARATSUBA_MUL_THRESHOLD       90
+#define TOOM3_MUL_THRESHOLD          167
+
+#define KARATSUBA_SQR_THRESHOLD      199
+#define TOOM3_SQR_THRESHOLD          399
+
+#define DC_THRESHOLD                1000 /* failed to determine */
+#define POWM_THRESHOLD               113
+
+#define FIB_THRESHOLD                202
+
+#define GCD_ACCEL_THRESHOLD            3
+#define GCDEXT_THRESHOLD              27
+
+#define DIVREM_1_NORM_THRESHOLD    0  /* (preinv always) */
+#define DIVREM_1_UNNORM_THRESHOLD  0
+#define MOD_1_NORM_THRESHOLD       0
+#define MOD_1_UNNORM_THRESHOLD     0
+#define USE_PREINV_MOD_1           1   /* (preinv always) */
+
+#define FFT_MUL_TABLE  { 496, 992, 1984, 3840, 9216, 20480, 49152, 0 }
+#define FFT_MODF_MUL_THRESHOLD       232
+#define FFT_MUL_THRESHOLD           1056
+
+#define FFT_SQR_TABLE  { 1104, 2336, 4928, 9984, 21504, 53248, 0 }
+#define FFT_MODF_SQR_THRESHOLD       600
+#define FFT_SQR_THRESHOLD           3616
