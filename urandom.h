2e82437f35205e3bcfc3a2101a6477ced0224f3d
tege <tege@gmplib.org>
2001-01-08 19:01:56 +0100

    Add a copyright year.

--- urandom.h
+++ urandom.h
@@ -1,6 +1,6 @@
 /* urandom.h -- define urandom returning a full unsigned long random value.
 
-Copyright 1995, 1996, 1997, 2000 Free Software Foundation, Inc.
+Copyright 1995, 1996, 1997, 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
