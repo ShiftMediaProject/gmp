145e0e4ee36e12a84320a25a2fd18a0f0f05763d
Kevin Ryde <user42@zip.com.au>
2001-01-18 22:37:05 +0100

    Add a copyright year.

--- tune/freq.c
+++ tune/freq.c
@@ -1,7 +1,7 @@
 /* CPU frequency determination. */
 
 /*
-Copyright (C) 1999, 2000 Free Software Foundation, Inc.
+Copyright 1999, 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
