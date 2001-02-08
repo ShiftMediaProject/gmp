339a76e0892c989cea53044997961a4659921a11
Kevin Ryde <user42@zip.com.au>
2001-02-09 00:08:09 +0100

    Regenerate for a copyright year.

--- demos/calc/calclex.c
+++ demos/calc/calclex.c
@@ -386,7 +386,7 @@ char *yytext;
 #define INITIAL 0
 /* Lexical analyzer for calc.y. */
 /*
-Copyright 2000 Free Software Foundation, Inc.
+Copyright 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
