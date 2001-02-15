be12a090a3f22f442da94b93bc33c521e2892668
Kevin Ryde <user42@zip.com.au>
2001-02-16 00:02:28 +0100

    Oops, no, revert this: 	* mpfr/mpf2mpfr.h: Add some #undef's.
    It's in the mpfr TODO file, that's enough.

--- mpfr/mpf2mpfr.h
+++ mpfr/mpf2mpfr.h
@@ -1,65 +1,6 @@
 /* types */
 #define mpf_t mpfr_t
 
-
-#undef mpf_ceil
-#undef mpf_clear
-#undef mpf_cmp
-#undef mpf_cmp_si
-#undef mpf_cmp_ui
-#undef mpf_eq
-#undef mpf_floor
-#undef mpf_get_d
-#undef mpf_get_prec
-#undef mpf_init
-#undef mpf_init2
-#undef mpf_random2
-#undef mpf_set_default_prec
-#undef mpf_set_prec
-#undef mpf_set_prec_raw
-#undef mpf_trunc
-#undef mpf_sgn
-#undef mpf_swap
-
-/* functions which take as argument the rounding mode */
-#undef mpf_abs
-#undef mpf_add
-#undef mpf_add_ui
-#undef mpf_div
-#undef mpf_div_ui
-#undef mpf_div_2exp
-#undef mpf_dump
-#undef mpf_get_str
-#undef mpf_inp_str
-#undef mpf_set_str
-#undef mpf_init_set
-#undef mpf_init_set_d
-#undef mpf_init_set_si
-#undef mpf_init_set_str
-#undef mpf_init_set_ui
-#undef mpf_mul
-#undef mpf_mul_2exp
-#undef mpf_mul_ui
-#undef mpf_neg
-#undef mpf_out_str
-#undef mpf_pow_ui
-#undef mpf_reldiff
-#undef mpf_set
-#undef mpf_set_d
-#undef mpf_set_q
-#undef mpf_set_si
-#undef mpf_set_ui
-#undef mpf_set_z
-#undef mpf_sqrt
-#undef mpf_sqrt_ui
-#undef mpf_sub
-#undef mpf_sub_ui
-#undef mpf_ui_div
-#undef mpf_ui_sub
-#undef mpf_urandomb
-
-
-
 /* functions which don't take as argument the rounding mode */
 #define mpf_ceil mpfr_ceil
 #define mpf_clear mpfr_clear
