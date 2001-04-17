c7d98f27abebf7b3f64112316fbfa193ac2f0e62
tege <tege@gmplib.org>
2001-04-17 17:14:07 +0200

    Fix comments about timings.  Misc comments char cleanups.

--- mpn/powerpc64/addmul_1.asm
+++ mpn/powerpc64/addmul_1.asm
@@ -1,40 +1,41 @@
-# PowerPC-64 mpn_addmul_1 -- Multiply a limb vector with a limb and add
-# the result to a second limb vector.
-#
-# ppc630: 6 to 12 cycles/limb, depending on multiplier
+dnl  PowerPC-64 mpn_addmul_1 -- Multiply a limb vector with a limb and add
+dnl  the result to a second limb vector.
 
-# Copyright 1999, 2000, 2001 Free Software Foundation, Inc.
+dnl  Copyright 1999, 2000, 2001 Free Software Foundation, Inc.
 
-# This file is part of the GNU MP Library.
+dnl  This file is part of the GNU MP Library.
 
-# The GNU MP Library is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 2.1 of the License, or (at your
-# option) any later version.
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 2.1 of the License, or (at
+dnl  your option) any later version.
 
-# The GNU MP Library is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-# License for more details.
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
 
-# You should have received a copy of the GNU Lesser General Public License
-# along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
-# the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-# MA 02111-1307, USA.
-
-
-# INPUT PARAMETERS
-# res_ptr	r3
-# s1_ptr	r4
-# size		r5
-# s2_limb	r6
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+dnl  the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+dnl  MA 02111-1307, USA.
 
 include(`../config.m4')
+
+C INPUT PARAMETERS
+C res_ptr	r3
+C s1_ptr	r4
+C size		r5
+C s2_limb	r6
+
+C PPC630: 6 to 18 cycles/limb, depending on multiplier.  This cannot be
+C improved unless floating-point operations are used instead of the slow
+C mulld/mulhdu.
 
 ASM_START()
 PROLOGUE(mpn_addmul_1)
 	mtctr	r5
-	li	r9,0		# cy_limb = 0
+	li	r9,0			C cy_limb = 0
 	addic	r0,r0,0
 	cal	r3,-8(r3)
 	cal	r4,-8(r4)
