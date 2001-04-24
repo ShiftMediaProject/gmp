39c4e26288b4f76918a898d53a150709871cb26e
Kevin Ryde <user42@zip.com.au>
2001-04-25 01:39:06 +0200

    Expunge mpz_divexact_si

--- tests/mpz/dive_ui.c
+++ tests/mpz/dive_ui.c
@@ -1,4 +1,4 @@
-/* Test mpz_divexact_si and mpz_divexact_ui.
+/* Test mpz_divexact_ui.
 
 Copyright 1996, 2001 Free Software Foundation, Inc.
 
@@ -32,59 +32,35 @@ check_random (int argc, char *argv[])
 {
   gmp_randstate_ptr rands = RANDS;
   int    reps = 5000;
-  mpz_t  a, q, d, got;
-  int    i, qneg, dneg;
+  mpz_t  a, q, got;
+  int    i, qneg;
+  unsigned long  d;
 
   if (argc == 2)
     reps = atoi (argv[1]);
 
   mpz_init (a);
   mpz_init (q);
-  mpz_init (d);
   mpz_init (got);
 
   for (i = 0; i < reps; i++)
     {
-      mpz_erandomb_nonzero (d, rands, BITS_PER_ULONG);
+      d = (unsigned long) urandom();
       mpz_erandomb (q, rands, 512);
-      mpz_mul (a, q, d);
+      mpz_mul_ui (a, q, d);
 
       for (qneg = 0; qneg <= 1; qneg++)
         {
-          for (dneg = 0; dneg <= 1; dneg++)
+          mpz_divexact_ui (got, a, d);
+          MPZ_CHECK_FORMAT (got);
+          if (mpz_cmp (got, q) != 0)
             {
-              if (mpz_fits_ulong_p (d))
-                {
-                  unsigned long  du = mpz_get_ui (d);
-                  mpz_divexact_ui (got, a, du);
-                  MPZ_CHECK_FORMAT (got);
-                  if (mpz_cmp (got, q) != 0)
-                    {
-                      printf    ("mpz_divexact_ui wrong\n");
-                      printf    ("   du=%lu\n", du);
-                    fail:
-                      mpz_trace ("    a", a);
-                      mpz_trace ("    d", d);
-                      mpz_trace ("    q", q);
-                      mpz_trace ("  got", got);
-                      abort ();
-                    }
-                }
-              if (mpz_fits_slong_p (d))
-                {
-                  unsigned long  ds = mpz_get_si (d);
-                  mpz_divexact_si (got, a, ds);
-                  MPZ_CHECK_FORMAT (got);
-                  if (mpz_cmp (got, q) != 0)
-                    {
-                      printf ("mpz_divexact_si wrong\n");
-                      printf ("   ds=%ld\n", ds);
-                      goto fail;
-                    }
-                }
-
-              mpz_neg (d, d);
-              mpz_neg (a, a);
+              printf    ("mpz_divexact_ui wrong\n");
+              mpz_trace ("    a", a);
+              printf    ("    d=%lu\n", d);
+              mpz_trace ("    q", q);
+              mpz_trace ("  got", got);
+              abort ();
             }
 
           mpz_neg (q, q);
@@ -95,7 +71,6 @@ check_random (int argc, char *argv[])
 
   mpz_clear (a);
   mpz_clear (q);
-  mpz_clear (d);
   mpz_clear (got);
 }
 
