9f8116c39874797af4c26f2d6ca47646bda8823b
Kevin Ryde <user42@zip.com.au>
2001-06-10 02:33:20 +0200

    * compat.c (__gmpz_legendre): Add compatibility entrypoint.

--- compat.c
+++ compat.c
@@ -1,7 +1,7 @@
 /* Old function entrypoints retained for binary compatibility. */
 
 /*
-Copyright 2000 Free Software Foundation, Inc.
+Copyright 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -26,7 +26,7 @@ MA 02111-1307, USA.
 #include "gmp-impl.h"
 
 
-/* mpn_divexact_by3 was a function in gmp 3.0, but as of gmp 3.1 it's a
+/* mpn_divexact_by3 was a function in gmp 3.0.1, but as of gmp 3.1 it's a
    macro calling mpn_divexact_by3c.  */
 mp_limb_t
 __MPN (divexact_by3) (mp_ptr dst, mp_srcptr src, mp_size_t size)
@@ -35,7 +35,7 @@ __MPN (divexact_by3) (mp_ptr dst, mp_srcptr src, mp_size_t size)
 }
 
 
-/* mpn_divmod_1 was a function in gmp 3.0 and earlier, but marked obsolete
+/* mpn_divmod_1 was a function in gmp 3.0.1 and earlier, but marked obsolete
    in gmp 2 and 3.  As of gmp 3.1 it's a macro calling mpn_divrem_1. */
 mp_limb_t
 __MPN (divmod_1) (mp_ptr dst, mp_srcptr src, mp_size_t size, mp_limb_t divisor)
@@ -44,3 +44,10 @@ __MPN (divmod_1) (mp_ptr dst, mp_srcptr src, mp_size_t size, mp_limb_t divisor)
 }
 
 
+/* mpz_legendre was a separate function in gmp 3.1.1 and earlier, but as of
+   3.2 it's a #define alias for mpz_jacobi.  */
+int
+__gmpz_legendre (mpz_srcptr a, mpz_srcptr b)
+{
+  return mpz_jacobi (a, b);
+}
