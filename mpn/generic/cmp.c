31be99b9fc43031a2e40b7f1f830312093dfd669
Kevin Ryde <user42@zip.com.au>
2001-06-12 00:40:30 +0200

    * gmp-h.in (mpn_cmp): Add an inline version. 	* mpn/generic/cmp.c: Use __GMP_FORCE_mpn_cmp to get code from gmp.h.

--- mpn/generic/cmp.c
+++ mpn/generic/cmp.c
@@ -19,35 +19,7 @@ along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
+#define __GMP_FORCE_mpn_cmp 1
+
 #include "gmp.h"
 #include "gmp-impl.h"
-
-/* Compare OP1_PTR/OP1_SIZE with OP2_PTR/OP2_SIZE.
-   There are no restrictions on the relative sizes of
-   the two arguments.
-   Return 1 if OP1 > OP2, 0 if they are equal, and -1 if OP1 < OP2.  */
-
-int
-mpn_cmp (mp_srcptr op1_ptr, mp_srcptr op2_ptr, mp_size_t size)
-{
-  mp_size_t i;
-  mp_limb_t op1_word, op2_word;
-
-  /* Suspect size==0 is required by mpf/cmp.c, though this isn't a
-     documented feature. */
-  ASSERT (size >= 0);
-
-  for (i = size - 1; i >= 0; i--)
-    {
-      op1_word = op1_ptr[i];
-      op2_word = op2_ptr[i];
-      if (op1_word != op2_word)
-	goto diff;
-    }
-  return 0;
- diff:
-  /* This can *not* be simplified to
-	op2_word - op2_word
-     since that expression might give signed overflow.  */
-  return (op1_word > op2_word) ? 1 : -1;
-}
