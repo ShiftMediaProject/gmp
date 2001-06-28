da4c5056e7dcb4e5f7d49188cb13bb2619126202
Kevin Ryde <user42@zip.com.au>
2001-06-29 01:09:42 +0200

    * assert.c: Add stdlib.h for abort prototype.

--- assert.c
+++ assert.c
@@ -24,6 +24,7 @@ the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "gmp.h"
 #include "gmp-impl.h"
 
