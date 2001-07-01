d9ca483611ee38579aa10fcfc5fd7bcf16ca78d9
Kevin Ryde <user42@zip.com.au>
2001-07-01 03:07:02 +0200

    * assert.c: Add stdlib.h for abort prototype. 	* tests/spinner.c, trace.c, t-constants.c, t-count_zeros.c, 	t-gmpmax.c, t-modlinv.c: Ditto.
    	* tests/misc.c, t-modlinv.c: Add string.h.

--- tests/t-modlinv.c
+++ tests/t-modlinv.c
@@ -1,6 +1,5 @@
-/* Test modlimb_invert. */
+/* Test modlimb_invert.
 
-/*
 Copyright 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
@@ -18,10 +17,11 @@ License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-MA 02111-1307, USA.
-*/
+MA 02111-1307, USA. */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "gmp.h"
 #include "gmp-impl.h"
 #include "longlong.h"
