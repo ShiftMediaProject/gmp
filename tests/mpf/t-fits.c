28046a2bf2609555ebfb8220104bab1b46ce198b
Kevin Ryde <user42@zip.com.au>
2001-07-01 03:10:49 +0200

    * assert.c: Add stdlib.h for abort prototype. 	* tests/mpf/t-fits.c, t-get_d.c, t-get_si.c, t-int_p.c, t-misc.c, 	t-trunc.c: Ditto.

--- tests/mpf/t-fits.c
+++ tests/mpf/t-fits.c
@@ -1,6 +1,5 @@
-/* Test mpf_fits_*_p */
+/* Test mpf_fits_*_p
 
-/*
 Copyright 2001 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
@@ -18,10 +17,10 @@ License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-MA 02111-1307, USA.
-*/
+MA 02111-1307, USA. */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "gmp.h"
 #include "gmp-impl.h"
 #include "tests.h"
