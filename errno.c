0e49c44f9526e49ac2d803fcf32c4fe9c0b93b27
Kevin Ryde <user42@zip.com.au>
2001-07-10 23:48:24 +0200

    More of: 	* errno.c, gmp-impl.h (__gmp_exception, __gmp_divide_by_zero, 	__gmp_sqrt_of_negative): New functions.

--- errno.c
+++ errno.c
@@ -19,7 +19,7 @@ along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
-
+#include <stdlib.h>
 #include "gmp.h"
 #include "gmp-impl.h"
 
