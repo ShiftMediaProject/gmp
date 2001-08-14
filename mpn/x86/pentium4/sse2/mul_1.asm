28516fa9bfc88d13bd885736b52924bf36a132e1
Kevin Ryde <user42@zip.com.au>
2001-08-14 02:00:51 +0200

    * mpn/x86/pentium4/sse2/mul_1.asm: New file.

---
+++ mpn/x86/pentium4/sse2/mul_1.asm
@@ -0,0 +1,87 @@
+dnl  Intel Pentium-4 mpn_mul_1 -- mpn by limb multiplication.
+dnl 
+dnl  P4: 4.0 cycles/limb
+
+dnl  Copyright 2001 Free Software Foundation, Inc.
+dnl 
+dnl  This file is part of the GNU MP Library.
+dnl 
+dnl  The GNU MP Library is free software; you can redistribute it and/or
+dnl  modify it under the terms of the GNU Lesser General Public License as
+dnl  published by the Free Software Foundation; either version 2.1 of the
+dnl  License, or (at your option) any later version.
+dnl 
+dnl  The GNU MP Library is distributed in the hope that it will be useful,
+dnl  but WITHOUT ANY WARRANTY; without even the implied warranty of
+dnl  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+dnl  Lesser General Public License for more details.
+dnl 
+dnl  You should have received a copy of the GNU Lesser General Public
+dnl  License along with the GNU MP Library; see the file COPYING.LIB.  If
+dnl  not, write to the Free Software Foundation, Inc., 59 Temple Place -
+dnl  Suite 330, Boston, MA 02111-1307, USA.
+
+
+include(`../config.m4')
+
+
+C mp_limb_t mpn_mul_1 (mp_ptr dst, mp_srcptr src, mp_size_t size,
+C                      mp_limb_t multiplier);
+C mp_limb_t mpn_mul_1c (mp_ptr dst, mp_srcptr src, mp_size_t size,
+C                       mp_limb_t multiplier, mp_limb_t carry);
+C
+C Only the carry limb propagation is on the dependent chain, hence 4 c/l.
+
+defframe(PARAM_CARRY,     20)
+defframe(PARAM_MULTIPLIER,16)
+defframe(PARAM_SIZE,      12)
+defframe(PARAM_SRC,       8)
+defframe(PARAM_DST,       4)
+
+	TEXT
+	ALIGN(16)
+PROLOGUE(mpn_mul_1c)
+deflit(`FRAME',0)
+	movd	PARAM_CARRY, %mm0
+	jmp	LF(mpn_mul_1,start_1c)
+EPILOGUE()
+
+PROLOGUE(mpn_mul_1)
+deflit(`FRAME',0)
+	pxor	%mm0, %mm0
+L(start_1c):
+	movl	PARAM_SRC, %eax
+	movl	PARAM_SIZE, %ecx
+	movl	PARAM_DST, %edx
+	movd	PARAM_MULTIPLIER, %mm7
+
+	leal	(%eax,%ecx,4), %eax	C src end
+	leal	(%edx,%ecx,4), %edx	C dst end
+	negl	%ecx			C -size
+
+L(top):
+	C eax	src end
+	C ebx
+	C ecx	counter, limbs, negative
+	C edx	dst end
+	C
+	C mm0	carry limb
+	C mm7	multiplier
+
+	movd	(%eax,%ecx,4), %mm1
+	pmuludq	%mm7, %mm1
+
+	paddq	%mm1, %mm0
+	movd	%mm0, (%edx,%ecx,4)
+
+	psrlq	$32, %mm0
+
+	addl	$1, %ecx
+	jnz	L(top)
+
+
+	movd	%mm0, %eax
+	emms
+	ret
+
+EPILOGUE()
