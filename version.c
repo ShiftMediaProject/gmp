2455a62a0636f815dec9ca4acef273f150ecb60e
Kevin Ryde <user42@zip.com.au>
2001-09-11 00:22:18 +0200

    * gmp-h.in, version.c (gmp_version): Make the pointer "const" as well 	as the string.

--- version.c
+++ version.c
@@ -1,7 +1,6 @@
-/* gmp_version -- version number compiled into the library */
+/* gmp_version -- version number compiled into the library.
 
-/*
-Copyright 1996, 1999, 2000  Free Software Foundation, Inc.
+Copyright 1996, 1999, 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -18,9 +17,9 @@ License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-MA 02111-1307, USA.  */
+MA 02111-1307, USA. */
 
 #include "gmp.h"
 #include "gmp-impl.h"
 
-const char *gmp_version = VERSION;
+const char * const gmp_version = VERSION;
