c4d527a48a9244345c9dbffedf9995d05a9973cb
tege <tege@gmplib.org>
2001-10-09 15:10:15 +0200

    Use L macros for labels. Quote L reloc operator.

--- mpn/hppa/hppa1_1/udiv_qrnnd.asm
+++ mpn/hppa/hppa1_1/udiv_qrnnd.asm
@@ -30,7 +30,7 @@ C d		gr23
 
 ASM_START()
 	RODATA
-	INT64(L$0000, 0x43f00000, 0x0)		C 2^64
+	INT64(L(0000), 0x43f00000, 0x0)	C 2^64
 
 PROLOGUE(mpn_udiv_qrnnd)
 C	.callinfo	frame=64,no_calls
@@ -40,24 +40,24 @@ C	.callinfo	frame=64,no_calls
 	stws		%r25,-16(0,%r30)	C n_hi
 	stws		%r24,-12(0,%r30)	C n_lo
 ifdef(`PIC',
-`	bl		L$0,%r31
+`	bl		L(0),%r31
 	dep		%r0,31,2,%r31
 
-	.label	L$0
-	addil		LR%L$0000-L$0,%r31
-	ldo		RR%L$0000-L$0(%r1),%r31',
+	.label	L(0)
+	addil		LR%L(0000)-L(0),%r31
+	ldo		RR%L(0000)-L(0)(%r1),%r31',
 `
-	ldil		L%L$0000,%r31
-	ldo		R%L$0000(%r31),%r31')
+	ldil		`L'%L(0000),%r31
+	ldo		R%L(0000)(%r31),%r31')
 
 	fldds		-16(0,%r30),%fr5
 	stws		%r23,-12(0,%r30)
-	comib,<=	0,%r25,L$1
+	comib,<=	0,%r25,L(1)
 	fcnvxf,dbl,dbl	%fr5,%fr5
 	fldds		0(0,%r31),%fr4
 	fadd,dbl	%fr4,%fr5,%fr5
 
-	.label	L$1
+	.label	L(1)
 	fcpy,sgl	%fr0,%fr6L
 	fldws		-12(0,%r30),%fr6R
 	fcnvxf,dbl,dbl	%fr6,%fr4
@@ -73,13 +73,13 @@ ifdef(`PIC',
 	ldws		-16(0,%r30),%r20
 	sub		%r24,%r21,%r22
 	subb		%r25,%r20,%r20
-	comib,=		0,%r20,L$2
+	comib,=		0,%r20,L(2)
 	ldo		-64(%r30),%r30
 
 	add		%r22,%r23,%r22
 	ldo		-1(%r28),%r28
 
-	.label	L$2
+	.label	L(2)
 	bv		0(%r2)
 	stws		%r22,0(0,%r26)
 EPILOGUE(mpn_udiv_qrnnd)
