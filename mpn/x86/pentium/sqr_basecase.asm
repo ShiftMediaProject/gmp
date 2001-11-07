3061a326f82b2f54cabc8e6f9a952ce566fc072a
Kevin Ryde <user42@zip.com.au>
2001-11-07 22:26:22 +0100

    * mpn/x86/k7/mmx/mod_1.asm, mpn/x86/pentium/sqr_basecase.asm, 	mpn/x86/k7/mmx/popham.asm: Remove some unnecessary "0" address offsets.

--- mpn/x86/pentium/sqr_basecase.asm
+++ mpn/x86/pentium/sqr_basecase.asm
@@ -4,7 +4,7 @@ dnl  P5: approx 8 cycles per crossproduct, or 15.5 cycles per triangular
 dnl  product at around 20x20 limbs.
 
 
-dnl  Copyright 1999, 2000 Free Software Foundation, Inc.
+dnl  Copyright 1999, 2000, 2001 Free Software Foundation, Inc.
 dnl 
 dnl  This file is part of the GNU MP Library.
 dnl 
@@ -411,7 +411,7 @@ L(corner):
 
 	mull	-8(%esi)		C src[size-1]*src[size-2]
 
-	movl	%ecx, 0(%edi)
+	movl	%ecx, (%edi)
 	addl	%eax, %ebx
 
 	adcl	$0, %edx
