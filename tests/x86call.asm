26d16b941c7324fc55eaf5582b6ef31d3a1a00fa
Kevin Ryde <user42@zip.com.au>
2001-11-15 23:16:10 +0100

    Remove a copyright year. (This file was moved and so is dated 2001 in the cvs, but hasn't changed since 2000.)

--- tests/x86call.asm
+++ tests/x86call.asm
@@ -1,6 +1,6 @@
 dnl  x86 calling conventions checking.
 
-dnl  Copyright 2000, 2001 Free Software Foundation, Inc.
+dnl  Copyright 2000 Free Software Foundation, Inc.
 dnl 
 dnl  This file is part of the GNU MP Library.
 dnl 
