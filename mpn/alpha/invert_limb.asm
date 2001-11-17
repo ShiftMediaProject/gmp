c8b3e3fb4e4c7cb8884b84306f81d1b6bb8748e8
Kevin Ryde <user42@zip.com.au>
2001-11-18 00:37:32 +0100

    Add a copyright year.

--- mpn/alpha/invert_limb.asm
+++ mpn/alpha/invert_limb.asm
@@ -1,6 +1,6 @@
 dnl  Alpha mpn_invert_limb -- Invert a normalized limb.
 
-dnl  Copyright 1996, 2000 Free Software Foundation, Inc.
+dnl  Copyright 1996, 2000, 2001 Free Software Foundation, Inc.
 dnl
 dnl  This file is part of the GNU MP Library.
 dnl
