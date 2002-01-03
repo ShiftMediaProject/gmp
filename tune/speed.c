978faffa3b8101e77bcc83c64a3983f9d5186dd8
tege <tege@gmplib.org>
2002-01-03 16:55:01 +0100

    Happy new year.

--- tune/speed.c
+++ tune/speed.c
@@ -1,6 +1,6 @@
 /* Speed measuring program.
 
-Copyright 1999, 2000, 2001 Free Software Foundation, Inc.
+Copyright 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
