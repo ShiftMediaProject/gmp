0baa3de9fbccac6b2a376866c126119f7c2a4bc3
Kevin Ryde <user42@zip.com.au>
2002-01-05 22:49:48 +0100

    Add a copyright year.

--- demos/expr/expr-impl.h
+++ demos/expr/expr-impl.h
@@ -1,6 +1,6 @@
 /* Implementation specifics for expression evaluation.
 
-Copyright 2000, 2001 Free Software Foundation, Inc.
+Copyright 2000, 2001, 2002 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
