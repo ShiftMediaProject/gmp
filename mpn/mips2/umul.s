9aa558c53fa75e6284c3462b284a2627f8344459
tege <tege@gmplib.org>
2002-02-17 14:44:57 +0100

    Make it actually work.

--- mpn/mips2/umul.s
+++ mpn/mips2/umul.s
@@ -19,12 +19,12 @@
 
 	.text
 	.align	2
-	.globl	__umul_ppmm
-	.ent	__umul_ppmm
-__umul_ppmm:
-	multu $5,$6
+	.globl	__gmpn_umul_ppmm
+	.ent	__gmpn_umul_ppmm
+__gmpn_umul_ppmm:
+	multu	$5,$6
 	mflo	$3
 	mfhi	$2
-	sw	$3,0($4)
 	j	$31
-	.end	__umul_ppmm
+	sw	$3,0($4)
+	.end	__gmpn_umul_ppmm
