b561815397124e493a7227bd4333290fb1f103c5
Kevin Ryde <user42@zip.com.au>
2002-04-09 00:36:19 +0200

    * mpn/generic/mul_1.c [nails]: Fix vl assert, add {up,n} assert.

--- mpn/generic/mul_1.c
+++ mpn/generic/mul_1.c
@@ -61,9 +61,10 @@ mpn_mul_1 (mp_ptr rp, mp_srcptr up, mp_size_t n, mp_limb_t vl)
 {
   mp_limb_t shifted_vl, ul, lpl, hpl, prev_hpl, xw, cl, xl;
 
-  ASSERT (vl >> GMP_NAIL_BITS == 0);
   ASSERT (n >= 1);
   ASSERT (MPN_SAME_OR_INCR_P (rp, up, n));
+  ASSERT_MPN (up, n);
+  ASSERT_LIMB (vl);
 
   shifted_vl = vl << GMP_NAIL_BITS;
   cl = 0;
