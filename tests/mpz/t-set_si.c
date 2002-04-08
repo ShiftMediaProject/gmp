b203611d7afd8012a268f425f68832943f41f834
Kevin Ryde <user42@zip.com.au>
2002-04-08 23:56:39 +0200

    * tests/mpz/t-set_si.c: Add nails support.

--- tests/mpz/t-set_si.c
+++ tests/mpz/t-set_si.c
@@ -1,6 +1,6 @@
 /* Test mpz_set_si and mpz_init_set_si.
 
-Copyright 2000, 2001 Free Software Foundation, Inc.
+Copyright 2000, 2001, 2002 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -29,20 +29,31 @@ MA 02111-1307, USA. */
 void
 check_data (void)
 {
+#if GMP_NUMB_BITS <= BITS_PER_ULONG
+#define ENTRY(n)   { n, { n, 0 } }
+#else
+#define ENTRY(n)   { n, { (n) & GMP_NUMB_MASK, (n) >> GMP_NUMB_BITS } }
+#endif
+
   static const struct {
     long       n;
     mp_size_t  want_size;
-    mp_limb_t  want_limb;
+    mp_limb_t  want_data[2];
   } data[] = {
 
     {  0L,  0 },
-    {  1L,  1, 1 },
-    { -1L, -1, 1 },
+    {  1L,  1, { 1 } },
+    { -1L, -1, { 1 } },
 
-    {  LONG_MAX,  1, LONG_MAX },
-    { -LONG_MAX, -1, LONG_MAX },
-
-    { LONG_HIGHBIT, -1, ULONG_HIGHBIT },
+#if GMP_NUMB_BITS >= BITS_PER_ULONG
+    { LONG_MAX,  1, { LONG_MAX, 0 } },
+    { -LONG_MAX,  -1, { LONG_MAX, 0 } },
+    { LONG_HIGHBIT,  -1, { ULONG_HIGHBIT, 0 } },
+#else
+    { LONG_MAX,  2, { LONG_MAX & GMP_NUMB_MASK, LONG_MAX >> GMP_NUMB_BITS } },
+    { -LONG_MAX,  -2, { LONG_MAX & GMP_NUMB_MASK, LONG_MAX >> GMP_NUMB_BITS }},
+    { LONG_HIGHBIT,  -2, { 0, ULONG_HIGHBIT >> GMP_NUMB_BITS } },
+#endif
   };
 
   mpz_t  n;
@@ -52,20 +63,24 @@ check_data (void)
     {
       mpz_init (n);
       mpz_set_si (n, data[i].n);
+      MPZ_CHECK_FORMAT (n);
       if (n->_mp_size != data[i].want_size
-          || (n->_mp_size != 0 && n->_mp_d[0] != data[i].want_limb))
+          || refmpn_cmp_allowzero (n->_mp_d, data[i].want_data,
+                                   ABS (data[i].want_size)) != 0)
         {
-          printf ("mpz_set_si wrong on data[%d]\n", i); 
-          abort();                                    
+          printf ("mpz_set_si wrong on data[%d]\n", i);
+          abort();
         }
       mpz_clear (n);
 
       mpz_init_set_si (n, data[i].n);
+      MPZ_CHECK_FORMAT (n);
       if (n->_mp_size != data[i].want_size
-          || (n->_mp_size != 0 && n->_mp_d[0] != data[i].want_limb))
+          || refmpn_cmp_allowzero (n->_mp_d, data[i].want_data,
+                                   ABS (data[i].want_size)) != 0)
         {
-          printf ("mpz_init_set_si wrong on data[%d]\n", i); 
-          abort();                                    
+          printf ("mpz_init_set_si wrong on data[%d]\n", i);
+          abort();
         }
       mpz_clear (n);
     }
