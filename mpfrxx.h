1ca5d0aa3a33121659227296027da91b3d430407
Kevin Ryde <user42@zip.com.au>
2002-04-17 00:14:39 +0200

    Add a copyright year.

--- mpfrxx.h
+++ mpfrxx.h
@@ -1,6 +1,6 @@
 /* mpfrxx.h -- C++ class wrapper for MPFR.  -*- C++ -*-
 
-Copyright 2001 Free Software Foundation, Inc.
+Copyright 2001, 2002 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
