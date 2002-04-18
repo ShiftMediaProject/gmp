ebbfdf0dc4f43529f7b72116c39777c2d8fc56f3
tege <tege@gmplib.org>
2002-04-18 20:35:32 +0200

    Nailify.

--- mpz/ui_pow_ui.c
+++ mpz/ui_pow_ui.c
@@ -23,17 +23,27 @@ MA 02111-1307, USA. */
 #include "gmp-impl.h"
 
 
-/* i386 gcc 2.95.3 doesn't recognise blimb can be eliminated when mp_limb_t
-   is an unsigned long, so only use a separate blimb when necessary.  */
-
 void
 mpz_ui_pow_ui (mpz_ptr r, unsigned long b, unsigned long e)
 {
+  if (GMP_LIMB_BITS != 0 && b > GMP_NUMB_MAX)
+    {
+      mp_limb_t bb[2];
+      bb[0] = b & GMP_NUMB_MASK;
+      bb[1] = b >> GMP_NUMB_BITS;
+      mpz_n_pow_ui (r, bb, (mp_size_t) 2, e);
+    }
+  else
+    {
 #ifdef _LONG_LONG_LIMB
-  mp_limb_t  blimb = b;
-  mpz_n_pow_ui (r, &blimb, (mp_size_t) (b != 0), e);
+      /* i386 gcc 2.95.3 doesn't recognise blimb can be eliminated when
+	 mp_limb_t is an unsigned long, so only use a separate blimb when
+	 necessary.  */
+      mp_limb_t  blimb = b;
+      mpz_n_pow_ui (r, &blimb, (mp_size_t) (b != 0), e);
 #else
-  mpz_n_pow_ui (r, &b,     (mp_size_t) (b != 0), e);
+      mpz_n_pow_ui (r, &b,     (mp_size_t) (b != 0), e);
 #endif
+    }
 }
 
