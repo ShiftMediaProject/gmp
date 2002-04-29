93ffa457caaf7babcb7cb1641b6044eabab153c1
tege <tege@gmplib.org>
2002-04-29 19:34:37 +0200

    Replace nonsense header comment.

--- insert-dbl.c
+++ insert-dbl.c
@@ -1,6 +1,6 @@
-/* __gmp_insert_double -- convert from array of mp_limb_t to double.
+/* __gmp_scale2 -- Scale a double by exponent manipulation.
 
-Copyright 1996, 1997, 1999, 2000, 2001 Free Software Foundation, Inc.
+Copyright 1996, 1997, 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
