17bce5dd485f81f3024c8f2d890271d65f4027e9
Kevin Ryde <user42@zip.com.au>
2002-05-08 01:15:33 +0200

    Add a copyright year.

--- mpf/fits_s.h
+++ mpf/fits_s.h
@@ -1,6 +1,6 @@
 /* mpf_fits_s*_p -- test whether an mpf fits a C signed type.
 
-Copyright 2001 Free Software Foundation, Inc.
+Copyright 2001, 2002 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
