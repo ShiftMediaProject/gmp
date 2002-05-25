522792af2cc3c45e9cd10e7ecdac30aa44dc898c
Kevin Ryde <user42@zip.com.au>
2002-05-25 02:33:01 +0200

    * tests/mpn/t-scan.c: Reduce the amount of testing, to go faster.

--- tests/mpn/t-scan.c
+++ tests/mpn/t-scan.c
@@ -72,6 +72,41 @@ check (void)
 void
 check_twobits (void)
 {
+#define TWOBITS(a, b) \
+  ((CNST_LIMB(1) << (a)) | (CNST_LIMB(1) << (b)))
+
+  refmpn_zero (x, SIZE);
+  x[0] = TWOBITS (1, 0);
+  check ();
+
+  refmpn_zero (x, SIZE);
+  x[0] = TWOBITS (GMP_NUMB_BITS-1, 1);
+  check ();
+
+  refmpn_zero (x, SIZE);
+  x[0] = CNST_LIMB(1);
+  x[1] = CNST_LIMB(1);
+  check ();
+
+  refmpn_zero (x, SIZE);
+  x[0] = CNST_LIMB(1) << (GMP_NUMB_BITS-1);
+  x[1] = CNST_LIMB(1);
+  check ();
+
+  refmpn_zero (x, SIZE);
+  x[1] = TWOBITS (1, 0);
+  check ();
+
+  refmpn_zero (x, SIZE);
+  x[1] = CNST_LIMB(1);
+  x[2] = CNST_LIMB(1);
+  check ();
+}
+
+/* This is unused, it takes too long, especially on 64-bit systems. */
+void
+check_twobits_exhaustive (void)
+{
   unsigned long  i, j;
 
   for (i = 0; i < GMP_NUMB_BITS * SIZE; i++)
