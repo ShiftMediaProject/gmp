98de0bedb9bcf55608582db44e09e137ffe936e2
Kevin Ryde <user42@zip.com.au>
2002-06-01 03:07:30 +0200

    * mpn/x86/k6/diveby3.asm: Speedup to 10 c/l, same as divexact_1. 	Anomaly pointed out by Alexander Kruppa.

--- mpn/x86/k6/diveby3.asm
+++ mpn/x86/k6/diveby3.asm
@@ -22,7 +22,7 @@ dnl  Suite 330, Boston, MA 02111-1307, USA.
 include(`../config.m4')
 
 
-C K6: 11.0 cycles/limb
+C K6: 10.0 cycles/limb
 
 
 C mp_limb_t mpn_divexact_by3c (mp_ptr dst, mp_srcptr src, mp_size_t size,
@@ -40,7 +40,9 @@ dnl  multiplicative inverse of 3, modulo 2^32
 deflit(INVERSE_3, 0xAAAAAAAB)
 
 	TEXT
-	ALIGN(32)
+	ALIGN(16)
+
+	nop	C code alignment
 
 PROLOGUE(mpn_divexact_by3c)
 deflit(`FRAME',0)
@@ -54,19 +56,16 @@ deflit(`FRAME',0)
 	movl	PARAM_DST, %edi
 	pushl	%ebx		defframe_pushl(SAVE_EBX)
 
-	movl	PARAM_CARRY, %ebx
+	movl	PARAM_CARRY, %edx
 	leal	(%esi,%ecx,4), %esi
 
-	pushl	$3		defframe_pushl(VAR_THREE)
-	leal	(%edi,%ecx,4), %edi
+	pushl	%ebp		defframe_pushl(SAVE_EBP)
+	movl	$3, %ebp
 
+	leal	0(%edi,%ecx,4), %edi
 	negl	%ecx
 
 
-	C Need 32 alignment for claimed speed, to avoid the movl store
-	C opcode/modrm crossing a cache line boundary
-
-	ALIGN(32)
 L(top):
 	C eax	scratch, low product
 	C ebx	carry limb (0 to 2)
@@ -74,32 +73,30 @@ L(top):
 	C edx	scratch, high product
 	C esi	&src[size]
 	C edi	&dst[size]
-	C ebp
-	C
-	C The 0(%esi,%ecx,4) form pads so the finishup "movl %ebx, %eax"
-	C doesn't cross a 32 byte boundary, saving a couple of cycles
-	C (that's a fixed couple, not per loop).
+	C ebp	3
 
-Zdisp(	movl,	0,(%esi,%ecx,4), %eax)
-	subl	%ebx, %eax
+	movl	(%esi,%ecx,4), %eax
+	subl	%edx, %eax
 
-	setc	%bl
+	sbbl	%ebx, %ebx		C borrow 0 or -1
 
 	imull	$INVERSE_3, %eax, %eax
 
 	movl	%eax, (%edi,%ecx,4)
 	addl	$2, %ecx
 
-	mull	VAR_THREE
+	mull	%ebp
 
-	addl	%edx, %ebx
+	subl	%ebx, %edx		C new carry
 	loop	L(top)
 
 
 	movl	SAVE_ESI, %esi
-	movl	%ebx, %eax
+	movl	%edx, %eax
 
 	movl	SAVE_EBX, %ebx
+
+	movl	SAVE_EBP, %ebp
 
 	movl	SAVE_EDI, %edi
 	addl	$FRAME, %esp
