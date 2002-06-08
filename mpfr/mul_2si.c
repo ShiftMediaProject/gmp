aeeb88d79ffdaf3d46e8d381948433e270d8419d
Kevin Ryde <user42@zip.com.au>
2002-06-08 02:31:01 +0200

    * mpfr/mul_2si.c: Workaround a mips gcc 2.95.3 bug under -O2 -mabi=n32.

--- mpfr/mul_2si.c
+++ mpfr/mul_2si.c
@@ -1,6 +1,6 @@
 /* mpfr_mul_2si -- multiply a floating-point number by a power of two
 
-Copyright 1999, 2001 Free Software Foundation, Inc.
+Copyright 1999, 2001, 2002 Free Software Foundation, Inc.
 
 This file is part of the MPFR Library.
 
@@ -24,6 +24,12 @@ MA 02111-1307, USA. */
 #include "mpfr.h"
 #include "mpfr-impl.h"
 
+
+/* In the overflow test "n > emax - emin" ensures "emax-n" doesn't wrap
+   around if n is big.  It used to done with "emax < MPFR_EMIN_MIN + n", but
+   that provoked a compiler bug on mips gcc 2.95.3 in -O2 -mabi=n32.
+   Similarly for the underflow test.  */
+
 int
 mpfr_mul_2si (mpfr_ptr y, mpfr_srcptr x, long int n, mp_rnd_t rnd_mode)
 {
@@ -33,12 +39,16 @@ mpfr_mul_2si (mpfr_ptr y, mpfr_srcptr x, long int n, mp_rnd_t rnd_mode)
 
   if (MPFR_IS_FP(y) && MPFR_NOTZERO(y))
     {
-      if (n > 0 && (__mpfr_emax < MPFR_EMIN_MIN + n ||
-                    MPFR_EXP(y) > __mpfr_emax - n))
+      if (n > 0
+          && ((unsigned long) n >
+              (unsigned long) (mp_exp_unsigned_t) (__mpfr_emax - __mpfr_emin)
+              || MPFR_EXP(y) > __mpfr_emax - n))
         return mpfr_set_overflow (y, rnd_mode, MPFR_SIGN(y));
 
-      if (n < 0 && (__mpfr_emin > MPFR_EMAX_MAX + n ||
-                    MPFR_EXP(y) < __mpfr_emin - n))
+      if (n < 0
+          && ((unsigned long) -n >
+              (unsigned long) (mp_exp_unsigned_t) (__mpfr_emax - __mpfr_emin)
+              || MPFR_EXP(y) < __mpfr_emin - n))
         return mpfr_set_underflow (y, rnd_mode, MPFR_SIGN(y));
 
       MPFR_EXP(y) += n;
