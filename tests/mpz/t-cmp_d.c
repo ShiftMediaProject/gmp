479d548ca4f75a903cbbef9bd7ed7455230388d7
Kevin Ryde <user42@zip.com.au>
2002-06-14 00:01:58 +0200

    * tests/mpz/t-cmp_d.c (check_one_2exp): Use volatile to force to 	double, fixes gcc 3.1 with -O4.  Reported by Michael Lee.

--- tests/mpz/t-cmp_d.c
+++ tests/mpz/t-cmp_d.c
@@ -33,7 +33,6 @@ MA 02111-1307, USA. */
 
 
 #define SGN(n)  ((n) > 0 ? 1 : (n) < 0 ? -1 : 0)
-double fudge _PROTO ((double x));
 
 
 void
@@ -205,22 +204,24 @@ check_low_z_one (void)
   mpz_clear (x);
 }
 
-/* Comparing 1 and 1+2^-n */
+/* Comparing 1 and 1+2^-n.  "y" is volatile to make gcc store and fetch it,
+   which forces it to a 64-bit double, whereas on x86 it would otherwise
+   remain on the float stack as an 80-bit long double.  */
 void
 check_one_2exp (void)
 {
-  mpz_t   x;
-  double  y;
-  double  e = 1.0;
-  int     i;
+  double           e;
+  mpz_t            x;
+  volatile double  y;
+  int              i;
 
   mpz_init (x);
 
+  e = 1.0;
   for (i = 0; i < 128; i++)
     {
       e /= 2.0;
-
-      y = fudge (1.0 + e);
+      y = 1.0 + e;
       if (y == 1.0)
         break;
 
@@ -234,15 +235,6 @@ check_one_2exp (void)
     }
 
   mpz_clear (x);
-}
-
-
-/* Stop the compiler getting too smart, in particular on x86 stop it keeping
-   a double in an 80-bit long double fp register.  */
-double
-fudge (double x)
-{
-  return x;
 }
 
 
