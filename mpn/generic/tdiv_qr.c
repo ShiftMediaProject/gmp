cd04959e34c4223991fc403231ad1aeb43a8c6d9
Kevin Ryde <user42@zip.com.au>
2002-07-27 00:17:16 +0200

    * mpn/generic/tdiv_qr.c: Inline mpn_rshift and MPN_COPY of 2 limbs.

--- mpn/generic/tdiv_qr.c
+++ mpn/generic/tdiv_qr.c
@@ -87,7 +87,9 @@ mpn_tdiv_qr (mp_ptr qp, mp_ptr rp, mp_size_t qxn,
 	    qhl = mpn_divrem_2 (qp, 0L, n2p, nn + (cy != 0), d2p);
 	    if (cy == 0)
 	      qp[nn - 2] = qhl;	/* always store nn-2+1 quotient limbs */
-	    mpn_rshift (rp, n2p, (mp_size_t) 2, cnt);
+            rp[0] = (n2p[0] >> cnt)
+              | ((n2p[1] << (GMP_NUMB_BITS - cnt)) & GMP_NUMB_MASK);
+	    rp[1] = (n2p[1] >> cnt);
 	  }
 	else
 	  {
@@ -96,7 +98,8 @@ mpn_tdiv_qr (mp_ptr qp, mp_ptr rp, mp_size_t qxn,
 	    MPN_COPY (n2p, np, nn);
 	    qhl = mpn_divrem_2 (qp, 0L, n2p, nn, d2p);
 	    qp[nn - 2] = qhl;	/* always store nn-2+1 quotient limbs */
-	    MPN_COPY (rp, n2p, 2);
+            rp[0] = n2p[0];
+            rp[1] = n2p[1];
 	  }
 	TMP_FREE (marker);
 	return;
