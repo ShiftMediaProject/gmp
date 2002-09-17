6d2d1ed37c7b15cf91508589aadb14451adbee11
Kevin Ryde <user42@zip.com.au>
2002-09-17 23:43:41 +0200

    Typo in the heading comment, spotted by Conrad Curry.

--- mpf/fits_ulong.c
+++ mpf/fits_ulong.c
@@ -1,6 +1,6 @@
-/* mpf_fits_ulong_p -- test whether an mpf fits an unsigned long.c
+/* mpf_fits_ulong_p -- test whether an mpf fits an unsigned long.
 
-Copyright 2001 Free Software Foundation, Inc.
+Copyright 2001, 2002 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
