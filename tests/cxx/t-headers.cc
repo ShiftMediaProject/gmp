c18b24632628420a5b540cec781e9aa4fa49e92b
Kevin Ryde <user42@zip.com.au>
2002-09-26 02:30:06 +0200

    2002-09-26  Gerardo Ballabio <gerardo.ballabio@unimib.it> Tweak a comment.

--- tests/cxx/t-headers.cc
+++ tests/cxx/t-headers.cc
@@ -1,6 +1,6 @@
-/* Test gmpxx.h doesn't depend on any other headers.
+/* Test that gmpxx.h compiles correctly.
 
-Copyright 2001 Free Software Foundation, Inc.
+Copyright 2001, 2002 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
