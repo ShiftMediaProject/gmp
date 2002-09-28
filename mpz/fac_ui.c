92f0153fc5748ca4c76dda3012bd477774f97734
Kevin Ryde <user42@zip.com.au>
2002-09-28 02:43:18 +0200

    2002-09-28  Jason Moxham <J.L.Moxham@maths.soton.ac.uk> 	* mpz/fac_ui.c: Rewrite.

--- mpz/fac_ui.c
+++ mpz/fac_ui.c
@@ -24,26 +24,15 @@ MA 02111-1307, USA. */
 #include "gmp-impl.h"
 #include "longlong.h"
 
+#include "fac_ui.h"
 
-/* Enhancements:
 
-   Data tables could be used for results up to 3 or 4 limbs to avoid
-   fiddling around with small quantities.
+static void odd_product _PROTO ((unsigned long low, unsigned long high, mpz_t * st));
+static void ap_product_small _PROTO ((mpz_t ret, mp_limb_t start, mp_limb_t step, unsigned long count, unsigned long nm));
 
-   The product accumulation might be worth splitting out into something that
-   could be used elsewhere too.
 
-   The tree of partial products should be done with TMP_ALLOC, not mpz_init.
-   It should be possible to know a maximum size at each level.
-
-   Factors of two could be stripped from k to save some multiplying (but not
-   very much).  The same could be done with factors of 3, perhaps.
-
-   The prime factorization of n! is easy to determine, it might be worth
-   using that rather than a simple 1 to n.  The powering of primes could do
-   some squaring instead of multiplying.  There's probably other ways to use
-   some squaring too.  */
-
+/* must be >=2	*/
+#define APCONST	5
 
 /* for single non-zero limb */
 #define MPZ_SET_1_NZ(z,n)       \
@@ -52,15 +41,6 @@ MA 02111-1307, USA. */
     ASSERT ((n) != 0);          \
     PTR(__z)[0] = (n);          \
     SIZ(__z) = 1;               \
-  } while (0)
-
-/* for single non-zero limb */
-#define MPZ_INIT_SET_1_NZ(z,n)                  \
-  do {                                          \
-    mpz_ptr  __iz = (z);                        \
-    ALLOC(__iz) = 1;                            \
-    PTR(__iz) = __GMP_ALLOCATE_FUNC_LIMBS (1);  \
-    MPZ_SET_1_NZ (__iz, n);                     \
   } while (0)
 
 /* for src>0 and n>0 */
@@ -85,130 +65,338 @@ MA 02111-1307, USA. */
   } while (0)
 
 
-void
-mpz_fac_ui (mpz_ptr result, unsigned long int n)
-{
-#if SIMPLE_FAC
-  /* Be silly.  Just multiply the numbers in ascending order.  O(n**2).  */
-  unsigned long int k;
-  mpz_set_ui (result, 1L);
-  for (k = 2; k <= n; k++)
-    mpz_mul_ui (result, result, k);
-#else
-
-  /* Be smarter.  Multiply groups of numbers in ascending order until the
-     product doesn't fit in a limb.  Multiply these partial product in a
-     balanced binary tree fashion, to make the operand have as equal sizes
-     as possible.  When the operands have about the same size, mpn_mul
-     becomes faster.  */
-
-  unsigned long  k;
-  mp_limb_t      p, p1, p0;
-
-  /* Stack of partial products, used to make the computation balanced
-     (i.e. make the sizes of the multiplication operands equal).  The
-     topmost position of MP_STACK will contain a one-limb partial product,
-     the second topmost will contain a two-limb partial product, and so
-     on.  MP_STACK[0] will contain a partial product with 2**t limbs.
-     To compute n! MP_STACK needs to be less than
-     log(n)**2/log(BITS_PER_MP_LIMB), so 30 is surely enough.  */
-#define MP_STACK_SIZE 30
-  mpz_t mp_stack[MP_STACK_SIZE];
-
-  /* TOP is an index into MP_STACK, giving the topmost element.
-     TOP_LIMIT_SO_FAR is the largets value it has taken so far.  */
-  int top, top_limit_so_far;
-
-  /* Count of the total number of limbs put on MP_STACK so far.  This
-     variable plays an essential role in making the compututation balanced.
-     See below.  */
-  unsigned int tree_cnt;
-
-  /* for testing with small limbs */
-  if (MP_LIMB_T_MAX < ULONG_MAX)
-    ASSERT_ALWAYS (n <= MP_LIMB_T_MAX);
-
-  top = top_limit_so_far = -1;
-  tree_cnt = 0;
-  p = 1;
-  for (k = 2; k <= n; k++)
-    {
-      /* Multiply the partial product in P with K.  */
-      umul_ppmm (p1, p0, p, (mp_limb_t) k);
-
-#if GMP_NAIL_BITS == 0
-#define OVERFLOW (p1 != 0)
-#else
-#define OVERFLOW ((p1 | (p0 >> GMP_NUMB_BITS)) != 0)
+#if BITS_PER_ULONG==BITS_PER_MP_LIMB
+#define BSWAP_ULONG(x,y)	BSWAP_LIMB(x,y)
 #endif
-      /* Did we get overflow into the high limb, i.e. is the partial
-	 product now more than one limb?  */
-      if OVERFLOW
-	{
-	  tree_cnt++;
 
-	  if (tree_cnt % 2 == 0)
-	    {
-	      mp_size_t i;
+#if BITS_PER_ULONG*2==BITS_PER_MP_LIMB
+#define BSWAP_ULONG(x,y)				\
+do{mp_limb_t __src=(y);					\
+   BSWAP_LIMB(__src,__src);				\
+   __src>>=BITS_PER_ULONG;				\
+   (x)=__src;						\
+  }while(0)
+#endif
 
-	      /* TREE_CNT is even (i.e. we have generated an even number of
-		 one-limb partial products), which means that we have a
-		 single-limb product on the top of MP_STACK.  */
+#if ! defined (BSWAP_ULONG)
+#define BSWAP_ULONG(dst, src)                           \
+  do {                                                  \
+    unsigned long  __bswapl_src = (src);                \
+    unsigned long  __dst = 0;                           \
+    int        __i;                                     \
+    for (__i = 0; __i < sizeof(unsigned long); __i++)   \
+      {                                                 \
+        __dst = (__dst << 8) | (__bswapl_src & 0xFF);   \
+        __bswapl_src >>= 8;                             \
+      }                                                 \
+    (dst) = __dst;                                      \
+  } while (0)
+#endif
 
-	      MPZ_MUL_1_POS (mp_stack[top], mp_stack[top], p);
+/* x is bit reverse of y */
+/* Note the divides below are all exact */
+#define BITREV_ULONG(x,y)							\
+do{unsigned long __dst;								\
+   BSWAP_ULONG(__dst,y);							\
+   __dst=(( (__dst>>4)&(ULONG_MAX/17) )|( (__dst<<4)&((ULONG_MAX/17)*16) ));	\
+   __dst=(( (__dst>>2)&(ULONG_MAX/5)  )|( (__dst<<2)&((ULONG_MAX/5)*4)   ));	\
+   __dst=(( (__dst>>1)&(ULONG_MAX/3)  )|( (__dst<<1)&((ULONG_MAX/3)*2)   ));	\
+   (x)=__dst;									\
+  }while(0)
+/* above could be improved if cpu has a nibble/bit swap/muxing instruction */
+/* above code is serialized , possible to write as a big parallel expression */
 
-	      /* If TREE_CNT is divisable by 4, 8,..., we have two
-		 similar-sized partial products with 2, 4,... limbs at
-		 the topmost two positions of MP_STACK.  Multiply them
-		 to form a new partial product with 4, 8,... limbs.  */
-	      for (i = 4; (tree_cnt & (i - 1)) == 0; i <<= 1)
-		{
-		  mpz_mul (mp_stack[top - 1],
-			   mp_stack[top], mp_stack[top - 1]);
-		  top--;
-		}
-	    }
-	  else
-	    {
-	      /* Put the single-limb partial product in P on the stack.
-		 (The next time we get a single-limb product, we will
-		 multiply the two together.)  */
-	      top++;
-	      if (top > top_limit_so_far)
-		{
-		  if (top > MP_STACK_SIZE)
-		    abort();
-		  /* The stack is now bigger than ever, initialize the top
-		     element.  */
-		  MPZ_INIT_SET_1_NZ (mp_stack[top], p);
-		  top_limit_so_far++;
-		}
-	      else
-		MPZ_SET_1_NZ (mp_stack[top], p);
-	    }
 
-	  /* We ignored the last result from umul_ppmm.  Put K in P as the
-	     first component of the next single-limb partial product.  */
-	  p = k;
-	}
-      else
-	/* We didn't get overflow in umul_ppmm.  Put p0 in P and try
-	   with one more value of K.  */
-	p = p0;
+
+void
+mpz_fac_ui (mpz_ptr x, unsigned long n)
+{
+
+  unsigned long z, z1, stt;
+  int i, j;
+  mpz_t t1, st[8 * sizeof (unsigned long) + 1 - APCONST];
+  mp_limb_t d[4];
+
+  static const mp_limb_t table[] = { ONE_LIMB_FACTORIAL_TABLE };
+
+  if (n < numberof (table))
+    {
+      MPZ_SET_1_NZ (x, table[n]);
+      return;
     }
 
-  /* We have partial products in mp_stack[0..top], in descending order.
-     We also have a small partial product in p.
-     Their product is the final result.  */
-  if (top < 0)
-    MPZ_SET_1_NZ (result, p);
-  else
-    MPZ_MUL_1_POS (result, mp_stack[top--], p);
-  while (top >= 0)
-    mpz_mul (result, result, mp_stack[top--]);
+  /*  NOTE : MUST have n>=3 here */
+  ASSERT (n >= 3);
+  /* for estimating the alloc sizes the calculation of these formula's is not
+     exact and also the formulars are only approxiamtions , also we ignore
+     the few "side" calculuations , corect allocation seems to speed up the
+     small sizes better , having very little effect on the large sizes */
 
-  /* Free the storage allocated for MP_STACK.  */
-  for (top = top_limit_so_far; top >= 0; top--)
-    mpz_clear (mp_stack[top]);
-#endif
+  /* estimate space for stack entrys see below 
+     number of bits for n! is 
+     (1+log_2(2*pi)/2)-n*log_2(exp(1))+(n+1/2)*log_2(n)=
+     2.325748065-n*1.442695041+(n+0.5)*log_2(n)  */
+  umul_ppmm (d[1], d[0], (mp_limb_t) n, (mp_limb_t) FAC2OVERE);
+  /* d[1] is 2n/e , d[0] ignored        */
+  count_leading_zeros (z, d[1]);
+  z = GMP_LIMB_BITS - z - 1;	/* z=floor(log_2(2n/e))   */
+  z1 = z - 1;			/* z1=floor(log_2(n/e))        */
+  umul_ppmm (d[1], d[0], (mp_limb_t) n, (mp_limb_t) z);
+  /* d=n*floor(log_2(2n/e))   */
+  d[0] = (d[0] >> 2) | (d[1] << (GMP_LIMB_BITS - 2));
+  d[1] >>= 2;
+  /* d=n*floor(log_2(2n/e))/4   */
+  z = d[0] + 1;			/* have to ignore any overflow */
+  /* so z is the number of bits wanted for st[0]    */
+
+
+  if (n <= ((unsigned long) 1) << (APCONST))
+    {
+      mpz_realloc2 (x, 4 * z);
+      ap_product_small (x, 2, 1, n - 1, 4);
+      return;
+    }
+  if (n <= ((unsigned long) 1) << (APCONST + 1))
+    {				/*  use n!=odd(1,n)*(n/2)!*2^(n/2)         */
+      mpz_init2 (t1, 2 * z);
+      mpz_realloc2 (x, 4 * z);
+      ap_product_small (x, 2, 1, n / 2 - 1, 4);
+      ap_product_small (t1, 3, 2, (n - 1) / 2, 4);
+      mpz_mul (x, x, t1);
+      mpz_clear (t1);
+      mpz_mul_2exp (x, x, n / 2);
+      return;
+    }
+  if (n <= ((unsigned long) 1) << (APCONST + 2))
+    {
+      /* use n!=C_2(1,n/2)^2*C_2(n/2,n)*(n/4)!*2^(n/2+n/4) all int divs
+         so need (BITS_IN_N-APCONST+1)=(APCONST+3-APCONST+1)=4 stack entrys */
+      mpz_init2 (t1, 2 * z);
+      mpz_realloc2 (x, 4 * z);
+      for (i = 0; i < 4; i++)
+	{
+	  mpz_init2 (st[i], z);
+	  z >>= 1;
+	}
+      odd_product (1, n / 2, st);
+      mpz_set (x, st[0]);
+      odd_product (n / 2, n, st);
+      mpz_mul (x, x, x);
+      ASSERT (n / 4 <= FACMUL4 + 6);
+      ap_product_small (t1, 2, 1, n / 4 - 1, 4);
+      /* must have 2^APCONST odd numbers max */
+      mpz_mul (t1, t1, st[0]);
+      for (i = 0; i < 4; i++)
+	mpz_clear (st[i]);
+      mpz_mul (x, x, t1);
+      mpz_clear (t1);
+      mpz_mul_2exp (x, x, n / 2 + n / 4);
+      return;
+    }
+
+  count_leading_zeros (stt, (mp_limb_t) n);
+  stt = GMP_LIMB_BITS - stt + 1 - APCONST;
+
+  for (i = 0; i < (signed long) stt; i++)
+    {
+      mpz_init2 (st[i], z);
+      z >>= 1;
+    }
+
+  count_leading_zeros (z, (mp_limb_t) (n / 3));
+  /* find z st 2^z>n/3 range for z is 1 <= z <= 8 * sizeof(unsigned long)-1 */
+  z = GMP_LIMB_BITS - z;
+
+  /*
+     n! = 2^e * PRODUCT_{i=0}^{i=z-1} C_2( n/2^{i+1} , n/2^i )^{i+1}
+     where 2^e || n!   3.2^z>n   C_2(a,b)=PRODUCT of odd z such that a<z<=b
+   */
+
+
+  mpz_init_set_ui (t1, 1);
+  for (j = 8 * sizeof (unsigned long) / 2; j != 0; j >>= 1)
+    {
+      MPZ_SET_1_NZ (x, 1);
+      for (i = 8 * sizeof (unsigned long) - j; i >= j; i -= 2 * j)
+	if ((signed long) z >= i)
+	  {
+	    odd_product (n >> i, n >> (i - 1), st);
+	    /* largest odd product when j=i=1 then we have 
+	       odd_product(n/2,n,st) which is approx (2n/e)^(n/4) 
+	       so log_base2(largest oddproduct)=n*log_base2(2n/e)/4
+	       number of bits is n*log_base2(2n/e)/4+1  */
+	    if (i != j)
+	      mpz_pow_ui (st[0], st[0], i / j);
+	    mpz_mul (x, x, st[0]);
+	  }
+      if ((signed long) z >= j && j != 1)
+	{
+	  mpz_mul (t1, t1, x);
+	  mpz_mul (t1, t1, t1);
+	}
+    }
+  for (i = 0; i < (signed long) stt; i++)
+    mpz_clear (st[i]);
+  mpz_mul (x, x, t1);
+  mpz_clear (t1);
+  popc_limb (i, (mp_limb_t) n);
+  mpz_mul_2exp (x, x, n - i);
+  return;
+}
+
+/* start,step are mp_limb_t although they will fit in unsigned long	*/
+static void
+ap_product_small (mpz_t ret, mp_limb_t start, mp_limb_t step,
+		  unsigned long count, unsigned long nm)
+{
+  unsigned long a;
+  mp_limb_t b;
+
+  ASSERT (count <= (((unsigned long) 1) << APCONST));
+/* count can never be zero ? check this and remove test below */
+  if (count == 0)
+    {
+      MPZ_SET_1_NZ (ret, 1);
+      return;
+    }
+  if (count == 1)
+    {
+      MPZ_SET_1_NZ (ret, start);
+      return;
+    }
+  switch (nm)
+    {
+    case 1:
+      MPZ_SET_1_NZ (ret, start);
+      b = start + step;
+      for (a = 0; a < count - 1; b += step, a++)
+	MPZ_MUL_1_POS (ret, ret, b);
+      return;
+    case 2:
+      MPZ_SET_1_NZ (ret, start * (start + step));
+      if (count == 2)
+	return;
+      for (b = start + 2 * step, a = count / 2 - 1; a != 0;
+	   a--, b += 2 * step)
+	MPZ_MUL_1_POS (ret, ret, b * (b + step));
+      if (count % 2 == 1)
+	MPZ_MUL_1_POS (ret, ret, b);
+      return;
+    case 3:
+      if (count == 2)
+	{
+	  MPZ_SET_1_NZ (ret, start * (start + step));
+	  return;
+	}
+      MPZ_SET_1_NZ (ret, start * (start + step) * (start + 2 * step));
+      if (count == 3)
+	return;
+      for (b = start + 3 * step, a = count / 3 - 1; a != 0;
+	   a--, b += 3 * step)
+	MPZ_MUL_1_POS (ret, ret, b * (b + step) * (b + 2 * step));
+      if (count % 3 == 2)
+	b = b * (b + step);
+      if (count % 3 != 0)
+	MPZ_MUL_1_POS (ret, ret, b);
+      return;
+    default:			/* ie nm=4      */
+      if (count == 2)
+	{
+	  MPZ_SET_1_NZ (ret, start * (start + step));
+	  return;
+	}
+      if (count == 3)
+	{
+	  MPZ_SET_1_NZ (ret, start * (start + step) * (start + 2 * step));
+	  return;
+	}
+      MPZ_SET_1_NZ (ret,
+		    start * (start + step) * (start + 2 * step) * (start +
+								   3 * step));
+      if (count == 4)
+	return;
+      for (b = start + 4 * step, a = count / 4 - 1; a != 0;
+	   a--, b += 4 * step)
+	MPZ_MUL_1_POS (ret, ret,
+		       b * (b + step) * (b + 2 * step) * (b + 3 * step));
+      if (count % 4 == 2)
+	b = b * (b + step);
+      if (count % 4 == 3)
+	b = b * (b + step) * (b + 2 * step);
+      if (count % 4 != 0)
+	MPZ_MUL_1_POS (ret, ret, b);
+      return;
+    }
+}
+
+/* return value in st[0]
+   odd_product(l,h)=sqrt((h/e)^h/(l/e)^l) using stirling approx and e=exp(1)
+   so st[0] needs enough bits for above , st[1] needs half these bits and 
+   st[2] needs 1/4 of these bits etc	*/
+static void
+odd_product (unsigned long low, unsigned long high, mpz_t * st)
+{
+  unsigned long stc = 1, stn = 0, n, y, mask, a, nm = 1;
+  signed long z;
+
+  low++;
+  if (low % 2 == 0)
+    low++;
+  if (high == 0)
+    high = 1;
+  if (high % 2 == 0)
+    high--;
+/* must have high>=low ? check this and remove test below */
+  if (high < low)
+    {
+      MPZ_SET_1_NZ (st[0], 1);
+      return;
+    }
+  if (high == low)
+    {
+      MPZ_SET_1_NZ (st[0], low);
+      return;
+    }
+  if (high <= FACMUL2 + 2)
+    {
+      nm = 2;
+      if (high <= FACMUL3 + 4)
+	{
+	  nm = 3;
+	  if (high <= FACMUL4 + 6)
+	    nm = 4;
+	}
+    }
+  high = (high - low) / 2 + 1;	/* high is now count,high<=2^(BITS_PER_ULONG-1) */
+  if (high <= (((unsigned long) 1) << APCONST))
+    {
+      ap_product_small (st[0], low, 2, high, nm);
+      return;
+    }
+  count_leading_zeros (n, (mp_limb_t) high);
+/* assumes clz above is LIMB based not NUMB based */
+  n = GMP_LIMB_BITS - n - APCONST;
+  mask = (((unsigned long) 1) << n);
+  a = mask << 1;
+  mask--;
+/* have 2^(BITS_IN_N-APCONST) iterations so need 
+   (BITS_IN_N-APCONST+1) stack entrys	*/
+  for (z = mask; z >= 0; z--)
+    {
+      BITREV_ULONG (y, z);
+      y >>= (BITS_PER_ULONG - n);
+      ap_product_small (st[stn], low + 2 * ((~y) & mask), a, (high + y) >> n,
+			nm);
+      ASSERT (((high + y) >> n) <= (((unsigned long) 1) << APCONST));
+      stn++;
+      y = stc++;
+      while ((y & 1) == 0)
+	{
+	  mpz_mul (st[stn - 2], st[stn - 2], st[stn - 1]);
+	  stn--;
+	  y >>= 1;
+	}
+    }
+  ASSERT (stn == 1);
+  return;
 }
