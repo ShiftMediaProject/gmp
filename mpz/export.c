1e567378095c6326cd895e44427f5f95f4a93e57
Kevin Ryde <user42@zip.com.au>
2002-11-01 23:22:23 +0100

    * mpz/import.c, mpz/export.c: Use char* subract from NULL to get 	pointer alignment, for the benefit of Cray vector systems.
    (This is an improvement, but -hnofastmd is required or the optimizer
    still gets it wrong.)

--- mpz/export.c
+++ mpz/export.c
@@ -83,7 +83,7 @@ mpz_export (void *data, size_t *countp, int order,
   if (endian == 0)
     endian = HOST_ENDIAN;
 
-  align = ((unsigned long) (char *) data) % sizeof (mp_limb_t);
+  align = ((char *) data - (char *) NULL) % sizeof (mp_limb_t);
 
   if (nail == GMP_NAIL_BITS)
     {
