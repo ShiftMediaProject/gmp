63cf349c721050784dd711ad02aec500acb4dc43
tege <tege@gmplib.org>
2002-11-01 12:56:25 +0100

    Add string.h for strcmp,strlen prototype.

--- tests/mpbsd/t-mtox.c
+++ tests/mpbsd/t-mtox.c
@@ -19,8 +19,9 @@ along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
+#include <string.h>		/* for strcmp, strlen */
+#include <stdlib.h>		/* for abort */
 #include <stdio.h>
-#include <stdlib.h>
 #include "gmp.h"
 #include "gmp-impl.h"
 #include "mp.h"
