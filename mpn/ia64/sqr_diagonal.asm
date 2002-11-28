7aebd019e0e6c66a3ce7d5b860b17eb73d0dce5f
tege <tege@gmplib.org>
2002-11-28 05:38:56 +0100

    Don't allocate registers.

--- mpn/ia64/sqr_diagonal.asm
+++ mpn/ia64/sqr_diagonal.asm
@@ -34,42 +34,38 @@ C Itanium 2:  2
 ASM_START()
 PROLOGUE(mpn_sqr_diagonal)
 	.prologue
-	.save	ar.pfs, r21
-		alloc		r21 = ar.pfs, 4, 12, 0, 16
 	.save	ar.lc, r2
 		mov		r2 = ar.lc
-		mov		r20 = ar.ec
-	.save	pr, r22
-		mov		r22 = pr
+		mov		r14 = ar.ec
+	.save	pr, r15
+		mov		r15 = pr
 	.body
 ifdef(`HAVE_ABI_32',
 `		addp4	r32 = 0, r32
 		addp4	r33 = 0, r33
-		sxt4	r34 = r34
+		zxt4	r34 = r34
 		;;
 ')
 		adds		r19 = -1, r34	C decr n
-		mov		r16 = r32	C rp for low limb
 		adds		r18 = 8, r32	C rp for high limb
-		mov		r17 = r33	C sp
 		;;
 		mov		ar.lc = r19
 		mov		ar.ec = 7
 		mov		pr.rot = 1<<16
 		;;
 .Loop:
-	  (p16)	ldf8		f32 = [r17], 8
+	  (p16)	ldf8		f32 = [r33], 8
 	  (p19)	xma.l		f36 = f35, f35, f0
-	  (p22)	stf8		[r16] = f39, 16
+	  (p22)	stf8		[r32] = f39, 16
 	  (p19)	xma.hu		f40 = f35, f35, f0
 	  (p22)	stf8		[r18] = f43, 16
-		br.ctop.sptk	.Loop
+		br.ctop.dptk.few	.Loop
 		;;
 
-		mov		pr = r22, 0x1ffff
+		mov		pr = r15, 0x1ffff
 		mov		ar.lc = r2
-		mov		ar.ec = r20
-		mov		ar.pfs = r21;;
+		mov		ar.ec = r14
+		;;
 		br.ret.sptk.many rp
 EPILOGUE(mpn_sqr_diagonal)
 ASM_END()
