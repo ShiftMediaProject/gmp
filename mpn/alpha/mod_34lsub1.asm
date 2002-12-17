c85f15159806415c304f100cce607d8981b5669d
tege <tege@gmplib.org>
2002-12-18 00:16:09 +0100

    Add code for big-endian, using existing little-endian code only if HAVE_LIMB_LITTLE_ENDIAN is defined.

--- mpn/alpha/mod_34lsub1.asm
+++ mpn/alpha/mod_34lsub1.asm
@@ -116,22 +116,34 @@ $L_012:	lda	r17, 2(r17)
 C Align and sum our 3 main accumulators and 3 carry accumulators
 $L_0:	srl	a0, 48, r2
 	srl	a1, 32, r4
-	insll	a1, 2, r1
-	zapnot	a0, 63, r0
+ifdef(`HAVE_LIMB_LITTLE_ENDIAN',
+`	insll	a1, 2, r1',		C (a1 & 0xffffffff) << 16
+`	zapnot	a1, 15, r25
+	sll	r25, 16, r1')
+	zapnot	a0, 63, r0		C a0 & 0xffffffffffff
 	srl	a2, 16, a1
-	inswl	a2, 4, r3
+ifdef(`HAVE_LIMB_LITTLE_ENDIAN',
+`	inswl	a2, 4, r3',		C (a2 & 0xffff) << 32
+`	zapnot	a2, 3, r25
+	sll	r25, 32, r3')
 	addq	r1, r4, r1
 	addq	r0, r2, r0
 	srl	c0, 32, a2
-	insll	c0, 2, r4
+ifdef(`HAVE_LIMB_LITTLE_ENDIAN',
+`	insll	c0, 2, r4',		C (c0 & 0xffffffff) << 16
+`	zapnot	c0, 15, r25
+	sll	r25, 16, r4')
 	addq	r0, r1, r0
 	addq	r3, a1, r3
 	addq	r0, r3, r0
 	srl	c1, 16, c0
-	inswl	c1, 4, r2
+ifdef(`HAVE_LIMB_LITTLE_ENDIAN',
+`	inswl	c1, 4, r2',		C (c1 & 0xffff) << 32
+`	zapnot	c1, 3, r25
+	sll	r25, 32, r2')
 	addq	r4, a2, r4
 C	srl	c2, 48, r3		C This will be 0 in practise
-	zapnot	c2, 63, r1
+	zapnot	c2, 63, r1		C r1 = c2 & 0xffffffffffff
 	addq	r0, r4, r0
 	addq	r2, c0, r2
 	addq	r0, r2, r0
