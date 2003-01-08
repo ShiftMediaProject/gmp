10e02b7490b5c94493e5caca248bb8d1ea1db591
Kevin Ryde <user42@zip.com.au>
2003-01-09 00:10:18 +0100

    # Add a copyright year.

--- mpfr/tests/tests.c
+++ mpfr/tests/tests.c
@@ -1,6 +1,6 @@
 /* Miscellaneous support for test programs.
 
-Copyright 2001, 2002 Free Software Foundation, Inc.
+Copyright 2001, 2002, 2003 Free Software Foundation, Inc.
 
 This file is part of the MPFR Library.
 
