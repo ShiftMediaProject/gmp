682f6a6b1bf57a5aac7c33bce15f871a1af2aa66
Kevin Ryde <user42@zip.com.au>
2003-03-29 00:31:47 +0100

    Add a copyright year.

--- tests/tests.h
+++ tests/tests.h
@@ -1,6 +1,6 @@
 /* Tests support prototypes etc.
 
-Copyright 2000, 2001, 2002 Free Software Foundation, Inc.
+Copyright 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
