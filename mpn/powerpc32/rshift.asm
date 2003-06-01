828e0e465a2c4daf57efae00981ddfa8d29a6e5d
tege <tege@gmplib.org>
2003-06-01 12:34:48 +0200

    Reformat code.

--- mpn/powerpc32/rshift.asm
+++ mpn/powerpc32/rshift.asm
@@ -1,23 +1,23 @@
 dnl  PowerPC-32 mpn_rshift -- Shift a number right.
-dnl
-dnl  Copyright 1995, 1998, 2000, 2002 Free Software Foundation, Inc.
-dnl
+
+dnl  Copyright 1995, 1998, 2000, 2002, 2003 Free Software Foundation, Inc.
+
 dnl  This file is part of the GNU MP Library.
-dnl 
-dnl  The GNU MP Library is free software; you can redistribute it and/or
-dnl  modify it under the terms of the GNU Lesser General Public License as
-dnl  published by the Free Software Foundation; either version 2.1 of the
-dnl  License, or (at your option) any later version.
-dnl 
-dnl  The GNU MP Library is distributed in the hope that it will be useful,
-dnl  but WITHOUT ANY WARRANTY; without even the implied warranty of
-dnl  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-dnl  Lesser General Public License for more details.
-dnl 
-dnl  You should have received a copy of the GNU Lesser General Public
-dnl  License along with the GNU MP Library; see the file COPYING.LIB.  If
-dnl  not, write to the Free Software Foundation, Inc., 59 Temple Place -
-dnl  Suite 330, Boston, MA 02111-1307, USA.
+
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 2.1 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+dnl  the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+dnl  MA 02111-1307, USA.
 
 include(`../config.m4')
 
@@ -26,15 +26,14 @@ C      cycles/limb
 C 604e:    3.0
 C 750:     3.0
 
+C INPUT PARAMETERS
+C rp	r3
+C up	r4
+C n	r5
+C cnt	r6
 
 ASM_START()
 PROLOGUE(mpn_rshift)
-
-	C r3	res_ptr
-	C r4	s1_ptr
-	C r5	size
-	C r6	cnt
-
 	cmpi	cr0, r5, 12	C more than 12 limbs?
 	addi	r7, r3, -4	C dst-4
 	bgt	L(BIG)		C branch if more than 12 limbs
@@ -81,14 +80,14 @@ L(BIG):
 	lwzu	r28, 4(r4)
 	bdz	L(xe0)
 
-L(oop0):
+L(loop0):
 	srw	r12, r28, r6
 	slw	r24, r28, r8
 	lwzu	r28, 4(r4)
 	or	r24, r0, r24
 	stwu	r24, 4(r7)
 	mr	r0, r12
-	bdnz	L(oop0)		C taken at most once!
+	bdnz	L(loop0)	C taken at most once!
 
 L(xe0):	srw	r12, r28, r6
 	slw	r24, r28, r8
@@ -96,7 +95,7 @@ L(xe0):	srw	r12, r28, r6
 	stwu	r24, 4(r7)
 	mr	r0, r12
 
-L(e):	srwi	r5, r5, 2		C count for unrolled loop
+L(e):	srwi	r5, r5, 2	C count for unrolled loop
 	addi	r5, r5, -1
 	mtctr	r5
 	lwz	r28, 4(r4)
@@ -104,7 +103,7 @@ L(e):	srwi	r5, r5, 2		C count for unrolled loop
 	lwz	r30, 12(r4)
 	lwzu	r31, 16(r4)
 
-L(oopU):
+L(loopU):
 	srw	r9, r28, r6
 	slw	r24, r28, r8
 	lwz	r28, 4(r4)
@@ -126,7 +125,7 @@ L(oopU):
 	or	r27, r11, r27
 	stwu	r27, 16(r7)
 	mr	r0, r12
-	bdnz	L(oopU)
+	bdnz	L(loopU)
 
 	srw	r9, r28, r6
 	slw	r24, r28, r8
@@ -148,5 +147,4 @@ L(oopU):
 	stw	r12, 20(r7)
 	lmw	r24, -32(r1)	C restore registers
 	blr
-
 EPILOGUE(mpn_rshift)
