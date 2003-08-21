f5603c03a56b9fe00adfe11e805219a9d489616f
tege <tege@gmplib.org>
2003-08-22 00:59:25 +0200

    *** empty log message ***

---
+++ mpn/x86_64/addmul_1.asm
@@ -0,0 +1,142 @@
+dnl  AMD64 mpn_addmul_1 -- Multiply a limb vector with a limb and add the
+dnl  result to a second limb vector.
+
+dnl  Copyright 2003 Free Software Foundation, Inc.
+
+dnl  This file is part of the GNU MP Library.
+
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 2.1 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+dnl  the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+dnl  MA 02111-1307, USA.
+
+include(`../config.m4')
+
+
+C         cycles/limb
+C Hammer:     3.75
+
+
+C INPUT PARAMETERS
+C rp	rdi
+C up	rsi
+C n	rdx
+C vl	rcx
+
+	TEXT
+	ALIGN(16)
+	.byte	0x66, 0x90, 0x66, 0x90, 0x66, 0x90, 0x66, 0x90
+ASM_START()
+PROLOGUE(mpn_addmul_1)
+	pushq	%r12			C				      2
+	pushq	%rbx			C				      1
+
+	movq	%rdx, %rbx		C				      3
+	xorq	%r8, %r8		C clear carry limb		      3
+	xorq	%r12, %r12		C maintain r12 = 0 FIXME: don't!      3
+	subq	$4, %rbx		C				      4
+	jb	.Lend			C				      2
+	.byte	0x66, 0x90, 0x66, 0x90, 0x66, 0x90
+.Loop:
+	movq	(%rsi), %rax		C				      3
+	mulq	%rcx			C				      3
+	movq	%r12, %r9		C				      3
+	addq	%rax, %r8		C				      3
+	adcq	%rdx, %r9		C				      3
+
+	movq	8(%rsi), %rax		C				      4
+	mulq	%rcx			C				      3
+	movq	%r12, %r10		C				      3
+	addq	%rax, %r9		C				      3
+	adcq	%rdx, %r10		C				      3
+
+	movq	16(%rsi), %rax		C				      4
+	mulq	%rcx			C				      3
+	movq	%r12, %r11		C				      3
+	addq	%rax, %r10		C				      3
+	adcq	%rdx, %r11		C				      3
+
+	movq	24(%rsi), %rax		C				      4
+	mulq	%rcx			C				      3
+	addq	%rax, %r11		C				      3
+	adcq	%r12, %rdx		C				      3
+
+	addq	%r8, (%rdi)		C				      3
+	adcq	%r9, 8(%rdi)		C				      4
+	adcq	%r10, 16(%rdi)		C				      4
+	adcq	%r11, 24(%rdi)		C				      4
+
+	movq	%r12, %r8		C				      3
+	adcq	%rdx, %r8		C				      3
+
+	leaq	32(%rsi), %rsi		C				      4
+	leaq	32(%rdi), %rdi		C				      4
+	subq	$4, %rbx		C				      4
+	jae	.Loop			C				      2
+
+	cmpl	$-4, %ebx		C				      3
+	jne	.Lend			C				      2
+
+	movq	%r8, %rax		C				      3
+	popq	%rbx			C				      1
+	popq	%r12			C				      2
+	ret				C				      1
+
+.Lend:	movq	(%rsi), %rax		C				      3
+	mulq	%rcx			C				      3
+	movq	%r12, %r9		C				      3
+	addq	%rax, %r8		C				      3
+	adcq	%rdx, %r9		C				      3
+
+	cmpl	$-3, %ebx		C				      3
+	jne	.L1			C				      2
+
+	addq	%r8, (%rdi)		C				      3
+	adcq	%r12, %r9		C				      3
+	movq	%r9, %rax		C				      3
+	popq	%rbx			C				      1
+	popq	%r12			C				      2
+	ret				C				      1
+
+.L1:	movq	8(%rsi), %rax		C				      4
+	mulq	%rcx			C				      3
+	movq	%r12, %r10		C				      3
+	addq	%rax, %r9		C				      3
+	adcq	%rdx, %r10		C				      3
+
+	cmpl	$-2, %ebx		C				      3
+	jne	.L2			C				      2
+
+	addq	%r8, (%rdi)		C				      3
+	adcq	%r9, 8(%rdi)		C				      4
+	adcq	%r12, %r10		C				      3
+	movq	%r10, %rax		C				      3
+	popq	%rbx			C				      1
+	popq	%r12			C				      2
+	ret				C				      1
+
+.L2:	movq	16(%rsi), %rax		C				      4
+	mulq	%rcx			C				      3
+	movq	%r12, %r11		C				      3
+	addq	%rax, %r10		C				      3
+	adcq	%rdx, %r11		C				      3
+
+	addq	%r8, (%rdi)		C				      3
+	adcq	%r9, 8(%rdi)		C				      4
+	adcq	%r10, 16(%rdi)		C				      4
+	adcq	%r12, %r11		C				      3
+	movq	%r11, %rax		C				      3
+	popq	%rbx			C				      1
+	popq	%r12			C				      2
+	ret				C				      1
+EPILOGUE()
---
+++ mpn/x86_64/sub_n.asm
@@ -0,0 +1,122 @@
+dnl  AMD64 mpn_sub_n -- Subtract two limb vectors of the same length > 0 and
+dnl  store difference in a third limb vector.
+
+dnl  Copyright 2003 Free Software Foundation, Inc.
+
+dnl  This file is part of the GNU MP Library.
+
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 2.1 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+dnl  the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+dnl  MA 02111-1307, USA.
+
+include(`../config.m4')
+
+
+C         cycles/limb
+C Hammer:     1.75
+
+
+C INPUT PARAMETERS
+C rp	rdi
+C up	rsi
+C vp	rdx
+C n	rcx
+
+	TEXT
+	ALIGN(16)
+ASM_START()
+PROLOGUE(mpn_sub_n)
+	pushq	%rbx			C				1
+	movq	%rcx, %r8		C				3
+	shrq	$2, %rcx		C				4
+	je	.Lend			C				2
+	andl	$3, %r8d		C				4
+	.byte	0x66, 0x90		C				2
+
+C Main loop.  16-byte aligned.  Blocks between blank lines take one cycle.
+.Loop:	movq	(%rsi), %rax		C				3
+	movq	8(%rsi), %rbx		C				4
+	leaq	32(%rsi), %rsi		C				4
+
+	sbbq	(%rdx), %rax		C				3
+	movq	%rax, (%rdi)		C				3
+
+	sbbq	8(%rdx), %rbx		C				4
+	movq	%rbx, 8(%rdi)		C				4
+
+	movq	-16(%rsi), %rax		C				4
+	movq	-8(%rsi), %rbx		C				4
+
+	sbbq	16(%rdx), %rax		C				4
+	movq	%rax, 16(%rdi)		C				4
+
+	sbbq	24(%rdx), %rbx		C				4
+	movq	%rbx, 24(%rdi)		C				4
+	decq	%rcx			C				3
+
+	leaq	32(%rdx), %rdx		C				4
+	leaq	32(%rdi), %rdi		C				4
+	jne	.Loop			C				2
+
+
+	incl	%r8d			C				3
+	decl	%r8d			C				3
+	jne	.L0			C				2
+
+	sbbl	%eax,%eax		C				2
+	negl	%eax			C				2
+	popq	%rbx			C				1
+	ret				C				1
+
+.Lend:	testl	%eax, %eax		C clear cy			2
+.L0:	decl	%r8d			C				3
+	jne	.L1			C				2
+
+	movq	(%rsi), %rax		C				3
+	sbbq	(%rdx), %rax		C				3
+	movq	%rax, (%rdi)		C				3
+	sbbl	%eax,%eax		C				2
+	negl	%eax			C				2
+	popq	%rbx			C				1
+	ret				C				1
+
+.L1:	decl	%r8d			C				3
+	jne	.L2			C				2
+
+	movq	(%rsi), %rax		C				3
+	movq	8(%rsi), %rbx		C				4
+	sbbq	(%rdx), %rax		C				3
+	movq	%rax, (%rdi)		C				3
+	sbbq	8(%rdx), %rbx		C				4
+	movq	%rbx, 8(%rdi)		C				4
+	sbbl	%eax,%eax		C				2
+	negl	%eax			C				2
+	popq	%rbx			C				1
+	ret				C				1
+
+.L2:
+	movq	(%rsi), %rax		C				3
+	movq	8(%rsi), %rbx		C				4
+	sbbq	(%rdx), %rax		C				3
+	movq	%rax, (%rdi)		C				3
+	sbbq	8(%rdx), %rbx		C				4
+	movq	%rbx, 8(%rdi)		C				4
+	movq	16(%rsi), %rax		C				4
+	sbbq	16(%rdx), %rax		C				4
+	movq	%rax, 16(%rdi)		C				4
+	sbbl	%eax,%eax		C				2
+	negl	%eax			C				2
+	popq	%rbx			C				1
+	ret				C				1
+EPILOGUE()
