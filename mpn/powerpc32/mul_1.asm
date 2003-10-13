5f66c110603b3db1c530445338394dc2a68e48f3
tege <tege@gmplib.org>
2003-10-14 01:48:01 +0200

    Add c/l data for common ppc processors.

--- mpn/powerpc32/mul_1.asm
+++ mpn/powerpc32/mul_1.asm
@@ -1,37 +1,39 @@
-dnl PowerPC-32 mpn_mul_1 -- Multiply a limb vector with a limb and store
-dnl the result in a second limb vector.
+dnl  PowerPC-32 mpn_mul_1 -- Multiply a limb vector with a limb and store the
+dnl  result in a second limb vector.
 
-dnl Copyright 1995, 1997, 2000, 2002 Free Software Foundation, Inc.
+dnl  Copyright 1995, 1997, 2000, 2002, 2003 Free Software Foundation, Inc.
 
-dnl This file is part of the GNU MP Library.
+dnl  This file is part of the GNU MP Library.
 
-dnl The GNU MP Library is free software; you can redistribute it and/or modify
-dnl it under the terms of the GNU Lesser General Public License as published by
-dnl the Free Software Foundation; either version 2.1 of the License, or (at your
-dnl option) any later version.
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 2.1 of the License, or (at
+dnl  your option) any later version.
 
-dnl The GNU MP Library is distributed in the hope that it will be useful, but
-dnl WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-dnl or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-dnl License for more details.
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
 
-dnl You should have received a copy of the GNU Lesser General Public License
-dnl along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
-dnl the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-dnl MA 02111-1307, USA.
-
-
-dnl INPUT PARAMETERS
-dnl res_ptr	r3
-dnl s1_ptr	r4
-dnl size	r5
-dnl s2_limb	r6
-
-dnl This is optimized for the PPC604 but it runs decently even on PPC601.  It
-dnl has not been tested on a PPC603 since I don't have access to any such
-dnl machines.
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+dnl  the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+dnl  MA 02111-1307, USA.
 
 include(`../config.m4')
+
+C                cycles/limb
+C 603e:            ?
+C 604e:            4.0
+C 75x (G3):        4.5-11
+C 7400,7410 (G4):  4.5-11
+C 744x,745x (G4+): 6.0
+
+C INPUT PARAMETERS
+C rp	r3
+C up	r4
+C n	r5
+C vl	r6
 
 ASM_START()
 PROLOGUE(mpn_mul_1)
@@ -47,7 +49,8 @@ C Start software pipeline
 	mulhwu	r0,r8,r6
 	bdz	L(end1)
 C Software pipelined main loop
-L(oop):	lwz	r8,4(r4)
+L(loop):
+	lwz	r8,4(r4)
 	mullw	r10,r9,r6
 	adde	r5,r11,r12
 	mulhwu	r12,r9,r6
@@ -58,7 +61,7 @@ L(oop):	lwz	r8,4(r4)
 	adde	r7,r10,r0
 	mulhwu	r0,r8,r6
 	stwu	r7,8(r3)
-	bdnz	L(oop)
+	bdnz	L(loop)
 C Finish software pipeline
 L(end1):
 	mullw	r10,r9,r6
