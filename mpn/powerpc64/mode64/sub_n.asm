5e5a375fa2682f11a7dca3e40eecc4a137e80af8
tege <tege@gmplib.org>
2003-10-19 13:19:14 +0200

    Move from "..".

---
+++ mpn/powerpc64/mode64/sub_n.asm
@@ -0,0 +1,64 @@
+dnl  PowerPC-64 mpn_sub_n -- Subtract two limb vectors of the same length > 0
+dnl  and store difference in a third limb vector.
+
+dnl  Copyright 1999, 2000, 2001, 2003 Free Software Foundation, Inc.
+
+dnl  This file is part of the GNU MP Library.
+
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 2.1 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+dnl  the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+dnl  MA 02111-1307, USA.
+
+include(`../config.m4')
+
+C		cycles/limb
+C POWER3/PPC630:     1.5
+C POWER4/PPC970:     2?
+
+C INPUT PARAMETERS
+C rp	r3
+C up	r4
+C vp	r5
+C n	r6
+
+ASM_START()
+PROLOGUE(mpn_sub_n)
+	mtctr	r6		C copy size into CTR
+	addic	r0,r6,-1	C set cy
+	ld	r8,0(r4)	C load least significant s1 limb
+	ld	r0,0(r5)	C load least significant s2 limb
+	addi	r3,r3,-8	C offset res_ptr, it's updated before it's used
+	bdz	.Lend		C If done, skip loop
+.Loop:	ld	r9,8(r4)	C load s1 limb
+	ld	r10,8(r5)	C load s2 limb
+	subfe	r7,r0,r8	C subtract limbs with cy, set cy
+	std	r7,8(r3)	C store result limb
+	bdz	.Lexit		C decrement CTR and exit if done
+	ldu	r8,16(r4)	C load s1 limb and update s1_ptr
+	ldu	r0,16(r5)	C load s2 limb and update s2_ptr
+	subfe	r7,r10,r9	C subtract limbs with cy, set cy
+	stdu	r7,16(r3)	C store result limb and update res_ptr
+	bdnz	.Loop		C decrement CTR and loop back
+
+.Lend:	subfe	r7,r0,r8
+	std	r7,8(r3)	C store ultimate result limb
+	subfe	r3,r0,r0	C load !cy into ...
+	subfic	r3,r3,0		C ... return value register
+	blr
+.Lexit:	subfe	r7,r10,r9
+	std	r7,16(r3)
+	subfe	r3,r0,r0	C load !cy into ...
+	subfic	r3,r3,0		C ... return value register
+	blr
+EPILOGUE()
---
+++ mpn/powerpc64/mode64/sublsh1_n.asm
@@ -0,0 +1,85 @@
+dnl  PowerPC-64 mpn_sublsh1_n -- rp[] = up[] - (vp[] << 1)
+
+dnl  Copyright 2003 Free Software Foundation, Inc.
+
+dnl  This file is part of the GNU MP Library.
+
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 2.1 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+dnl  the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+dnl  MA 02111-1307, USA.
+
+include(`../config.m4')
+
+C		cycles/limb
+C POWER3/PPC630:     2		(1.5 c/l should be possible)
+C POWER4/PPC970:     ?		(2.0 c/l should be possible)
+
+C INPUT PARAMETERS
+C rp	r3
+C up	r4
+C vp	r5
+C n	r6
+
+define(`rp',`r3')
+define(`up',`r4')
+define(`vp',`r5')
+
+define(`s0',`r6')
+define(`s1',`r7')
+define(`u0',`r8')
+define(`v0',`r10')
+define(`v1',`r11')
+
+ASM_START()
+PROLOGUE(mpn_sublsh1_n)
+	mtctr	r6		C put n in ctr
+
+	ld	v0, 0(vp)	C load v limb
+	ld	u0, 0(up)	C load u limb
+	addic	up, up, -8	C update up; set cy
+	addi	rp, rp, -8	C update rp
+	sldi	s1, v0, 1
+	bdz	.Lend		C If done, skip loop
+
+.Loop:	ld	v1, 8(vp)	C load v limb
+	subfe	s1, s1, u0	C add limbs with cy, set cy
+	std	s1, 8(rp)	C store result limb
+	srdi	s0, v0, 63	C shift down previous v limb
+	ldu	u0, 16(up)	C load u limb and update up
+	rldimi	s0, v1, 1, 0	C left shift v limb and merge with prev v limb
+
+	bdz	.Lexit		C decrement ctr and exit if done
+
+	ldu	v0, 16(vp)	C load v limb and update vp
+	subfe	s0, s0, u0	C add limbs with cy, set cy
+	stdu	s0, 16(rp)	C store result limb and update rp
+	srdi	s1, v1, 63	C shift down previous v limb
+	ld	u0, 8(up)	C load u limb
+	rldimi	s1, v0, 1, 0	C left shift v limb and merge with prev v limb
+
+	bdnz	.Loop		C decrement ctr and loop back
+
+.Lend:	subfe	r7, s1, u0
+	std	r7, 8(rp)	C store last result limb
+	srdi	r3, v0, 63
+	subfze	r3, r3
+	neg	r3, r3
+	blr
+.Lexit:	subfe	r7, s0, u0
+	std	r7, 16(rp)	C store last result limb
+	srdi	r3, v1, 63
+	subfze	r3, r3
+	neg	r3, r3
+	blr
+EPILOGUE()
