d1c885975bf9bfb2da0deb6b9c49e55424bc3675
tege <tege@gmplib.org>
2003-10-27 03:03:33 +0100

    Apply cross-jumping.

--- mpn/sparc32/v9/sqr_diagonal.asm
+++ mpn/sparc32/v9/sqr_diagonal.asm
@@ -1,6 +1,6 @@
 dnl  SPARC v9 32-bit mpn_sqr_diagonal.
 
-dnl  Copyright 2001 Free Software Foundation, Inc.
+dnl  Copyright 2001, 2003 Free Software Foundation, Inc.
 
 dnl  This file is part of the GNU MP Library.
 
@@ -30,9 +30,6 @@ C n	i2
 C This code uses a very deep software pipeline, due to the need for moving data
 C forth and back between the integer registers and floating-point registers.
 C
-C The code is very large, probably unnecessarily large.  Cross-jumping
-C transformation of the wind-down code could reduce the code size considerably.
-C
 C A VIS variant of this code would make the pipeline less deep, since the
 C masking now done in the integer unit could take place in the floating-point
 C unit using the FAND instruction.  It would be possible to save several cycles
@@ -46,7 +43,7 @@ C variant could run three cycles faster than the corresponding non-VIS code.
 
 C This is non-pipelined code showing the algorithm:
 C
-C L(loop):
+C .Loop:
 C	lduw	[up+0],%g4		C 00000000hhhhllll
 C	sllx	%g4,16,%g3		C 0000hhhhllll0000
 C	or	%g3,%g4,%g2		C 0000hhhhXXXXllll
@@ -72,7 +69,7 @@ C	srlx	%g1,32,%l0
 C	stw	%l0,[rp+4]
 C	add	up,4,up
 C	subcc	n,1,n
-C	bne,pt	%icc,L(loop)
+C	bne,pt	%icc,.Loop
 C	add	rp,8,rp
 
 define(`fanop',`fitod %f12,%f10')	dnl  A quasi nop running in the FA pipe
@@ -81,17 +78,17 @@ ASM_START()
 
 	TEXT
 	ALIGN(4)
-L(noll):
+.Lnoll:
 	.word	0
 
 PROLOGUE(mpn_sqr_diagonal)
 	save	%sp,-256,%sp
 
 ifdef(`PIC',
-`L(pc):	rd	%pc,%o7
-	ld	[%o7+L(noll)-L(pc)],%f8',
-`	sethi	%hi(L(noll)),%g1
-	ld	[%g1+%lo(L(noll))],%f8')
+`.Lpc:	rd	%pc,%o7
+	ld	[%o7+.Lnoll-.Lpc],%f8',
+`	sethi	%hi(.Lnoll),%g1
+	ld	[%g1+%lo(.Lnoll)],%f8')
 
 	sethi	%hi(0xffff0000),%g5
 	add	%i1,-8,%i1
@@ -101,18 +98,68 @@ ifdef(`PIC',
 	sllx	%g4,16,%g3		C 0000hhhhllll0000
 	or	%g3,%g4,%g2		C 0000hhhhXXXXllll
 	subcc	%i2,1,%i2
-	be,pn	%icc,L(end1)
+	bne,pt	%icc,.L_grt_1
 	andn	%g2,%g5,%g2		C 0000hhhh0000llll
 
+	add	%i1,4,%i1		C s1_ptr++
+	stx	%g2,[%fp+80]
+	ld	[%i1],%f9
+	ldd	[%fp+80],%f0
+	fxtod	%f8,%f2
+	fitod	%f0,%f4
+	fitod	%f1,%f6
+	fmuld	%f2,%f4,%f4
+	fmuld	%f2,%f6,%f6
+	fdtox	%f4,%f4
+	fdtox	%f6,%f6
+	std	%f4,[%fp-24]
+	std	%f6,[%fp-16]
+
+	add	%fp, 80, %l3
+	add	%fp, -24, %l4
+	add	%fp, 72, %l5
+	b	.L1
+	add	%fp, -40, %l6
+
+.L_grt_1:
 	stx	%g2,[%fp+80]
 	lduw	[%i1+8],%g4
 	add	%i1,4,%i1		C s1_ptr++
 	sllx	%g4,16,%g3		C 0000hhhhllll0000
 	or	%g3,%g4,%g2		C 0000hhhhXXXXllll
 	subcc	%i2,1,%i2
-	be,pn	%icc,L(end2)
+	bne,pt	%icc,.L_grt_2
 	andn	%g2,%g5,%g2		C 0000hhhh0000llll
 
+	stx	%g2,[%fp+72]
+	ld	[%i1],%f9
+	add	%i1,4,%i1		C s1_ptr++
+	ldd	[%fp+80],%f0
+	fxtod	%f8,%f2
+	fitod	%f0,%f4
+	fitod	%f1,%f6
+	fmuld	%f2,%f4,%f4
+	ld	[%i1],%f9
+	fmuld	%f2,%f6,%f6
+	ldd	[%fp+72],%f0
+	fdtox	%f4,%f4
+	fdtox	%f6,%f6
+	std	%f4,[%fp-24]
+	fxtod	%f8,%f2
+	std	%f6,[%fp-16]
+	fitod	%f0,%f4
+	fitod	%f1,%f6
+	fmuld	%f2,%f4,%f4
+	fmuld	%f2,%f6,%f6
+	fdtox	%f4,%f4
+
+	add	%fp, 72, %l3
+	add	%fp, -40, %l4
+	add	%fp, 80, %l5
+	b	.L2
+	add	%fp, -24, %l6
+
+.L_grt_2:
 	stx	%g2,[%fp+72]
 	lduw	[%i1+8],%g4
 	ld	[%i1],%f9
@@ -122,9 +169,36 @@ ifdef(`PIC',
 	or	%g3,%g4,%g2		C 0000hhhhXXXXllll
 	subcc	%i2,1,%i2
 	fxtod	%f8,%f2
-	be,pn	%icc,L(end3)
+	bne,pt	%icc,.L_grt_3
 	andn	%g2,%g5,%g2		C 0000hhhh0000llll
 
+	stx	%g2,[%fp+80]
+	fitod	%f0,%f4
+	fitod	%f1,%f6
+	fmuld	%f2,%f4,%f4
+	ld	[%i1],%f9
+	fmuld	%f2,%f6,%f6
+	add	%i1,4,%i1		C s1_ptr++
+	ldd	[%fp+72],%f0
+	fdtox	%f4,%f4
+	fdtox	%f6,%f6
+	std	%f4,[%fp-24]
+	fxtod	%f8,%f2
+	std	%f6,[%fp-16]
+	fitod	%f0,%f4
+	fitod	%f1,%f6
+	fmuld	%f2,%f4,%f4
+	ld	[%i1],%f9
+	add	%fp, 80, %l3
+	fmuld	%f2,%f6,%f6
+	add	%fp, -24, %l4
+	ldd	[%fp+80],%f0
+	add	%fp, 72, %l5
+	fdtox	%f4,%f4
+	b	.L3
+	add	%fp, -40, %l6
+
+.L_grt_3:
 	stx	%g2,[%fp+80]
 	fitod	%f0,%f4
 	lduw	[%i1+8],%g4
@@ -142,9 +216,25 @@ ifdef(`PIC',
 	std	%f4,[%fp-24]
 	fxtod	%f8,%f2
 	std	%f6,[%fp-16]
-	be,pn	%icc,L(end4)
+	bne,pt	%icc,.L_grt_4
 	andn	%g2,%g5,%g2		C 0000hhhh0000llll
 
+	stx	%g2,[%fp+72]
+	fitod	%f0,%f4
+	fitod	%f1,%f6
+	add	%fp, 72, %l3
+	fmuld	%f2,%f4,%f4
+	add	%fp, -40, %l4
+	ld	[%i1],%f9
+	fmuld	%f2,%f6,%f6
+	add	%i1,4,%i1		C s1_ptr++
+	ldd	[%fp+80],%f0
+	add	%fp, 80, %l5
+	fdtox	%f4,%f4
+	b	.L4
+	add	%fp, -24, %l6
+
+.L_grt_4:
 	stx	%g2,[%fp+72]
 	fitod	%f0,%f4
 	lduw	[%i1+8],%g4
@@ -162,15 +252,14 @@ ifdef(`PIC',
 	std	%f4,[%fp-40]
 	fxtod	%f8,%f2
 	std	%f6,[%fp-32]
-	be,pn	%icc,L(end5)
+	be,pn	%icc,.L5
 	andn	%g2,%g5,%g2		C 0000hhhh0000llll
 
-	b,a	L(loop)
+	b,a	.Loop
 
 	.align	16
 C --- LOOP BEGIN
-L(loop):
-	nop
+.Loop:	nop
 	nop
 	stx	%g2,[%fp+80]
 	fitod	%f0,%f4
@@ -217,7 +306,7 @@ C ---
 C ---
 	std	%f6,[%fp-16]
 	andn	%g2,%g5,%g2		C 0000hhhh0000llll
-	be,pn	%icc,L(loope)
+	be,pn	%icc,.Lend
 	fanop
 C ---  LOOP MIDDLE
 	nop
@@ -267,16 +356,25 @@ C ---
 C ---
 	std	%f6,[%fp-32]
 	andn	%g2,%g5,%g2		C 0000hhhh0000llll
-	bne,pt	%icc,L(loop)
+	bne,pt	%icc,.Loop
 	fanop
 C --- LOOP END
 
-L(end5):
-	stx	%g2,[%fp+80]
+.L5:	add	%fp, 80, %l3
+	add	%fp, -24, %l4
+	add	%fp, 72, %l5
+	b	.Ltail
+	add	%fp, -40, %l6
+
+.Lend:	add	%fp, 72, %l3
+	add	%fp, -40, %l4
+	add	%fp, 80, %l5
+	add	%fp, -24, %l6
+.Ltail:	stx	%g2,[%l3]
 	fitod	%f0,%f4
 	fitod	%f1,%f6
-	ldx	[%fp-24],%g2		C p16
-	ldx	[%fp-16],%g1		C p0
+	ldx	[%l4],%g2		C p16
+	ldx	[%l4+8],%g1		C p0
 	fmuld	%f2,%f4,%f4
 	sllx	%g2,16,%g2		C align p16
 	add	%i0,8,%i0		C res_ptr++
@@ -284,40 +382,40 @@ L(end5):
 	fmuld	%f2,%f6,%f6
 	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
 	add	%i1,4,%i1		C s1_ptr++
-	ldd	[%fp+72],%f0
+	ldd	[%l5],%f0
 	srlx	%g1,32,%l0
 	stw	%g1,[%i0-8]
 	fdtox	%f4,%f4
 	stw	%l0,[%i0-4]
-	fdtox	%f6,%f6
-	std	%f4,[%fp-24]
+.L4:	fdtox	%f6,%f6
+	std	%f4,[%l4]
 	fxtod	%f8,%f2
-	std	%f6,[%fp-16]
+	std	%f6,[%l4+8]
 
 	fitod	%f0,%f4
 	fitod	%f1,%f6
-	ldx	[%fp-40],%g2		C p16
-	ldx	[%fp-32],%g1		C p0
+	ldx	[%l6],%g2		C p16
+	ldx	[%l6+8],%g1		C p0
 	fmuld	%f2,%f4,%f4
 	sllx	%g2,16,%g2		C align p16
 	add	%i0,8,%i0		C res_ptr++
 	ld	[%i1],%f9
 	fmuld	%f2,%f6,%f6
 	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	ldd	[%fp+80],%f0
+	ldd	[%l3],%f0
 	srlx	%g1,32,%l0
 	stw	%g1,[%i0-8]
 	fdtox	%f4,%f4
 	stw	%l0,[%i0-4]
-	fdtox	%f6,%f6
-	std	%f4,[%fp-40]
+.L3:	fdtox	%f6,%f6
+	std	%f4,[%l6]
 	fxtod	%f8,%f2
-	std	%f6,[%fp-32]
+	std	%f6,[%l6+8]
 
 	fitod	%f0,%f4
 	fitod	%f1,%f6
-	ldx	[%fp-24],%g2		C p16
-	ldx	[%fp-16],%g1		C p0
+	ldx	[%l4],%g2		C p16
+	ldx	[%l4+8],%g1		C p0
 	fmuld	%f2,%f4,%f4
 	sllx	%g2,16,%g2		C align p16
 	add	%i0,8,%i0		C res_ptr++
@@ -327,12 +425,12 @@ L(end5):
 	stw	%g1,[%i0-8]
 	fdtox	%f4,%f4
 	stw	%l0,[%i0-4]
-	fdtox	%f6,%f6
-	std	%f4,[%fp-24]
-	std	%f6,[%fp-16]
+.L2:	fdtox	%f6,%f6
+	std	%f4,[%l4]
+	std	%f6,[%l4+8]
 
-	ldx	[%fp-40],%g2		C p16
-	ldx	[%fp-32],%g1		C p0
+	ldx	[%l6],%g2		C p16
+	ldx	[%l6+8],%g1		C p0
 	sllx	%g2,16,%g2		C align p16
 	add	%i0,8,%i0		C res_ptr++
 	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
@@ -340,8 +438,8 @@ L(end5):
 	stw	%g1,[%i0-8]
 	stw	%l0,[%i0-4]
 
-	ldx	[%fp-24],%g2		C p16
-	ldx	[%fp-16],%g1		C p0
+.L1:	ldx	[%l4],%g2		C p16
+	ldx	[%l4+8],%g1		C p0
 	sllx	%g2,16,%g2		C align p16
 	add	%i0,8,%i0		C res_ptr++
 	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
@@ -352,281 +450,4 @@ L(end5):
 	ret
 	restore	%g0,%g0,%o0
 
-L(loope):
-	stx	%g2,[%fp+72]
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	ldx	[%fp-40],%g2		C p16
-	ldx	[%fp-32],%g1		C p0
-	fmuld	%f2,%f4,%f4
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	ld	[%i1],%f9
-	fmuld	%f2,%f6,%f6
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	add	%i1,4,%i1		C s1_ptr++
-	ldd	[%fp+80],%f0
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	fdtox	%f4,%f4
-	stw	%l0,[%i0-4]
-	fdtox	%f6,%f6
-	std	%f4,[%fp-40]
-	fxtod	%f8,%f2
-	std	%f6,[%fp-32]
-
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	ldx	[%fp-24],%g2		C p16
-	ldx	[%fp-16],%g1		C p0
-	fmuld	%f2,%f4,%f4
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	ld	[%i1],%f9
-	fmuld	%f2,%f6,%f6
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	ldd	[%fp+72],%f0
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	fdtox	%f4,%f4
-	stw	%l0,[%i0-4]
-	fdtox	%f6,%f6
-	std	%f4,[%fp-24]
-	fxtod	%f8,%f2
-	std	%f6,[%fp-16]
-
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	ldx	[%fp-40],%g2		C p16
-	ldx	[%fp-32],%g1		C p0
-	fmuld	%f2,%f4,%f4
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	fmuld	%f2,%f6,%f6
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	fdtox	%f4,%f4
-	stw	%l0,[%i0-4]
-	fdtox	%f6,%f6
-	std	%f4,[%fp-40]
-	std	%f6,[%fp-32]
-
-	ldx	[%fp-24],%g2		C p16
-	ldx	[%fp-16],%g1		C p0
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	stw	%l0,[%i0-4]
-
-	ldx	[%fp-40],%g2		C p16
-	ldx	[%fp-32],%g1		C p0
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	stw	%l0,[%i0-4]
-
-	ret
-	restore	%g0,%g0,%o0
-
-L(end1):
-	add	%i1,4,%i1		C s1_ptr++
-	stx	%g2,[%fp+80]
-	ld	[%i1],%f9
-	ldd	[%fp+80],%f0
-	fxtod	%f8,%f2
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	fmuld	%f2,%f4,%f4
-	fmuld	%f2,%f6,%f6
-	fdtox	%f4,%f4
-	fdtox	%f6,%f6
-	std	%f4,[%fp-24]
-	std	%f6,[%fp-16]
-	ldx	[%fp-24],%g2		C p16
-	ldx	[%fp-16],%g1		C p0
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	stw	%l0,[%i0-4]
-	ret
-	restore	%g0,%g0,%o0
-
-L(end2):
-	stx	%g2,[%fp+72]
-	ld	[%i1],%f9
-	add	%i1,4,%i1		C s1_ptr++
-	ldd	[%fp+80],%f0
-	fxtod	%f8,%f2
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	fmuld	%f2,%f4,%f4
-	ld	[%i1],%f9
-	fmuld	%f2,%f6,%f6
-	ldd	[%fp+72],%f0
-	fdtox	%f4,%f4
-	fdtox	%f6,%f6
-	std	%f4,[%fp-24]
-	fxtod	%f8,%f2
-	std	%f6,[%fp-16]
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	fmuld	%f2,%f4,%f4
-	fmuld	%f2,%f6,%f6
-	fdtox	%f4,%f4
-	fdtox	%f6,%f6
-	std	%f4,[%fp-40]
-	std	%f6,[%fp-32]
-	ldx	[%fp-24],%g2		C p16
-	ldx	[%fp-16],%g1		C p0
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	stw	%l0,[%i0-4]
-	ldx	[%fp-40],%g2		C p16
-	ldx	[%fp-32],%g1		C p0
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	stw	%l0,[%i0-4]
-	ret
-	restore	%g0,%g0,%o0
-
-L(end3):
-	stx	%g2,[%fp+80]
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	fmuld	%f2,%f4,%f4
-	ld	[%i1],%f9
-	fmuld	%f2,%f6,%f6
-	add	%i1,4,%i1		C s1_ptr++
-	ldd	[%fp+72],%f0
-	fdtox	%f4,%f4
-	fdtox	%f6,%f6
-	std	%f4,[%fp-24]
-	fxtod	%f8,%f2
-	std	%f6,[%fp-16]
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	fmuld	%f2,%f4,%f4
-	ld	[%i1],%f9
-	fmuld	%f2,%f6,%f6
-	ldd	[%fp+80],%f0
-	fdtox	%f4,%f4
-	fdtox	%f6,%f6
-	std	%f4,[%fp-40]
-	fxtod	%f8,%f2
-	std	%f6,[%fp-32]
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	ldx	[%fp-24],%g2		C p16
-	ldx	[%fp-16],%g1		C p0
-	fmuld	%f2,%f4,%f4
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	fmuld	%f2,%f6,%f6
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	fdtox	%f4,%f4
-	stw	%l0,[%i0-4]
-	fdtox	%f6,%f6
-	std	%f4,[%fp-24]
-	std	%f6,[%fp-16]
-	ldx	[%fp-40],%g2		C p16
-	ldx	[%fp-32],%g1		C p0
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	stw	%l0,[%i0-4]
-	ldx	[%fp-24],%g2		C p16
-	ldx	[%fp-16],%g1		C p0
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	stw	%l0,[%i0-4]
-	ret
-	restore	%g0,%g0,%o0
-
-L(end4):
-	stx	%g2,[%fp+72]
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	fmuld	%f2,%f4,%f4
-	ld	[%i1],%f9
-	fmuld	%f2,%f6,%f6
-	add	%i1,4,%i1		C s1_ptr++
-	ldd	[%fp+80],%f0
-	fdtox	%f4,%f4
-	fdtox	%f6,%f6
-	std	%f4,[%fp-40]
-	fxtod	%f8,%f2
-	std	%f6,[%fp-32]
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	ldx	[%fp-24],%g2		C p16
-	ldx	[%fp-16],%g1		C p0
-	fmuld	%f2,%f4,%f4
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	ld	[%i1],%f9
-	fmuld	%f2,%f6,%f6
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	ldd	[%fp+72],%f0
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	fdtox	%f4,%f4
-	stw	%l0,[%i0-4]
-	fdtox	%f6,%f6
-	std	%f4,[%fp-24]
-	fxtod	%f8,%f2
-	std	%f6,[%fp-16]
-	fitod	%f0,%f4
-	fitod	%f1,%f6
-	ldx	[%fp-40],%g2		C p16
-	ldx	[%fp-32],%g1		C p0
-	fmuld	%f2,%f4,%f4
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	fmuld	%f2,%f6,%f6
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	fdtox	%f4,%f4
-	stw	%l0,[%i0-4]
-	fdtox	%f6,%f6
-	std	%f4,[%fp-40]
-	std	%f6,[%fp-32]
-	ldx	[%fp-24],%g2		C p16
-	ldx	[%fp-16],%g1		C p0
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	stw	%l0,[%i0-4]
-	ldx	[%fp-40],%g2		C p16
-	ldx	[%fp-32],%g1		C p0
-	sllx	%g2,16,%g2		C align p16
-	add	%i0,8,%i0		C res_ptr++
-	add	%g2,%g1,%g1		C add p16 to p0 (ADD1)
-	srlx	%g1,32,%l0
-	stw	%g1,[%i0-8]
-	stw	%l0,[%i0-4]
-	ret
-	restore	%g0,%g0,%o0
 EPILOGUE(mpn_sqr_diagonal)
