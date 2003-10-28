49d6371921b735373e0b0198edec7eda58293223
Kevin Ryde <user42@zip.com.au>
2003-10-28 23:21:09 +0100

    * mpfr/*: Update to mpfr-2-0-2-branch 2003-10-29.

--- mpfr/tests/thyperbolic.c
+++ mpfr/tests/thyperbolic.c
@@ -1,23 +1,23 @@
 /* Test file for hyperbolic function : mpfr_cosh, mpfr_sinh, mpfr_tanh, mpfr_acosh, mpfr_asinh, mpfr_atanh.
- 
+
 Copyright 2001, 2002, 2003 Free Software Foundation, Inc.
- 
+
 This file is part of the MPFR Library.
- 
+
 The MPFR Library is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published
 by
 the Free Software Foundation; either version 2.1 of the License, or (at youroption) any later version.
- 
+
 The MPFR Library is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITYor FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 License for more details.
- 
+
 You should have received a copy of the GNU Lesser General Public License
 along with the MPFR Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
-                   
+
 #include <stdio.h>
 #include "gmp.h"
 #include "gmp-impl.h"
@@ -30,6 +30,7 @@ check_NAN (void)
 {
   mpfr_t t, ch,sh,th,ach,ash,ath;
   int tester;
+  int fail = 0;
 
   mpfr_init2(t,200);
   mpfr_init2(ch,200);
@@ -47,14 +48,8 @@ check_NAN (void)
   if (!MPFR_IS_NAN(ch) || tester!=0)
     {
       printf("cosh NAN \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******sinh********/
@@ -63,14 +58,8 @@ check_NAN (void)
   if (!MPFR_IS_NAN(sh) || tester!=0)
     {
       printf("sinh NAN \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******tanh********/
@@ -79,14 +68,8 @@ check_NAN (void)
   if (!MPFR_IS_NAN(th) || tester!=0)
     {
       printf("tanh NAN \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******acosh********/
@@ -95,7 +78,8 @@ check_NAN (void)
   if (!MPFR_IS_NAN(ach) || tester!=0)
     {
       printf("acosh NAN \n");
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******asinh********/
@@ -104,14 +88,8 @@ check_NAN (void)
   if (!MPFR_IS_NAN(ash) || tester!=0)
     {
       printf("asinh NAN \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******atanh********/
@@ -120,16 +98,11 @@ check_NAN (void)
   if (!MPFR_IS_NAN(ath) || tester!=0)
     {
       printf("atanh NAN \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
+ clean_up:
   mpfr_clear(t);
   mpfr_clear(ch);
   mpfr_clear(sh);
@@ -138,7 +111,7 @@ check_NAN (void)
   mpfr_clear(ash);
   mpfr_clear(ath);
 
-  return(0);
+  return fail;
 }
 
 static int
@@ -146,6 +119,7 @@ check_zero (void)
 {
   mpfr_t t, ch,sh,th,ach,ash,ath;
   int tester;
+  int fail = 0;
 
   mpfr_init2(t,200);
   mpfr_init2(ch,200);
@@ -163,14 +137,8 @@ check_zero (void)
   if (mpfr_cmp_ui(ch, 1) || tester)
     {
       printf("cosh(0) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******sinh********/
@@ -179,14 +147,8 @@ check_zero (void)
   if (!MPFR_IS_ZERO(sh) || tester)
     {
       printf("sinh(0) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******tanh********/
@@ -195,14 +157,8 @@ check_zero (void)
   if (!MPFR_IS_ZERO(th) || tester)
     {
       printf("tanh(0) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******acosh********/
@@ -211,14 +167,8 @@ check_zero (void)
   if (!MPFR_IS_NAN(ach) || tester)
     {
       printf("acosh(0) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******asinh********/
@@ -227,14 +177,8 @@ check_zero (void)
   if (!MPFR_IS_ZERO(ash) || tester)
     {
       printf("asinh(0) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******atanh********/
@@ -243,16 +187,11 @@ check_zero (void)
   if (!MPFR_IS_ZERO(ath) || tester)
     {
       printf("atanh(0) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
+ clean_up:
   mpfr_clear(t);
   mpfr_clear(ch);
   mpfr_clear(sh);
@@ -261,7 +200,7 @@ check_zero (void)
   mpfr_clear(ash);
   mpfr_clear(ath);
 
-  return(0);
+  return fail;
 }
 
 static int
@@ -269,6 +208,7 @@ check_INF (void)
 {
   mpfr_t t, ch,sh,th,ach,ash,ath;
   int tester;
+  int fail = 0;
 
   mpfr_init2(t,200);
   mpfr_init2(ch,200);
@@ -290,14 +230,8 @@ check_INF (void)
   if (!MPFR_IS_INF(ch) || MPFR_SIGN(ch) < 0 || tester!=0)
     {
       printf("cosh(INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******sinh********/
@@ -306,14 +240,8 @@ check_INF (void)
   if (!MPFR_IS_INF(sh) || MPFR_SIGN(sh) < 0  || tester!=0)
     {
       printf("sinh(INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******tanh********/
@@ -322,14 +250,8 @@ check_INF (void)
   if (mpfr_cmp_ui(th,1) != 0 || tester!=0)
     {
       printf("tanh(INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******acosh********/
@@ -338,14 +260,8 @@ check_INF (void)
   if (!MPFR_IS_INF(ach) || MPFR_SIGN(ach) < 0  || tester!=0)
     {
       printf("acosh(INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******asinh********/
@@ -354,30 +270,18 @@ check_INF (void)
   if (!MPFR_IS_INF(ash) || MPFR_SIGN(ash) < 0  || tester!=0)
     {
       printf("asinh(INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******atanh********/
 
-  tester=mpfr_atanh(ath,t,GMP_RNDD);  
+  tester=mpfr_atanh(ath,t,GMP_RNDD);
   if (!MPFR_IS_INF(ath) || tester != 0)
     {
       printf("atanh(INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   MPFR_CHANGE_SIGN(t);
@@ -388,14 +292,8 @@ check_INF (void)
   if (!MPFR_IS_INF(ch) || MPFR_SIGN(ch) < 0  || tester!=0)
     {
       printf("cosh(-INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******sinh********/
@@ -404,14 +302,8 @@ check_INF (void)
   if (!MPFR_IS_INF(sh)  || MPFR_SIGN(sh) > 0 || tester!=0)
     {
       printf("sinh(-INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******tanh********/
@@ -420,30 +312,18 @@ check_INF (void)
   if (!mpfr_cmp_ui(th,-1) || tester!=0)
     {
       printf("tanh(-INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******acosh********/
 
   tester=mpfr_acosh(ach,t,GMP_RNDD);
-  if (!MPFR_IS_INF(ach) || MPFR_SIGN(ach) < 0  || tester!=0)
+  if (!MPFR_IS_NAN(ach) || tester!=0)
     {
       printf("acosh(-INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******asinh********/
@@ -452,14 +332,8 @@ check_INF (void)
   if (!MPFR_IS_INF(ash) || MPFR_SIGN(ash) > 0  || tester!=0)
     {
       printf("asinh(-INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
   /******atanh********/
@@ -468,16 +342,11 @@ check_INF (void)
   if (!MPFR_IS_INF(ath) || MPFR_SIGN(ath) > 0 || tester != 0)
     {
       printf("atanh(-INF) \n");
-      mpfr_clear(t);
-      mpfr_clear(ch);
-      mpfr_clear(sh);
-      mpfr_clear(th);
-      mpfr_clear(ach);
-      mpfr_clear(ash);
-      mpfr_clear(ath);
-      return(1);
+      fail = 1;
+      goto clean_up;
     }
 
+ clean_up:
   mpfr_clear(t);
   mpfr_clear(ch);
   mpfr_clear(sh);
@@ -486,7 +355,7 @@ check_INF (void)
   mpfr_clear(ash);
   mpfr_clear(ath);
 
-  return(0);
+  return fail;
 }
 
 int
@@ -494,15 +363,24 @@ main(void)
 {
   tests_start_mpfr ();
 
-  if (check_zero())
-    printf("Error in evaluation at 0\n");
+  if (check_zero ())
+    {
+      printf ("Error in evaluation at 0\n");
+      exit (1);
+    }
 
-  if (check_INF())
-    printf("Error in evaluation of INF\n");
+  if (check_INF ())
+    {
+      printf ("Error in evaluation of INF\n");
+      exit (1);
+    }
 
-  if (check_NAN())
-    printf("Error in evaluation of NAN\n");
+  if (check_NAN ())
+    {
+      printf ("Error in evaluation of NAN\n");
+      exit (1);
+    }
 
   tests_end_mpfr ();
-  return(0);
+  return 0;
 }
