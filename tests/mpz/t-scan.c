9ab2e2ce99e00bac8df90819bf9a7d840ac15adc
Kevin Ryde <user42@zip.com.au>
2003-12-13 00:50:00 +0100

    * tests/mpz/t-scan.c (check_ref): Remove unused variable "isigned".

--- tests/mpz/t-scan.c
+++ tests/mpz/t-scan.c
@@ -1,6 +1,6 @@
 /* Tests of mpz_scan0 and mpz_scan1.
 
-Copyright 2000, 2001, 2002 Free Software Foundation, Inc.
+Copyright 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -65,7 +65,6 @@ check_ref (void)
   mpz_t          z;
   int            test, neg, sought, oindex, o;
   mp_size_t      size, isize;
-  long           isigned;
   unsigned long  start, got, want;
 
   mpz_init (z);
