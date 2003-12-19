caf4bca6206a9377bdfa53bd99c9d13cd5ffec26
tege <tege@gmplib.org>
2003-12-19 21:28:03 +0100

    Make ONE_LIMB case actually work for nails.

--- mpn/generic/get_d.c
+++ mpn/generic/get_d.c
@@ -42,7 +42,7 @@ union ieee_double_extract {
 #endif
 
 /* To force use of the generic C code for testing, put
-   "#define _GMP_IEEE_FLOATS 0" at this point.  */
+   "#define _GMP_IEEE_FLOATS 0" at this point.	*/
 
 
 
@@ -51,7 +51,7 @@ union ieee_double_extract {
    wrong if that addition overflows.
 
    The workaround here avoids this bug by ensuring n is not a literal
-   constant.  Note that this is alpha specific.  The offending transformation
+   constant.  Note that this is alpha specific.	 The offending transformation
    is/was in alpha.c alpha_emit_conditional_branch() under "We want to use
    cmpcc/bcc".
 
@@ -59,15 +59,15 @@ union ieee_double_extract {
    alphaev5-cray-unicosmk2.0.6.X, and has the same solution.  Don't know why
    or how.  */
 
-#if HAVE_HOST_CPU_FAMILY_alpha                          \
-  && ((defined (__GNUC__) && ! __GMP_GNUC_PREREQ(3,4))  \
+#if HAVE_HOST_CPU_FAMILY_alpha				\
+  && ((defined (__GNUC__) && ! __GMP_GNUC_PREREQ(3,4))	\
       || defined (_CRAY))
 static volatile const long CONST_1024 = 1024;
 static volatile const long CONST_NEG_1023 = -1023;
 static volatile const long CONST_NEG_1022_SUB_53 = -1022 - 53;
 #else
-#define CONST_1024            (1024)
-#define CONST_NEG_1023        (-1023)
+#define CONST_1024	      (1024)
+#define CONST_NEG_1023	      (-1023)
 #define CONST_NEG_1022_SUB_53 (-1022 - 53)
 #endif
 
@@ -83,7 +83,7 @@ static volatile const long CONST_NEG_1022_SUB_53 = -1022 - 53;
    In the past conversions had attempted (imperfectly) to let the hardware
    float rounding mode take effect, but that gets tricky since multiple
    roundings need to be avoided, or taken into account, and denorms mean the
-   effective precision of the mantissa is not constant.  (For reference,
+   effective precision of the mantissa is not constant.	 (For reference,
    mpz_get_d on IEEE systems was ok, except it operated on the absolute
    value.  mpf_get_d and mpq_get_d suffered from multiple roundings and from
    not always using enough bits to get the rounding right.)
@@ -104,7 +104,7 @@ static volatile const long CONST_NEG_1022_SUB_53 = -1022 - 53;
 
 
    Hardware traps for overflow to infinity, underflow to zero, or
-   unsupported denorms may or may not be taken.  The IEEE code works bitwise
+   unsupported denorms may or may not be taken.	 The IEEE code works bitwise
    and so probably won't trigger them, the generic code works by float
    operations and so probably will.  This difference might be thought less
    than ideal, but again its felt straightforward code is better than trying
@@ -122,13 +122,13 @@ mpn_get_d (mp_srcptr ptr, mp_size_t size, mp_size_t sign, long exp)
     return 0.0;
 
   /* Adjust exp to a radix point just above {ptr,size}, guarding against
-     overflow.  After this exp can of course be reduced to anywhere within
+     overflow.	After this exp can of course be reduced to anywhere within
      the {ptr,size} region without underflow.  */
   if (UNLIKELY ((unsigned long) (GMP_NUMB_BITS * size)
-                > (unsigned long) (LONG_MAX - exp)))
+		> (unsigned long) (LONG_MAX - exp)))
     {
       if (_GMP_IEEE_FLOATS)
-        goto ieee_infinity;
+	goto ieee_infinity;
 
       /* generic */
       exp = LONG_MAX;
@@ -143,11 +143,11 @@ mpn_get_d (mp_srcptr ptr, mp_size_t size, mp_size_t sign, long exp)
 
   if (_GMP_IEEE_FLOATS && (ONE_LIMB || TWO_LIMBS))
     {
-      union ieee_double_extract  u;
-      mp_limb_t  m0, m1, m2, rmask;
-      int        lshift, rshift;
+      union ieee_double_extract	 u;
+      mp_limb_t	 m0, m1, m2, rmask;
+      int	 lshift, rshift;
 
-      m0 = ptr[size-1];                     /* high limb */
+      m0 = ptr[size-1];			    /* high limb */
       m1 = (size >= 2 ? ptr[size-2] : 0);   /* second highest limb */
       count_leading_zeros (lshift, m0);
 
@@ -155,82 +155,83 @@ mpn_get_d (mp_srcptr ptr, mp_size_t size, mp_size_t sign, long exp)
       exp -= (lshift - GMP_NAIL_BITS) + 1;
 
       if (ONE_LIMB)
-        {
-          /* lshift to have high of m0 non-zero, and collapse nails */
-          rshift = GMP_NUMB_BITS - lshift;
-          rmask = (GMP_NAIL_BITS == 0 && lshift == 0 ? 0 : MP_LIMB_T_MAX);
-          m0 = (m0 << lshift) | ((m1 >> rshift) & rmask);
+	{
+	  /* lshift to have high of m0 non-zero, and collapse nails */
+	  rshift = GMP_LIMB_BITS - lshift;
+	  m1 <<= GMP_NAIL_BITS;
+	  rmask = GMP_NAIL_BITS == 0 && lshift == 0 ? 0 : MP_LIMB_T_MAX;
+	  m0 = (m0 << lshift) | ((m1 >> rshift) & rmask);
 
-          /* rshift back to have bit 53 of m0 the high non-zero */
-          m0 >>= 11;
-        }
+	  /* rshift back to have bit 53 of m0 the high non-zero */
+	  m0 >>= 11;
+	}
       else /* TWO_LIMBS */
-        {
-          m2 = (size >= 3 ? ptr[size-3] : 0);  /* third highest limb */
+	{
+	  m2 = (size >= 3 ? ptr[size-3] : 0);  /* third highest limb */
 
-          /* collapse nails from m1 and m2 */
+	  /* collapse nails from m1 and m2 */
 #if GMP_NAIL_BITS != 0
-          m1 = (m1 << GMP_NAIL_BITS) | (m2 >> (GMP_NUMB_BITS-GMP_NAIL_BITS));
-          m2 <<= 2*GMP_NAIL_BITS;
+	  m1 = (m1 << GMP_NAIL_BITS) | (m2 >> (GMP_NUMB_BITS-GMP_NAIL_BITS));
+	  m2 <<= 2*GMP_NAIL_BITS;
 #endif
 
-          /* lshift to have high of m0:m1 non-zero, collapse nails from m0 */
-          rshift = GMP_LIMB_BITS - lshift;
-          rmask = (GMP_NAIL_BITS == 0 && lshift == 0 ? 0 : MP_LIMB_T_MAX);
-          m0 = (m0 << lshift) | ((m1 >> rshift) & rmask);
-          m1 = (m1 << lshift) | ((m2 >> rshift) & rmask);
+	  /* lshift to have high of m0:m1 non-zero, collapse nails from m0 */
+	  rshift = GMP_LIMB_BITS - lshift;
+	  rmask = (GMP_NAIL_BITS == 0 && lshift == 0 ? 0 : MP_LIMB_T_MAX);
+	  m0 = (m0 << lshift) | ((m1 >> rshift) & rmask);
+	  m1 = (m1 << lshift) | ((m2 >> rshift) & rmask);
 
-          /* rshift back to have bit 53 of m0:m1 the high non-zero */
-          m1 = (m1 >> 11) | (m0 << (GMP_LIMB_BITS-11));
-          m0 >>= 11;
-        }
+	  /* rshift back to have bit 53 of m0:m1 the high non-zero */
+	  m1 = (m1 >> 11) | (m0 << (GMP_LIMB_BITS-11));
+	  m0 >>= 11;
+	}
 
       if (UNLIKELY (exp >= CONST_1024))
-        {
-          /* overflow, return infinity */
-        ieee_infinity:
-          m0 = 0;
-          m1 = 0;
-          exp = 1024;
-        }
+	{
+	  /* overflow, return infinity */
+	ieee_infinity:
+	  m0 = 0;
+	  m1 = 0;
+	  exp = 1024;
+	}
       else if (UNLIKELY (exp <= CONST_NEG_1023))
-        {
-          if (LIKELY (exp <= CONST_NEG_1022_SUB_53))
-            return 0.0;  /* denorm underflows to zero */
+	{
+	  if (LIKELY (exp <= CONST_NEG_1022_SUB_53))
+	    return 0.0;	 /* denorm underflows to zero */
 
-          rshift = -1022 - exp;
-          ASSERT (rshift > 0 && rshift < 53);
-          if (ONE_LIMB)
-            {
-              m0 >>= rshift;
-            }
-          else /* TWO_LIMBS */
-            {
-              if (rshift >= 32)
-                {
-                  m1 = m0;
-                  m0 = 0;
-                  rshift -= 32;
-                }
-              lshift = GMP_LIMB_BITS - rshift;
-              m1 = (m1 >> rshift) | (rshift == 0 ? 0 : m0 << lshift);
-              m0 >>= rshift;
-            }
-          exp = -1023;
-        }
+	  rshift = -1022 - exp;
+	  ASSERT (rshift > 0 && rshift < 53);
+	  if (ONE_LIMB)
+	    {
+	      m0 >>= rshift;
+	    }
+	  else /* TWO_LIMBS */
+	    {
+	      if (rshift >= 32)
+		{
+		  m1 = m0;
+		  m0 = 0;
+		  rshift -= 32;
+		}
+	      lshift = GMP_LIMB_BITS - rshift;
+	      m1 = (m1 >> rshift) | (rshift == 0 ? 0 : m0 << lshift);
+	      m0 >>= rshift;
+	    }
+	  exp = -1023;
+	}
 
       if (ONE_LIMB)
-        {
-#if GMP_LIMB_BITS > 32  /* avoid compiler warning about big shift */
-          u.s.manh = m0 >> 32;
+	{
+#if GMP_LIMB_BITS > 32	/* avoid compiler warning about big shift */
+	  u.s.manh = m0 >> 32;
 #endif
-          u.s.manl = m0;
-        }
+	  u.s.manl = m0;
+	}
       else /* TWO_LIMBS */
-        {
-          u.s.manh = m0;
-          u.s.manl = m1;
-        }
+	{
+	  u.s.manh = m0;
+	  u.s.manl = m1;
+	}
 
       u.s.exp = exp + 1023;
       u.s.sig = (sign < 0);
@@ -240,13 +241,13 @@ mpn_get_d (mp_srcptr ptr, mp_size_t size, mp_size_t sign, long exp)
     {
       /* Non-IEEE or strange limb size, do something generic. */
 
-      mp_size_t      i;
-      mp_limb_t      limb, bit;
-      int            shift;
-      double         base, factor, prev_factor, d, new_d, diff;
+      mp_size_t	     i;
+      mp_limb_t	     limb, bit;
+      int	     shift;
+      double	     base, factor, prev_factor, d, new_d, diff;
 
       /* "limb" is "ptr[i]" the limb being examined, "bit" is a mask for the
-         bit being examined, initially the highest non-zero bit.  */
+	 bit being examined, initially the highest non-zero bit.  */
       i = size-1;
       limb = ptr[i];
       count_leading_zeros (shift, limb);
@@ -256,64 +257,64 @@ mpn_get_d (mp_srcptr ptr, mp_size_t size, mp_size_t sign, long exp)
       exp -= (shift - GMP_NAIL_BITS) + 1;
 
       /* Power up "factor" to 2^exp, being the value of the "bit" in "limb"
-         being examined.  */
+	 being examined.  */
       base = (exp >= 0 ? 2.0 : 0.5);
       exp = ABS (exp);
       factor = 1.0;
       for (;;)
-        {
-          if (exp & 1)
-            {
-              prev_factor = factor;
-              factor *= base;
-              FORCE_DOUBLE (factor);
-              if (factor == 0.0)
-                return 0.0;     /* underflow */
-              if (factor == prev_factor)
-                {
-                  d = factor;     /* overflow, apparent infinity */
-                  goto generic_done;
-                }
-            }
-          exp >>= 1;
-          if (exp == 0)
-            break;
-          base *= base;
-        }
+	{
+	  if (exp & 1)
+	    {
+	      prev_factor = factor;
+	      factor *= base;
+	      FORCE_DOUBLE (factor);
+	      if (factor == 0.0)
+		return 0.0;	/* underflow */
+	      if (factor == prev_factor)
+		{
+		  d = factor;	  /* overflow, apparent infinity */
+		  goto generic_done;
+		}
+	    }
+	  exp >>= 1;
+	  if (exp == 0)
+	    break;
+	  base *= base;
+	}
 
       /* Add a "factor" for each non-zero bit, working from high to low.
-         Stop if any rounding occurs, hence implementing a truncation.
+	 Stop if any rounding occurs, hence implementing a truncation.
 
-         Note no attention is paid to DBL_MANT_DIG, since the effective
-         number of bits in the mantissa isn't constant when in denorm range.
-         We also encountered an ARM system with an apparently broken
-         emulator where DBL_MANT_DIG claimed 53 bits but only 32 worked.  */
+	 Note no attention is paid to DBL_MANT_DIG, since the effective
+	 number of bits in the mantissa isn't constant when in denorm range.
+	 We also encountered an ARM system with an apparently broken
+	 emulator where DBL_MANT_DIG claimed 53 bits but only 32 worked.  */
 
       d = factor;  /* high bit */
       for (;;)
-        {
-          factor *= 0.5;  /* next bit */
-          bit >>= 1;
-          if (bit == 0)
-            {
-              /* next limb, if any */
-              i--;
-              if (i < 0)
-                break;
-              limb = ptr[i];
-              bit = GMP_NUMB_HIGHBIT;
-            }
+	{
+	  factor *= 0.5;  /* next bit */
+	  bit >>= 1;
+	  if (bit == 0)
+	    {
+	      /* next limb, if any */
+	      i--;
+	      if (i < 0)
+		break;
+	      limb = ptr[i];
+	      bit = GMP_NUMB_HIGHBIT;
+	    }
 
-          if (bit & limb)
-            {
-              new_d = d + factor;
-              FORCE_DOUBLE (new_d);
-              diff = new_d - d;
-              if (diff != factor)
-                break;   /* rounding occured, stop now */
-              d = new_d;
-            }
-        }
+	  if (bit & limb)
+	    {
+	      new_d = d + factor;
+	      FORCE_DOUBLE (new_d);
+	      diff = new_d - d;
+	      if (diff != factor)
+		break;	 /* rounding occured, stop now */
+	      d = new_d;
+	    }
+	}
 
     generic_done:
       return (sign >= 0 ? d : -d);
