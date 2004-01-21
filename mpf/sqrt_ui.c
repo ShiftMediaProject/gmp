210e157a62ef2c325e3e7a4a86fc7f68753df1d5
Kevin Ryde <user42@zip.com.au>
2004-01-22 00:19:57 +0100

    * mpf/sqrt_ui.c: Change rsize calculation to get prec limbs result, 	previously got prec+1.

--- mpf/sqrt_ui.c
+++ mpf/sqrt_ui.c
@@ -1,6 +1,6 @@
 /* mpf_sqrt_ui -- Compute the square root of an unsigned integer.
 
-Copyright 1993, 1994, 1996, 2000, 2001 Free Software Foundation, Inc.
+Copyright 1993, 1994, 1996, 2000, 2001, 2004 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -23,6 +23,30 @@ MA 02111-1307, USA. */
 #include "gmp.h"
 #include "gmp-impl.h"
 
+
+/* As usual the aim is to produce PREC(r) limbs of result with the high limb
+   non-zero.  That high limb will end up floor(sqrt(u)), and limbs below are
+   produced by padding the input with zeros, two for each desired result
+   limb, being 2*(prec-1) for a total 2*prec-1 limbs passed to mpn_sqrtrem.
+   The way mpn_sqrtrem calculates floor(sqrt(x)) ensures the root is correct
+   to the intended accuracy, ie. truncated to prec limbs.
+
+   u==0 is a special case so the rest of the code can assume the result is
+   non-zero (ie. will have a non-zero high limb on the result).
+
+   Not done:
+
+   No attempt is made to identify perfect squares.  It's considered this can
+   be left to an application if it might occur with any frequency.  As it
+   stands, mpn_sqrtrem does it's normal amount of work on a perfect square
+   followed by zero limbs, though of course only an mpn_sqrtrem1 would be
+   actually needed.  We also end up leaving our mpf result with lots of low
+   trailing zeros, slowing down subsequent operations.
+
+   We're not aware of any optimizations that can be made using the fact the
+   input has lots of trailing zeros (apart from the perfect square
+   case).  */
+
 void
 mpf_sqrt_ui (mpf_ptr r, unsigned long int u)
 {
@@ -31,7 +55,7 @@ mpf_sqrt_ui (mpf_ptr r, unsigned long int u)
   mp_size_t prec;
   TMP_DECL (marker);
 
-  if (u == 0)
+  if (UNLIKELY (u == 0))
     {
       r->_mp_size = 0;
       r->_mp_exp = 0;
@@ -41,7 +65,7 @@ mpf_sqrt_ui (mpf_ptr r, unsigned long int u)
   TMP_MARK (marker);
 
   prec = r->_mp_prec;
-  rsize = 2 * prec + 1;
+  rsize = 2 * prec - 1;
 
   tp = (mp_ptr) TMP_ALLOC (rsize * BYTES_PER_MP_LIMB);
 
@@ -50,7 +74,7 @@ mpf_sqrt_ui (mpf_ptr r, unsigned long int u)
 
   mpn_sqrtrem (r->_mp_d, NULL, tp, rsize);
 
-  r->_mp_size = prec + 1;
+  r->_mp_size = prec;
   r->_mp_exp = 1;
   TMP_FREE (marker);
 }
