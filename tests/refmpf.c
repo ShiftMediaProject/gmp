e3a7832364a99dcb67359547ddb569608cae8d52
Kevin Ryde <user42@zip.com.au>
2004-01-24 00:18:32 +0100

    Includes for: 	* tests/refmpf.c, tests/tests.h (refmpf_add_ulp, 	refmpf_set_prec_limbs): New functions.

--- tests/refmpf.c
+++ tests/refmpf.c
@@ -19,6 +19,9 @@ along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "gmp.h"
 #include "gmp-impl.h"
 #include "tests.h"
