f04895f273d5c415b83e92a9c3da96a80dd04301
Kevin Ryde <user42@zip.com.au>
2004-02-15 22:24:53 +0100

    * mpn/generic/addmul_1.c, mpn/generic/submul_1.c [nails==1]: Add code.

--- mpn/generic/addmul_1.c
+++ mpn/generic/addmul_1.c
@@ -3,7 +3,8 @@
    pointed to by RP.  Return the most significant limb of the product,
    adjusted for carry-out from the addition.
 
-Copyright 1992, 1993, 1994, 1996, 2000, 2002 Free Software Foundation, Inc.
+Copyright 1992, 1993, 1994, 1996, 2000, 2002, 2004 Free Software Foundation,
+Inc.
 
 This file is part of the GNU MP Library.
 
@@ -59,7 +60,39 @@ mpn_addmul_1 (mp_ptr rp, mp_srcptr up, mp_size_t n, mp_limb_t vl)
 #endif
 
 #if GMP_NAIL_BITS == 1
-you lose
+
+mp_limb_t
+mpn_addmul_1 (mp_ptr rp, mp_srcptr up, mp_size_t n, mp_limb_t vl)
+{
+  mp_limb_t shifted_vl, ul, rl, lpl, hpl, prev_hpl, cl, xl, c1, c2, c3;
+
+  ASSERT (n >= 1);
+  ASSERT (MPN_SAME_OR_SEPARATE_P (rp, up, n));
+  ASSERT_MPN (rp, n);
+  ASSERT_MPN (up, n);
+  ASSERT_LIMB (vl);
+
+  shifted_vl = vl << GMP_NAIL_BITS;
+  cl = 0;
+  prev_hpl = 0;
+  do
+    {
+      ul = *up++;
+      rl = *rp;
+      umul_ppmm (hpl, lpl, ul, shifted_vl);
+      lpl >>= GMP_NAIL_BITS;
+      ADDC_LIMB (c1, xl, prev_hpl, lpl);
+      ADDC_LIMB (c2, xl, xl, rl);
+      ADDC_LIMB (c3, xl, xl, cl);
+      cl = c1 + c2 + c3;
+      *rp++ = xl;
+      prev_hpl = hpl;
+    }
+  while (--n != 0);
+
+  return prev_hpl + cl;
+}
+
 #endif
 
 #if GMP_NAIL_BITS >= 2
