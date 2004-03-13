1b347cf79b4f4a5066df2e92e832cb222c882537
Kevin Ryde <user42@zip.com.au>
2004-03-13 23:55:09 +0100

    * tests/mpf/t-set_q.c, tests/mpf/t-ui_div.c (check_various): Amend 	bogus 99/4 test. 	* tests/mpf/t-ui_div.c (check_rand): Exercise r==v overlap.

--- tests/mpf/t-ui_div.c
+++ tests/mpf/t-ui_div.c
@@ -27,13 +27,11 @@ MA 02111-1307, USA. */
 
 
 void
-check_one (mpf_ptr got, unsigned long u, mpf_srcptr v)
+check_one (const char *desc, mpf_ptr got, unsigned long u, mpf_srcptr v)
 {
   mpf_t      uf;
   mp_limb_t  ulimbs[2];
   mp_size_t  usize;
-
-  mpf_ui_div (got, u, v);
 
   ulimbs[0] = u & GMP_NUMB_MASK;
   usize = (u != 0);
@@ -51,6 +49,7 @@ check_one (mpf_ptr got, unsigned long u, mpf_srcptr v)
       mp_trace_base = -16;
       printf    ("  u 0x%lX  (%lu)\n", u, u);
       mpf_trace ("  v", v);
+      printf    ("  %s\n", desc);
       abort ();
     }
 }
@@ -90,7 +89,21 @@ check_rand (void)
       if (gmp_urandomb_ui (rands, 1L))
         mpf_neg (v, v);
 
-      check_one (got, u, v);
+      if ((i % 2) == 0)
+        {
+          /* src != dst */
+          mpf_ui_div (got, u, v);
+          check_one ("separate", got, u, v);
+        }
+      else
+        {
+          /* src == dst */
+          prec = refmpf_set_overlap (got, v);
+          mpf_ui_div (got, u, got);
+          check_one ("overlap src==dst", got, u, v);
+
+          mpf_set_prec_raw (got, prec);
+        }
     }
 
   mpf_clear (got);
@@ -105,20 +118,24 @@ check_various (void)
   mpf_init (got);
   mpf_init (v);
 
-  /* 99/4 == 24 */
+  /* 100/4 == 25 */
   mpf_set_prec (got, 20L);
   mpf_set_ui (v, 4L);
-  mpf_ui_div (got, 99L, v);
+  mpf_ui_div (got, 100L, v);
   MPF_CHECK_FORMAT (got);
-  ASSERT_ALWAYS (mpf_cmp_ui (got, 24L));
+  ASSERT_ALWAYS (mpf_cmp_ui (got, 25L) == 0);
 
-  /* 1/(2^n+1), a case where truncating the divisor would be wrong */
-  mpf_set_prec (got, 500L);
-  mpf_set_prec (v, 900L);
-  mpf_set_ui (v, 1L);
-  mpf_mul_2exp (v, v, 800L);
-  mpf_add_ui (v, v, 1L);
-  check_one (got, 1L, v);
+  {
+    /* 1/(2^n+1), a case where truncating the divisor would be wrong */
+    unsigned long  u = 1L;
+    mpf_set_prec (got, 500L);
+    mpf_set_prec (v, 900L);
+    mpf_set_ui (v, 1L);
+    mpf_mul_2exp (v, v, 800L);
+    mpf_add_ui (v, v, 1L);
+    mpf_ui_div (got, u, v);
+    check_one ("1/2^n+1, separate", got, u, v);
+  }
 
   mpf_clear (got);
   mpf_clear (v);
@@ -129,8 +146,8 @@ main (void)
 {
   tests_start ();
 
-  check_rand ();
   check_various ();
+  check_rand ();
 
   tests_end ();
   exit (0);
