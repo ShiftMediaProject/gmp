2e5f6be1d4f16f403c56ef9e2cc57f9319849acd
Kevin Ryde <user42@zip.com.au>
2004-04-04 01:38:41 +0200

    * gmp-impl.h (BSWAP_LIMB) [amd64]: New macro.

--- gmp-impl.h
+++ gmp-impl.h
@@ -2509,6 +2509,13 @@ __GMP_DECLSPEC extern const unsigned char  modlimb_invert_table[128];
   } while (0)
 #endif
 
+#if defined (__amd64__) && BITS_PER_MP_LIMB == 64
+#define BSWAP_LIMB(dst, src)						\
+  do {									\
+    __asm__ ("bswap %q0" : "=r" (dst) : "0" (src));			\
+  } while (0)
+#endif
+
 #if defined (__GNUC__) && ! defined (NO_ASM)    \
   && defined (__ia64) && BITS_PER_MP_LIMB == 64
 #define BSWAP_LIMB(dst, src)						\
