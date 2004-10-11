2a7dae1e619f30799ab6909d70471c0aa16f51fe
tege <tege@gmplib.org>
2004-10-11 06:58:41 +0200

    Add a copyright year.

--- mpn/ia64/popcount.asm
+++ mpn/ia64/popcount.asm
@@ -1,6 +1,6 @@
 dnl  IA-64 mpn_popcount.
 
-dnl  Copyright 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
+dnl  Copyright 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
 
 dnl  This file is part of the GNU MP Library.
 
