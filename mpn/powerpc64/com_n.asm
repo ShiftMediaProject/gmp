17c18121be571653ec70e4b0aba8656a6c0c265f
tege <tege@gmplib.org>
2004-11-08 22:37:10 +0100

    *** empty log message ***

---
+++ mpn/powerpc64/com_n.asm
@@ -0,0 +1,75 @@
+dnl  PowerPC-64 mpn_com_n.
+
+dnl  Copyright 2003, 2004 Free Software Foundation, Inc.
+
+dnl  This file is part of the GNU MP Library.
+
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 2.1 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+dnl  the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+dnl  MA 02111-1307, USA.
+
+include(`../config.m4')
+
+C		cycles/limb
+C POWER3/PPC630:     1?
+C POWER4/PPC970:     1.6
+
+C TODO
+C  * 8-way unrolling brings timing down to about 1.3 cycles/limb.
+
+C INPUT PARAMETERS
+C rp	r3
+C up	r4
+C n	r5
+
+ASM_START()
+PROLOGUE(mpn_com_n)
+
+	rldicl.	r0, r5, 3, 59	C r0 = (r5 & 3) << 3; cr0 = (n == 4t)?
+	cmpldi	cr6, r0, 16	C cr6 = (n cmp 4t + 2)?
+
+	addi	r5, r5, 3	C compute...
+	srdi	r5, r5, 2	C ...branch count
+	mtctr	r5
+
+	add	r4, r4, r0	C offset up
+	add	r3, r3, r0	C offset rp
+
+	beq	cr0, .LL00
+	blt	cr6, .LL01
+	beq	cr6, .LL10
+	b	.LL11
+
+.LL00:	addi	r4, r4, 32
+	addi	r3, r3, 32
+
+	ALIGN(16)
+.Loop:	ld	r6, -32(r4)
+	nor	r6, r6, r6
+	std	r6, -32(r3)
+.LL11:	ld	r6, -24(r4)
+	nor	r6, r6, r6
+	std	r6, -24(r3)
+.LL10:	ld	r6, -16(r4)
+	nor	r6, r6, r6
+	std	r6, -16(r3)
+.LL01:	ld	r6, -8(r4)
+	nor	r6, r6, r6
+	addi	r4, r4, 32
+	std	r6, -8(r3)
+	addi	r3, r3, 32
+	bdnz	.Loop
+
+	blr
+EPILOGUE()
