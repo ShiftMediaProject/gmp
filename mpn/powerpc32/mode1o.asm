ce4a2362de2815bb055f13bf52062d7eb3502f80
tege <tege@gmplib.org>
2005-05-29 00:47:14 +0200

    Rewrite to actually work.

--- mpn/powerpc32/mode1o.asm
+++ mpn/powerpc32/mode1o.asm
@@ -22,10 +22,11 @@ dnl  Suite 330, Boston, MA 02111-1307, USA.
 include(`../config.m4')
 
 
-C      cycles/limb
-C 604e:   6.0
-C 750:    6.0-13.0, depending on divisor
-C 7400:   6.0-13.0, depending on divisor
+C                cycles/limb
+C 604e:		   6.0
+C 750:		   6.0-13.0, depending on divisor
+C 7400:		   6.0-13.0, depending on divisor
+C power5:	   12.0
 
 
 C mp_limb_t mpn_modexact_1_odd (mp_srcptr src, mp_size_t size,
@@ -46,127 +47,66 @@ C the divisor.  So the fastest is for instance divisor==1 (inverse==-1), and
 C the slowest is anything giving a full 32-bits in both, such as
 C divisor==0xDEADBEEF (inverse==0x904B300F).  These establish the stated
 C range above for 750 and 7400.
-C
+
 
 ASM_START()
 PROLOGUE(mpn_modexact_1_odd)
-
-	C r3	src
-	C r4	size
-	C r5	divisor
-
 	li	r6, 0
 
 PROLOGUE(mpn_modexact_1c_odd)
 
-	C r3	src
-	C r4	size
-	C r5	divisor
-	C r6	carry
+	mtctr	r4			C size
 
-	mtctr	r4		C size
-
-ifdef(`PIC',`
-	rlwinm	r7, r5, 1,28,28	C (divisor << 1) & 8
-	rlwinm	r8, r5, 2,28,28	C (divisor << 2) & 8
-
-	xor	r7, r7, r8	C ((divisor << 1) ^ (divisor << 2)) & 8
-	rlwinm	r4, r5, 0,28,31	C divisor low 4 bits, speedup mullw
-
-	xor	r4, r4, r7	C inverse, 4 bits
-
-	mullw	r7, r4, r4	C i*i
-	slwi	r4, r4, 1	C 2*i
-
-	rlwinm	r8, r5, 0,24,31	C divisor low 8 bits, speedup mullw
-
-	mullw	r7, r7, r8	C i*i*d
-
-	C
-
-	sub	r4, r4, r7	C inverse, 8 bits
+ifdef(`PIC_SLOW',`
+C Load from our table with PIC is so slow on Linux and Darwin that we avoid it
+	rlwinm	r7, r5, 1,28,28		C (divisor << 1) & 8
+	rlwinm	r8, r5, 2,28,28		C (divisor << 2) & 8
+	xor	r7, r7, r8		C ((divisor << 1) ^ (divisor << 2)) & 8
+	rlwinm	r4, r5, 0,28,31		C divisor low 4 bits, speedup mullw
+	xor	r4, r4, r7		C inverse, 4 bits
+	mullw	r7, r4, r4		C i*i
+	slwi	r4, r4, 1		C 2*i
+	rlwinm	r8, r5, 0,24,31		C divisor low 8 bits, speedup mullw
+	mullw	r7, r7, r8		C i*i*d
+	sub	r4, r4, r7		C inverse, 8 bits
 ',`
-	dnl  non-PIC
-
-	lis	r7, modlimb_invert_table@ha
-	rlwinm	r4, r5, 31,25,31 C (divisor/2) & 0x7F
-
-	la	r7, modlimb_invert_table@l(r7)
-
-	lbzx	r4, r4,r7	C inverse, 8 bits
+	LEA(	r7, modlimb_invert_table)
+	rlwinm	r4, r5, 31,25,31	C (divisor/2) & 0x7F
+	lbzx	r4, r4,r7		C inverse, 8 bits
 ')
 
-	mullw	r7, r4, r4	C i*i
-	slwi	r4, r4, 1	C 2*i
+	mullw	r7, r4, r4		C i*i
+	slwi	r4, r4, 1		C 2*i
+	mullw	r7, r5, r7		C i*i*d   [i*i is 16 bits, so second operand]
+	sub	r4, r4, r7		C inverse, 16 bits
+	mullw	r7, r4, r4		C i*i
+	slwi	r4, r4, 1		C 2*i
+	mullw	r7, r7, r5		C i*i*d
+	lwz	r0, 0(r3)		C src[0]
+	sub	r4, r4, r7		C inverse, 32 bits
+	subfc	r7, r6, r0		C l = src[0] - carry
 
-	mullw	r7, r5, r7	C i*i*d   [i*i is 16 bits, so second operand]
-
-	C
-
-	sub	r4, r4, r7	C inverse, 16 bits
-
-	mullw	r7, r4, r4	C i*i
-	slwi	r4, r4, 1	C 2*i
-
-	C
-
-	mullw	r7, r7, r5	C i*i*d
-	lwz	r0, 0(r3)	C src[0]
-
-	C
-
-	sub	r4, r4, r7	C inverse, 32 bits
-	ASSERT(eq,`
-	mullw	r10, r4, r5
-	cmpwi	cr0, r10, 1
-	')
-	subfc	r7, r6, r0	C l = src[0] - carry
-
-	mullw	r7, r7, r4	C q = l * inverse
+	mullw	r7, r7, r4		C q = l * inverse
 	bdz	L(one)
 
-	lwzu	r0, 4(r3)	C src[1]
-
-	C
-
-	mulhwu	r6, r7, r5	C carry = high(q*divisor)
-
-	C
-
-	subfe	r7, r6, r0	C l = src[1] - carry
+	lwzu	r0, 4(r3)		C src[1]
+	mulhwu	r6, r7, r5		C carry = high(q*divisor)
+	subfe	r7, r6, r0		C l = src[1] - carry
 	bdz	L(two)
 
-
 L(top):
-	C r0
-	C r3	src, incrementing
-	C r4	inverse
-	C r5	divisor
-	C r6
-	C r7	l
-
-	mullw	r7, r7, r4	C q = l * inverse
-	lwzu	r0, 4(r3)	C src[i]
-
-	C
-
-	mulhwu	r6, r7, r5	C carry = high(q*divisor)
-
-	C
-
-	subfe	r7, r6, r0	C l = src[i] - carry
+	mullw	r7, r7, r4		C q = l * inverse
+	lwzu	r0, 4(r3)		C src[i]
+	mulhwu	r6, r7, r5		C carry = high(q*divisor)
+	subfe	r7, r6, r0		C l = src[i] - carry
 	bdnz	L(top)
 
-
-L(two):
-	mullw	r7, r7, r4	C q = l * inverse
-
-L(one):
-	subfe	r3, r3, r3	C ca 0 or -1
-	mulhwu	r6, r7, r5	C carry = high(q*divisor)
-
-	subf	r3, r3, r6	C carry + ca
+L(two):	mullw	r7, r7, r4		C q = l * inverse
+L(one):	subfe	r3, r3, r3		C ca 0 or -1
+	mulhwu	r6, r7, r5		C carry = high(q*divisor)
+	subf	r3, r3, r6		C carry + ca
 	blr
 
 EPILOGUE(mpn_modexact_1c_odd)
 EPILOGUE(mpn_modexact_1_odd)
+ASM_END()
