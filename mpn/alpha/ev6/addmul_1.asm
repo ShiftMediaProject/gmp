d44cb207590a0ca9681093f0ea7739a6e000339c
tege <tege@gmplib.org>
2005-06-11 13:10:07 +0200

    Remove redundant initial loads.

--- mpn/alpha/ev6/addmul_1.asm
+++ mpn/alpha/ev6/addmul_1.asm
@@ -63,10 +63,10 @@ dnl  later.
 ASM_START()
 PROLOGUE(mpn_addmul_1)
 	ldq	r3,	0(r17)		C
-	and	r18,	7,	r20
-	lda	r18,	-9(r18)
-	cmpeq	r20,	1,	r21
-	beq	r21,	$L1
+	and	r18,	7,	r20	C
+	lda	r18,	-9(r18)		C
+	cmpeq	r20,	1,	r21	C
+	beq	r21,	$L1		C
 
 $1mod8:	ldq	r5,	0(r16)		C
 	mulq	r19,	r3,	r7	C
@@ -75,39 +75,37 @@ $1mod8:	ldq	r5,	0(r16)		C
 	cmpult	r23,	r7,	r20	C
 	addq	r8,	r20,	r0	C
 	stq	r23,	0(r16)		C
-	bge	r18,	$ent1
-	ret	r31,	(r26),	1
+	bge	r18,	$ent1		C
+	ret	r31,	(r26),	1	C
 
 $L1:	lda	r8,	0(r31)		C zero carry reg
 	lda	r24,	0(r31)		C zero carry reg
-	cmpeq	r20,	2,	r21
-	bne	r21,	$2mod8
+	cmpeq	r20,	2,	r21	C
+	bne	r21,	$2mod8		C
+	cmpeq	r20,	3,	r21	C
+	bne	r21,	$3mod8		C
+	cmpeq	r20,	4,	r21	C
+	bne	r21,	$4mod8		C
+	cmpeq	r20,	5,	r21	C
+	bne	r21,	$5mod8		C
+	cmpeq	r20,	6,	r21	C
+	bne	r21,	$6mod8		C
+	cmpeq	r20,	7,	r21	C
+	beq	r21,	$0mod8		C
 
-	cmpeq	r20,	3,	r21
-	bne	r21,	$3mod8
-	cmpeq	r20,	4,	r21
-	bne	r21,	$4mod8
-	cmpeq	r20,	5,	r21
-	bne	r21,	$5mod8
-	cmpeq	r20,	6,	r21
-	bne	r21,	$6mod8
-	cmpeq	r20,	7,	r21
-	beq	r21,	$0mod8
-
-$7mod8:	ldq	r1,	0(r17)		C
-	ldq	r5,	0(r16)		C
-	lda	r17,	8(r17)
-	mulq	r19,	r1,	r7	C
-	umulh	r19,	r1,	r24	C
+$7mod8:	ldq	r5,	0(r16)		C
+	lda	r17,	8(r17)		C
+	mulq	r19,	r3,	r7	C
+	umulh	r19,	r3,	r24	C
 	addq	r5,	r7,	r23	C
 	cmpult	r23,	r7,	r20	C
 	addq	r24,	r20,	r24	C
 	stq	r23,	0(r16)		C
-	lda	r16,	8(r16)
-$6mod8:	ldq	r0,	0(r17)		C
-	ldq	r1,	8(r17)		C
-	mulq	r19,	r0,	r25	C
-	umulh	r19,	r0,	r3	C
+	lda	r16,	8(r16)		C
+	ldq	r3,	0(r17)		C
+$6mod8:	ldq	r1,	8(r17)		C
+	mulq	r19,	r3,	r25	C
+	umulh	r19,	r3,	r3	C
 	mulq	r19,	r1,	r28	C
 	ldq	r0,	16(r17)		C
 	ldq	r4,	0(r16)		C
@@ -120,15 +118,15 @@ $6mod8:	ldq	r0,	0(r17)		C
 	umulh	r19,	r0,	r6	C
 	addq	r4,	r25,	r4	C lo + acc
 	mulq	r19,	r1,	r7	C
-	br	r31,	$ent6
+	br	r31,	$ent6		C
 
-$ent1:	lda	r17,	8(r17)
-	lda	r16,	8(r16)
-	lda	r8,	0(r0)
-$0mod8:	ldq	r0,	0(r17)		C
-	ldq	r1,	8(r17)		C
-	mulq	r19,	r0,	r2	C
-	umulh	r19,	r0,	r6	C
+$ent1:	lda	r17,	8(r17)		C
+	lda	r16,	8(r16)		C
+	lda	r8,	0(r0)		C
+	ldq	r3,	0(r17)		C
+$0mod8:	ldq	r1,	8(r17)		C
+	mulq	r19,	r3,	r2	C
+	umulh	r19,	r3,	r6	C
 	mulq	r19,	r1,	r7	C
 	ldq	r0,	16(r17)		C
 	ldq	r4,	0(r16)		C
@@ -139,25 +137,24 @@ $0mod8:	ldq	r0,	0(r17)		C
 	umulh	r19,	r0,	r3	C
 	addq	r4,	r2,	r4	C lo + acc
 	mulq	r19,	r1,	r28	C
-	lda	r16,	-16(r16)
-	br	r31,	$ent0
+	lda	r16,	-16(r16)	C
+	br	r31,	$ent0		C
 
-$3mod8:	ldq	r1,	0(r17)		C
-	ldq	r5,	0(r16)		C
-	lda	r17,	8(r17)
-	mulq	r19,	r1,	r7	C
-	umulh	r19,	r1,	r8	C
+$3mod8:	ldq	r5,	0(r16)		C
+	lda	r17,	8(r17)		C
+	mulq	r19,	r3,	r7	C
+	umulh	r19,	r3,	r8	C
 	addq	r5,	r7,	r23	C
 	cmpult	r23,	r7,	r20	C
 	addq	r8,	r20,	r24	C
 	stq	r23,	0(r16)		C
-	lda	r16,	8(r16)
-$2mod8:	ldq	r0,	0(r17)		C
-	ldq	r1,	8(r17)		C
-	mulq	r19,	r0,	r25	C
-	umulh	r19,	r0,	r3	C
+	lda	r16,	8(r16)		C
+	ldq	r3,	0(r17)		C
+$2mod8:	ldq	r1,	8(r17)		C
+	mulq	r19,	r3,	r25	C
+	umulh	r19,	r3,	r3	C
 	mulq	r19,	r1,	r28	C
-	ble	r18,	$n23
+	ble	r18,	$n23		C
 	ldq	r0,	16(r17)		C
 	ldq	r4,	0(r16)		C
 	umulh	r19,	r1,	r8	C
@@ -169,22 +166,21 @@ $2mod8:	ldq	r0,	0(r17)		C
 	umulh	r19,	r0,	r6	C
 	addq	r4,	r25,	r4	C lo + acc
 	mulq	r19,	r1,	r7	C
-	br	r31,	$ent2
+	br	r31,	$ent2		C
 
-$5mod8:	ldq	r1,	0(r17)		C
-	ldq	r5,	0(r16)		C
-	lda	r17,	8(r17)
-	mulq	r19,	r1,	r7	C
-	umulh	r19,	r1,	r24	C
+$5mod8:	ldq	r5,	0(r16)		C
+	lda	r17,	8(r17)		C
+	mulq	r19,	r3,	r7	C
+	umulh	r19,	r3,	r24	C
 	addq	r5,	r7,	r23	C
 	cmpult	r23,	r7,	r20	C
 	addq	r24,	r20,	r8	C
 	stq	r23,	0(r16)		C
-	lda	r16,	8(r16)
-$4mod8:	ldq	r0,	0(r17)		C
-	ldq	r1,	8(r17)		C
-	mulq	r19,	r0,	r2	C
-	umulh	r19,	r0,	r6	C
+	lda	r16,	8(r16)		C
+	ldq	r3,	0(r17)		C
+$4mod8:	ldq	r1,	8(r17)		C
+	mulq	r19,	r3,	r2	C
+	umulh	r19,	r3,	r6	C
 	mulq	r19,	r1,	r7	C
 	ldq	r0,	16(r17)		C
 	ldq	r4,	0(r16)		C
@@ -199,7 +195,7 @@ $4mod8:	ldq	r0,	0(r17)		C
 	mulq	r19,	r1,	r28	C
 	cmpult	r4,	r2,	r20	C L0 lo add => carry
 	addq	r4,	r8,	r22	C U0 hi add => answer
-	ble	r18,	$Lend
+	ble	r18,	$Lend		C
 ALIGN(16)
 $Loop:
 	bis	r31,	r31,	r31	C U1 mt
@@ -342,55 +338,53 @@ $ent6:
 	ldl	r31,	256(r17)	C prefetch up[]
 	bgt	r18,	$Loop		C U1 bookkeeping
 
-$Lend:
-	cmpult	r22,	r8,	r21	C L0 hi add => carry
-	addq	r6,	r20,	r6	C U0 hi mul + carry
-	addq	r5,	r7,	r23	C L0 lo + acc
-	addq	r6,	r21,	r6	C L0 hi mul + carry
-	ldq	r4,	0(r16)		C L1
-	umulh	r19,	r1,	r8	C U1
-	cmpult	r23,	r7,	r20	C L0 lo add => carry
-	addq	r23,	r6,	r23	C U0 hi add => answer
-	cmpult	r23,	r6,	r21	C L0 hi add => carry
-	addq	r24,	r20,	r24	C U0 hi mul + carry
-	ldq	r5,	8(r16)		C L1
-	addq	r4,	r25,	r4	C L0 lo + acc
-	stq	r22,	-16(r16)	C L0
-	stq	r23,	-8(r16)		C L1
-	addq	r24,	r21,	r24	C U0 hi mul + carry
-	cmpult	r4,	r25,	r20	C L0 lo add => carry
-	addq	r4,	r24,	r22	C U0 hi add => answer
-	cmpult	r22,	r24,	r21	C L0 hi add => carry
-	addq	r3,	r20,	r3	C U0 hi mul + carry
-	addq	r5,	r28,	r23	C L0 lo + acc
-	addq	r3,	r21,	r3	C U0 hi mul + carry
-	cmpult	r23,	r28,	r20	C L0 lo add => carry
-	addq	r23,	r3,	r23	C U0 hi add => answer
-	cmpult	r23,	r3,	r21	C L0 hi add => carry
-	addq	r8,	r20,	r8	C U0 hi mul + carry
-	stq	r22,	0(r16)		C L0
-	stq	r23,	8(r16)		C L1
-	addq	r8,	r21,	r0	C L0 exit high-with-carrys-in-0
-	ret	r31,	(r26),	1
+$Lend:	cmpult	r22,	r8,	r21	C
+	addq	r6,	r20,	r6	C
+	addq	r5,	r7,	r23	C
+	addq	r6,	r21,	r6	C
+	ldq	r4,	0(r16)		C
+	umulh	r19,	r1,	r8	C
+	cmpult	r23,	r7,	r20	C
+	addq	r23,	r6,	r23	C
+	cmpult	r23,	r6,	r21	C
+	addq	r24,	r20,	r24	C
+	ldq	r5,	8(r16)		C
+	addq	r4,	r25,	r4	C
+	stq	r22,	-16(r16)	C
+	stq	r23,	-8(r16)		C
+	addq	r24,	r21,	r24	C
+	cmpult	r4,	r25,	r20	C
+	addq	r4,	r24,	r22	C
+	cmpult	r22,	r24,	r21	C
+	addq	r3,	r20,	r3	C
+	addq	r5,	r28,	r23	C
+	addq	r3,	r21,	r3	C
+	cmpult	r23,	r28,	r20	C
+	addq	r23,	r3,	r23	C
+	cmpult	r23,	r3,	r21	C
+	addq	r8,	r20,	r8	C
+	stq	r22,	0(r16)		C
+	stq	r23,	8(r16)		C
+	addq	r8,	r21,	r0	C
+	ret	r31,	(r26),	1	C
 
 $n23:	ldq	r4,	0(r16)		C
 	ldq	r5,	8(r16)		C
 	umulh	r19,	r1,	r8	C
-	addq	r4,	r25,	r4	C lo + acc
-	cmpult	r4,	r25,	r20	C L0 lo add => carry
-	addq	r4,	r24,	r22	C U0 hi add => answer
-	cmpult	r22,	r24,	r21	C L0 hi add => carry
-	addq	r3,	r20,	r3	C U0 hi mul + carry
-	addq	r5,	r28,	r23	C L0 lo + acc
-	addq	r3,	r21,	r3	C U0 hi mul + carry
-	cmpult	r23,	r28,	r20	C L0 lo add => carry
-	addq	r23,	r3,	r23	C U0 hi add => answer
-	cmpult	r23,	r3,	r21	C L0 hi add => carry
-	addq	r8,	r20,	r8	C U0 hi mul + carry
-	stq	r22,	0(r16)		C L0
-	stq	r23,	8(r16)		C L1
-	addq	r8,	r21,	r0	C L0 exit high-with-carrys-in-0
-	ret	r31,	(r26),	1
-
+	addq	r4,	r25,	r4	C
+	cmpult	r4,	r25,	r20	C
+	addq	r4,	r24,	r22	C
+	cmpult	r22,	r24,	r21	C
+	addq	r3,	r20,	r3	C
+	addq	r5,	r28,	r23	C
+	addq	r3,	r21,	r3	C
+	cmpult	r23,	r28,	r20	C
+	addq	r23,	r3,	r23	C
+	cmpult	r23,	r3,	r21	C
+	addq	r8,	r20,	r8	C
+	stq	r22,	0(r16)		C
+	stq	r23,	8(r16)		C
+	addq	r8,	r21,	r0	C
+	ret	r31,	(r26),	1	C
 EPILOGUE()
 ASM_END()
