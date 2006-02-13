000053bc0b3466d42bd6bec6035d7f53032e225d
tege <tege@gmplib.org>
2006-02-13 19:31:43 +0100

    Trivially reoder loop insns to save 1 c/l.

--- mpn/powerpc32/diveby3.asm
+++ mpn/powerpc32/diveby3.asm
@@ -1,6 +1,6 @@
 dnl  PowerPC-32 mpn_divexact_by3 -- mpn by 3 exact division
 
-dnl  Copyright 2002, 2003, 2005 Free Software Foundation, Inc.
+dnl  Copyright 2002, 2003, 2005, 2006 Free Software Foundation, Inc.
 dnl
 dnl  This file is part of the GNU MP Library.
 dnl
@@ -23,12 +23,12 @@ include(`../config.m4')
 
 C                cycles/limb
 C 603e:             ?
-C 604e:             7.0
-C 75x (G3):        10.0
-C 7400,7410 (G4):  10.0
-C 744x,745x (G4+): 10.0
-C power4/ppc970:   16.0
-C power5:          12.0
+C 604e:             7 UPDATE
+C 75x (G3):         9
+C 7400,7410 (G4):   9
+C 744x,745x (G4+):  9
+C power4/ppc970:   13
+C power5:           ?
 
 C void mpn_divexact_by3 (mp_ptr dst, mp_srcptr src, mp_size_t size);
 C
@@ -45,55 +45,26 @@ C so, on average.
 
 ASM_START()
 PROLOGUE(mpn_divexact_by3c)
-
-	C r3	dst
-	C r4	src
-	C r5	size
-	C r6	carry
-
 	mtctr	r5		C size
 	lwz	r7, 0(r4)	C src[0]
-
 	lis	r5, 0xAAAA	C inverse high
 	subi	r3, r3, 4	C adjust dst for first stwu
-
 	li	r0, 3		C multiplier 3
 	ori	r5, r5, 0xAAAB	C inverse low
-
 	subfc	r7, r6, r7	C l = src[0] - carry
 	bdz	L(one)
 
-L(top):
-	C r0	3
-	C r3	dst, incrementing
-	C r4	src, incrementing
-	C r5	inverse
-	C r6	carry
-	C r7	l
-
-	mullw	r8, r5, r7	C q = inverse * l
+L(top):	mullw	r8, r5, r7	C q = inverse * l
 	lwzu	r7, 4(r4)	C src[i]
-
-	C
-
 	mulhwu	r6, r8, r0	C c = high(3*q)
-	stwu	r8, 4(r3)	C dst[i-1] = q
-
-	C serialize
 	subfe	r7, r6, r7	C l = s - carry
+	stwu	r8, 4(r3)	C dst[i-1] = q
 	bdnz	L(top)
 
-
-L(one):
-	subfe	r4, r4, r4	C ca 0 or -1
-
+L(one):	subfe	r4, r4, r4	C ca 0 or -1
 	mullw	r8, r7, r5	C q = l * inverse
-
 	mulhwu	r6, r8, r0	C c = high(3*q)
 	stwu	r8, 4(r3)	C dst[i] = q
-
 	subf	r3, r4, r6	C carry + ca
-
 	blr
-
 EPILOGUE()
