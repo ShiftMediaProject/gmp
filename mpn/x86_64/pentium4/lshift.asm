fbed1168dec209d536d8e46697b9f30215529ee3
tege <tege@gmplib.org>
2007-01-25 02:28:57 +0100

    Swap some loop insns for a small speedup.

--- mpn/x86_64/pentium4/lshift.asm
+++ mpn/x86_64/pentium4/lshift.asm
@@ -1,6 +1,6 @@
-dnl  AMD64 mpn_lshift -- mpn left shift.
+dnl  x86-64 mpn_lshift optimized for Pentium 4.
 
-dnl  Copyright 2003, 2005 Free Software Foundation, Inc.
+dnl  Copyright 2003, 2005, 2007 Free Software Foundation, Inc.
 dnl
 dnl  This file is part of the GNU MP Library.
 dnl
@@ -23,9 +23,13 @@ include(`../config.m4')
 
 
 C		    cycles/limb
-C Hammer:		2.375
-C Prescott/Nocona:	3.5
+C K8:			2.5
+C Prescott/Nocona:	3.33
+C P6-15:		2.0
 
+C TODO
+C  This runs faster than mpn_rshift, in spite that the inner loops are similar.
+C  Proper scheduling should cut a few cycles.
 
 C INPUT PARAMETERS
 C rp	rdi
@@ -36,6 +40,7 @@ C cnt	rcx
 define(`rp',`%rdi')
 define(`up',`%rsi')
 define(`n',`%rdx')
+define(`cnt',`%cl')
 
 ASM_START()
 	TEXT
@@ -43,17 +48,18 @@ ASM_START()
 PROLOGUE(mpn_lshift)
 	mov	-8(up,n,8), %rax
 	movd	%ecx, %mm4
-	negl	%ecx			C put rsh count in cl
+	neg	%ecx			C put rsh count in cl
 	and	$63, %ecx
 	movd	%ecx, %mm5
 
-	shrq	%cl, %rax		C function return value
+	lea	1(n), %r8d
 
-	leal	1(n), %r8d
-	andl	$3, %r8d
+	shr	%cl, %rax		C function return value
+
+	and	$3, %r8d
 	je	.Lrolx			C jump for n = 3, 7, 11, ...
 
-	decl	%r8d
+	dec	%r8d
 	jne	.L1
 C	n = 4, 8, 12, ...
 	movq	-8(up,n,8), %mm2
@@ -62,10 +68,10 @@ C	n = 4, 8, 12, ...
 	psrlq	%mm5, %mm0
 	por	%mm0, %mm2
 	movq	%mm2, -8(rp,n,8)
-	decq	n
+	dec	n
 	jmp	.Lroll
 
-.L1:	decl	%r8d
+.L1:	dec	%r8d
 	je	.L1x			C jump for n = 1, 5, 9, 13, ...
 C	n = 2, 6, 10, 16, ...
 	movq	-8(up,n,8), %mm2
@@ -74,9 +80,9 @@ C	n = 2, 6, 10, 16, ...
 	psrlq	%mm5, %mm0
 	por	%mm0, %mm2
 	movq	%mm2, -8(rp,n,8)
-	decq	n
+	dec	n
 .L1x:
-	cmpq	$1, n
+	cmp	$1, n
 	je	.Last
 	movq	-8(up,n,8), %mm2
 	psllq	%mm4, %mm2
@@ -90,7 +96,7 @@ C	n = 2, 6, 10, 16, ...
 	por	%mm1, %mm3
 	movq	%mm2, -8(rp,n,8)
 	movq	%mm3, -16(rp,n,8)
-	subq	$2, n
+	sub	$2, n
 
 .Lroll:
 .Lrolx:	movq	-8(up,n,8), %mm2
@@ -98,11 +104,9 @@ C	n = 2, 6, 10, 16, ...
 	movq	-16(up,n,8), %mm3
 	psllq	%mm4, %mm3
 
-	subq	$4, n			C				      4
+	sub	$4, n			C				      4
 	jb	.Lend			C				      2
 	ALIGN(32)
-	nop
-	nop
 .Loop:
 	C finish stuff from lsh block
 	movq	16(up,n,8), %mm0
@@ -110,12 +114,12 @@ C	n = 2, 6, 10, 16, ...
 	psrlq	%mm5, %mm0
 	por	%mm0, %mm2
 	psrlq	%mm5, %mm1
+	movq	0(up,n,8), %mm0
 	por	%mm1, %mm3
+	movq	-8(up,n,8), %mm1
 	movq	%mm2, 24(rp,n,8)
 	movq	%mm3, 16(rp,n,8)
 	C start two new rsh
-	movq	0(up,n,8), %mm0
-	movq	-8(up,n,8), %mm1
 	psrlq	%mm5, %mm0
 	psrlq	%mm5, %mm1
 
@@ -125,13 +129,13 @@ C	n = 2, 6, 10, 16, ...
 	psllq	%mm4, %mm2
 	por	%mm2, %mm0
 	psllq	%mm4, %mm3
+	movq	-8(up,n,8), %mm2
 	por	%mm3, %mm1
+	movq	-16(up,n,8), %mm3
 	movq	%mm0, 8(rp,n,8)
 	movq	%mm1, 0(rp,n,8)
 	C start two new lsh
-	movq	-8(up,n,8), %mm2
-	movq	-16(up,n,8), %mm3
-	subq	$4, n
+	sub	$4, n
 	psllq	%mm4, %mm2
 	psllq	%mm4, %mm3
 
