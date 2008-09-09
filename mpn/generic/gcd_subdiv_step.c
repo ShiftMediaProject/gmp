cafa00367bd4b5443295aaa160d463b5a18a5368
Niels MÃ¶ller <nisse@lysator.liu.se>
2008-09-09 11:06:39 +0200

    gcd_subdiv_step.c (mpn_gcd_subdiv_step): Simplified function. The special cancellation logic is not needed here.

--- mpn/generic/gcd_subdiv_step.c
+++ mpn/generic/gcd_subdiv_step.c
@@ -26,74 +26,84 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 #include "longlong.h"
 
 /* Used when mpn_hgcd or mpn_hgcd2 has failed. Then either one of a or
-   b is very small, or the difference is very small. Perform one
-   subtraction followed by one division. If the gcd is found, stores
-   it in gp and *gn, and returns zero. Otherwise, compute the reduced
-   a and b, and return the new size. */
+   b is small, or the difference is small. Perform one subtraction
+   followed by one division. If the gcd is found, stores it in gp and
+   *gn, and returns zero. Otherwise, compute the reduced a and b, and
+   return the new size. */
 
 /* FIXME: Check when the smaller number is a single limb, and invoke
  * mpn_gcd_1. */
 mp_size_t
 mpn_gcd_subdiv_step (mp_ptr gp, mp_size_t *gn,
-		      mp_ptr ap, mp_ptr bp, mp_size_t n, mp_ptr tp)
+		     mp_ptr ap, mp_ptr bp, mp_size_t n, mp_ptr tp)
 {
   mp_size_t an, bn;
 
   ASSERT (n > 0);
   ASSERT (ap[n-1] > 0 || bp[n-1] > 0);
 
-  /* First, make sure that an >= bn, and subtract an -= bn */
-  for (an = n; an > 0; an--)
-    if (ap[an-1] != bp[an-1])
-      break;
-
-  if (an == 0)
-    {
-      /* Done */
-      MPN_COPY (gp, ap, n);
-      *gn = n;
-      return 0;
-    }
-
-  if (ap[an-1] < bp[an-1])
-    MP_PTR_SWAP (ap, bp);
-
-  bn = n;
-  MPN_NORMALIZE (bp, bn);
-  if (bn == 0)
-    {
-      MPN_COPY (gp, ap, n);
-      *gn = n;
-      return 0;
-    }
-
-  ASSERT_NOCARRY (mpn_sub_n (ap, ap, bp, an));
+  an = bn = n;
   MPN_NORMALIZE (ap, an);
+  MPN_NORMALIZE (bp, bn);
 
-  ASSERT (an > 0);
-	  
+  if (UNLIKELY (an == 0))
+    {
+    return_b:
+      MPN_COPY (gp, bp, bn);
+      *gn = bn;
+      return 0;
+    }
+  else if (UNLIKELY (bn == 0))
+    {
+    return_a:
+      MPN_COPY (gp, ap, an);
+      *gn = an;
+      return 0;
+    }
+
+  /* Arrange so that a > b, subtract an -= bn, and maintain
+     normalization. */
   if (an < bn)
     MPN_PTR_SWAP (ap, an, bp, bn);
   else if (an == bn)
     {
       int c;
       MPN_CMP (c, ap, bp, an);
-      if (c < 0)
+      if (UNLIKELY (c == 0))
+	goto return_a;
+      else if (c < 0)
+	MP_PTR_SWAP (ap, bp);
+    }
+
+  ASSERT_NOCARRY (mpn_sub (ap, ap, an, bp, bn));
+  MPN_NORMALIZE (ap, an);
+  ASSERT (an > 0);
+
+  /* Arrange so that a > b, and divide a = q b + r */
+  /* FIXME: an < bn happens when we have cancellation. If that is the
+     common case, then we could reverse the roles of a and b to avoid
+     the swap. */
+  if (an < bn)
+    MPN_PTR_SWAP (ap, an, bp, bn);
+  else if (an == bn)
+    {
+      int c;
+      MPN_CMP (c, ap, bp, an);
+      if (UNLIKELY (c == 0))
+	goto return_a;
+      else if (c < 0)
 	MP_PTR_SWAP (ap, bp);
     }
 
   mpn_tdiv_qr (tp + bn, tp, 0, ap, an, bp, bn);
 
   /* Normalizing seems to be the simplest way to test if the remainder
-     is zero. */
+     is zero. FIXME: Use mpn_zero_p, whenever that is added to
+     gmp-impl.h*/
   an = bn;
   MPN_NORMALIZE (tp, an);
   if (an == 0)
-    {
-      MPN_COPY (gp, bp, bn);
-      *gn = bn;
-      return 0;
-    }
+    goto return_b;
 
   MPN_COPY (ap, tp, bn);
 
