4a6dddf1e0825acb9d26c6386969172bec9660ab
Torbjorn Granlund <tege@gmplib.org>
2008-10-29 17:13:44 +0100

    (__gmpn_cpuvec_init): Handle "extended" fields for model and family.

--- mpn/x86/fat/fat.c
+++ mpn/x86/fat/fat.c
@@ -195,8 +195,8 @@ __gmpn_cpuvec_init (void)
       vendor_string[12] = 0;
 
       fms = __gmpn_cpuid (dummy_string, 1);
-      family = (fms >> 8) & 15;
-      model = (fms >> 4) & 15;
+      family = ((fms >> 8) & 0xf) + ((fms >> 20) & 0xff);
+      model = ((fms >> 4) & 0xf) + ((fms >> 12) & 0xf0);
 
       if (strcmp (vendor_string, "GenuineIntel") == 0)
         {
