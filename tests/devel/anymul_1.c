41794343cf5440278595de5fb3b23578bb46b0d9
Torbjorn Granlund <tege@gmplib.org>
2008-11-21 15:06:10 +0100

    Include <string.h>.

--- tests/devel/anymul_1.c
+++ tests/devel/anymul_1.c
@@ -18,6 +18,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "gmp.h"
 #include "gmp-impl.h"
