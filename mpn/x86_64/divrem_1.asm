6303dfa1e13cbfc40145c6ac971f34a8d1352e73
Torbjorn Granlund <tege@gmplib.org>
2008-11-23 19:35:15 +0100

    Rewrite.

--- mpn/x86_64/divrem_1.asm
+++ mpn/x86_64/divrem_1.asm
@@ -1,6 +1,6 @@
-dnl  AMD64 mpn_divrem_1 -- mpn by limb division.
+dnl  x86-64 mpn_divrem_1 -- mpn by limb division.
 
-dnl  Copyright 2004, 2005 Free Software Foundation, Inc.
+dnl  Copyright 2004, 2005, 2007, 2008 Free Software Foundation, Inc.
 
 dnl  This file is part of the GNU MP Library.
 
@@ -19,204 +19,260 @@ dnl  along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.
 
 include(`../config.m4')
 
-C	   integer part    fraction part
-C	    cycles/limb     cycles/limb
-C K8,K9:	17		15
-C K10:		17		15
-C P4:		83		57
-C P6-15 (Core2):32		21
-C P6-28 (Atom):	70		39
 
-
-C mp_limb_t mpn_divrem_1 (mp_ptr qp, mp_size_t fn,
-C                         mp_srcptr np, mp_size_t nn, mp_limb_t d);
+C		norm	unorm	frac
+C K8		13	13	12
+C P4		44.2	44.2	42.3
+C P6-15 (Core2)	24.5	24.5	19.3
+C P6-15 (Atom)	42	52	37
 
 C TODO
-C  * Address FIXME below about leading quotient limbs optimization
-C  * Optimize by doing some software pipelining and local scheduling
-C  * Optimize by using mmx instructions for shifting
-C  * Perhaps compute the inverse without relying on the 71 cycle divq?  Could
-C    either use Newton's method and mulq, or perhaps the faster fdiv.
-C  * Perhaps handle nn+fn < 3 using divq directly (if divq is ever to be used).
+C  * Compute the inverse without relying on the div instruction.
+C    Newton's method and mulq, or perhaps the faster fdiv.
+C  * Tune prologue.
+C  * Optimize for Core 2.
+
+C The code for unnormalized divisors works also for normalized divisors, but
+C for some reason it runs really slowly (on K8) for that case.  Use special
+C code until we can address this.  The Intel Atom is also affected, but
+C understandably (shld slowness).
+define(`SPECIAL_CODE_FOR_NORMALIZED_DIVISOR',1)
 
 C mp_limb_t
 C mpn_divrem_1 (mp_ptr qp, mp_size_t fn,
-C		mp_srcptr np, mp_size_t nn, mp_limb_t d)
+C               mp_srcptr np, mp_size_t nn, mp_limb_t d)
 
 C mp_limb_t
 C mpn_preinv_divrem_1 (mp_ptr qp, mp_size_t fn,
-C		       mp_srcptr np, mp_size_t nn, mp_limb_t d,
-C		       mp_limb_t dinv, int shift)
+C                      mp_srcptr np, mp_size_t nn, mp_limb_t d,
+C                      mp_limb_t dinv, int cnt)
 
 C INPUT PARAMETERS
-define(`qp',`%rdi')
-define(`fn',`%rsi')
-define(`np_param',`%rdx')
-define(`nn_param',`%rcx')
-define(`d',`%r8')
-define(`dinv',`%r9')
-C shift %r10 FIXME
-define(`np',`%r11')
-define(`nn',`%rbp')
-define(`rem',`%r10')
+define(`qp',		`%rdi')
+define(`fn_param',	`%rsi')
+define(`up_param',	`%rdx')
+define(`un_param',	`%rcx')
+define(`d',		`%r8')
+define(`dinv',		`%r9')		C only for mpn_preinv_divrem_1
+C       shift passed on stack		C only for mpn_preinv_divrem_1
 
+define(`cnt',		`%rcx')
+define(`up',		`%rsi')
+define(`fn',		`%r12')
+define(`un',		`%rbx')
+
+
+C rax rbx rcx rdx rsi rdi rbp r8  r9  r10 r11 r12 r13 r14 r15
+C         cnt         qp      d  dinv
 
 ASM_START()
 	TEXT
 	ALIGN(16)
 PROLOGUE(mpn_preinv_divrem_1)
-C	pushq	%r15
-	pushq	%r14
-C	pushq	%r13
-	pushq	%r12
-	pushq	%rbp
-	pushq	%rbx
+	xor	%eax, %eax
+	push	%r13
+	push	%r12
+	push	%rbp
+	push	%rbx
 
-	movq	np_param, np
-	movq	nn_param, nn
-	addq	fn, nn_param
-	je	L(ret0)			C we're done if both nn and fn are zero
+	mov	fn_param, fn
+	mov	un_param, un
+	add	fn_param, un_param
+	mov	up_param, up
 
-	leaq	(qp,fn,8), qp		C point qp at least sign. integral limb
+	lea	-8(qp,un_param,8), qp
 
-	movq	40(%rsp), %rcx
-	salq	%cl, d			C d = normalized divisor
-
-	jmp	L(comm)
-L(ret0):
-	xorl	%eax, %eax
-	jmp	L(ret)
+	test	d, d
+	js	L(nent)
+	mov	40(%rsp), R8(cnt)
+	shl	R8(cnt), d
+	jmp	L(uent)
 EPILOGUE()
 
 	ALIGN(16)
 PROLOGUE(mpn_divrem_1)
+	xor	%eax, %eax
+	push	%r13
+	push	%r12
+	push	%rbp
+	push	%rbx
 
-C	pushq	%r15
-	pushq	%r14
-C	pushq	%r13
-	pushq	%r12
-	pushq	%rbp
-	pushq	%rbx
+	mov	fn_param, fn
+	mov	un_param, un
+	add	fn_param, un_param
+	mov	up_param, up
+	je	L(ret)
 
-	movq	np_param, np
-	movq	nn_param, nn
-	addq	fn, nn_param
-	je	L(ret0)			C we're done if both nn and fn are zero
+	lea	-8(qp,un_param,8), qp
+	xor	R32(%rbp), R32(%rbp)
 
-	leaq	(qp,fn,8), qp		C point qp at least sign. integral limb
 
-	bsrq	d,%rcx
-	xorl	$63, %ecx		C %ecx = count_leading_zeros (divisor)
+ifdef(`SPECIAL_CODE_FOR_NORMALIZED_DIVISOR',`
+	test	d, d
+	jns	L(unnormalized)
 
-	movq	$-1, %rdx
-	salq	%cl, d			C d = normalized divisor
+L(normalized):
+	test	un, un
+	je	L(8)			C un == 0
+	mov	-8(up,un,8), %rbp
+	dec	un
+	mov	%rbp, %rax
+	sub	d, %rbp
+	cmovb	%rax, %rbp
+	sbb	%eax, %eax
+	inc	%eax
+	mov	%rax, (qp)
+	lea	-8(qp), qp
+L(8):
+	mov	d, %rdx
+	mov	$-1, %rax
+	not	%rdx
+	div	d			C FREE rax rdx rcx r9 r10 r11
+	mov	%rax, dinv
+	mov	%rbp, %rax
+	jmp	L(nent)
 
-	movq	%rdx, %rax
-	subq	d, %rdx
-	divq	d
-	movq	%rax, dinv		C dinv = invert_limb (d)
-L(comm):
-	xorl	%ebx, %ebx
-	xorl	%eax, %eax
+	ALIGN(16)
+L(nloop):				C		    cycK8  cycP6  cycP4
+	mov	(up,un,8), %r10		C
+	lea	1(%rax), %rbp		C
+	mul	dinv			C		     0,13   0,19  0,45
+	add	%r10, %rax		C		     4      8     12
+	adc	%rbp, %rdx		C		     5      9     13
+	mov	%rax, %rbp		C		     5      9     13
+	mov	%rdx, %r13		C		     6      11    23
+	imul	d, %rdx			C		     6      11    23
+	sub	%rdx, %r10		C		     10     16    33
+	mov	d, %rax			C
+	add	%r10, %rax		C		     11     17    34
+	cmp	%rbp, %r10		C		     11     17    34
+	cmovb	%r10, %rax		C		     12     18    35
+	adc	$-1, %r13		C
+	cmp	d, %rax			C
+	jae	L(nfx)			C
+L(nok):	mov	%r13, (qp)		C
+	sub	$8, qp			C
+L(nent):dec	un			C
+	jns	L(nloop)		C
 
-	testq	nn, nn
-	je	L(frac)			C no integer quotient limbs
-	movq	-8(np,nn,8), %r14
-	shldq	%cl, %r14, %rax
-C FIXME: Should compare rem and d here, and conditionally generate a quotient
-C FIXME: limb for the right outcome
-	subq	$2, nn
-	js	L(intlast)
-	ALIGN(64)
-L(intloop):
-	movq	(np,nn,8), %rdx
-	shldq	%cl, %rdx, %r14		C n10
-	leaq	(d,%r14), %r12
-	btq	$63, %r14
-	cmovnc	%r14, %r12
-	movq	%rax, rem
-	adcq	$0, %rax
+	xor	%ecx, %ecx
+	jmp	L(87)
 
-	mulq	dinv			C %rax = dinv * n
+L(nfx):	sub	d, %rax
+	inc	%r13
+	jmp	L(nok)
+')
 
-	addq	%r12, %rax
-	movq	d, %rax
-	adcq	rem, %rdx
-	notq	%rdx
-	movq	%rdx, %r12
+L(unnormalized):
+	test	un, un
+	je	L(44)
+	mov	-8(up,un,8), %rax
+	cmp	d, %rax
+	jae	L(44)
+	mov	%rbp, (qp)
+	mov	%rax, %rbp
+	lea	-8(qp), qp
+	je	L(ret)
+	dec	un
+L(44):
+	bsr	d, %rcx
+	not	%ecx
+	sal	%cl, d
+	sal	%cl, %rbp
+	mov	d, %rdx
+	mov	$-1, %rax
+	not	%rdx
+	div	d			C FREE rax rdx r9 r10 r11
+	test	un, un
+	mov	%rax, dinv
+	mov	%rbp, %rax
+	je	L(87)
+L(uent):
+	mov	-8(up,un,8), %rbp
+	shr	%cl, %rax
+	shld	%cl, %rbp, %rax
+	sub	$2, un
+	js	L(ulast)
 
-	mulq	%rdx
+	ALIGN(16)
+L(uloop):
+	mov	(up,un,8), %r10
+	lea	1(%rax), %r11
+	shld	%cl, %r10, %rbp
+	mul	dinv
+	add	%rbp, %rax
+	adc	%r11, %rdx
+	mov	%rax, %r11
+	mov	%rdx, %r13
+	imul	d, %rdx
+	sub	%rdx, %rbp
+	mov	d, %rax
+	add	%rbp, %rax
+	cmp	%r11, %rbp
+	cmovb	%rbp, %rax
+	adc	$-1, %r13
+	cmp	d, %rax
+	jae	L(ufx)
+L(uok):	mov	%r13, (qp)
+	sub	$8, qp
+	dec	un
+	mov	%r10, %rbp
+	jns	L(uloop)
+L(ulast):
+	lea	1(%rax), %r11
+	sal	%cl, %rbp
+	mul	dinv
+	add	%rbp, %rax
+	adc	%r11, %rdx
+	mov	%rax, %r11
+	mov	%rdx, %r13
+	imul	d, %rdx
+	sub	%rdx, %rbp
+	mov	d, %rax
+	add	%rbp, %rax
+	cmp	%r11, %rbp
+	cmovb	%rbp, %rax
+	adc	$-1, %r13
+	cmp	d, %rax
+	jae	L(93)
+L(69):	mov	%r13, (qp)
+	sub	$8, qp
+	jmp	L(87)
 
-	addq	%rax, %r14
-	adcq	%rdx, rem
-	subq	d, rem			C 00..00 or 11.11
-	leaq	(d,%r14), %rax		C remainder
-	cmovnc	%r14, %rax		C remainder
-	subq	%r12, rem		C quotient
-	movq	(np,nn,8), %r14
-	movq	rem, 8(qp,nn,8)
-	decq	nn
-	jns	L(intloop)
-L(intlast):
-	salq	%cl, %r14
-	leaq	(d,%r14), %r12
-	btq	$63, %r14
-	cmovnc	%r14, %r12
-	movq	%rax, rem
-	adcq	$0, %rax
+L(ufx):	sub	d, %rax
+	inc	%r13
+	jmp	L(uok)
 
-	mulq	dinv
+L(93):	sub	d, %rax
+	inc	%r13
+	jmp	L(69)
 
-	addq	%r12, %rax
-	movq	d, %rax
-	adcq	rem, %rdx
-	notq	%rdx
-	movq	%rdx, %r12
+L(87):	mov	d, %rbp
+	neg	%rbp
+	jmp	L(87b)
 
-	mulq	%rdx
+	ALIGN(16)
+L(floop):				C		    cycK8  cycP6  cycP4
+	lea	1(%rax), %r11		C
+	mul	dinv			C		     0,12
+	add	%r11, %rdx		C		     5
+	mov	%rax, %r11		C		     4
+	mov	%rdx, %r13		C		     6
+	imul	%rbp, %rdx		C		     6
+	mov	d, %rax			C
+	add	%rdx, %rax		C		     10
+	cmp	%r11, %rdx		C		     10
+	cmovb	%rdx, %rax		C		     11
+	adc	$-1, %r13		C
+	mov	%r13, (qp)		C
+	sub	$8, qp			C
+L(87b):	dec	fn			C
+	jns	L(floop)		C
 
-	addq	%rax, %r14
-	adcq	%rdx, rem
-	subq	d, rem
-	leaq	(d,%r14), %rax		C remainder
-	cmovnc	%r14, %rax		C remainder
-	subq	%r12, rem
-	movq	rem, (qp)
-
-L(frac):
-	leaq	(,fn,8), %rdx
-	subq	%rdx, qp
-	decq	fn
-	js	L(fracskip)
-	ALIGN(4)
-L(fracloop):
-	movq	%rax, rem
-
-	mulq	dinv
-
-	movq	d, %rax
-	addq	rem, %rdx
-	notq	%rdx
-	movq	%rdx, %r12
-
-	mulq	%rdx
-
-	addq	rem, %rdx
-	subq	d, %rdx
-	leaq	(d,%rax), %r14
-	cmovc	%r14, %rax		C remainder
-	subq	%r12, %rdx
-	movq	%rdx, (qp,fn,8)
-	decq	fn
-	jns	L(fracloop)
-L(fracskip):
-	shrq	%cl, %rax
-L(ret):
-	popq	%rbx
-	popq	%rbp
-	popq	%r12
-C	popq	%r13
-	popq	%r14
-C	popq	%r15
+	shr	%cl, %rax
+L(ret):	pop	%rbx
+	pop	%rbp
+	pop	%r12
+	pop	%r13
 	ret
-EPILOGUE()
+EPILOGUE(mpn_divrem_1)
