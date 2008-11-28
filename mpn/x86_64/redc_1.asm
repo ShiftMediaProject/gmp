4a845ea2df10d1d5c8c9599ce178ac50ffa80992
Torbjorn Granlund <tege@gmplib.org>
2008-11-28 02:23:37 +0100

    Rewrite.

--- mpn/x86_64/redc_1.asm
+++ mpn/x86_64/redc_1.asm
@@ -1,6 +1,6 @@
 dnl  AMD64 mpn_redc_1 -- Montgomery reduction with a one-limb modular inverse.
 
-dnl  Copyright 2004 Free Software Foundation, Inc.
+dnl  Copyright 2004, 2008 Free Software Foundation, Inc.
 dnl
 dnl  This file is part of the GNU MP Library.
 dnl
@@ -20,317 +20,313 @@ dnl  along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.
 include(`../config.m4')
 
 
-C K8: 3.25 cycles/limb (4.0 cycles/limb at 40/20 limbs).
+C	     cycles/limb
+C	     cycles/limb
+C K8,K9:	 2.5
+C K10:		 2.5
+C P4:		 ?
+C P6-15 (Core2): 5.3
+C P6-28 (Atom):	 ?
 
 C TODO
-C  * Do feed-in like in mul_basecase.asm, i.e., jump into mid-loop.
-C  * Generate new limb-multiplier earlier, in one outer loop iteration earlier.
-C  * Don't reserve rdi (aka rp) over inner loops.
-C  * See mul_basecase.asm for additional items.
+C  * Handle certain sizes, e.g., 1, 2, 3, 4, 8, with single-loop code.
+C    The code for 1, 2, 3, 4 should perhaps be completely register based.
+C  * Perhaps align outer loops.
+C  * The sub_n at the end leaks side-channel data.  How do we fix that?
+C  * Write mpn_addsub_n computing R = A + B - C.  It should run at 2 c/l.
+C  * We could software pipeline the IMUL stuff, by putting it before the
+C    outer loops and before the end of the outer loops.  The last outer
+C    loop iteration would then compute an unneeded product, but it is at
+C    least not a stray read fro up[], since it is at up[n].
+C  * Can we combine both the add_n and sub_n into the loops, somehow?
 
 C INPUT PARAMETERS
-define(`rp',	`%rdi')
-define(`up',	`%rsi')
+define(`rp',	  `%rdi')
+define(`up',	  `%rsi')
 define(`param_mp',`%rdx')
-define(`n',	`%rcx')
-define(`invm',	`%r8')
+define(`n',	  `%rcx')
+define(`invm',	  `%r8')
 
-define(`mp',`%rbx')
+define(`mp',	  `%r13')
+define(`i',	  `%r11')
+define(`nneg',	  `%r12')
 
 ASM_START()
 	TEXT
 	ALIGN(32)
 PROLOGUE(mpn_redc_1)
-	pushq	%rbp
-	pushq	%rbx
-	pushq	%r12
-	pushq	n
-	leaq	(param_mp,n,8), mp	C mp += n
-	leaq	(up,n,8), up		C up += n
-	movq	n, %rbp
-	xorl	%r10d, %r10d		C keep r10 zero
-	negq	%rbp			C rbp = -n
-	movl	%ecx, %eax
-	andl	$3, %eax
-	je	L(b00)
-	cmpl	$1, %eax
-	je	L(b01)
-	cmpl	$2, %eax
-	je	L(b10)
-	nop
+	push	%rbp
+	push	%rbx
+	push	%r12
+	push	%r13
+	push	%r14
+	push	n
 
-L(b11):	movq	(up,%rbp,8), %r12	C load up[0]
-	imulq	invm, %r12
-	leaq	6(%rbp), %r11		C inner-loop count
+	lea	(param_mp,n,8), mp	C mp += n
+	lea	(up,n,8), up		C up += n
 
-	movq	(mp,%rbp,8), %rax
-	mulq	%r12
-	addq	(up,%rbp,8), %rax
-	adcq	%r10, %rdx
-	movq	%rdx, %r9
-	movq	-40(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-40(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, -40(up,%r11,8)	C next iteration up[0]
-	adcq	%rdx, %r9
-	movq	-32(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-32(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, -32(up,%r11,8)
-	adcq	%rdx, %r9
+	mov	n, nneg
+	neg	nneg
 
-	testq	%r11, %r11
-	jns	L(c11)			C FIXME: Special exit for un=vn=1?
+	mov	R32(n), R32(%rax)
+	and	$3, R32(%rax)
+	jz	L(b0)
+	cmp	$2, R32(%rax)
+	jz	L(b2)
+	jg	L(b3)
 
-	ALIGN(8)
-L(i11):	movq	-24(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-24(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%rax, -24(up,%r11,8)
-	movq	%r10, %r9
-	adcq	%rdx, %r9
-	movq	-16(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-16(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, -16(up,%r11,8)
-	adcq	%rdx, %r9
-	movq	-8(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-8(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, -8(up,%r11,8)
-	adcq	%rdx, %r9
-	movq	(mp,%r11,8), %rax
-	mulq	%r12
-	addq	(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, (up,%r11,8)
-	adcq	%rdx, %r9
-	addq	$4, %r11
-	jae	L(i11)
+L(b1):	C lea	(mp), mp
+	lea	-16(up), up
+L(o1):	mov	nneg, i
+	mov	16(up,nneg,8), %rbp	C up[0]
+	imul	invm, %rbp
 
-L(c11):	movq	%r9, (up,%rbp,8)
-	addq	$8, up
-	decq	n
-	jne	L(b11)
+	mov	(mp,i,8), %rax
+	xor	%ebx, %ebx
+	mul	%rbp
+	add	$1, i
+	jnz	1f
+	add	%rax, 8(up,i,8)
+	adc	$0, %rdx
+	mov	%rdx, %r14
+	jmp	L(n1)
+
+1:	mov	%rax, %r9
+	mov	(mp,i,8), %rax
+	mov	%rdx, %r14
+	jmp	L(mi1)
+
+	ALIGN(16)
+L(lo1):	add	%r10, (up,i,8)
+	adc	%rax, %r9
+	mov	(mp,i,8), %rax
+	adc	%rdx, %r14
+L(mi1):	xor	%r10d, %r10d
+	mul	%rbp
+	add	%r9, 8(up,i,8)
+	adc	%rax, %r14
+	adc	%rdx, %rbx
+	mov	8(mp,i,8), %rax
+	mul	%rbp
+	add	%r14, 16(up,i,8)
+	adc	%rax, %rbx
+	adc	%rdx, %r10
+	mov	16(mp,i,8), %rax
+	mul	%rbp
+	xor	%r9d, %r9d
+	xor	%r14d, %r14d
+	add	%rbx, 24(up,i,8)
+	adc	%rax, %r10
+	mov	24(mp,i,8), %rax
+	adc	%rdx, %r9
+	xor	%ebx, %ebx
+	mul	%rbp
+	add	$4, i
+	js	L(lo1)
+L(ed1):	add	%r10, (up)
+	adc	%rax, %r9
+	adc	%rdx, %r14
+	xor	%r10d, %r10d
+	add	%r9, 8(up)
+	adc	$0, %r14
+L(n1):	mov	%r14, 16(up,nneg,8)	C up[0]
+	add	$8, up
+	dec	n
+	jnz	L(o1)
+C	lea	(mp), mp
+	lea	16(up), up
+	jmp	L(common)
+
+L(b0):	C lea	(mp), mp
+	lea	-16(up), up
+L(o0):	mov	nneg, i
+	mov	16(up,nneg,8), %rbp	C up[0]
+	imul	invm, %rbp
+
+	mov	(mp,i,8), %rax
+	xor	%r10d, %r10d
+	mul	%rbp
+	mov	%rax, %r14
+	mov	%rdx, %rbx
+	jmp	L(mi0)
+
+	ALIGN(16)
+L(lo0):	add	%r10, (up,i,8)
+	adc	%rax, %r9
+	mov	(mp,i,8), %rax
+	adc	%rdx, %r14
+	xor	%r10d, %r10d
+	mul	%rbp
+	add	%r9, 8(up,i,8)
+	adc	%rax, %r14
+	adc	%rdx, %rbx
+L(mi0):	mov	8(mp,i,8), %rax
+	mul	%rbp
+	add	%r14, 16(up,i,8)
+	adc	%rax, %rbx
+	adc	%rdx, %r10
+	mov	16(mp,i,8), %rax
+	mul	%rbp
+	xor	%r9d, %r9d
+	xor	%r14d, %r14d
+	add	%rbx, 24(up,i,8)
+	adc	%rax, %r10
+	mov	24(mp,i,8), %rax
+	adc	%rdx, %r9
+	xor	%ebx, %ebx
+	mul	%rbp
+	add	$4, i
+	js	L(lo0)
+L(ed0):	add	%r10, (up)
+	adc	%rax, %r9
+	adc	%rdx, %r14
+	xor	%r10d, %r10d
+	add	%r9, 8(up)
+	adc	$0, %r14
+	mov	%r14, 16(up,nneg,8)	C up[0]
+	add	$8, up
+	dec	n
+	jnz	L(o0)
+C	lea	(mp), mp
+	lea	16(up), up
 	jmp	L(common)
 
 
-	ALIGN(8)
-L(b10):	movq	(up,%rbp,8), %r12	C load up[0]
-	imulq	invm, %r12
-	leaq	5(%rbp), %r11		C inner-loop count
+L(b3):	lea	-8(mp), mp
+	lea	-24(up), up
+L(o3):	mov	nneg, i
+	mov	24(up,nneg,8), %rbp	C up[0]
+	imul	invm, %rbp
 
-	movq	(mp,%rbp,8), %rax
-	mulq	%r12
-	addq	(up,%rbp,8), %rax
-	adcq	%r10, %rdx
-	movq	%rdx, %r9
-	movq	-32(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-32(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, -32(up,%r11,8)	C next iteration up[0]
-	adcq	%rdx, %r9
+	mov	8(mp,i,8), %rax
+	mul	%rbp
+	mov	%rax, %rbx
+	mov	%rdx, %r10
+	jmp	L(mi3)
 
-	testq	%r11, %r11
-	jns	L(c10)			C FIXME: Special exit for un=vn=1?
-
-	ALIGN(8)
-L(i10):	movq	-24(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-24(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%rax, -24(up,%r11,8)
-	movq	%r10, %r9
-	adcq	%rdx, %r9
-	movq	-16(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-16(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, -16(up,%r11,8)
-	adcq	%rdx, %r9
-	movq	-8(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-8(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, -8(up,%r11,8)
-	adcq	%rdx, %r9
-	movq	(mp,%r11,8), %rax
-	mulq	%r12
-	addq	(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, (up,%r11,8)
-	adcq	%rdx, %r9
-	addq	$4, %r11
-	jae	L(i10)
-
-L(c10):	movq	%r9, (up,%rbp,8)
-	addq	$8, up
-	decq	n
-	jne	L(b10)
+	ALIGN(16)
+L(lo3):	add	%r10, (up,i,8)
+	adc	%rax, %r9
+	mov	(mp,i,8), %rax
+	adc	%rdx, %r14
+	xor	%r10d, %r10d
+	mul	%rbp
+	add	%r9, 8(up,i,8)
+	adc	%rax, %r14
+	adc	%rdx, %rbx
+	mov	8(mp,i,8), %rax
+	mul	%rbp
+	add	%r14, 16(up,i,8)
+	adc	%rax, %rbx
+	adc	%rdx, %r10
+L(mi3):	mov	16(mp,i,8), %rax
+	mul	%rbp
+	xor	%r9d, %r9d
+	xor	%r14d, %r14d
+	add	%rbx, 24(up,i,8)
+	adc	%rax, %r10
+	mov	24(mp,i,8), %rax
+	adc	%rdx, %r9
+	xor	%ebx, %ebx
+	mul	%rbp
+	add	$4, i
+	js	L(lo3)
+L(ed3):	add	%r10, 8(up)
+	adc	%rax, %r9
+	adc	%rdx, %r14
+	xor	%r10d, %r10d
+	add	%r9, 16(up)
+	adc	$0, %r14
+	mov	%r14, 24(up,nneg,8)	C up[0]
+	add	$8, up
+	dec	n
+	jnz	L(o3)
+	lea	8(mp), mp
+	lea	24(up), up
 	jmp	L(common)
 
+L(b2):	lea	-16(mp), mp
+	lea	-32(up), up
+L(o2):	mov	nneg, i
+	mov	32(up,nneg,8), %rbp	C up[0]
+	imul	invm, %rbp
 
-	ALIGN(8)
-L(b00):	movq	(up,%rbp,8), %r12	C load up[0]
-	imulq	invm, %r12
-	leaq	3(%rbp), %r11		C inner-loop count
-	xorl	%r9d, %r9d		C clear carry limb
+	mov	16(mp,i,8), %rax
+	mul	%rbp
+	xor	%r14d, %r14d
+	mov	%rax, %r10
+	mov	24(mp,i,8), %rax
+	mov	%rdx, %r9
+	jmp	L(mi2)
 
-	ALIGN(8)
-L(i00):	movq	-24(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-24(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%rax, -24(up,%r11,8)
-	movq	%r10, %r9
-	adcq	%rdx, %r9
-	movq	-16(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-16(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, -16(up,%r11,8)
-	adcq	%rdx, %r9
-	movq	-8(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-8(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, -8(up,%r11,8)
-	adcq	%rdx, %r9
-	movq	(mp,%r11,8), %rax
-	mulq	%r12
-	addq	(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, (up,%r11,8)
-	adcq	%rdx, %r9
-	addq	$4, %r11
-	jae	L(i00)
+	ALIGN(16)
+L(lo2):	add	%r10, (up,i,8)
+	adc	%rax, %r9
+	mov	(mp,i,8), %rax
+	adc	%rdx, %r14
+	xor	%r10d, %r10d
+	mul	%rbp
+	add	%r9, 8(up,i,8)
+	adc	%rax, %r14
+	adc	%rdx, %rbx
+	mov	8(mp,i,8), %rax
+	mul	%rbp
+	add	%r14, 16(up,i,8)
+	adc	%rax, %rbx
+	adc	%rdx, %r10
+	mov	16(mp,i,8), %rax
+	mul	%rbp
+	xor	%r9d, %r9d
+	xor	%r14d, %r14d
+	add	%rbx, 24(up,i,8)
+	adc	%rax, %r10
+	mov	24(mp,i,8), %rax
+	adc	%rdx, %r9
+L(mi2):	xor	%ebx, %ebx
+	mul	%rbp
+	add	$4, i
+	js	L(lo2)
+L(ed2):	add	%r10, 16(up)
+	adc	%rax, %r9
+	adc	%rdx, %r14
+	xor	%r10d, %r10d
+	add	%r9, 24(up)
+	adc	$0, %r14
+	mov	%r14, 32(up,nneg,8)	C up[0]
+	add	$8, up
+	dec	n
+	jnz	L(o2)
+	lea	16(mp), mp
+	lea	32(up), up
 
-L(c00):	movq	%r9, (up,%rbp,8)
-	addq	$8, up
-	decq	n
-	jne	L(b00)
-	jmp	L(common)
-
-
-	ALIGN(8)
-L(b01):	movq	(up,%rbp,8), %r12	C load up[0]
-	imulq	invm, %r12
-	leaq	4(%rbp), %r11		C inner-loop count
-
-	movq	(mp,%rbp,8), %rax
-	mulq	%r12
-	addq	(up,%rbp,8), %rax
-	adcq	%r10, %rdx
-	movq	%rdx, %r9
-
-	testq	%r11, %r11
-	js	L(i01)
-
-	addq	(up), %rdx
-	jnc	1f
-	subq	-8(mp), %rdx
-1:	movq	%rdx, (rp)
-	jmp	L(ret)
-
-	ALIGN(8)
-L(i01):	movq	-24(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-24(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%rax, -24(up,%r11,8)
-	movq	%r10, %r9
-	adcq	%rdx, %r9
-	movq	-16(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-16(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, -16(up,%r11,8)
-	adcq	%rdx, %r9
-	movq	-8(mp,%r11,8), %rax
-	mulq	%r12
-	addq	-8(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, -8(up,%r11,8)
-	adcq	%rdx, %r9
-	movq	(mp,%r11,8), %rax
-	mulq	%r12
-	addq	(up,%r11,8), %rax
-	adcq	%r10, %rdx
-	addq	%r9, %rax
-	movq	%r10, %r9
-	movq	%rax, (up,%r11,8)
-	adcq	%rdx, %r9
-	addq	$4, %r11
-	jae	L(i01)
-
-L(c01):	movq	%r9, (up,%rbp,8)
-	addq	$8, up
-	decq	n
-	jne	L(b01)
 
 L(common):
-	leaq	(mp,%rbp,8), mp		C restore entry mp
+	lea	(mp,nneg,8), mp		C restore entry mp
 
 C   cy = mpn_add_n (rp, up, up - n, n);
 C		    rdi rsi  rdx    rcx
-	leaq	(up,%rbp,8), up		C up -= n
-	leaq	(up,%rbp,8), %rdx	C rdx = up - n [up entry value]
-	movq	rp, %rbp		C preserve rp over first call
-	movq	(%rsp), %rcx		C pass entry n
-	movq	rp, %rdi
+	lea	(up,nneg,8), up		C up -= n
+	lea	(up,nneg,8), %rdx	C rdx = up - n [up entry value]
+	mov	rp, nneg		C preserve rp over first call
+	mov	(%rsp), %rcx		C pass entry n
+C	mov	rp, %rdi
 	CALL(	mpn_add_n)
-	testl	%eax, %eax
+	test	R32(%rax), R32(%rax)
 	jz	L(ret)
 
 C     mpn_sub_n (rp, rp, mp, n);
 C		 rdi rsi rdx rcx
-	movq	%rbp, %rdi
-	movq	%rbp, %rsi
-	movq	mp, %rdx
-	movq	(%rsp), %rcx		C pass entry n
+	mov	nneg, %rdi
+	mov	nneg, %rsi
+	mov	mp, %rdx
+	mov	(%rsp), %rcx		C pass entry n
 	CALL(	mpn_sub_n)
 
-L(ret):	popq	n			C just increment rsp
-	popq	%r12
-	popq	%rbx
-	popq	%rbp
+L(ret):	pop	n			C just increment rsp
+	pop	%r14
+	pop	%r13
+	pop	%r12
+	pop	%rbx
+	pop	%rbp
 	ret
 EPILOGUE()
