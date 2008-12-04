d64bd442b965ce0c4963f0e708c496623323fb8b
Torbjorn Granlund <tege@gmplib.org>
2008-12-04 11:12:38 +0100

    Retune.

--- mpn/x86_64/core2/gmp-mparam.h
+++ mpn/x86_64/core2/gmp-mparam.h
@@ -28,8 +28,8 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 #define MUL_KARATSUBA_THRESHOLD          18
 #define MUL_TOOM3_THRESHOLD              65
 
-#define SQR_BASECASE_THRESHOLD           10
-#define SQR_KARATSUBA_THRESHOLD          23
+#define SQR_BASECASE_THRESHOLD            0  /* always (native) */
+#define SQR_KARATSUBA_THRESHOLD          24
 #define SQR_TOOM3_THRESHOLD              89
 
 #define MULLOW_BASECASE_THRESHOLD         0  /* always */
@@ -37,7 +37,7 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 #define MULLOW_MUL_N_THRESHOLD          330
 
 #define DIV_SB_PREINV_THRESHOLD           0  /* always */
-#define DIV_DC_THRESHOLD                 54
+#define DIV_DC_THRESHOLD                 74
 #define POWM_THRESHOLD                  122
 
 #define MATRIX22_STRASSEN_THRESHOLD      25
@@ -55,13 +55,13 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 
 #define GET_STR_DC_THRESHOLD             11
 #define GET_STR_PRECOMPUTE_THRESHOLD     19
-#define SET_STR_DC_THRESHOLD            248
-#define SET_STR_PRECOMPUTE_THRESHOLD   2083
+#define SET_STR_DC_THRESHOLD            552
+#define SET_STR_PRECOMPUTE_THRESHOLD   2052
 
 #define MUL_FFT_TABLE  { 336, 672, 1600, 2816, 7168, 20480, 81920, 196608, 786432, 0 }
 #define MUL_FFT_MODF_THRESHOLD          352
 #define MUL_FFT_THRESHOLD              1920
 
 #define SQR_FFT_TABLE  { 368, 736, 1472, 2816, 7168, 20480, 49152, 196608, 786432, 0 }
-#define SQR_FFT_MODF_THRESHOLD          264
-#define SQR_FFT_THRESHOLD              1664
+#define SQR_FFT_MODF_THRESHOLD          320
+#define SQR_FFT_THRESHOLD              1920
