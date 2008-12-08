a6b61e9ac716362dec3db08d1b1aed8b7ed50687
Torbjorn Granlund <tege@gmplib.org>
2008-12-08 01:25:50 +0100

    Handle some small exponents locally.

--- mpz/pow_ui.c
+++ mpz/pow_ui.c
@@ -1,6 +1,6 @@
 /* mpz_pow_ui -- mpz raised to ulong.
 
-Copyright 2001 Free Software Foundation, Inc.
+Copyright 2001, 2008 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -23,5 +23,20 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 void
 mpz_pow_ui (mpz_ptr r, mpz_srcptr b, unsigned long int e)
 {
-  mpz_n_pow_ui (r, PTR(b), (mp_size_t) SIZ(b), e);
+  /* We test some small exponents here, mainly to avoid the overhead of
+     mpz_n_pow_ui for small bases and exponents.  */
+  switch (e)
+    {
+    case 0:
+      mpz_set_ui (r, 1);
+      break;
+    case 1:
+      mpz_set (r, b);
+      break;
+    case 2:
+      mpz_mul (r, b, b);
+      break;
+    default:
+      mpz_n_pow_ui (r, PTR(b), (mp_size_t) SIZ(b), e);
+    }
 }
