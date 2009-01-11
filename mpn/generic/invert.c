e5a56ef709021403e18b7d5fe88b2b55684de9fd
Torbjorn Granlund <tege@gmplib.org>
2009-01-11 09:24:40 +0100

    Correct license text. Misc minor changes.

--- mpn/generic/invert.c
+++ mpn/generic/invert.c
@@ -4,22 +4,30 @@ Copyright (C) 2007 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
-The GNU MP Library is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the Free
-Software Foundation; either version 3 of the License, or (at your option) any
-later version.
+The GNU MP Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
 
 The GNU MP Library is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-details.
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
 
-You should have received a copy of the GNU General Public License along with
-the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 
 #include <stdlib.h>
 #include "gmp.h"
 #include "gmp-impl.h"
+
+/* Formulas:
+	z = 2z-(zz)d
+	z = 2z-(zd)z
+	z = z(2-zd)
+	z = z-z*(zd-1)
+	z = z+z*(1-zd)
+*/
 
 mp_size_t
 mpn_invert_itch (mp_size_t n)
--- mpn/generic/toom_interpolate_5pts.c
+++ mpn/generic/toom_interpolate_5pts.c
@@ -22,8 +22,8 @@ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
 for more details.
 
-You should have received a copy of the GNU General Public License along with
-the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 
 #include "gmp.h"
 #include "gmp-impl.h"
