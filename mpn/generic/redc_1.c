ed5ef36972e23fd963b26f42eda9edd405726c4b
Torbjorn Granlund <tege@gmplib.org>
2009-02-04 11:31:47 +0100

    Update to current FSF header.

--- mpn/generic/redc_1.c
+++ mpn/generic/redc_1.c
@@ -8,9 +8,11 @@ Copyright (C) 2000, 2001, 2002, 2004, 2008 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
+This file is part of the GNU MP Library.
+
 The GNU MP Library is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation; either version 2.1 of the License, or (at your
+the Free Software Foundation; either version 3 of the License, or (at your
 option) any later version.
 
 The GNU MP Library is distributed in the hope that it will be useful, but
@@ -19,9 +21,7 @@ or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
-the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-MA 02111-1307, USA. */
+along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 
 #include "gmp.h"
 #include "gmp-impl.h"
