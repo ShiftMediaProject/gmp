d11007d018bbfb067d5ecae93430ce92b25b50d3
Niels Möller <nisse@lysator.liu.se>
2009-10-23 10:17:47 +0200

    New test program for toom52.

---
+++ tests/mpn/t-toom52.c
@@ -0,0 +1,9 @@
+#define mpn_toomMN_mul mpn_toom52_mul
+/* #define mpn_toomMN_mul_itch mpn_toom52_mul_itch */
+#define mpn_toomMN_mul_itch(an,bn) 1
+
+#define MIN_AN 18
+#define MIN_BN(an) (((an) + 9) / (size_t) 5)
+#define MAX_BN(an) (((an) - 3) >> 1)
+
+#include "toom-shared.h"
