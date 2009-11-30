e39827efc947494dea00e2b3be36b85c2d167832
Marco Bodrato <bodrato@mail.dm.unipi.it>
2009-11-30 17:18:36 +0100

    Use mpn_rsblsh2_n ;-)

--- mpn/generic/fib2_ui.c
+++ mpn/generic/fib2_ui.c
@@ -24,8 +24,6 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 #include <stdio.h>
 #include "gmp.h"
 #include "gmp-impl.h"
-#include "longlong.h"
-
 
 /* change this to "#define TRACE(x) x" for diagnostics */
 #define TRACE(x)
@@ -65,7 +63,6 @@ mpn_fib2_ui (mp_ptr fp, mp_ptr f1p, unsigned long int n)
   mp_ptr         xp, yp;
   mp_size_t      size;
   unsigned long  nfirst, mask;
-  TMP_DECL;
 
   TRACE (printf ("mpn_fib2_ui n=%lu\n", n));
 
@@ -85,6 +82,7 @@ mpn_fib2_ui (mp_ptr fp, mp_ptr f1p, unsigned long int n)
   if (mask != 1)
     {
       mp_size_t  alloc;
+      TMP_DECL;
 
       TMP_MARK;
       alloc = MPN_FIB2_SIZE (n);
@@ -127,15 +125,21 @@ mpn_fib2_ui (mp_ptr fp, mp_ptr f1p, unsigned long int n)
 
 	  /* Calculate F[2k+1] = 4*F[k]^2 - F[k-1]^2 + 2*(-1)^k.
 	     n&mask is the low bit of our implied k.  */
+#if HAVE_NATIVE_mpn_rsblsh2_n
+	  fp[size] = mpn_rsblsh2_n (fp, yp, xp, size);
+	  if ((n & mask) == 0) MPN_INCR_U(fp, size + 1, 2);
+	  c = fp[size];
+#else
 	  c = mpn_lshift (fp, xp, size, 2);
 	  fp[0] |= (n & mask ? 0 : 2);	 /* possible +2 */
 	  c -= mpn_sub_n (fp, fp, yp, size);
+	  fp[size] = c;
+#endif
 	  ASSERT (n & (mask << 1) ? fp[0] != 0 && fp[0] != 1 : 1);
 	  fp[0] -= (n & mask ? 2 : 0);	 /* possible -2 */
 	  ASSERT (alloc >= size+1);
 	  xp[size] = 0;
 	  yp[size] = 0;
-	  fp[size] = c;
 	  size += (c != 0);
 
 	  /* Calculate F[2k-1] = F[k]^2 + F[k-1]^2.
