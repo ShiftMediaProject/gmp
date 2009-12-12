5073d1aa195b6edd555b6c9d977ee0a8a31db963
Marco Bodrato <bodrato@mail.dm.unipi.it>
2009-12-12 21:59:46 +0100

    Rewrite of mpn_invert, use Newton iterations and mulmod_bnm1.

--- gmp-impl.h
+++ gmp-impl.h
@@ -1703,7 +1703,7 @@ __GMP_DECLSPEC unsigned long int gmp_nextprime (gmp_primesieve_t *);
 #endif
 
 #ifndef INV_NEWTON_THRESHOLD
-#define INV_NEWTON_THRESHOLD     654
+#define INV_NEWTON_THRESHOLD      30
 #endif
 
 #ifndef BINV_NEWTON_THRESHOLD
