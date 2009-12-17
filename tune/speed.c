e355186a9214a33c2d21f02b8320a0374cafdb50
Marco Bodrato <bodrato@mail.dm.unipi.it>
2009-12-17 13:40:21 +0100

    Nussbaumer multiplication.

--- tune/speed.c
+++ tune/speed.c
@@ -299,6 +299,7 @@ const struct routine_t {
   { "mpn_toom44_mul",    speed_mpn_toom44_mul       },
   { "mpn_toom32_mul",    speed_mpn_toom32_mul       },
   { "mpn_toom42_mul",    speed_mpn_toom42_mul       },
+  { "mpn_nussbaumer_mul",    speed_mpn_nussbaumer_mul    },
   { "mpn_mul_fft_full",      speed_mpn_mul_fft_full      },
   { "mpn_mul_fft_full_sqr",  speed_mpn_mul_fft_full_sqr  },
 
