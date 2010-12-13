8c3e656e83daf1b28e3a6cf088ac4e4090caedb5
Torbjorn Granlund <tege@gmplib.org>
2010-12-13 21:40:50 +0100

    Rewrite mpz/mod.c.

--- mpz/mod.c
+++ mpz/mod.c
@@ -1,6 +1,6 @@
 /* mpz_mod -- The mathematical mod function.
 
-Copyright 1991, 1993, 1994, 1995, 1996, 2001, 2002, 2005 Free Software
+Copyright 1991, 1993, 1994, 1995, 1996, 2001, 2002, 2005, 2010 Free Software
 Foundation, Inc.
 
 This file is part of the GNU MP Library.
@@ -25,33 +25,34 @@ void
 mpz_mod (mpz_ptr rem, mpz_srcptr dividend, mpz_srcptr divisor)
 {
   mp_size_t divisor_size = divisor->_mp_size;
-  mpz_t temp_divisor;		/* N.B.: lives until function returns! */
+  mp_size_t rn, bn;
+  mpz_t temp_divisor;
   TMP_DECL;
 
   TMP_MARK;
+
+  bn = ABSIZ(divisor);
 
   /* We need the original value of the divisor after the remainder has been
      preliminary calculated.  We have to copy it to temporary space if it's
      the same variable as REM.  */
   if (rem == divisor)
     {
-      MPZ_TMP_INIT (temp_divisor, ABS (divisor_size));
-      mpz_set (temp_divisor, divisor);
-      divisor = temp_divisor;
+      PTR(temp_divisor) = TMP_ALLOC_LIMBS (bn);
+      MPN_COPY (PTR(temp_divisor), PTR(divisor), bn);
     }
+  else
+    {
+      PTR(temp_divisor) = PTR(divisor);
+    }
+  SIZ(temp_divisor) = bn;
+  divisor = temp_divisor;
 
   mpz_tdiv_r (rem, dividend, divisor);
 
-  if (rem->_mp_size != 0)
-    {
-      if (dividend->_mp_size < 0)
-	{
-	  if (divisor->_mp_size < 0)
-	    mpz_sub (rem, rem, divisor);
-	  else
-	    mpz_add (rem, rem, divisor);
-	}
-    }
+  rn = SIZ (rem);
+  if (rn < 0)
+    mpz_add (rem, rem, divisor);
 
   TMP_FREE;
 }
