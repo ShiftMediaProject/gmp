55a68821ea1fdc60d8ea06831f47be1ac2438adb
Torbjorn Granlund <tege@gmplib.org>
2011-01-31 09:55:15 +0100

    Retune.

--- mpn/powerpc64/mode64/p6/gmp-mparam.h
+++ mpn/powerpc64/mode64/p6/gmp-mparam.h
@@ -45,12 +45,12 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 #define MUL_TOOM42_TO_TOOM53_THRESHOLD      73
 #define MUL_TOOM42_TO_TOOM63_THRESHOLD      66
 
-#define SQR_BASECASE_THRESHOLD               9
+#define SQR_BASECASE_THRESHOLD              12
 #define SQR_TOOM2_THRESHOLD                 30
 #define SQR_TOOM3_THRESHOLD                 53
 #define SQR_TOOM4_THRESHOLD                148
 #define SQR_TOOM6_THRESHOLD                226
-#define SQR_TOOM8_THRESHOLD                430
+#define SQR_TOOM8_THRESHOLD                296
 
 #define MULMOD_BNM1_THRESHOLD               14
 #define SQRMOD_BNM1_THRESHOLD               14
@@ -105,14 +105,14 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 #define SQR_FFT_TABLE3_SIZE 80
 #define SQR_FFT_THRESHOLD                 2368
 
-#define MULLO_BASECASE_THRESHOLD             7
-#define MULLO_DC_THRESHOLD                  28
-#define MULLO_MUL_N_THRESHOLD             2742
+#define MULLO_BASECASE_THRESHOLD             8
+#define MULLO_DC_THRESHOLD                  31
+#define MULLO_MUL_N_THRESHOLD             6253
 
-#define DC_DIV_QR_THRESHOLD                 32
+#define DC_DIV_QR_THRESHOLD                 27
 #define DC_DIVAPPR_Q_THRESHOLD             112
 #define DC_BDIV_QR_THRESHOLD                29
-#define DC_BDIV_Q_THRESHOLD                 78
+#define DC_BDIV_Q_THRESHOLD                 86
 
 #define INV_MULMOD_BNM1_THRESHOLD           58
 #define INV_NEWTON_THRESHOLD                93
@@ -129,11 +129,11 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 
 #define MATRIX22_STRASSEN_THRESHOLD         13
 #define HGCD_THRESHOLD                      69
-#define GCD_DC_THRESHOLD                   400
+#define GCD_DC_THRESHOLD                   501
 #define GCDEXT_DC_THRESHOLD                201
 #define JACOBI_BASE_METHOD                   4
 
 #define GET_STR_DC_THRESHOLD                17
 #define GET_STR_PRECOMPUTE_THRESHOLD        29
-#define SET_STR_DC_THRESHOLD               532
+#define SET_STR_DC_THRESHOLD               638
 #define SET_STR_PRECOMPUTE_THRESHOLD      1648
