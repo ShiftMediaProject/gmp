e2bcdf106b5186e0799e4109cbb77120e3fef0b1
Marco Bodrato <bodrato@mail.dm.unipi.it>
2011-02-07 08:05:27 +0100

    Update some copyright year.

--- gmp-impl.h
+++ gmp-impl.h
@@ -4,7 +4,7 @@
    BE SUBJECT TO INCOMPATIBLE CHANGES IN FUTURE GNU MP RELEASES.
 
 Copyright 1991, 1993, 1994, 1995, 1996, 1997, 1999, 2000, 2001, 2002, 2003,
-2004, 2005, 2006, 2007, 2008, 2009, 2010 Free Software Foundation, Inc.
+2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
--- mpz/lucnum_ui.c
+++ mpz/lucnum_ui.c
@@ -1,6 +1,6 @@
 /* mpz_lucnum_ui -- calculate Lucas number.
 
-Copyright 2001, 2003, 2005 Free Software Foundation, Inc.
+Copyright 2001, 2003, 2005, 2011 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
