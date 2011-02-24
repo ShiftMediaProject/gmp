1d578983d7e54c556c0aa4ae55213ae9d368aa0e
Torbjorn Granlund <tege@gmplib.org>
2011-02-24 22:19:20 +0100

    Fix typo in MULFUNC_PROLOGUE.

--- mpn/x86/p6/sse2/mod_1_4.asm
+++ mpn/x86/p6/sse2/mod_1_4.asm
@@ -1,26 +1,23 @@
 dnl  Intel P6/SSE2 mpn_mod_1_4.
 
-dnl  Copyright 2009 Free Software Foundation, Inc.
+dnl  Copyright 2009, 2011 Free Software Foundation, Inc.
 dnl
 dnl  This file is part of the GNU MP Library.
 dnl
-dnl  The GNU MP Library is free software; you can redistribute it and/or
-dnl  modify it under the terms of the GNU Lesser General Public License as
-dnl  published by the Free Software Foundation; either version 3 of the
-dnl  License, or (at your option) any later version.
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 3 of the License, or (at
+dnl  your option) any later version.
 dnl
-dnl  The GNU MP Library is distributed in the hope that it will be useful,
-dnl  but WITHOUT ANY WARRANTY; without even the implied warranty of
-dnl  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-dnl  Lesser General Public License for more details.
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
 dnl
 dnl  You should have received a copy of the GNU Lesser General Public License
 dnl  along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.
 
 include(`../config.m4')
 
-C TODO
-C  * Write P6 specific SSE2 code.
-
-MULFUNC_PROLOGUE(mpn_addmul_1)
+MULFUNC_PROLOGUE(mpn_mod_1s_4p)
 include_mpn(`x86/pentium4/sse2/mod_1_4.asm')
