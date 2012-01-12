8f0e8f8f1c67d6eb259d421a8f5b1f786248318c
Torbjorn Granlund <tege@gmplib.org>
2012-01-12 13:41:41 +0100

    Test longer bit strings.

--- tests/t-popc.c
+++ tests/t-popc.c
@@ -1,6 +1,6 @@
 /* Test popc_limb.
 
-Copyright 2002 Free Software Foundation, Inc.
+Copyright 2002, 2012 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -48,6 +48,20 @@ main (void)
           mpn_trace ("  want", &want, (mp_size_t) 1);
           mpn_trace ("  got ", &got,  (mp_size_t) 1);
           abort ();
+        }
+    }
+
+  src = 0;
+  want = 0;
+  for (i = 0; i < GMP_LIMB_BITS; i++)
+    {
+      src += CNST_LIMB(1) << i;
+      want += 1;
+
+      popc_limb (got, src);
+      if (got != want)
+        {
+	  goto error;
         }
     }
 
