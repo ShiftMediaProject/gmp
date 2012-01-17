a197d98a719df846920b5a083a45895497c73f08
Torbjorn Granlund <tege@gmplib.org>
2012-01-17 16:00:28 +0100

    Add forgotten copyright years.

--- tests/rand/t-lc2exp.c
+++ tests/rand/t-lc2exp.c
@@ -1,6 +1,6 @@
 /* Exercise the lc2exp random functions.
 
-Copyright 2002 Free Software Foundation, Inc.
+Copyright 2002, 2011 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
