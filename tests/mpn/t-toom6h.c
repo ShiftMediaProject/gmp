7fe03b817fec10f39c47f1987fbb647725d703f3
Torbjorn Granlund <tege@gmplib.org>
2012-02-02 14:18:32 +0100

    (MIN_BN): Make more consistent with ASSERT in tested function.

--- tests/mpn/t-toom6h.c
+++ tests/mpn/t-toom6h.c
@@ -6,7 +6,7 @@
 /* Smaller sizes not supported; may lead to recursive calls to
    toom22_mul, toom33_mul, or toom44_mul with invalid input size. */
 #define MIN_AN MUL_TOOM6H_MIN
-#define MIN_BN(an) (MAX ((an*3)>>3, 42) )
+#define MIN_BN(an) (MAX ((an*3)>>3, 46))
 
 #define COUNT 1000
 
