0f980f0b3e680f31aa1503fbc2ec6e5f2d8cb712
Marco Bodrato <bodrato@mail.dm.unipi.it>
2012-05-07 19:19:49 +0200

    typos in previous commit (was 32-bit only)

--- tune/tuneup.c
+++ tune/tuneup.c
@@ -2642,7 +2642,7 @@ tune_fac_ui (void)
   param.function = speed_mpz_fac_ui_tune;
 
   param.name = "FAC_DSC_THRESHOLD";
-  param.min_size = 42;
+  param.min_size = 70;
   param.max_size = FAC_DSC_THRESHOLD_LIMIT;
   one (&fac_dsc_threshold, &param);
 
