83f65392dadb2e56fc7bfb1237f5aefa8b9ccd7f
Marco Bodrato <bodrato@mail.dm.unipi.it>
2012-05-18 12:29:07 +0200

    mpz/primorial_ui.c: Disable unused primesieve functions

--- mpz/primorial_ui.c
+++ mpz/primorial_ui.c
@@ -72,8 +72,10 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 /* Section sieve: sieving functions and tools for primes */
 /*********************************************************/
 
+#if WANT_ASSERT
 static mp_limb_t
 bit_to_n (mp_limb_t bit) { return (bit*3+4)|1; }
+#endif
 
 /* id_to_n (x) = bit_to_n (x-1) = (id*3+1)|1*/
 static mp_limb_t
@@ -83,8 +85,10 @@ id_to_n  (mp_limb_t id)  { return id*3+1+(id&1); }
 static mp_limb_t
 n_to_bit (mp_limb_t n) { return ((n-5)|1)/3U; }
 
+#if WANT_ASSERT
 static mp_size_t
 primesieve_size (mp_limb_t n) { return n_to_bit(n) / GMP_LIMB_BITS + 1; }
+#endif
 
 /*********************************************************/
 /* Section primorial: implementation                     */
