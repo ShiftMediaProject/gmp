b2000d44455ef604d99d05b7d6ee0d2a29ec9ddf
Torbjorn Granlund <tege@gmplib.org>
2013-03-10 09:28:51 +0100

    Add missing header.

--- mpn/x86_64/mulx/adx/missing.asm
+++ mpn/x86_64/mulx/adx/missing.asm
@@ -1,3 +1,25 @@
+dnl  AMD64 MULX/ADX simultion support.
+
+dnl  Contributed to the GNU project by Torbjörn Granlund.
+
+dnl  Copyright 2013 Free Software Foundation, Inc.
+
+dnl  This file is part of the GNU MP Library.
+
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 3 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.
+
+
 include(`../config.m4')
 
 ASM_START()
