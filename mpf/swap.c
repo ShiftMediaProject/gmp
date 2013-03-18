f1b01cc447c5bbd91374243e36ed967a94e093db
Marco Bodrato <bodrato@mail.dm.unipi.it>
2013-03-18 07:31:44 +0100

    wap.c: Reduce the number of variables.

--- mpf/swap.c
+++ mpf/swap.c
@@ -1,6 +1,6 @@
 /* mpf_swap (U, V) -- Swap U and V.
 
-Copyright 1997, 1998, 2000, 2001 Free Software Foundation, Inc.
+Copyright 1997, 1998, 2000, 2001, 2013 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -23,28 +23,24 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 void
 mpf_swap (mpf_ptr u, mpf_ptr v) __GMP_NOTHROW
 {
-  mp_ptr up, vp;
-  mp_size_t usize, vsize;
-  mp_size_t uprec, vprec;
-  mp_exp_t  uexp, vexp;
+  mp_ptr tptr;
+  mp_size_t tprec;
+  mp_size_t tsiz;
+  mp_exp_t  texp;
 
-  uprec = u->_mp_prec;
-  vprec = v->_mp_prec;
-  v->_mp_prec = uprec;
-  u->_mp_prec = vprec;
+  tprec = PREC(u);
+  PREC(u) = PREC(v);
+  PREC(v) = tprec;
 
-  usize = u->_mp_size;
-  vsize = v->_mp_size;
-  v->_mp_size = usize;
-  u->_mp_size = vsize;
+  tsiz = SIZ(u);
+  SIZ(u) = SIZ(v);
+  SIZ(v) = tsiz;
 
-  uexp = u->_mp_exp;
-  vexp = v->_mp_exp;
-  v->_mp_exp = uexp;
-  u->_mp_exp = vexp;
+  texp = EXP(u);
+  EXP(u) = EXP(v);
+  EXP(v) = texp;
 
-  up = u->_mp_d;
-  vp = v->_mp_d;
-  v->_mp_d = up;
-  u->_mp_d = vp;
+  tptr = PTR(u);
+  PTR(u) = PTR(v);
+  PTR(v) = tptr;
 }
