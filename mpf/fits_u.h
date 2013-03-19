fcdbe068d2ca3ab9460c40d05a98c9b07df374a7
Marco Bodrato <bodrato@mail.dm.unipi.it>
2013-03-19 22:06:09 +0100

    mpf/fits_u.h: accept numbers truncating to zero before checking the sign.

--- mpf/fits_u.h
+++ mpf/fits_u.h
@@ -1,6 +1,6 @@
 /* mpf_fits_u*_p -- test whether an mpf fits a C unsigned type.
 
-Copyright 2001, 2002 Free Software Foundation, Inc.
+Copyright 2001, 2002, 2013 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -31,13 +31,13 @@ FUNCTION (mpf_srcptr f) __GMP_NOTHROW
   mp_exp_t   exp;
   mp_limb_t  fl;
 
+  exp = EXP(f);
+  if (exp < 1)
+    return 1;  /* -1 < f < 1 truncates to zero, so fits */
+
   fn = SIZ(f);
   if (fn <= 0)
     return fn == 0;  /* zero fits, negatives don't */
-
-  exp = EXP(f);
-  if (exp < 1)
-    return 1;  /* 0 < f < 1 truncates to zero, so fits */
 
   fp = PTR(f);
 
