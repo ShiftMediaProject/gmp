36c9e5ef5c854f7370597d5f0dda9cf4552e38fa
Torbjorn Granlund <tege@gmplib.org>
2013-08-03 00:30:40 +0200

    Provide sandybridge mul_2.

---
+++ mpn/x86_64/coreisbr/mul_2.asm
@@ -0,0 +1,143 @@
+dnl  X86-64 mpn_mul_2 optimised for Intel Sandy Bridge.
+
+dnl  Copyright 2003, 2004, 2005, 2007, 2008, 2011, 2012, 2013 Free Software
+dnl  Foundation, Inc.
+
+dnl  This file is part of the GNU MP Library.
+
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 3 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.
+
+include(`../config.m4')
+
+C	     cycles/limb
+C AMD K8,K9
+C AMD K10
+C AMD bull
+C AMD pile
+C AMD bobcat
+C AMD jaguar
+C Intel P4
+C Intel core
+C Intel NHM
+C Intel SBR	 2.57		2.52
+C Intel IBR	 2.35		2.32
+C Intel HWL	 2.02		this (for now)
+C Intel BWL
+C Intel atom
+C VIA nano
+
+C When playing with pointers, set this to $2 to fall back to conservative
+C indexing in wind-dowm code.
+define(`I',`$1')
+
+define(`rp',      `%rdi')   C rcx
+define(`up',      `%rsi')   C rdx
+define(`n_param', `%rdx')   C r8
+define(`vp',      `%rcx')   C r9
+
+define(`n',	  `%rcx')
+define(`v0',      `%rbx')
+define(`v1',      `%rbp')
+
+define(`w0',	`%r8')
+define(`w1',	`%r9')
+define(`w2',	`%r10')
+define(`w3',	`%r11')
+
+ASM_START()
+	TEXT
+	ALIGN(32)
+PROLOGUE(mpn_mul_2)
+	push	%rbx
+	push	%rbp
+
+	mov	(vp), v0
+	mov	8(vp), v1
+
+	mov	(up), %rax
+	lea	(up,n_param,8), up
+	lea	(rp,n_param,8), rp
+
+	test	$1, R8(n_param)
+	jnz	L(b1)
+
+L(b0):	mov	$0, R32(n)
+	sub	n_param, n
+	xor	w0, w0
+	mul	v0
+	mov	%rax, w2
+	mov	%rdx, w1
+	mov	(up,n,8), %rax
+	jmp	L(lo0)
+
+L(b1):	mov	$1, R32(n)
+	sub	n_param, n
+	xor	w2, w2
+	mul	v0
+	mov	%rax, w0
+	mov	%rdx, w3
+	mov	-8(up,n,8), %rax
+	mul	v1
+	jmp	L(lo1)
+
+	ALIGN(32)
+L(top):	mul	v0
+	add	%rax, w0		C 1
+	mov	%rdx, w3		C 2
+	adc	$0, w3			C 2
+	mov	-8(up,n,8), %rax
+	mul	v1
+	add	w1, w0			C 1
+	adc	$0, w3			C 2
+L(lo1):	add	%rax, w2		C 2
+	mov	w0, -8(rp,n,8)		C 1
+	mov	%rdx, w0		C 3
+	adc	$0, w0			C 3
+	mov	(up,n,8), %rax
+	mul	v0
+	add	%rax, w2		C 2
+	mov	%rdx, w1		C 3
+	adc	$0, w1			C 3
+	add	w3, w2			C 2
+	mov	(up,n,8), %rax
+	adc	$0, w1			C 1
+L(lo0):	mul	v1
+	mov	w2, (rp,n,8)		C 2
+	add	%rax, w0		C 3
+	mov	%rdx, w2		C 4
+	mov	8(up,n,8), %rax
+	adc	$0, w2			C 4
+	add	$2, n
+	jnc	L(top)
+
+L(end):	mul	v0
+	add	%rax, w0
+	mov	%rdx, w3
+	adc	$0, w3
+	mov	I(-8(up),-8(up,n,8)), %rax
+	mul	v1
+	add	w1, w0
+	adc	$0, w3
+	add	%rax, w2
+	mov	w0, I(-8(rp),-8(rp,n,8))
+	adc	$0, %rdx
+	add	w3, w2
+	mov	w2, I((rp),(rp,n,8))
+	adc	$0, %rdx
+	mov	%rdx, %rax
+
+	pop	%rbp
+	pop	%rbx
+	ret
+EPILOGUE()
