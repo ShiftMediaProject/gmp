4494bf99ba64f51dd0b9e8feee0878cbf2bed862
Torbjorn Granlund <tege@gmplib.org>
2013-09-15 17:20:43 +0200

    Rewrite for a slight speed-up for small and large operands.

--- mpn/x86_64/coreisbr/aorsmul_1.asm
+++ mpn/x86_64/coreisbr/aorsmul_1.asm
@@ -21,29 +21,34 @@ dnl  along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.
 include(`../config.m4')
 
 C	     cycles/limb
-C AMD K8,K9	 4.77
-C AMD K10	 4.77
-C AMD bd1	 ?
-C AMD bobcat	 5.78
-C Intel P4	15-17
-C Intel core2	 5.4
-C Intel NHM	 5.23
-C Intel SBR	 3.25
-C Intel atom	 ?
-C VIA nano	 5.5
+C AMD K8,K9
+C AMD K10
+C AMD bull
+C AMD pile
+C AMD steam
+C AMD bobcat
+C AMD jaguar
+C Intel P4
+C Intel core
+C Intel NHM
+C Intel SBR	 3.24 (average, fluctuating in 3.20-3.57)
+C Intel IBR	 3.04
+C Intel HWL
+C Intel BWL
+C Intel atom
+C VIA nano
 
 C The loop of this code is the result of running a code generation and
-C optimisation tool suite written by David Harvey and Torbjorn Granlund.
-
-C TODO
-C  * The loop is great, but the prologue code was quickly written.  Tune it!
+C optimization tool suite written by David Harvey and Torbjörn Granlund.
 
 define(`rp',      `%rdi')   C rcx
 define(`up',      `%rsi')   C rdx
 define(`n_param', `%rdx')   C r8
 define(`v0',      `%rcx')   C r9
 
-define(`n',	  `%rbx')
+define(`n',       `%rbx')
+
+define(`I',`$1')
 
 ifdef(`OPERATION_addmul_1',`
       define(`ADDSUB',        `add')
@@ -64,12 +69,10 @@ IFDOS(`	define(`rp',     ``%rcx'')') dnl
 IFDOS(`	define(`v0',     ``%r9'')') dnl
 IFDOS(`	define(`r9',     ``rdi'')') dnl
 IFDOS(`	define(`n_param',``%r8'')') dnl
-IFDOS(`	define(`n',      ``%r8'')') dnl
-IFDOS(`	define(`r8',     ``rbx'')') dnl
 
 ASM_START()
 	TEXT
-	ALIGN(16)
+	ALIGN(32)
 PROLOGUE(func)
 
 IFDOS(``push	%rsi		'')
@@ -78,106 +81,117 @@ IFDOS(``mov	%rdx, %rsi	'')
 
 	mov	(up), %rax
 	push	%rbx
-	mov	R32(`n_param'), R32(%rdx)
-IFSTD(`	mov	n_param, n		')
+	lea	(up,n_param,8), up
+	lea	(rp,n_param,8), rp
 
-	lea	-8(up,n_param,8), up
-	and	$3, R32(%rdx)
-	jz	L(b0)
-	cmp	$2, R32(%rdx)
-	jz	L(b2)
-	jnc	L(b3)
+	test	$1, R8(n_param)
+	jnz	L(b13)
 
-L(b1):	mov	(rp), %r8
-	lea	-8(rp,n,8), rp
-	neg	n
-	mov	$0, R32(%r11)
-	add	$4, n
-	jc	L(end)
-	jmp	L(top)
+L(b02):	xor	R32(%r11), R32(%r11)
+	test	$2, R8(n_param)
+	jnz	L(b2)
 
-L(b2):	mov	(rp), %r10
-	lea	-8(rp,n,8), rp
-	neg	n
-	add	$1, n
-	mul	v0
-	ADDSUB	%rax, %r10
-	mov	8(up,n,8), %rax
-	mov	%rdx, %r11
-	mov	$0, R32(%r9)
-	jmp	L(L2)
-
-L(b3):	mov	(rp), %r8
-	lea	-8(rp,n,8), rp
-	neg	n
-	add	$2, n
+L(b0):	mov	$1, R32(n)
+	sub	n_param, n
 	mul	v0
 	mov	%rdx, %r9
-	mov	$0, R32(%r11)
-	jmp	L(L3)
-
-L(b0):	mov	(rp), %r10
-	lea	-8(rp,n,8), rp
-	neg	n
-	add	$3, n
-	mul	v0
-	ADDSUB	%rax, %r10
-	mov	%rdx, %r11
-	mov	-8(up,n,8), %rax
-	adc	$0, %r11
-	mov	$0, R32(%r9)
-	jmp	L(L0)
+	mov	-8(rp,n,8), %r8
+	jmp	L(e0)
 
 	ALIGN(16)
-L(top):	mul	v0
-	ADDSUB	%rax, %r8
-	mov	%rdx, %r9
-	adc	$0, %r9
-	mov	-16(up,n,8), %rax
-	ADDSUB	%r11, %r8
-	mov	-16(rp,n,8), %r10
-	adc	$0, %r9
+L(b2):	mov	$-1, n
+	sub	n_param, n
 	mul	v0
-	ADDSUB	%rax, %r10
+	mov	8(rp,n,8), %r8
+	mov	%rdx, %r9
+	jmp	L(e2)
+
+	ALIGN(16)
+L(b13):	xor	R32(%r9), R32(%r9)
+	test	$2, R8(n_param)
+	jnz	L(b3)
+
+L(b1):	mov	$2, R32(n)
+	sub	n_param, n
+	jns	L(1)
+	mul	v0
+	mov	-16(rp,n,8), %r10
 	mov	%rdx, %r11
+	jmp	L(e1)
+
+	ALIGN(16)
+L(b3):	xor	R32(n), R32(n)
+	sub	n_param, n
+	mul	v0
+	mov	(rp,n,8), %r10
+	jmp	L(e3)
+
+	ALIGN(32)
+L(top):	mul	v0
+	mov	-16(rp,n,8), %r10
+	ADDSUB	%r11, %r8
+	mov	%rdx, %r11
+	adc	$0, %r9
+	mov	%r8, -24(rp,n,8)
+L(e1):	ADDSUB	%rax, %r10
 	mov	-8(up,n,8), %rax
 	adc	$0, %r11
-	mov	%r8, -24(rp,n,8)
-L(L0):	mul	v0
+	mul	v0
 	ADDSUB	%r9, %r10
+	mov	%rdx, %r9
 	mov	-8(rp,n,8), %r8
 	adc	$0, %r11
-	mov	%rdx, %r9
 	mov	%r10, -16(rp,n,8)
-L(L3):	ADDSUB	%rax, %r8
+L(e0):	ADDSUB	%rax, %r8
 	adc	$0, %r9
 	mov	(up,n,8), %rax
-	ADDSUB	%r11, %r8
-	adc	$0, %r9
-	mov	(rp,n,8), %r10
 	mul	v0
+	mov	(rp,n,8), %r10
+	ADDSUB	%r11, %r8
+	mov	%r8, -8(rp,n,8)
+	adc	$0, %r9
+L(e3):	mov	%rdx, %r11
 	ADDSUB	%rax, %r10
 	mov	8(up,n,8), %rax
-	mov	%rdx, %r11
-	mov	%r8, -8(rp,n,8)
-L(L2):	adc	$0, %r11
+	adc	$0, %r11
+	mul	v0
 	mov	8(rp,n,8), %r8
 	ADDSUB	%r9, %r10
-	adc	$0, %r11
+	mov	%rdx, %r9
 	mov	%r10, (rp,n,8)
+	adc	$0, %r11
+L(e2):	ADDSUB	%rax, %r8
+	adc	$0, %r9
+	mov	16(up,n,8), %rax
 	add	$4, n
 	jnc	L(top)
 
 L(end):	mul	v0
-	ADDSUB	%rax, %r8
-	mov	%rdx, %rax
-	adc	$0, %rax
+	mov	I(-8(rp),-16(rp,n,8)), %r10
 	ADDSUB	%r11, %r8
-	adc	$0, %rax
-	mov	%r8, (rp)
+	mov	%rdx, %r11
+	adc	$0, %r9
+	mov	%r8, I(-16(rp),-24(rp,n,8))
+	ADDSUB	%rax, %r10
+	adc	$0, %r11
+	ADDSUB	%r9, %r10
+	adc	$0, %r11
+	mov	%r10, I(-8(rp),-16(rp,n,8))
+	mov	%r11, %rax
 
 	pop	%rbx
 IFDOS(``pop	%rdi		'')
 IFDOS(``pop	%rsi		'')
 	ret
+
+	ALIGN(16)
+L(1):	mul	v0
+	ADDSUB	%rax, -8(rp)
+	mov	%rdx, %rax
+	adc	$0, %rax
+	pop	%rbx
+IFDOS(``pop	%rdi		'')
+IFDOS(``pop	%rsi		'')
+	ret
 EPILOGUE()
+ASM_END()
