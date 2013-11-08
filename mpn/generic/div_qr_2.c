8bbc436fc503897e81dd2ac4feea3996c437c58c
Torbjorn Granlund <tege@gmplib.org>
2013-11-08 21:53:14 +0100

    Test HAVE_HOST_CPU_FAMILY_x86, not i386.

--- mpn/generic/div_qr_2.c
+++ mpn/generic/div_qr_2.c
@@ -48,7 +48,7 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 */
 #if defined (__GNUC__)  && ! defined (__INTEL_COMPILER)
 
-#if (defined (__i386__) || defined (__i486__)) && W_TYPE_SIZE == 32
+#if HAVE_HOST_CPU_FAMILY_x86 && W_TYPE_SIZE == 32
 #define add_sssaaaa(s2, s1, s0, a1, a0, b1, b0)				\
   __asm__ ("add\t%7, %k2\n\tadc\t%5, %k1\n\tadc\t$0, %k0"		\
 	   : "=r" (s2), "=&r" (s1), "=&r" (s0)				\
