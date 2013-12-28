0fa711a9be153a8d7fad2801d6ffe912a2932d27
Torbjorn Granlund <tege@gmplib.org>
2013-12-28 16:33:45 +0100

    Add a copyright year.

--- mpn/generic/sec_div.c
+++ mpn/generic/sec_div.c
@@ -8,7 +8,7 @@
    SAFE TO REACH THEM THROUGH DOCUMENTED INTERFACES.  IN FACT, IT IS ALMOST
    GUARANTEED THAT THEY WILL CHANGE OR DISAPPEAR IN A FUTURE GNU MP RELEASE.
 
-Copyright 2011, 2012 Free Software Foundation, Inc.
+Copyright 2011-2013 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
