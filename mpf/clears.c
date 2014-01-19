f424ee7ffe9a3cb06167b2d06c164ebacf59a1e1
Torbjorn Granlund <tege@gmplib.org>
2014-01-19 13:00:33 +0100

    Add a copyright year.

--- mpf/clears.c
+++ mpf/clears.c
@@ -1,6 +1,6 @@
 /* mpf_clears() -- Clear multiple mpf_t variables.
 
-Copyright 2009 Free Software Foundation, Inc.
+Copyright 2009, 2014 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
