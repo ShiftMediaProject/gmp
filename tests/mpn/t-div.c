7bb57d76c599684d75ab2c6c34059332cc054343
Torbjorn Granlund <tege@gmplib.org>
2014-02-15 14:13:39 +0100

    Fix typo.

--- tests/mpn/t-div.c
+++ tests/mpn/t-div.c
@@ -482,9 +482,9 @@ main (int argc, char **argv)
 	  /* mpn_div_qr_1 */
 	  mp_limb_t qh;
 	  qp[nn-1] = qran1;
-	  rp[0] = mpn_div_qr_1 (qp, &qh, np, nn, dnp[dn-1]);
-	  ASSERT_ALWAYS (qp[-1] = qran0); ASSERT_ALWAYS (qp[nn-1] == qran1);
-	  qp[nn-1] = qh;
+	  rp[0] = mpn_div_qr_1 (qp, &qh, np, nn, dnp[dn - 1]);
+	  ASSERT_ALWAYS (qp[-1] == qran0); ASSERT_ALWAYS (qp[nn - 1] == qran1);
+	  qp[nn - 1] = qh;
 	  check_one (qp, rp, np, nn,  dnp + dn - 1, 1, "mpn_div_qr_1", 0);
 	}
     }
