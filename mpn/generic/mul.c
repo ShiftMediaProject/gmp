f002e708d400dd6a66cd92f98246051ca5a5271f
Marco Bodrato <bodrato@mail.dm.unipi.it>
2014-06-08 05:49:55 +0200

    mpn/generic/mul.c: Smaller scratch allocation in the Toom3X branch.

--- mpn/generic/mul.c
+++ mpn/generic/mul.c
@@ -211,7 +211,6 @@ mpn_mul (mp_ptr prodp,
 
 #define ITCH_TOOMX2 (9 * vn / 2 + GMP_NUMB_BITS * 2)
       scratch = TMP_SALLOC_LIMBS (ITCH_TOOMX2);
-      ASSERT (mpn_toom42_mul_itch (2 * vn, vn) <= ITCH_TOOMX2); /* 3vn+ */
       ASSERT (mpn_toom22_mul_itch ((5*vn-1)/4, vn) <= ITCH_TOOMX2); /* 5vn/2+ */
       ASSERT (mpn_toom32_mul_itch ((7*vn-1)/4, vn) <= ITCH_TOOMX2); /* 7vn/6+ */
       ASSERT (mpn_toom42_mul_itch (3 * vn - 1, vn) <= ITCH_TOOMX2); /* 9vn/2+ */
@@ -282,7 +281,15 @@ mpn_mul (mp_ptr prodp,
 	  mp_ptr scratch;
 	  TMP_DECL; TMP_MARK;
 
-	  scratch = TMP_ALLOC_LIMBS (ITCH);
+#define ITCH_TOOMX3 (4 * vn + GMP_NUMB_BITS)
+	  scratch = TMP_SALLOC_LIMBS (ITCH_TOOMX3);
+	  ASSERT (mpn_toom33_mul_itch ((7*vn-1)/6, vn) <= ITCH_TOOMX3); /* 7vn/2+ */
+	  ASSERT (mpn_toom43_mul_itch ((3*vn-1)/2, vn) <= ITCH_TOOMX3); /* 9vn/4+ */
+	  ASSERT (mpn_toom32_mul_itch ((7*vn-1)/4, vn) <= ITCH_TOOMX3); /* 7vn/6+ */
+	  ASSERT (mpn_toom53_mul_itch ((11*vn-1)/6, vn) <= ITCH_TOOMX3); /* 11vn/3+ */
+	  ASSERT (mpn_toom42_mul_itch ((5*vn-1)/2, vn) <= ITCH_TOOMX3); /* 15vn/4+ */
+	  ASSERT (mpn_toom63_mul_itch ((5*vn-1)/2, vn) <= ITCH_TOOMX3); /* 15vn/4+ */
+#undef ITCH_TOOMX3
 
 	  if (2 * un >= 5 * vn)
 	    {
