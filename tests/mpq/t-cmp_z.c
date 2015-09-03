11244acd6d4e5a46b24ccc41770d0bf12dd16175
Marco Bodrato <bodrato@mail.dm.unipi.it>
2015-09-03 21:34:24 +0200

    tests/mpq/t-cmp_z.c (sizes_test): New function, testing different sizes.

--- tests/mpq/t-cmp_z.c
+++ tests/mpq/t-cmp_z.c
@@ -44,6 +44,58 @@ ref_mpq_cmp_z (mpq_t a, mpz_t b)
 #define SIZE 8	/* increasing this lowers the probability of finding an error */
 #endif
 
+#ifndef MAXN
+#define MAXN 5	/* increasing this impatcs on total timing */
+#endif
+
+void
+sizes_test (int m)
+{
+  mpq_t a;
+  mpz_t b;
+  int i, j, k, s;
+  int cc, ccref;
+
+  mpq_init (a);
+  mpz_init (b);
+
+  for (i = 0; i <= MAXN ; ++i)
+    {
+      mpz_setbit (DEN (a), i*m); /* \sum_0^i 2^(i*m) */
+      for (j = 0; j <= MAXN; ++j)
+	{
+	  mpz_set_ui (NUM (a), 0);
+	  mpz_setbit (NUM (a), j*m); /* 2^(j*m) */
+	  for (k = 0; k <= MAXN; ++k)
+	    {
+	      mpz_set_ui (b, 0);
+	      mpz_setbit (b, k*m); /* 2^(k*m) */
+	      if (i == 0) /* Denominator is 1, compare the two exponents */
+		ccref = (j>k)-(j<k);
+	      else
+		ccref = j-i > k ? 1 : -1;
+	      for (s = 1; s >= -1; s -= 2)
+		{
+		  cc = mpq_cmp_z (a, b);
+
+		  if (ccref != SGN (cc))
+		    {
+		      fprintf (stderr, "i=%i, j=%i, k=%i, m=%i, s=%i\n; ccref= %i, cc= %i\n", i, j, k, m, s, ccref, cc);
+		      abort ();
+		    }
+
+		  mpq_neg (a, a);
+		  mpz_neg (b, b);
+		  ccref = - ccref;
+		}
+	    }
+	}
+    }
+
+  mpq_clear (a);
+  mpz_clear (b);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -64,6 +116,8 @@ main (int argc, char **argv)
 
   for (i = 0; i < reps; i++)
     {
+      if (i % 8192 == 0)
+	sizes_test (urandom () % (i + 1) + 1);	  
       size = urandom () % SIZE - SIZE/2;
       mpz_random2 (NUM (a), size);
       do
