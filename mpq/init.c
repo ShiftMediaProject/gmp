6fe78311020c0148d8e93b5b65aa885928f55fc7
Marco Bodrato <bodrato@mail.dm.unipi.it>
2015-11-13 07:23:32 +0100

    mpq/init.c: Remove conditional code for __CHECKER__.

--- mpq/init.c
+++ mpq/init.c
@@ -1,6 +1,6 @@
 /* mpq_init -- Make a new rational number with value 0/1.
 
-Copyright 1991, 1994, 1995, 2000-2002 Free Software Foundation, Inc.
+Copyright 1991, 1994, 1995, 2000-2002, 2015 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -41,9 +41,4 @@ mpq_init (mpq_t x)
   PTR(DEN(x)) = __GMP_ALLOCATE_FUNC_LIMBS (1);
   PTR(DEN(x))[0] = 1;
   SIZ(DEN(x)) = 1;
-
-#ifdef __CHECKER__
-  /* let the low limb look initialized, for the benefit of mpz_get_ui etc */
-  PTR(NUM(x))[0] = 0;
-#endif
 }
