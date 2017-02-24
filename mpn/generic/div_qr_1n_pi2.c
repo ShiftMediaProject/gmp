01580e5020ea294113cdbddb14258c10982f381e
Torbjorn Granlund <tg@gmplib.org>
2017-02-24 21:52:10 +0100

    Define add_sssaaaa also for arm64.

--- mpn/generic/div_qr_1n_pi2.c
+++ mpn/generic/div_qr_1n_pi2.c
@@ -71,6 +71,13 @@ see https://www.gnu.org/licenses/.  */
 	     "%2" ((UDItype)(a0)), "rme" ((UDItype)(b0)))
 #endif
 
+#if defined (__aarch64__) && W_TYPE_SIZE == 64
+#define add_sssaaaa(s2, s1, s0, a1, a0, b1, b0)				\
+  __asm__ ("adds\t%2, %x6, %7\n\tadcs\t%1, %x4, %x5\n\tadc\t%0, %3, xzr"\
+	   : "=r" (s2), "=&r" (s1), "=&r" (s0)				\
+	   : "rZ" (s2), "%rZ"  (a1), "rZ" (b1), "%rZ" (a0), "rI" (b0))
+#endif
+
 #if HAVE_HOST_CPU_FAMILY_powerpc && !defined (_LONG_LONG_LIMB)
 /* This works fine for 32-bit and 64-bit limbs, except for 64-bit limbs with a
    processor running in 32-bit mode, since the carry flag then gets the 32-bit
--- mpn/generic/div_qr_2.c
+++ mpn/generic/div_qr_2.c
@@ -49,10 +49,10 @@ see https://www.gnu.org/licenses/.  */
 #endif
 
 /* Define some longlong.h-style macros, but for wider operations.
-   * add_sssaaaa is like longlong.h's add_ssaaaa but the propagating
-     carry-out into an additional sum operand.
-   * add_csaac accepts two addends and a carry in, and generates a sum
-     and a carry out.  A little like a "full adder".
+   * add_sssaaaa is like longlong.h's add_ssaaaa but propagating carry-out into
+     an additional sum operand.
+   * add_csaac accepts two addends and a carry in, and generates a sum and a
+     carry out.  A little like a "full adder".
 */
 #if defined (__GNUC__)  && ! defined (__INTEL_COMPILER) && ! defined (NO_ASM)
 
@@ -82,6 +82,13 @@ see https://www.gnu.org/licenses/.  */
 	   : "=r" (co), "=r" (s)					\
 	   : "rm"  ((UDItype)(ci)), "0" (CNST_LIMB(0)),			\
 	     "%1" ((UDItype)(a)), "g" ((UDItype)(b)))
+#endif
+
+#if defined (__aarch64__) && W_TYPE_SIZE == 64
+#define add_sssaaaa(s2, s1, s0, a1, a0, b1, b0)				\
+  __asm__ ("adds\t%2, %x6, %7\n\tadcs\t%1, %x4, %x5\n\tadc\t%0, %3, xzr"\
+	   : "=r" (s2), "=&r" (s1), "=&r" (s0)				\
+	   : "rZ" (s2), "%rZ"  (a1), "rZ" (b1), "%rZ" (a0), "rI" (b0) __CLOBBER_CC)
 #endif
 
 #if HAVE_HOST_CPU_FAMILY_powerpc && !defined (_LONG_LONG_LIMB)
