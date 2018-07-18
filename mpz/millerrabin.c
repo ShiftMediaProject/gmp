b9e0524ff6c556eaf10366141ab1aeeddb05842b
Marco Bodrato <bodrato@mail.dm.unipi.it>
2018-07-18 18:24:57 +0200

    mpz/millerrabin.c (mod_eq_m1): New function, check equality computing -1 on the fly.

--- mpz/millerrabin.c
+++ mpz/millerrabin.c
@@ -42,7 +42,7 @@ see https://www.gnu.org/licenses/.  */
 
 #include "gmp-impl.h"
 
-static int millerrabin (mpz_srcptr, mpz_srcptr,
+static int millerrabin (mpz_srcptr,
 			mpz_ptr, mpz_ptr,
 			mpz_srcptr, unsigned long int);
 
@@ -50,22 +50,22 @@ int
 mpz_millerrabin (mpz_srcptr n, int reps)
 {
   int r;
-  mpz_t nm1, nm3, x, y, q;
+  mpz_t nm, x, y, q;
   unsigned long int k;
   gmp_randstate_t rstate;
   int is_prime;
   TMP_DECL;
   TMP_MARK;
 
-  MPZ_TMP_INIT (nm1, SIZ (n) + 1);
-  mpz_sub_ui (nm1, n, 1L);
+  MPZ_TMP_INIT (nm, SIZ (n) + 1);
+  mpz_sub_ui (nm, n, 1L); /* nm = n - 1 */
 
   MPZ_TMP_INIT (x, SIZ (n) + 1);
   MPZ_TMP_INIT (y, 2 * SIZ (n)); /* mpz_powm_ui needs excessive memory!!! */
 
   /* Perform a Fermat test.  */
   mpz_set_ui (x, 210L);
-  mpz_powm (y, x, nm1, n);
+  mpz_powm (y, x, nm, n);
   if (mpz_cmp_ui (y, 1L) != 0)
     {
       TMP_FREE;
@@ -75,13 +75,12 @@ mpz_millerrabin (mpz_srcptr n, int reps)
   MPZ_TMP_INIT (q, SIZ (n));
 
   /* Find q and k, where q is odd and n = 1 + 2**k * q.  */
-  k = mpz_scan1 (nm1, 0L);
-  mpz_tdiv_q_2exp (q, nm1, k);
+  k = mpz_scan1 (nm, 0L); /* or mpz_scan1 (n, 1) */
+  mpz_tdiv_q_2exp (q, n, k);
 
   /* n-3 */
-  MPZ_TMP_INIT (nm3, SIZ (n) + 1);
-  mpz_sub_ui (nm3, n, 3L);
-  ASSERT (mpz_cmp_ui (nm3, 1L) >= 0);
+  mpz_sub_ui (nm, nm, 2L); /* nm = n - 1 - 2 = n - 3 */
+  ASSERT (mpz_cmp_ui (nm, 1L) >= 0);
 
   gmp_randinit_default (rstate);
 
@@ -89,10 +88,10 @@ mpz_millerrabin (mpz_srcptr n, int reps)
   for (r = 0; r < reps && is_prime; r++)
     {
       /* 2 to n-2 inclusive, don't want 1, 0 or -1 */
-      mpz_urandomm (x, rstate, nm3);
+      mpz_urandomm (x, rstate, nm);
       mpz_add_ui (x, x, 2L);
 
-      is_prime = millerrabin (n, nm1, x, y, q, k);
+      is_prime = millerrabin (n, x, y, q, k);
     }
 
   gmp_randclear (rstate);
@@ -102,20 +101,52 @@ mpz_millerrabin (mpz_srcptr n, int reps)
 }
 
 static int
-millerrabin (mpz_srcptr n, mpz_srcptr nm1, mpz_ptr x, mpz_ptr y,
+mod_eq_m1 (mpz_srcptr x, mpz_srcptr m)
+{
+  mp_size_t ms;
+  mp_srcptr mp, xp;
+  TMP_DECL;
+
+  ms = SIZ (m);
+  if (SIZ (x) != ms)
+    return 0;
+  ASSERT (ms > 0);
+
+  mp = PTR (m);
+  xp = PTR (x);
+  ASSERT ((mp[0] - 1) == (mp[0] ^ 1)); /* n is odd */
+
+  if ((*xp ^ CNST_LIMB(1) ^ *mp) != CNST_LIMB(0)) /* xp[0] == mp[0] - 1 */
+    return 0;
+  else
+    {
+      int cmp;
+
+      --ms;
+      ++xp;
+      ++mp;
+
+      MPN_CMP (cmp, xp, mp, ms);
+
+      return cmp == 0;
+    }
+}
+
+static int
+millerrabin (mpz_srcptr n, mpz_ptr x, mpz_ptr y,
 	     mpz_srcptr q, unsigned long int k)
 {
   unsigned long int i;
 
   mpz_powm (y, x, q, n);
 
-  if (mpz_cmp_ui (y, 1L) == 0 || mpz_cmp (y, nm1) == 0)
+  if (mpz_cmp_ui (y, 1L) == 0 || mod_eq_m1 (y, n))
     return 1;
 
   for (i = 1; i < k; i++)
     {
       mpz_powm_ui (y, y, 2L, n);
-      if (mpz_cmp (y, nm1) == 0)
+      if (mod_eq_m1 (y, n))
 	return 1;
       /* y == 1 means that the previous y was a non-trivial square root
 	 of 1 (mod n). y == 0 means that n is a power of the base.
