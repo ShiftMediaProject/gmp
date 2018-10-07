5d64094935fe08a9e5cf53d5d80f3f9931caa1fb
Matt Oliver <protogonoi@gmail.com>
2018-10-08 03:41:56 +1100

    project: Add WinRT/UWP configurations.

--- SMP/unistd.h
+++ SMP/unistd.h
@@ -1,6 +1,5 @@
 /*
- * MSVC unistd.h compatability header.
- * Copyright (c) 2015 Matthew Oliver
+ * MSVC unistd.h compatibility header.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,24 +20,17 @@
  * THE SOFTWARE.
  */
 
-#ifndef _SMP_UNISTD_H_
-#define _SMP_UNISTD_H_
+#ifndef SMP_UNISTD_H
+#define SMP_UNISTD_H
 
 #ifndef _MSC_VER
 #   include_next <unistd.h>
 #else
 
-#ifndef __STRICT_ANSI__
-
 #include <stdlib.h>
-//#include <io.h>
 #include <process.h>
 #include <direct.h>
 #include <fcntl.h>
-
-#define __UNISTD_GETOPT__
-//#include <getopt.h>
-#undef __UNISTD_GETOPT__
 
 #define R_OK    4       /* Test for read permission.  */
 #define W_OK    2       /* Test for write permission.  */
@@ -65,8 +57,6 @@ typedef int mode_t;
 #include <BaseTsd.h>
 typedef SSIZE_T ssize_t;
 
-#endif /* __STRICT_ANSI__ */
-
 #endif /* _MSC_VER */
 
-#endif /* _SMP_UNISTD_H_ */
+#endif /* SMP_UNISTD_H */
