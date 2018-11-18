777d03620a27d149d3b8b03df2008d7a6311f123
Torbjorn Granlund <tg@gmplib.org>
2018-11-18 01:27:02 +0100

    Optimise.

--- mpn/powerpc64/mode64/p9/addmul_1.asm
+++ mpn/powerpc64/mode64/p9/addmul_1.asm
@@ -1,4 +1,4 @@
-dnl  POWER9 mpn_addmul_1.
+dnl  Power9 mpn_addmul_1.
 
 dnl  Copyright 2017, 2018 Free Software Foundation, Inc.
 
@@ -40,8 +40,9 @@ C POWER8		 -
 C POWER9		 2.5
 
 C TODO
-C  * Schedule for POWER9 pipeline.
-C  * Unroll to at least 4x if that proves beneficial.
+C  * Schedule for Power9 pipeline.
+C  * Unroll 4x if that proves beneficial.
+C  * This is marginally faster (but much smaller) than ../aorsmul_1.asm.
 
 C INPUT PARAMETERS
 define(`rp', `r3')
@@ -51,41 +52,37 @@ define(`v0', `r6')
 
 ASM_START()
 PROLOGUE(mpn_addmul_1)
-	std	r31, -8(r1)
-
 	cmpdi	cr6, n, 2
-
 	addi	r0, n, -1	C FIXME: postpone
 	srdi	r0, r0, 1	C FIXME: postpone
 	mtctr	r0		C FIXME: postpone
-
 	rldicl.	r0, n, 0,63	C r0 = n & 3, set cr0
 	bne	cr0, L(b1)
 
 L(b0):	ld	r10, 0(rp)
 	ld	r12, 0(up)
 	ld	r11, 8(rp)
-	ld	r31, 8(up)
-	maddld(	r0, r12, v0, r10)
+	ld	r0, 8(up)
+	maddld(	r9, r12, v0, r10)
 	maddhdu(r7, r12, v0, r10)
 	ble	cr6, L(2)
 	ld	r10, 16(rp)
 	ld	r12, 16(up)
-	maddld(	r8, r31, v0, r11)
-	maddhdu(r5, r31, v0, r11)
+	maddld(	r8, r0, v0, r11)
+	maddhdu(r5, r0, v0, r11)
 	addic	up, up, 16
 	addi	rp, rp, -8
 	b	L(mid)
 
 L(b1):	ld	r11, 0(rp)
-	ld	r31, 0(up)
+	ld	r0, 0(up)
 	ble	cr6, L(1)
 	ld	r10, 8(rp)
 	ld	r12, 8(up)
-	maddld(	r0, r31, v0, r11)
-	maddhdu(r5, r31, v0, r11)
+	maddld(	r8, r0, v0, r11)
+	maddhdu(r5, r0, v0, r11)
 	ld	r11, 16(rp)
-	ld	r31, 16(up)
+	ld	r0, 16(up)
 	maddld(	r9, r12, v0, r10)
 	maddhdu(r7, r12, v0, r10)
 	addic	up, up, 24
@@ -94,43 +91,40 @@ L(b1):	ld	r11, 0(rp)
 	ALIGN(16)
 L(top):	ld	r10, 24(rp)
 	ld	r12, 0(up)
-	std	r0, 0(rp)
-	adde	r0, r5, r9
-	maddld(	r8, r31, v0, r11)	C W:0,2,4
-	maddhdu(r5, r31, v0, r11)	C W:1,3,5
+	std	r8, 0(rp)
+	adde	r9, r5, r9
+	maddld(	r8, r0, v0, r11)	C W:0,2,4
+	maddhdu(r5, r0, v0, r11)	C W:1,3,5
 L(mid):	ld	r11, 32(rp)
-	ld	r31, 8(up)
-	std	r0, 8(rp)
-	adde	r0, r7, r8
+	ld	r0, 8(up)
+	std	r9, 8(rp)
+	adde	r8, r7, r8
 	maddld(	r9, r12, v0, r10)	C W:1,3,5
 	maddhdu(r7, r12, v0, r10)	C W:2,4,6
 	addi	rp, rp, 16
 	addi	up, up, 16
 	bdnz	L(top)
 
-L(end):	std	r0, 0(rp)
-	adde	r0, r5, r9
-	maddld(	r8, r31, v0, r11)
-	maddhdu(r5, r31, v0, r11)
-	std	r0, 8(rp)
-	adde	r0, r7, r8
-	std	r0, 16(rp)
+L(end):	std	r8, 0(rp)
+	maddld(	r8, r0, v0, r11)
+	adde	r9, r5, r9
+	maddhdu(r5, r0, v0, r11)
+	std	r9, 8(rp)
+	adde	r8, r7, r8
+	std	r8, 16(rp)
 	addze	r3, r5
-	ld	r31, -8(r1)
 	blr
 
-L(2):	maddld(	r8, r31, v0, r11)
-	maddhdu(r5, r31, v0, r11)
-	std	r0, 0(rp)
-	addc	r0, r7, r8
-	std	r0, 8(rp)
+L(2):	maddld(	r8, r0, v0, r11)
+	maddhdu(r5, r0, v0, r11)
+	std	r9, 0(rp)
+	addc	r8, r7, r8
+	std	r8, 8(rp)
 	addze	r3, r5
-	ld	r31, -8(r1)
 	blr
 
-L(1):	maddld(	r0,  r31, v0, r11)
-	std	r0, 0(rp)
-	maddhdu(r3, r31, v0, r11)
-	ld	r31, -8(r1)
+L(1):	maddld(	r8,  r0, v0, r11)
+	std	r8, 0(rp)
+	maddhdu(r3, r0, v0, r11)
 	blr
 EPILOGUE()
