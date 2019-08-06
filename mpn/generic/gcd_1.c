91b19b4d34ac4c38ac3d54ffbe1bda1a0de35b00
Niels Möller <nisse@lysator.liu.se>
2019-08-06 16:54:40 +0200

    New function mpn_gcd_11

--- mpn/generic/gcd_1.c
+++ mpn/generic/gcd_1.c
@@ -1,6 +1,6 @@
 /* mpn_gcd_1 -- mpn and limb greatest common divisor.
 
-Copyright 1994, 1996, 2000, 2001, 2009, 2012 Free Software Foundation, Inc.
+Copyright 1994, 1996, 2000, 2001, 2009, 2012, 2019 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
@@ -70,7 +70,7 @@ mpn_gcd_1 (mp_srcptr up, mp_size_t size, mp_limb_t vlimb)
 	goto done;
 
       count_trailing_zeros (c, ulimb);
-      ulimb = (ulimb >> 1) >> c;
+      ulimb >>= c;
     }
   else
     {
@@ -92,48 +92,11 @@ mpn_gcd_1 (mp_srcptr up, mp_size_t size, mp_limb_t vlimb)
 	    goto done;
 
 	  count_trailing_zeros (c, ulimb);
-	  ulimb = (ulimb >> 1) >> c;
-	}
-      else
-	{
-	  ASSERT (ulimb & 1);
-	  ulimb >>= 1;
+	  ulimb >>= c;
 	}
     }
 
-  ASSERT (vlimb & 1);
-  vlimb >>= 1;
-
-  /* In this loop, we represent the odd numbers ulimb and vlimb
-     without the redundant least significant one bit. This reduction
-     in size by one bit ensures that the high bit of t, below, is set
-     if and only if vlimb > ulimb. */
-  while (ulimb != vlimb)
-    {
-      mp_limb_t t;
-      mp_limb_t vgtu;
-
-      t = ulimb - vlimb;
-      vgtu = LIMB_HIGHBIT_TO_MASK (t);
-
-      /* v <-- min (u, v) */
-      vlimb += (vgtu & t);
-
-      /* u <-- |u - v| */
-      ulimb = (t ^ vgtu) - vgtu;
-
-      count_trailing_zeros (c, t);
-      /* We have c <= GMP_LIMB_BITS - 2 here, so that
-
-	   ulimb >>= (c + 1);
-
-	 would be safe. But unlike the addition c + 1, a separate
-	 shift by 1 is independent of c, and can be executed in
-	 parallel with count_trailing_zeros. */
-      ulimb = (ulimb >> 1) >> c;
-    }
-
-  vlimb = (vlimb << 1) | 1;
+  vlimb = mpn_gcd_11 (ulimb, vlimb);
 
  done:
   return vlimb << zero_bits;
