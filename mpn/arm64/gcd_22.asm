ed7ec7953b483f3b5207cfabfd2507501b0b6773
Torbjorn Granlund <tg@gmplib.org>
2019-09-05 22:53:04 +0200

    Rewrite to make better use of Arm conditional execution.

--- mpn/arm64/gcd_22.asm
+++ mpn/arm64/gcd_22.asm
@@ -38,7 +38,7 @@ C Cortex-A53	 7.26
 C Cortex-A55	 ?
 C Cortex-A57	 ?
 C Cortex-A72	 5.72
-C Cortex-A73	 7.12
+C Cortex-A73	 6.43
 C Cortex-A75	 ?
 C Cortex-A76	 ?
 C Cortex-A77	 ?
@@ -51,10 +51,8 @@ define(`v0',    `x3')
 
 define(`t0',    `x5')
 define(`t1',    `x6')
-define(`s0',    `x7')
-define(`s1',    `x8')
-define(`cnt',   `x9')
-define(`tnc',   `x10')
+define(`cnt',   `x7')
+define(`tnc',   `x8')
 
 ASM_START()
 PROLOGUE(mpn_gcd_22)
@@ -62,22 +60,21 @@ PROLOGUE(mpn_gcd_22)
 	ALIGN(16)
 L(top):	subs	t0, u0, v0		C 0 6
 	cbz	t0, L(lowz)
-	sbc	t1, u1, v1		C 1 7
+	sbcs	t1, u1, v1		C 1 7
 
-	subs	s0, v0, u0		C 0
-	sbcs	s1, v1, u1		C 1 s = v - u, cs = (u < v)
+	rbit	cnt, t0			C 1
 
-L(bck):	rbit	cnt, t0			C 1
+	cneg	t0, t0, cc		C 2
+	cinv	t1, t1, cc		C 2 u = |u - v|
+L(bck):	csel	v0, v0, u0, cs		C 2
+	csel	v1, v1, u1, cs		C 2 v = min(u,v)
+
 	clz	cnt, cnt		C 2
-	csel	s0, t0, s0, cc		C 2
-	csel	s1, t1, s1, cc		C 2 u = |u - v|
 	sub	tnc, xzr, cnt		C 3
-	csel	v0, v0, u0, cc		C 2
-	csel	v1, v1, u1, cc		C 2 v = min(u,v)
 
-	lsr	u0, s0, cnt		C 3
-	lsl	x14, s1, tnc		C 4
-	lsr	u1, s1, cnt		C 3
+	lsr	u0, t0, cnt		C 3
+	lsl	x14, t1, tnc		C 4
+	lsr	u1, t1, cnt		C 3
 	orr	u0, u0, x14		C 5
 
 	orr	x11, u1, v1
@@ -105,8 +102,8 @@ L(lowz):C We come here when v0 - u0 = 0
 	subs	t0, u1, v1
 	beq	L(end)
 	mov	t1, #0
-	subs	s0, v1, u1
-	mov	s1, #0
+	rbit	cnt, t0			C 1
+	cneg	t0, t0, cc		C 2
 	b	L(bck)			C FIXME: make conditional
 
 L(end):	mov	x0, v0
