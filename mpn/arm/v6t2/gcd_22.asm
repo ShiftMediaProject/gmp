276ef736145b3229c153c82576786e2d0f3f2ff2
Torbjorn Granlund <tg@gmplib.org>
2019-09-05 22:47:48 +0200

    Rewrite to make better use of Arm conditional execution.

--- mpn/arm/v6t2/gcd_22.asm
+++ mpn/arm/v6t2/gcd_22.asm
@@ -34,14 +34,14 @@ include(`../config.m4')
 C	     cycles/bit (approx)
 C StrongARM	 -
 C XScale	 -
-C Cortex-A5	13.2
-C Cortex-A7	10.3
-C Cortex-A8	 8.4
+C Cortex-A5	10.1
+C Cortex-A7	 9.1
+C Cortex-A8	 6.3
 C Cortex-A9	 ?
-C Cortex-A12	 9.1
-C Cortex-A15	 8.0
+C Cortex-A12	 7.7
+C Cortex-A15	 5.7
 C Cortex-A17	 ?
-C Cortex-A53	 8.5
+C Cortex-A53	 7.0
 
 
 define(`gp',    `r0')
@@ -53,37 +53,31 @@ define(`v0',    `r4')
 
 define(`t0',    `r5')
 define(`t1',    `r6')
-define(`s0',    `r7')
-define(`s1',    `r8')
-define(`cnt',   `r9')
+define(`cnt',   `r7')
 
 ASM_START()
 PROLOGUE(mpn_gcd_22)
-	push	{ r4-r9 }
+	push	{ r4-r7 }
 
-	ldr	v0, [sp,#24]		C
+	ldr	v0, [sp,#16]		C
 
 L(top):	subs	t0, u0, v0		C 0 7
 	beq	L(lowz)
-	sbc	t1, u1, v1		C 1 8
+	sbcs	t1, u1, v1		C 1 8
 
-	mov	s0, u0
-	subs	u0, v0, u0		C 0
-	mov	s1, u1
-	sbcs	u1, v1, u1		C 1
+	rbit	cnt, t0			C 1
 
-L(bck):	rbit	cnt, t0			C 1
+	negcc	t0, t0
+	mvncc	t1, t1
+L(bck):	movcc	v0, u0
+	movcc	v1, u1
+
 	clz	cnt, cnt		C 2
-
-	movcc	u0, t0			C 6
-	movcc	u1, t1			C 4
 	rsb	r12, cnt, #32		C 3
-	movcs	v0, s0			C 6
-	movcs	v1, s1			C 4
 
-	lsr	u0, u0, cnt		C 3
-	lsl	r12, u1, r12		C 4
-	lsr	u1, u1, cnt		C 3
+	lsr	u0, t0, cnt		C 3
+	lsl	r12, t1, r12		C 4
+	lsr	u1, t1, cnt		C 3
 	orr	u0, u0, r12		C 5
 
 	orrs	r12, u1, v1
@@ -92,14 +86,14 @@ L(bck):	rbit	cnt, t0			C 1
 
 	str	r12, [gp,#4]		C high result limb <= 0
 
-	mov	r8, gp
+	mov	r6, gp
 	mov	r0, u0			C pass 1st argument
 	mov	r1, v0			C pass 2nd argument
-	mov	r9, r14			C preserve link register
+	mov	r7, r14			C preserve link register
 	bl	mpn_gcd_11
-	str	r0, [r8,#0]
-	mov	r14, r9
-	pop	{ r4-r9 }
+	str	r0, [r6,#0]
+	mov	r14, r7
+	pop	{ r4-r7 }
 	bx	r14
 
 L(lowz):C We come here when v0 - u0 = 0
@@ -108,14 +102,12 @@ L(lowz):C We come here when v0 - u0 = 0
 	subs	t0, u1, v1
 	beq	L(end)
 	mov	t1, #0
-	mov	s0, u0
-	mov	s1, u1
-	subs	u0, v1, u1
-	mov	u1, #0
+	rbit	cnt, t0			C 1
+	negcc	t0, t0
 	b	L(bck)
 
 L(end):	str	v0, [gp,#0]
 	str	v1, [gp,#4]
-	pop	{ r4-r9 }
+	pop	{ r4-r7 }
 	bx	r14
 EPILOGUE()
