8898c62ac3c3b5f4c4a6cce0a45c6fe78195531d
Marco Bodrato <bodrato@mail.dm.unipi.it>
2020-09-17 10:05:42 +0200

    mini-gmp/mini-gmp.h: Update FILE tests from gmp-h (spotted by Vincent Lefevre)

--- mini-gmp/mini-gmp.h
+++ mini-gmp/mini-gmp.h
@@ -291,7 +291,8 @@ int mpz_init_set_str (mpz_t, const char *, int);
   || defined (_MSL_STDIO_H)           /* Metrowerks */          \
   || defined (_STDIO_H_INCLUDED)      /* QNX4 */		\
   || defined (_ISO_STDIO_ISO_H)       /* Sun C++ */		\
-  || defined (__STDIO_LOADED)         /* VMS */
+  || defined (__STDIO_LOADED)         /* VMS */			\
+  || defined (__DEFINED_FILE)         /* musl */
 size_t mpz_out_str (FILE *, int, const mpz_t);
 #endif
 
